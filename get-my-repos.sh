6febe2d3acd819adc127cbdb7c4080eeee7fd710
Hari Sekhon <harisekhon@gmail.com>
2016-01-17 12:42:53 +0000

    added get-my-repos.sh, update-my-repos.sh

---
+++ get-my-repos.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2016-01-17 12:14:06 +0000 (Sun, 17 Jan 2016)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+srcdir="`dirname $0`"
+
+for repo in $(sed 's/#.*//' < "$srcdir/repolist.txt"); do
+    [ -d "$repo" ] || git clone "https://github.com/harisekhon/$repo"
+done
