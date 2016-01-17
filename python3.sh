326a933978bc53ca01669286613fef961f99b35d
Hari Sekhon <harisekhon@gmail.com>
2016-01-17 12:48:23 +0000

    added python3.sh

---
+++ python3.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-05-25 01:38:24 +0100 (Mon, 25 May 2015)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+
+pip install caniusepython3
+echo "Testing module dependencies for Python 3 compatibility"
+caniusepython3 -r requirements.txt
