62d6ae0e40f9eaefeafb31f7566ec3326e4dcba9
Hari Sekhon <harisekhon@gmail.com>
2016-01-17 12:47:33 +0000

    added travis.sh

---
+++ travis.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2016-01-15 00:33:52 +0000 (Fri, 15 Jan 2016)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+
+if [ -z "${TRAVIS:-}" ]; then
+    gem install travis --no-rdoc --no-ri
+    travis lint
+fi
