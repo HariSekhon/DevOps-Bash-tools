5cdacdce1da843b7d7154eddc8d5ec180717c01b
Hari Sekhon <harisekhon@gmail.com>
2016-01-23 00:06:18 +0000

    added excluded.sh

---
+++ excluded.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-05-25 01:38:24 +0100 (Mon, 25 May 2015)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+# intended only to be sourced by utils.sh
+#
+# split from utils.sh as this is specific to this repo
+
+set -eu
+
+isExcluded(){
+    local prog="$1"
+    [[ "$prog" =~ ^\* ]] && return 0
+    commit="$(git log "$prog" | head -n1 | grep 'commit')"
+    if [ -z "$commit" ]; then
+        return 0
+    fi
+    return 1
+}
