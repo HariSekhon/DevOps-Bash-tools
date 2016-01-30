f709dfb4975a453d13d123ce920786c20686aaca
Hari Sekhon <harisekhon@gmail.com>
2016-01-30 10:32:55 +0000

    added perl_syntax.sh

---
+++ perl_syntax.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-05-25 01:38:24 +0100 (Mon, 25 May 2015)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+set -eu
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+. "$srcdir/utils.sh"
+
+section "Perl Syntax Checks"
+
+for x in $(find . -maxdepth 2 -type f -iname '*.pl' -o -iname '*.pm'); do
+    isExcluded "$x" && continue
+    #printf "%-50s" "$x:"
+    #$perl -Tc $I_lib ./$x
+    perl -WTc ./$x
+done
+section "All Perl programs passed syntax check"
+echo
+echo
