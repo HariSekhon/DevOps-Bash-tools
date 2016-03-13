77277002d1c9065cd0bff914e373c35fd020a8ca
Hari Sekhon <harisekhon@gmail.com>
2016-03-13 22:28:53 +0000

    added pylint.sh

---
+++ pylint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-05-25 01:38:24 +0100 (Mon, 25 May 2015)
+#
+#  https://github.com/harisekhon/pylib
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+set -eu
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+. "$srcdir/utils.sh"
+
+for x in $(find ${1:-.} -iname '*.py' -o -iname '*.jy'); do
+    isExcluded "$x" && continue
+    if which pylint &>/dev/null; then
+        echo "pylint -E $x"
+        echo
+        pylint -E $x
+        hr; echo
+    fi
+done
