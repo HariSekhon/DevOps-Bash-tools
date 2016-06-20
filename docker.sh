08ca25a1e922ff4f59b781502a62a0ed309d5214
Hari Sekhon <harisekhon@gmail.com>
2016-06-20 14:39:12 +0100

    updated docker.sh

--- docker.sh
+++ docker.sh
@@ -54,7 +54,7 @@ launch_container(){
     local ports="${@:3}"
     local startupwait2="${startupwait:-30}"
     is_travis && let startupwait2*=2
-    if [ -n "${TRAP:-}" ] || is_ci; then
+    if [ -n "${TRAP:-}" ] || is_CI; then
         trap_container "$container"
     fi
     if external_docker; then
