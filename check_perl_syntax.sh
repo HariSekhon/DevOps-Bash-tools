acc10e73f3e38dae2436866fdfa0c7b1b8359cb6
Hari Sekhon <harisekhon@gmail.com>
2016-06-30 21:58:19 +0100

    updated check_perl_syntax.sh

--- check_perl_syntax.sh
+++ check_perl_syntax.sh
@@ -20,7 +20,7 @@ srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$srcdir/utils.sh"
 
 if [ -z "$(find -L "${1:-.}" -maxdepth 2 -type f -iname '*.pl' -o -iname '*.pm' -o -iname '*.t')" ]; then
-    return &>/dev/null
+    return 0 &>/dev/null || :
     exit 0
 fi
 
