8c45b2dd40f8923dbc883b0d903a0e2129b8a753
Hari Sekhon <harisekhon@gmail.com>
2016-06-30 15:07:55 +0100

    added check_sbt_build.sh

---
+++ check_sbt_build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2016-06-30 14:46:43 +0100 (Thu, 30 Jun 2016)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -eu #o pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+echo "
+# ============================================================================ #
+#                                     S B T
+# ============================================================================ #
+"
+
+if which sbt &>/dev/null; then
+    find -L "${1:-.}" -name build.sbt |
+    while read build_sbt; do
+        pushd "$(dirname $build_sbt)" >/dev/null
+        echo "Validating $build_sbt"
+        echo q | sbt reload || exit $?
+        popd >/dev/null
+        echo
+    done
+else
+    echo "SBT not found in \$PATH, skipping maven pom checks"
+fi
+
+echo
+echo
