dad33bebb1a5dd84826c49f32eabee52015900b7
Hari Sekhon <harisekhon@gmail.com>
2017-08-15 23:31:35 +0100

    updated check_shell_syntax.sh

--- check_shell_syntax.sh
+++ check_shell_syntax.sh
@@ -45,8 +45,6 @@ end_time="$(date +%s)"
 let time_taken=$end_time-$start_time
 echo "Completed in $time_taken secs"
 echo
-hr2
-echo "All Shell programs passed syntax check"
-hr2
+section2 "All Shell programs passed syntax check"
 echo
 echo
