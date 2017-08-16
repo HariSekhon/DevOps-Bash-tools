0b00300471d8b28dcdaa6e4f4f5ace7c4bb6b6a6
Hari Sekhon <harisekhon@gmail.com>
2017-08-16 11:03:19 +0100

    shortened code using timer functions

--- install_maven.sh
+++ install_maven.sh
@@ -25,9 +25,7 @@ MAVEN_VERSION=${MAVEN_VERSION:-3.3.9}
 
 BASE=/opt
 
-date
-start_time="$(date +%s)"
-echo
+start_time="$(start_timer)"                                                                                                                                                          |
 
 if ! [ -e "$BASE/maven" ]; then
     mkdir -p "$BASE"
@@ -51,13 +49,6 @@ export PATH=\$PATH:\$MAVEN_HOME/bin
 EOF
 fi
 
-echo
-date
-echo
-end_time="$(date +%s)"
-# if start and end time are the same let returns exit code 1
-let time_taken=$end_time-$start_time || :
-echo "Completed in $time_taken secs"
-echo
+time_taken "$start_time"
 section2 "Maven Install Completed"
 echo
