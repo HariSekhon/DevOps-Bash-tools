637e46c7b2eaa2297d7cb782f62dae8a1215c197
Hari Sekhon <harisekhon@gmail.com>
2017-08-16 10:56:19 +0100

    shortened code using timer functions

--- check_sbt_build.sh
+++ check_sbt_build.sh
@@ -26,9 +26,7 @@ fi
 
 section "S B T"
 
-date
-start_time="$(date +%s)"
-echo
+start_time="$(start_timer)"
 
 if which sbt &>/dev/null; then
     find -L "${1:-.}" -name build.sbt |
@@ -44,13 +42,6 @@ else
     echo "SBT not found in \$PATH, skipping maven pom checks"
 fi
 
-echo
-date
-echo
-end_time="$(date +%s)"
-# if start and end time are the same let returns exit code 1
-let time_taken=$end_time-$start_time || :
-echo "Completed in $time_taken secs"
-echo
+time_taken "$start_time"
 section2 "SBT checks passed"
 echo
