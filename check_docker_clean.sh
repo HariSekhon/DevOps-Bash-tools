acb2c604bd47647616d0f1d0576f9960c1c58aa3
Hari Sekhon <harisekhon@gmail.com>
2017-10-06 01:30:40 +0200

    updated check_docker_clean.sh

--- check_docker_clean.sh
+++ check_docker_clean.sh
@@ -20,18 +20,26 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . "$srcdir/docker.sh"
 
 if is_inside_docker; then
-    section "Docker Clean Checks"
+    # Python pip => .cache
+    # Perl       => .cpan / .cpanm
+    # Ruby       => .gem
+    # Java / Scala / Groovy:
+    #   Maven    => .m2
+    #   Ivy      => .ivy .ivy2
+    #       (Sbt / Gradle)
     for x in \
-        /root/.cache \
-        /root/.cpan \
-        /root/.cpanm \
-        ~/.cache \
-        ~/.cpan \
-        ~/.cpanm \
+        .cache \
+        .cpan \
+        .cpanm \
+        .m2 \
+        .ivy \
+        .ivy2 \
         ; do
-        if [ -e "$x" ]; then
-            echo "$x detected, should have been removed from docker build"
-            exit 1
-        fi
+        for y in /root ~; do
+            if [ -e "$y/$x" ]; then
+                echo "$y/$x detected, should have been removed from docker build"
+                exit 1
+            fi
+        done
     done
 fi
