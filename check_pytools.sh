3d8f0c320a5d84e37dc3ee2efe8d0ffc057c1716
Hari Sekhon <harisekhon@gmail.com>
2017-10-13 14:29:04 +0200

    excluded xml.properties from ini properties file validation

--- check_pytools.sh
+++ check_pytools.sh
@@ -97,7 +97,9 @@ for x in "$pytools_dir"/validate_*.py; do
         # if the arg is quoted then would have to eval $x$opts below to get the quotes to be interpreted properly by shell rather than --exclude
         #
         # gradle's cache.properties is often empty just a single commented date line
-        opts=" --exclude 'zookeeper-.*/.*contrib/rest/conf/log4j.properties|.gradle/.+/taskArtifacts/cache.properties'"
+        #
+        # exclude kafka nagios plugin's /target/resolution-cache/check_kafka/check_kafka_2.10/0.1.0/resolved.xml.properties
+        opts=" --exclude 'zookeeper-.*/.*contrib/rest/conf/log4j\.properties|\.gradle/.+/taskArtifacts/cache\.properties|\.xml\.properties'"
     fi
     echo "$x$opts: "
     $x$opts .
