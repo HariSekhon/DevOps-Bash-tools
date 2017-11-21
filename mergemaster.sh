92027331e26285cec7602bf92a02cf6d39c45abd
Hari Sekhon <harisekhon@gmail.com>
2017-11-21 10:58:03 +0100

    added mergemaster.sh

---
+++ mergemaster.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2017-11-21 10:43:47 +0100 (Tue, 21 Nov 2017)
+#
+#  https://github.com/harisekhon/Dockerfiles
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$srcdir"
+
+. "$srcdir/git.sh"
+
+foreachbranch 'git merge --no-edit && git merge master --no-edit'
+
+git checkout master
