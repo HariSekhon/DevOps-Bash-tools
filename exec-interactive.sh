4e6c0c7d5379fa089e7690fb699b2d64c79283fb
Hari Sekhon <harisekhon@gmail.com>
2019-02-15 12:56:49 +0000

    added exec-interactive.sh

---
+++ exec-interactive.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-02-15 12:51:17 +0000 (Fri, 15 Feb 2019)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+(
+exec ${SHELL:-sh} -i 3<<EOF 4<&0 <&3
+    $@
+  exec 3>&- <&4
+EOF
+)
