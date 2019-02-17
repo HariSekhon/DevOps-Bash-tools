5d2daa7ea7fb2c29ac20d8ac9eec39e12f3cc0e8
Hari Sekhon <harisekhon@gmail.com>
2019-02-17 21:07:39 +0000

    added brew-install-packages.sh

---
+++ brew-install-packages.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-02-15 21:31:10 +0000 (Fri, 15 Feb 2019)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Mac OSX - HomeBrew install packages in a forgiving way
+
+set -eu
+[ -n "${DEBUG:-}" ] && set -x
+
+if [ $# = 0 ]; then
+    echo "usage: ${0##*/} <filename> <filename> ..."
+    exit 1
+fi
+
+echo "Installing Mac HomeBrew Packages"
+
+brew_packages="$(sed 's/#.*//; /^[[:space:]]*$/d' "$@")"
+
+# Sudo is not required as running Homebrew as root is extremely dangerous and no longer supported as
+# Homebrew does not drop privileges on installation you would be giving all build scripts full access to your system
+
+[ -n "${NO_UPDATE:-}" ] || brew update
+
+if [ -n "${NOFAIL:-}" ]; then
+    for package in $apk_packages; do
+        brew install "$package" || :
+    done
+else
+    brew install $apk_packages
+fi
