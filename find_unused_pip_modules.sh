c587102e25f302e357bceb30f38ec83c6e13d130
Hari Sekhon <harisekhon@gmail.com>
2019-02-28 01:20:21 +0000

    added find_unused_pip_modules.sh

---
+++ find_unused_pip_modules.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-02-27 11:49:47 +0000 (Wed, 27 Feb 2019)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Script to find unused Python Pip / PyPI modules in the current codebase
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "$0")"
+
+usage(){
+    if [ -n "$*" ]; then
+        echo "$@"
+        echo
+    fi
+    cat <<EOF
+
+usage: ${0##*/} <files>
+
+EOF
+    exit 3
+}
+
+for x in $@; do
+    case $x in
+    -h|--help)  usage
+                ;;
+    esac
+done
+
+found=0
+
+while read module; do
+    #egrep -q "import[[:space:]]+$module" "$@" || echo "$module"
+    git grep "import[[:space:]]\+$module\|from[[:space:]]\+$module[[:space:]]\+import[[:space:]]\+" | grep -v requirements.txt | grep -q . || echo "$module"
+    let found+=1
+done < <(
+    sed 's/#.*//;
+         s/[<>=].*//;
+         s/^[[:space:]]*//;
+         s/[[:space:]]*$//;
+         /^[[:space:]]*$/d;' "$@" |
+         sort -u |
+         "$srcdir/python_module_to_import_name.sh"
+)
+
+if [ $found -gt 0 ]; then
+    exit 1
+fi
