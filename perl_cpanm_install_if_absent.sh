4663fb5f736eac6b7ace997390ba1356612a00a5
Hari Sekhon <harisekhon@gmail.com>
2019-03-19 21:26:40 +0000

    updated perl_cpanm_install_if_absent.sh

--- perl_cpanm_install_if_absent.sh
+++ perl_cpanm_install_if_absent.sh
@@ -39,5 +39,8 @@ fi
 
 for cpan_module in $cpan_modules; do
     perl_module="${cpan_module%%@*}"
-    perl -e "use $perl_module;" || $SUDO ${CPANM:-cpanm} $opts --notest "$cpan_module"
+    if ! perl -e "use $perl_module;" &>/dev/null; then
+        echo "Installing $perl_module"
+        $SUDO ${CPANM:-cpanm} $opts --notest "$cpan_module"
+    fi
 done
