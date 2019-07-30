5714f84597e0d4d76197a4894e90e4df582aa4a2
Hari Sekhon <harisekhon@gmail.com>
2019-07-30 20:19:31 +0100

    updated check_python_asserts.sh

--- check_python_asserts.sh
+++ check_python_asserts.sh
@@ -17,6 +17,7 @@ set -eu
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
 
 if [ -z "$(find "${1:-.}" -maxdepth 2 -type f -iname '*.py' -o -iname '*.jy')" ]; then
