e4e0cbd44c0a48f7681d9a31c666f39609a5adf4
Hari Sekhon <harisekhon@gmail.com>
2019-07-30 15:51:37 +0100

    updated kafka.sh source path

--- kafka_wrappers/kafka_producer_perf_test.sh
+++ kafka_wrappers/kafka_producer_perf_test.sh
@@ -18,6 +18,6 @@ srcdir="$(dirname "$0")"
 
 # sources heap, kerberos, brokers, zookeepers etc
 # shellcheck disable=SC1090
-. "$srcdir/.bash.d/kafka.sh"
+. "$srcdir/../.bash.d/kafka.sh"
 
 kafka-producer-perf-test.sh "$@"
--- kafka_wrappers/kafka_topics.sh
+++ kafka_wrappers/kafka_topics.sh
@@ -18,7 +18,7 @@ srcdir="$(dirname "$0")"
 
 # sources heap, kerberos, brokers, zookeepers etc
 # shellcheck disable=SC1090
-. "$srcdir/.bash.d/kafka.sh"
+. "$srcdir/../.bash.d/kafka.sh"
 
 # it's assigned in .bash.d/kafka.sh
 # shellcheck disable=SC2154
