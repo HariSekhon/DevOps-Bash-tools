1aa591e08a7faabfacbac8da8d6897e8fbd623a5
Hari Sekhon <harisekhon@gmail.com>
2019-07-30 15:52:29 +0100

    updated kafka.sh

--- .bash.d/kafka.sh
+++ .bash.d/kafka.sh
@@ -14,7 +14,7 @@
 #
 
 export PATH="$PATH:/usr/hdp/current/kafka-broker/bin"
-export PATH="$PATH::$(dirname "${BASH_SOURCE[0]}")/../kafka_wrappers"
+export PATH="$PATH:$(dirname "${BASH_SOURCE[0]}")/../kafka_wrappers"
 
 # HDP defaults to 8GB, on VMs that often breaks cli commands which try to claim too much ram and fail
 export KAFKA_OPTS="$KAFKA_OPTS -Xms1G -Xmx1G"
