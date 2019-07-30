6e8adf1270557896caeced3ada0ac535e036fd51
Hari Sekhon <harisekhon@gmail.com>
2019-07-30 15:34:34 +0100

    added kafka_console_consumer.sh, kafka_console_producer.sh, kafka_consumer_groups.sh, kafka_consumer_perf_test.sh, kafka_producer_perf_test.sh, kafka_zookeeper_shell.sh

---
+++ kafka_console_consumer.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-07-30 15:16:58 +0100 (Tue, 30 Jul 2019)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "$0")"
+
+# sources heap, kerberos, brokers, zookeepers etc
+# shellcheck disable=SC1090
+. "$srcdir/.bash.d/kafka.sh"
+
+# it's assigned in .bash.d/kafka.sh
+# shellcheck disable=SC2154
+kafka-console-consumer.sh "$bootstrap_servers" "$@"
