15cc127e6e8a7cf509b6c341c7a91c0c32f81897
Hari Sekhon <harisekhon@gmail.com>
2019-08-11 02:41:29 +0100

    updated functions.sh

--- .bash.d/functions.sh
+++ .bash.d/functions.sh
@@ -61,6 +61,88 @@ yy(){
 
 # ============================================================================ #
 
+timestamp(){
+    printf "%s" "$(date '+%F %T')  $*"
+    [ $# -gt 0 ] && printf "\n"
+}
+alias tstamp=timestamp
+
+timestampcmd(){
+    local output
+    output="$(eval "$@" 2>&1)"
+    timestamp "$output"
+}
+alias tstampcmd=timestampcmd
+
+# ============================================================================ #
+
+bak(){
+    # TODO: switch this to a .backupstore folder for keeping this stuff instead
+    for filename in "$@"; do
+        [ -n "$filename" ] || { echo "usage: bak filename"; return 1; }
+        [ -f "$filename" ] || { echo "file '$filename' does not exist"; return 1; }
+        [[ $filename =~ .*\.bak\..* ]] && continue
+        local bakfile
+        bakfile="$filename.bak.$(date '+%F_%T' | sed 's/:/-/g')"
+        until ! [ -f "$bakfile" ]; do
+            echo "WARNING: bakfile '$bakfile' already exists, retrying with a new timestamp"
+            sleep 1
+            bakfile="$filename.bak.$(date '+%F_%T' | sed 's/:/-/g')"
+        done
+        cp -av "$filename" "$bakfile"
+    done
+}
+
+unbak(){
+    # restores the most recent backup of a file
+    for filename in "$@"; do
+        #[ -n "$filename" -o "${filename: -4}" != ".bak" ] || { echo "usage: unbak filename.bak"; return 1; }
+        [ -n "$filename" ] || { echo "usage: unbak filename"; return 1; }
+        #[ -f "$filename" ] || { echo "file '$filename' does not exist"; return 1; }
+        local bakfile
+        # don't use -t switch, we want the newest by name, not one that got touched recently
+        bakfile="$(find . -name "$filename.bak.*" 2>/dev/null | sort | tail -n 1)"
+        echo "restoring $bakfile"
+        cp -av "$bakfile" "$filename"
+    done
+}
+
+orig(){
+    if [ $# -lt 1 ]; then
+        echo "usage: orig file1 file2 file3 ..."
+        return 1
+    fi
+    for filename in "$@"; do
+        [ -f "$filename" ] || { echo "file '$filename' does not exist"; return 1; }
+        [ -f "$filename.org" ] && { echo "$filename.orig already exists, aborting..."; return 1; }
+    done
+    for filename in "$@"; do
+        cp -av "$filename" "$filename.orig"
+    done
+}
+
+unorig(){
+    if [ $# -lt 1 ]; then
+        echo "usage: unorig file1.orig file2.orig file3.orig ..."
+        return 1
+    fi
+    for filename in "$@"; do
+        if [ -z "$filename" ] || [ "${filename: -5}" != ".orig" ]; then
+            echo "usage: unorig filename.orig"
+            return 1
+        fi
+        if ! [ -f "$filename" ]; then
+            echo "file '$filename' does not exist"
+            return 1
+        fi
+    done
+    for filename in "$@"; do
+        cp -av "$filename" "${filename%.orig}"
+    done
+}
+
+# ============================================================================ #
+
 strLastIndexOf(){
     local str="$1"
     local substr="$2"
@@ -69,10 +151,7 @@ strLastIndexOf(){
     echo $lastIndex
 }
 
-# this idea's is a bust so far...
-#function c(){
-#    screen -t "$@" bash -c ". ~/.bashrc && eval $@"
-#}
+# ============================================================================ #
 
 progs(){
     find "${@:-.}" -type f |
