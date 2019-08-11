153324cb11d15749034f02479790db044febad35
Hari Sekhon <harisekhon@gmail.com>
2019-08-11 15:00:06 +0100

    exported more code

--- .bash.d/network.sh
+++ .bash.d/network.sh
@@ -39,7 +39,67 @@ checkhost(){
 
 alias google="while true; do ping www.google.com && break; sleep 1 || break; done"
 alias g=google
+
+# watch_url.pl is in DevOps-Perl-tools repo which should be in $PATH
 alias wg="watch_url.pl google.com"
+
+getip(){
+    host "$@" | grep "has address" | awk '{print $4; exit}'
+}
+
+tping(){
+    while true; do
+        #echo -n "`date '+%F %T'`   "
+        local output
+        output="$(ping -c 1 "$pingwait" 2 "$1" |
+                  grep -v -e statistics \
+                          -e "transmitted" \
+                          -e "rtt min/avg/max/mdev" \
+                          -e "bytes of data" \
+                          -e "^\s*$" \
+                          -e "^PING " \
+                          -e "round-trip"
+                 )"
+        echo "$(date '+%F %T')   ${output:-no response from $1}"
+        sleep 1
+    done
+}
+
+port(){
+    if [ -z "$2" ]; then
+        echo "You must supply a hostname/ip address to test followed by a port number"
+        return 1
+    fi
+    #sudo nmap $1 -p $2 ${@:3} -P0 | grep tcp
+    nc -zv "$1" "$2" 2>&1 | grep -v "DNS fwd/rev misma" | sed 's/[^]]*\] //'
+}
+
+testport(){
+    if [ -z "$2" ]; then
+        echo "You must supply a hostname/ip address to test followed by a port number"
+        return 1
+    fi
+    while true; do
+        timestampcmd port "$1" "$2"
+        sleep 1
+    done
+}
+
+hammerport(){
+    for i in {1..500}; do
+        printf "%-3s: " "$i"
+        nc -z -v "$1" "$2"
+    done
+}
+
+halfopen(){
+    while true; do
+        echo -n "half-open connections: "
+        netstat -ant | grep c SYN_RECV
+        sleep 1
+    done
+}
+
 get_gw(){
     local gw
     gw="$(netstat -rn | awk '/^default/ {print $2;exit}')"
@@ -69,4 +129,87 @@ z(){
         echo "Couldn't find gateway, cannot test upstream connectivity!"
         return 1
     fi
+}
+
+myip(){
+    ifconfig | grep 'inet[[:space:]]' | grep -v 127.0.0.1 | awk '{print $2}'
+}
+
+whatismyip(){
+    #lynx -dump $(lynx -dump www.whatismyip.com | tail -n 1)
+    lynx -useragent="Mozilla" -dump www.whatismyip.com 2>/dev/null | awk '/Address Lookup Your IP|Your Public IPv6 is:/ {print $6}'
+}
+
+downorjustme(){
+    browser "http://www.downforeveryoneorjustme.com/$1"
+}
+
+# directs to the same as downorjustme
+isupme(){
+    browser "http://www.isup.me/$1"
+}
+
+
+# ============================================================================ #
+#                                 M a c   O S X
+# ============================================================================ #
+
+if [ -z "$APPLE" ]; then
+    return
+fi
+
+flushdns(){
+    dscacheutil -flushcache
+    sudo killall -HUP mDNSResponder
+}
+alias flushcache=flushdns
+
+# Cisco AnyConnect set these rules which mess up my ability to connect directly to VirtualBox VMs on HostOnly Networking
+cleardeny(){
+    sudo ipfw delete "$(sudo ipfw list | grep deny | awk '{print $1}')"
+}
+ipfwqflush(){
+    sudo ipfw -q flush
+}
+
+isMacNetworkService(){
+    local interface="$1"
+    if [ "$interface" != "Thunderbolt Ethernet" ] &&
+       [ "$interface" != "Wi-Fi" ]; then
+        echo "interface must be one of Thunderbolt Ethernet or Wi-Fi"
+        return 1
+    fi
+}
+
+set_dns(){
+    get_apple_interfaces |
+    while read -r interface; do
+        sudo networksetup -setdnsservers "$interface" "$@"
+    done
+}
+
+set_dns_search(){
+    get_apple_interfaces |
+    while read -r interface; do
+        sudo networksetup -setsearchdomains "$interface" "$@"
+    done
+}
+
+set_dns_search_empty(){
+        set_dns_search "Empty"
+}
+# this wasn't found as an alias from another function
+clear_dns_search(){
+    set_dns_search_empty
+}
+
+function publicdns(){
+    set_dns 4.2.2.1 4.2.2.2 4.2.2.3 4.2.2.4 4.2.2.5 4.2.2.6
+    set_dns_search_empty
+}
+
+function dhcpdns(){
+    clear_dns_search # hangs without this as I think it tries to query DNS for all the suffixes in the list
+    set_dns "Empty"
+    #networksetup -setsearchdomains <networkservice> <domain1> [domain2]
 }
