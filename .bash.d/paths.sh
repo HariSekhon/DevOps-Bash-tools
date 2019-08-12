ab76efdd8d827f072d1e72c9760f8c45d54b575e
Hari Sekhon <harisekhon@gmail.com>
2019-08-12 02:16:18 +0100

    updated paths.sh

--- .bash.d/paths.sh
+++ .bash.d/paths.sh
@@ -23,3 +23,38 @@
 if [ -d /usr/local/parquet-tools ]; then
     add_PATH "/usr/local/parquet-tools"
 fi
+
+# my main GitHub repos
+add_PATH "$github/bash-tools"
+add_PATH "$github/pytools"
+add_PATH "$github/tool"
+add_PATH "$github/tools"
+add_PATH "$github/go-tools"
+add_PATH "$github/nagios-plugins"
+add_PATH "$github/nagios-plugin-kafka"
+add_PATH "$github/spotify"
+
+# gems will be installed to ~/.gem/ruby/x.y.z/bin
+# add newest ruby to path first
+for ruby_bin in $(ls -d ~/.gem/ruby/*/bin 2>/dev/null | tail -r); do
+    add_PATH "$ruby_bin"
+done
+
+# don't use Mongo any more
+#add_PATH "$github/mtools"
+
+#add_PATH "/usr/local/etcd"
+
+#add_PATH "/usr/local/artifactory-oss/bin"
+#add_PATH "/usr/local/jmeter/bin"
+#add_PATH "/usr/local/jruby/bin"
+#add_PATH "/usr/local/jython/bin"
+#add_PATH "/usr/local/mongodb/bin"
+#add_PATH "/usr/local/mysql/bin"
+#add_PATH "/usr/local/mysql/bin"
+#add_PATH "/usr/local/neo4j/bin"
+#add_PATH "/usr/local/riak/bin"
+#add_PATH "/usr/local/scala/bin"
+
+#add_PATH "$HOME/bin/expect"
+#add_PATH "$RANCID_HOME/bin"
