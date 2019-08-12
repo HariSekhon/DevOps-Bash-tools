43df8d61382c2f2974543d14e3ab8627a3a11cc8
Hari Sekhon <harisekhon@gmail.com>
2019-08-12 01:43:45 +0100

    added virtualbox.sh

---
+++ .bash.d/virtualbox.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-01-14 22:13:51 +0000
+#  Original: circa 2011-2012
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# ============================================================================ #
+#                              V i r t u a l B o x
+# ============================================================================ #
+
+vboxmanage_list_vms="VBoxManage list runningvms"
+# shellcheck disable=SC2139
+alias vbox_running="echo $vboxmanage_list_vms; $vboxmanage_list_vms"
+alias vr=vbox_running
+unset vboxmanage_list_vms
+
+alias startvm="VBoxManage startvm"
+alias stopvm="VBoxManage controlvm acpipowerbutton"
+alias poweroff="VBoxManage controlvm poweroff"
+alias savestate="VBoxManage savestate"
+alias controlvm="VBoxManage controlvm"
+
+#docker(){
+#    local vm='boot2docker-vm'
+#    VBoxManage list runningvms | grep -q "$vm" || VBoxManage startvm "$vm"
+#    command docker "$@"
+#}
+
+# fixvbox() in .bash.d/apple.sh restarts VirtualBox on Mac OSX only
+
+fixvboxnet(){
+    sudo ifconfig vboxnet0 down
+    sudo ifconfig vboxnet0 up
+}
