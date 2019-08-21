2a478583fdd7172bfd7f41d076bb9ff5940715fd
Hari Sekhon <harisekhon@gmail.com>
2019-08-21 14:09:38 +0100

    updated find_duplicate_cpan_requirements.sh

--- find_duplicate_cpan_requirements.sh
+++ find_duplicate_cpan_requirements.sh
@@ -23,7 +23,7 @@ srcdir="$(dirname "$0")"
 . "$srcdir/lib/utils.sh"
 
 # shellcheck disable=SC2034
-usage_args="<files>"
+usage_args="[<setup/cpan_requirements.txt>]"
 
 for x in "$@"; do
     case "$x" in
@@ -32,20 +32,38 @@ for x in "$@"; do
     esac
 done
 
+if [ $# -gt 0 ]; then
+    requirements_files="$*"
+else
+    # might be more confusing in perl tools to find unused modules in subdirs like perl lib, so just stick to local
+    #requirements_files="$(find . -name cpan-requirements.txt)"
+    requirements_files="$(find . -maxdepth 2 -name cpan-requirements.txt)"
+    if [ -z "$requirements_files" ]; then
+        usage "No requirements files found, please specify explicit path to cpan-requirements.txt"
+    fi
+fi
+
 found=0
 
-while read -r module; do
-    grep "^$module\>" "$@"
-    ((found + 1))
-done < <(
+# want splitting of requirements files to separate files
+# shellcheck disable=SC2086
+duplicate_cpan_modules="$(
     sed 's/#.*//;
          s/@.*//;
          s/^[[:space:]]*//;
          s/[[:space:]]*$//;
-         /^[[:space:]]*$/d;' "$@" |
+         /^[[:space:]]*$/d;' $requirements_files |
     sort |
     uniq -d
-)
+)"
+
+while read -r module; do
+    [ -z "$module" ] && continue
+    # want splitting of requirements files to separate files
+    # shellcheck disable=SC2086
+    grep "^$module\\>" $requirements_files
+    ((found + 1))
+done <<< "$duplicate_cpan_modules"
 
 if [ $found -gt 0 ]; then
     exit 1
