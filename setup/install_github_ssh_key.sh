18c451662f89981234861e3fca4b19daf2f188bf
Hari Sekhon <harisekhon@gmail.com>
2019-09-16 13:38:03 +0100

    added install_github_ssh_key.sh

---
+++ setup/install_github_ssh_key.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+#  Author: Hari Sekhon
+#  Date: 2019-09-16
+#
+#  https://github.com/harisekhon/devops-bash-tools
+#
+#  License: see accompanying LICENSE file
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Installs the SSH Key(s) from user's GitHub to the local $HOME/.ssh/authorized_keys
+#
+# Uses $GITHUB_USER or $USER as the expected GitHub user
+#
+# set $AUTHORIZED_KEYS to specify an alternative ssh keys location
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+home="${HOME:-$(cd && pwd)}"
+authorized_keys="${AUTHORIZED_KEYS:-$home/.ssh/authorized_keys}"
+
+if [[ "$USER" =~ hari|sekhon ]]; then
+    GITHUB_USER="${GITHUB_USER:-harisekhon}"
+else
+    GITHUB_USER="${GITHUB_USER:-$USER}"
+fi
+
+echo "Fetching SSH Key(s) from GitHub for account:  $GITHUB_USER"
+echo
+while read -r ssh_key; do
+    echo "Processing key:  $ssh_key"
+    #algo_hash="$(awk '{print $1" "$2}' <<< "$ssh_key")"
+    algo_hash="${ssh_key%%==*}"
+    if [ -f "$authorized_keys" ] &&
+        grep -Fq "$algo_hash" "$authorized_keys"; then
+        echo "Key already found in $authorized_keys, skipping..."
+    else
+        echo "Adding key to $authorized_keys"
+        mkdir -vp "$(dirname "$authorized_keys")"
+        echo "$ssh_key from GitHub" >> "$authorized_keys"
+    fi
+    echo
+done < <(curl -s "https://github.com/$GITHUB_USER.keys")
+echo Done
