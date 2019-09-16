938a0ab3905ae82515b2ce7597f60e4d6f4b7798
Hari Sekhon <harisekhon@gmail.com>
2019-09-16 16:11:03 +0100

    updated .bash.d/git.sh

--- .bash.d/git.sh
+++ .bash.d/git.sh
@@ -125,8 +125,8 @@ st(){
             pushd "$target" >/dev/null || { echo "Error: failed to pushd to $target"; return 1; }
             echo "> git stash list" >&2
             git stash list && echo
-            echo "> git status $target $*" >&2
-            git -c color.status=always status . "$@"
+            echo "> git status -sb $target $*" >&2
+            git -c color.status=always status -sb . "$@"
         else
             pushd "$target_dirname" >/dev/null || { echo "Error: failed to pushed to '$target_dirname'"; return 1; }
             echo "> git status $target $*" >&2
