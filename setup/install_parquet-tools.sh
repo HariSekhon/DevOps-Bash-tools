245f9620dcd96f3ff57943eed55e735a136d3019
Hari Sekhon <harisekhon@gmail.com>
2019-09-17 18:01:45 +0100

    added install_parquet-tools.sh

---
+++ setup/install_parquet-tools.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+#  Author: Hari Sekhon
+#  Date: 2019-09-17
+#
+#  https://github.com/harisekhon/devops-bash-tools
+#
+#  License: see accompanying LICENSE file
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Installs Parquet Tools to local ~/bin
+#
+# add ~/bin/parquet-tools-* to $PATH (automatically detected and done via advanced bashrc in this repo)
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+PARQUET_VERSION="${PARQUET_VERSION:-1.5.0}"
+
+zipfile="parquet-tools-$PARQUET_VERSION-bin.zip"
+zipdir="${zipfile%-bin.zip}"
+
+URL="${URL:-http://search.maven.org/remotecontent?filepath=com/twitter/parquet-tools/$PARQUET_VERSION/$zipfile}"
+
+bin="${BIN:-~/bin}"
+
+mkdir -pv "$bin"
+
+cd "$bin"
+
+if command -v parquet-cat; then
+    echo "parquet-tools already found in \$PATH:"
+    echo
+    dirname "$(command -v parquet-cat)"
+elif [ -f "$zipdir/parquet-cat" ]; then
+    echo "parquet-tools already installed in local dir ($PWD/$zipdir)"
+else
+    echo "parquet-tools not found in \$PATH, nor in the local ${zipfile%.zip} directory"
+    echo
+    echo "downloading parquet-tools to $bin"
+    wget -t 100 --retry-connrefused -c -O "$zipfile" "$URL"
+    echo
+    echo "unzipping parquet-tools"
+    unzip "$zipfile"
+    echo
+    echo "removing zipfile"
+    rm -f "$zipfile"
+    echo
+    echo "Done"
+fi
+
+echo
+echo "Ensure $bin/${zipfile%.zip} is in the \$PATH (it's auto-detected in new shells if sourcing this repo's .bashrc)"
+echo
