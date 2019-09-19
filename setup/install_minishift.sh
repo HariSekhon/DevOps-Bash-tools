aa79f08e44189238b8bc7a55863d53c180a2bcd7
Hari Sekhon <harisekhon@gmail.com>
2019-09-19 16:06:49 +0100

    updated install_minishift.sh

--- setup/install_minishift.sh
+++ setup/install_minishift.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 #  Author: Hari Sekhon
-#  Date: 2019-09-16
+#  Date: Aug 2019
 #
 #  https://github.com/harisekhon/devops-bash-tools
 #
@@ -18,13 +18,24 @@ set -u
 #[ -n "${DEBUG:-}" ] &&
 set -x
 
+srcdir="$(dirname "$0")"
+
 if [ "$(uname -s)" = Darwin ]; then
-    brew cask install minishift
-    brew cask install --force minishift
-    brew install docker-machine-driver-xhyve
+    if ! command -v minishift &>/dev/null; then
+        if ! command -v brew &>/dev/null; then
+            echo "HomeBrew needs to be installed first, trying to install now"
+            "$srcdir/install_homebrew.sh"
+        fi
+        brew update
+        brew cask install minishift
+        brew cask install --force minishift
+        brew install docker-machine-driver-xhyve
+    fi
     sudo chown root:wheel "$(brew --prefix)"/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
     sudo chmod u+s /usr/local/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-    minishift start --vm-driver=virtualbox
+    if ! minishift status | grep -i started; then
+        minishift start --vm-driver=virtualbox
+    fi
     # .bash.d/kubernetes.sh automatically sources this so 'oc' command is available in all new shells
     minishift oc-env > ~/.minishift.env
 else
