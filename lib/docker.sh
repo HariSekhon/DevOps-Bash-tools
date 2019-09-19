d8ad8e94588d7edf514b6a3197044348989d3ca5
Hari Sekhon <harisekhon@gmail.com>
2019-09-19 13:31:08 +0100

    updated lib/docker.sh

--- lib/docker.sh
+++ lib/docker.sh
@@ -171,6 +171,7 @@ docker_compose_port(){
         #env_var="$(tr '[[:lower:]]' '[[:upper:]]' <<< "$env_var")_PORT"
         # doesn't work on Mac
         #env_var="$(sed 's/.*/\U&/;s/[^[:alnum:]]/_/g' <<< "$env_var")_PORT"
+        # shellcheck disable=SC2001
         env_var="$(sed 's/[^[:alnum:]]/_/g' <<< "$env_var" | tr '[:lower:]' '[:upper:]')_PORT"
     fi
     if [ -z "${DOCKER_SERVICE:-}" ]; then
