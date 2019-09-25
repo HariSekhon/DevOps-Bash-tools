0e65fc70d2bef2f27df3381c4b2ee97df4aa6417
Hari Sekhon <harisekhon@gmail.com>
2019-09-25 12:16:34 +0100

    updated install_ansible.sh

--- setup/install_ansible.sh
+++ setup/install_ansible.sh
@@ -61,8 +61,9 @@ elif [ "$os" = "Linux" ]; then
         echo "Installing via APT"
         if grep -q Ubuntu /etc/*release; then
             $sudo apt update
-            $sudo apt install software-properties-common
-            $sudo apt-add-repository --yes --update ppa:ansible/ansible
+            $sudo apt install -y software-properties-common
+            $sudo apt-add-repository -y --update ppa:ansible/ansible
+            $sudo apt update
             $sudo apt install -y ansible
         else
             # assume Debian
