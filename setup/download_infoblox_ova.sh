f29b2847337244e950f85ce2684fc3aa9a2d6a3a
Hari Sekhon <harisekhon@gmail.com>
2019-09-25 16:11:01 +0100

    updated download_infoblox_ova.sh

--- setup/download_infoblox_ova.sh
+++ setup/download_infoblox_ova.sh
@@ -18,21 +18,40 @@ set -euo pipefail
 
 INFOBLOX_URL=https://go.infoblox.com/downloads/vnios/8.4.4/nios-8.4.4-386831-2019-08-02-03-45-48-ddi.ova
 
+VM_NAME="${VM_NAME:-VNIOS System}"
+
+CPUS="${CPUS:-2}"
+RAM="${RAM:-4096}"  # MB
+VRAM="${VRAM:-12}"  # Video RAM MB
+
 echo "Downloading InfoBlox OVA to ~/Downloads/"
 
 cd ~/Downloads/
 
 infoblox_ova="${INFOBLOX_URL##*/}"
 
-if ! [ "$infoblox_ova" ]; then
+if [ "$infoblox_ova" ]; then
+    echo "Found $infoblox_ova, skipping download"
+else
     wget "$INFOBLOX_URL"
 fi
 
-if [ "$(uname -s)" = Darwin ]; then
-    echo "Opening $infoblox_ova, will run import in GUI and prompt you to accept license agreement"
-    open "$infoblox_ova"
+if vboxmanage list vms | grep "^\"$VM_NAME\""; then
+    echo "$VM_NAME VM already found, skipping import"
 else
-    echo "Load $PWD/$infoblox_ova to VirtualBox - you will be prompted to accept license agreement"
     # vboxmanage import nios-8.4.4-386831-2019-08-02-03-45-48-ddi.ova
     #VBoxManage: error: Cannot import until the license agreement listed above is accepted.
+    echo "Importing $infoblox_ova"
+    # could set $VM_NAME here but want to leave it as the default import
+    vboxmanage import --vsys 0 --vmname "$VM_NAME" --eula accept "$infoblox_ova"
+fi
+
+if vboxmanage showvminfo "$VM_NAME" | grep -q "^State:[[:space:]]*running"; then
+    echo "VM already running, skipping configure and start"
+else
+    echo "Configuring $VM_NAME with $CPUS CPUs, $RAM MB RAM, $VRAM MB Video RAM"
+    vboxmanage modifyvm "$VM_NAME" --cpus "$CPUS" --memory "$RAM" --vram "$VRAM"
+
+    echo "Starting $VM_NAME"
+    vboxmanage startvm "$VM_NAME"
 fi
