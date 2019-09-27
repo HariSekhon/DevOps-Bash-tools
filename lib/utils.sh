1b8b5c47c05d4a2a327340c8c16db1503430859a
Hari Sekhon <harisekhon@gmail.com>
2019-09-27 11:29:22 +0100

    updated lib/utils.sh

--- lib/utils.sh
+++ lib/utils.sh
@@ -177,8 +177,7 @@ is_mac(){
 is_jenkins(){
     if [ -n "${JENKINS_URL:-}" ] &&
        [ -n "${BUILD_NUMBER:-}" ] &&
-       [ -n "${BUILD_URL:-}" ]
-       ; then
+       [ -n "${BUILD_URL:-}" ]; then
         return 0
     fi
     return 1
