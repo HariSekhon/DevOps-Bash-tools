d340b64e581fa208c315837a4dca515146efc3c5
Hari Sekhon <harisekhon@gmail.com>
2019-10-07 18:12:21 +0100

    split perl_cpanm_install.sh off from perl_cpanm_install_if_absent.sh

--- perl_cpanm_install_if_absent.sh
+++ perl_cpanm_install_if_absent.sh
@@ -15,8 +15,10 @@
 
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Installing CPAN Modules"
+echo "Installing CPAN Modules that are not already installed"
+echo
 
 cpan_modules=""
 for x in "$@"; do
@@ -30,38 +32,12 @@ for x in "$@"; do
     cpan_modules="$(tr ' ' ' \n' <<< "$cpan_modules" | sort -u | tr '\n' ' ')"
 done
 
-opts=""
-if [ -n "${TRAVIS:-}" ]; then
-    echo "running in quiet mode"
-    opts="-q"
-fi
-
-if [ "$(uname -s)" = "Darwin" ]; then
-    # needed to build Crypt::SSLeay
-    export OPENSSL_INCLUDE=/usr/local/opt/openssl/include
-    export OPENSSL_LIB=/usr/local/opt/openssl/lib
-fi
-
-SUDO=""
-if [ $EUID != 0 ] &&
-   [ -z "${PERLBREW_PERL:-}" ]; then
-    SUDO=sudo
-fi
-
 for cpan_module in $cpan_modules; do
     perl_module="${cpan_module%%@*}"
     if perl -e "use $perl_module;" &>/dev/null; then
         echo "perl cpan module '$perl_module' already installed, skipping..."
     else
         echo "installing perl cpan module '$perl_module'"
-        if [ "$(uname -s)" = "Darwin" ]; then
-            # need to send OPENSSL_INCLUDE and OPENSSL_LIB through sudo explicitly
-            $SUDO \
-                OPENSSL_INCLUDE="$OPENSSL_INCLUDE" \
-                OPENSSL_LIB="$OPENSSL_LIB" \
-                "${CPANM:-cpanm}" $opts --notest "$cpan_module"
-        else
-            $SUDO "${CPANM:-cpanm}" $opts --notest "$cpan_module"
-        fi
+        "$srcdir/perl_cpanm_install.sh" "$cpan_module"
     fi
 done
