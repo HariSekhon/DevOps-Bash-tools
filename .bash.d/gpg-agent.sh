286dcbcb2aaa2353d8d8a63fe300cdd41a726e43
Hari Sekhon <harisekhon@gmail.com>
2019-10-25 11:46:38 +0100

    updated gpg-agent.sh

--- .bash.d/gpg-agent.sh
+++ .bash.d/gpg-agent.sh
@@ -24,28 +24,31 @@
 gpg_agent(){
     if [ $UID != 0 ]; then
         if type -P gpg-agent &>/dev/null; then
-            GPG_ENV_FILE=~/.gpg-agent.env
-            if [ -f "$GPG_ENV_FILE" ]; then
+            # looks like gpg-agent not longer outputs the pid to stdout to capture
+            #GPG_ENV_FILE=~/.gpg-agent.env
+            #if [ -f "$GPG_ENV_FILE" ]; then
                 # shellcheck disable=SC1090
-                . "$GPG_ENV_FILE" > /dev/null
+                #. "$GPG_ENV_FILE" > /dev/null
 
-                GPG_AGENT_PID="${GPG_AGENT_INFO#*:}"
-                GPG_AGENT_PID="${GPG_AGENT_PID%:*}"
-                if ! kill -0 "$GPG_AGENT_PID" > /dev/null 2>&1; then
-                    echo "Stale gpg-agent found. Spawning new agent..."
-                    killall -9 gpg-agent
-                    eval "$(gpg-agent --daemon | tee "$GPG_ENV_FILE")"
-                elif [ "$(ps -p "$GPG_AGENT_PID" -o comm=)" != "gpg-agent" ]; then
-                    echo "gpg-agent PID does not belong to gpg-agent, spawning new agent..."
-                    eval "$(gpg-agent --daemon | tee "$GPG_ENV_FILE")"
-                fi
-            else
+                #GPG_AGENT_PID="${GPG_AGENT_INFO#*:}"
+                #GPG_AGENT_PID="${GPG_AGENT_PID%:*}"
+                #if ! kill -0 "$GPG_AGENT_PID" > /dev/null 2>&1; then
+                #    echo "Stale gpg-agent found. Spawning new agent..."
+                #    killall -9 gpg-agent
+                #    eval "$(gpg-agent --daemon | tee "$GPG_ENV_FILE")"
+                #elif [ "$(ps -p "$GPG_AGENT_PID" -o comm=)" != "gpg-agent" ]; then
+                #    echo "gpg-agent PID does not belong to gpg-agent, spawning new agent..."
+                #    eval "$(gpg-agent --daemon | tee "$GPG_ENV_FILE")"
+                #fi
+            if ! pgrep -qf gpg-agent.*--daemon; then
                 echo "Starting gpg-agent..."
                 killall -9 gpg-agent
-                eval "$(gpg-agent --daemon | tee "$GPG_ENV_FILE")"
+                #eval "$(gpg-agent --daemon | tee "$GPG_ENV_FILE")"
+                gpg-agent --daemon
             fi
             #clear
         fi
     fi
 }
-gpg_agent
+# don't really use this any more anyway so don't bother starting it
+#gpg_agent
