4af54dca1794068bf03e8fe2be4f3737f71ccba4
Hari Sekhon <harisekhon@gmail.com>
2019-11-02 23:14:24 +0000

    added pygmentize.sh

---
+++ pygmentize.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-11-02 21:50:00 +0000 (Sat, 02 Nov 2019)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+#set -euo pipefail
+#[ -n "${DEBUG:-}" ] && set -x
+
+case "$1" in
+    *.ad[asb]|\
+    *.asm|\
+    *.awk|\
+    *.axp|\
+    *.diff|\
+    *.ebuild|\
+    *.eclass|\
+    *.groff|\
+    *.hh|\
+    *.inc|\
+    *.java|\
+    *.js|\
+    *.lsp|\
+    *.l|\
+    *.m4|\
+    *.pas|\
+    *.patch|\
+    *.php|\
+    *.pl|\
+    *.pm|\
+    *.pod|\
+    *.pov|\
+    *.ppd|\
+    *.py|\
+    *.p|\
+    *.rb|\
+    *.sh|\
+    *.sql|\
+    *.xml|\
+    *.xps|\
+    *.xsl|\
+    *.[ch]pp|\
+    *.[ch]xx|\
+    *.[ch]\
+        )   pygmentize -f 256 "$1"
+            ;;
+
+    .bash*) pygmentize -f 256 -l sh "$1"
+            ;;
+
+    *)
+        if grep -q '#!.*bash' "$1" 2> /dev/null; then
+            pygmentize -f 256 -l sh "$1"
+        else
+            exit 1
+        fi
+esac
+
+exit 0
