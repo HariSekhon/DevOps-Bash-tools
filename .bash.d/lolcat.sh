4d3277f72814cf3ddb7f344a168043b3943bf1d4
Hari Sekhon <harisekhon@gmail.com>
2019-11-03 11:33:18 +0000

    added lolcat.sh

---
+++ .bash.d/lolcat.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-11-03 11:31:02 +0000 (Sun, 03 Nov 2019)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+lolz(){
+    exec 1> >(lolcat >&2)
+}
