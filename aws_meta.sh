285e4ae10b46a06c31e76635184e600ad1305087
Hari Sekhon <harisekhon@gmail.com>
2019-11-27 20:49:16 +0000

    added aws_meta.sh

---
+++ aws_meta.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-11-27 20:41:35 +0000 (Wed, 27 Nov 2019)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+usage(){
+    echo "
+Simple query wrapper to AWS's Metadata API for speed of typing
+
+${0##*/} <resource>
+
+eg. ${0##*/} public-ipv4
+    ${0##*/} public-hostname
+    ${0##*/} local-ipv4
+    ${0##*/} local-hostname
+    ${0##*/} instance-type
+    ${0##*/} ami-id
+    ${0##*/} placement/availability-zone
+
+See also ec2-metadata script which is a more complete shell script
+"
+    exit 3
+}
+
+#if [ $# -ne 1 ]; then
+#    usage
+#fi
+
+if [[ "$1" =~ -.* ]]; then
+    usage
+fi
+
+curl "http://169.254.169.254/latest/meta-data/${1:-}"
