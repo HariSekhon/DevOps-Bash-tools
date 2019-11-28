4eb6b935e01561e2483ef969630892e49eeb2ab5
Hari Sekhon <harisekhon@gmail.com>
2019-11-28 10:27:32 +0000

    updated hdfs_find_replication_factor_1.sh

--- hdfs_find_replication_factor_1.sh
+++ hdfs_find_replication_factor_1.sh
@@ -27,10 +27,15 @@ Calls HDFS command which is assumed to be in \$PATH
 
 Make sure to kinit before running this if using a production Kerberized cluster
 
+Setting environment variable SKIP_ZERO_BYTE_FILES to any value will not list files with zero bytes
+Setting environment variable SET_REPLICATION_FACTOR_3 to any value will set any files found with
+replication factor 1 back to replication factor 3
+
+
 See also:
 
 hdfs_find_replication_factor_1.py in DevOps Python tools repo which can
-reset these found files back to replication factor 3 to fix the issue
+also reset these found files back to replication factor 3 to fix the issue
 
 https://github.com/harisekhon/devops-python-tools
 
@@ -54,8 +59,17 @@ skip_zero_byte_files(){
     fi
 }
 
+set_replication_factor_3(){
+    if [ -n "${SET_REPLICATION_FACTOR_3:-}" ]; then
+        xargs hdfs dfs -setrep 3
+    else
+        cat
+    fi
+}
+
 hdfs dfs -ls -R "$@" |
 grep -v '^d' |
 skip_zero_byte_files |
-awk '{ if($2=1) { $1=$2=$3=$4=$5=$6=$7=""; print } }' |
-sed 's/^[[:space:]]*//'
+awk '{ if ($2 == 1) { $1=$2=$3=$4=$5=$6=$7=""; print } }' |
+sed 's/^[[:space:]]*//' |
+set_replication_factor_3
