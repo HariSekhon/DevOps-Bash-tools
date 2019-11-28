4f3ff0242067d6a2dbd2b06a1239d5f70e9ed2b8
Hari Sekhon <harisekhon@gmail.com>
2019-11-28 10:12:31 +0000

    updated hdfs_checksum_paths.sh

--- hdfs_checksum_paths.sh
+++ hdfs_checksum_paths.sh
@@ -56,7 +56,7 @@ skip_zero_byte_files(){
 hdfs dfs -ls -R "$@" |
 grep -v '^d' |
 skip_zero_byte_files |
-awk '{$1=$2=$3=$4=$5=$6=$7="";print}' |
+awk '{ $1=$2=$3=$4=$5=$6=$7=""; print }' |
 #sed 's/^[[:space:]]*//' |
 while read -r filepath; do
     hdfs dfs -checksum "$filepath"
