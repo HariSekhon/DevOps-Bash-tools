36334eeb2e7432d2c81202ce0e1bec4476543188
Hari Sekhon <harisekhon@gmail.com>
2019-12-03 16:54:53 +0000

    updated gce_is_preempted.sh

--- gce_is_preempted.sh
+++ gce_is_preempted.sh
@@ -37,14 +37,12 @@ if [[ "${1:-}" =~ -.* ]]; then
     usage
 fi
 
-if ! curl -s --connect-timeout 2 -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/" &>/dev/null; then
-    echo "This script must be run from within a GCE instance as that is the only place the GCP GCE Metadata API is available"
-    exit 2
-fi
+#if ! curl -s --connect-timeout 2 -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/" &>/dev/null; then
+#    echo "This script must be run from within a GCE instance as that is the only place the GCP GCE Metadata API is available"
+#    exit 2
+#fi
 
-output="$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/preempted")"
-# shellcheck disable=SC2181
-if [ $? -eq 0 ]; then
+if output="$(curl -s --connect-timeout 5 -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/preempted")"; then
     if grep -q TRUE <<< "$output"; then
         echo "preempted"
         exit 0
