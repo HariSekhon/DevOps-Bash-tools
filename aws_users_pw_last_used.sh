b89282179e6d9cfcd64094a5207cf5ce1db4dc2e
Hari Sekhon <harisekhon@gmail.com>
2019-12-05 17:55:44 +0000

    added aws_users_pw_last_used.sh

---
+++ aws_users_pw_last_used.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-12-05 17:02:15 +0000 (Thu, 05 Dec 2019)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+aws iam list-users |
+jq -r '.Users[] | [.UserName, .PasswordLastUsed] | @tsv' |
+column -t
