8b18e85f79edadd04a882360b9a4254510bc41ef
Hari Sekhon <harisekhon@gmail.com>
2019-12-06 13:45:09 +0000

    updated install_homebrew.sh

--- setup/install_homebrew.sh
+++ setup/install_homebrew.sh
@@ -22,6 +22,10 @@ else
     echo "Installing HomeBrew"
     echo "==================="
     echo
+    if ! type -P git &>/dev/null; then
+        echo "Must have git installed before installing HomeBrew!"
+        exit 1
+    fi
     # automatically sending Enter to Continue
 	if [ "$(uname -s)" = Linux ]; then
 		sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)" <<< ""
