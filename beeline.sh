59afe386d3a5cee29be6ab1f20c03c0cdbb7d117
Hari Sekhon <harisekhon@gmail.com>
2019-12-10 11:54:19 +0000

    updated beeline.sh

--- beeline.sh
+++ beeline.sh
@@ -36,20 +36,24 @@ if [ -z "${HIVESERVER2_HOST:-}" ]; then
     exit 3
 fi
 
-ssl=""
+opts=""
+if [ -n "${BEELINE_OPTS:-}" ]; then
+    opts="$opts;$BEELINE_OPTS"
+fi
+
 if [ -n "${HIVESERVER2_SSL:-}" ] ||
    grep -A1 'hive.server2.use.SSL' /etc/hive/conf/hive-site.xml 2>/dev/null |
    grep -q true; then
-    ssl=";ssl=true"
+    opts="$opts;ssl=true"
     # works without this but enable if you need
     #set +o pipefail
     #trust_file="$(find /opt/cloudera/security/jks -maxdepth 1 -name '*-trust.jks' 2>/dev/null | head -n1)"
     #set -o pipefail
     #if [ -f "$trust_file" ]; then
-    #    ssl="$ssl;sslTrustStore=$trust_file"
+    #    opts="$opts;sslTrustStore=$trust_file"
     #fi
 fi
 
 realm="${HIVESERVER2_HOST#*.}"
 
-beeline -u "jdbc:hive2://$HIVESERVER2_HOST:10000/default;principal=hive/_HOST@${realm}${ssl}" "$@"
+beeline -u "jdbc:hive2://$HIVESERVER2_HOST:10000/default;principal=hive/_HOST@${realm}${opts}" "$@"
