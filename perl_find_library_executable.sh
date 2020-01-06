bc87ce57860f7e1df7d184debfd0b97f80e4f435
Hari Sekhon <harisekhon@gmail.com>
2020-01-06 17:59:17 +0000

    added perl_find_library_executable.sh

---
+++ perl_find_library_executable.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2230
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-09-15 09:59:01 +0100 (Sun, 15 Sep 2019)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Returns the first argument that is found as an executable in the $PATH
+#
+# Useful for Macs to find where libraries executable scripts like fatpacks are, which may get installed locally in $HOME/perl5/bin to avoid Mac OS X System Integrity Protection
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+perl="${PERL:-perl}"
+
+perl_path=""
+
+while read -r path; do
+    bin="${path%/lib/perl*/site-packages*}/bin"
+    if [ -d "$bin" ]; then
+        perl_path="$perl_path:$bin"
+    fi
+done < <("$perl" -e 'print join("\n", @INC);')
+
+if [ -d ~/perl5/bin ]; then
+    perl_path="$perl_path:"~/perl5/bin
+fi
+
+if [ $# -lt 1 ]; then
+    cat <<EOF
+Find where one or more CLI programs are installed by searching all the perl library locations
+
+Especially useful when perl tools get installed to places not in your \$PATH - where the 'which' command can't help
+
+usage: ${0##*/} <program> [<program2> <program3> ...]
+
+EOF
+    exit 1
+fi
+
+set +o pipefail
+found="$(PATH="$perl_path" type -P "$@" | head -n 1)"
+set -o pipefail
+
+if [ -n "$found" ]; then
+    echo "$found"
+else
+    echo "no perl executable was found matching any of: $*" >&2
+    echo "\$PATH searched was: $PATH" >&2
+    exit 1
+fi
