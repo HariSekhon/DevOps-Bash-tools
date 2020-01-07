10a0e8fd2d0f458ca4884cd90f61b693bbaabd92
Hari Sekhon <harisekhon@gmail.com>
2020-01-07 12:04:54 +0000

    added aws_policies_unattached.sh

---
+++ aws_policies_unattached.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-01-07 11:57:18 +0000 (Tue, 07 Jan 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Dumps all unattached policies in JSON format
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+aws iam list-policies |
+jq '.Policies[] | select(.IsAttachable==true) | select (.AttachmentCount==0)'
