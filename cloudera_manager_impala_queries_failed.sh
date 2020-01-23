f7dbb0f8e6858b25c2eb6b75df482550c2508e0e
Hari Sekhon <harisekhon@gmail.com>
2020-01-23 16:14:27 +0000

    added cloudera_manager_impala_queries_failed.sh

---
+++ cloudera_manager_impala_queries_failed.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-01-23 11:51:09 +0000 (Thu, 23 Jan 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Script to show recent Impala query metadata errors via Cloudera Manager API
+#
+# TSV output format:
+#
+# <time>    <database>      <user>      <query error>
+
+# Tested on Cloudera Enterprise 5.10
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+"$srcdir/cloudera_manager_impala_queries.sh" |
+jq -r '.queries[] |
+       select(.attributes.query_status | test("failed"; "i")) |
+       [.startTime, .database, .user, .statement, .attributes.query_status] |
+       @tsv'
