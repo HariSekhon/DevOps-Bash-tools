dae65002247d0b47d0b5afee3c318c994d43a692
Hari Sekhon <harisekhon@gmail.com>
2020-01-24 10:27:47 +0000

    updated install_gcloud.sh

--- setup/install_gcloud.sh
+++ setup/install_gcloud.sh
@@ -83,7 +83,8 @@ EOF
 elif type -P apt-get &>/dev/null; then
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | $sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
     $sudo apt-get install -y apt-transport-https ca-certificates
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | $sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+    curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg |
+        $sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
     $sudo apt-get update
     $sudo apt-get install -y google-cloud-sdk #=$apt_version
     # want splitting to single line
