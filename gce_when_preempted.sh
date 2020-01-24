401e0591f48d4ab42849931ec54c6e6823a7823c
Hari Sekhon <harisekhon@gmail.com>
2020-01-24 10:31:31 +0000

    updated gce_when_preempted.sh

--- gce_when_preempted.sh
+++ gce_when_preempted.sh
@@ -48,12 +48,12 @@ if [[ "${1:-}" =~ -.* ]]; then
     usage
 fi
 
-#if ! curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/" &>/dev/null; then
+#if ! curl -sS -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/" &>/dev/null; then
 #    echo "This script must be run from within a GCE instance as that is the only place the GCP GCE Metadata API is available"
 #    exit 2
 #fi
 
-if output="$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/preempted?wait_for_change=true")"; then
+if output="$(curl -sS -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/preempted?wait_for_change=true")"; then
     if grep -q TRUE <<< "$output"; then
         eval "$@"
     else
