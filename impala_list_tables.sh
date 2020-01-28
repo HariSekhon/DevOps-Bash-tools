641238ba6d316723bdfb0a5e257d567386019118
Hari Sekhon <harisekhon@gmail.com>
2020-01-28 17:33:27 +0000

    added impala_list_tables.sh

---
+++ impala_list_tables.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-12-10 11:33:52 +0000 (Tue, 10 Dec 2019)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Lists all Impala tables in all databases using adjacent impala_shell.sh script
+#
+# Tested on Impala 2.7.0, 2.12.0 on CDH 5.10, 5.16 with Kerberos and SSL
+
+# For more documentation see the comments at the top of impala_shell.sh
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "$0")"
+
+"$srcdir/impala_shell.sh" -Bq 'show databases' |
+while read -r db rest; do
+    "$srcdir/impala_shell.sh" -Bq "use $db; show tables" |
+    sed "s/^/$db./"
+done
