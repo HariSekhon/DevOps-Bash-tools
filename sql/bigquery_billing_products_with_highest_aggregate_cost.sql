489471864fabc45f37a451a42011cd050ab186f2
Hari Sekhon <harisekhon@gmail.com>
2020-02-02 00:47:04 +0000

    added bigquery_billing_products_with_highest_aggregate_cost.sql

---
+++ sql/bigquery_billing_products_with_highest_aggregate_cost.sql
@@ -0,0 +1,25 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-02-02 00:37:06 +0000 (Sun, 02 Feb 2020)
+--
+--  vim:ts=4:sts=4:sw=4:et
+--
+--  https://github.com/harisekhon/bash-tools
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- BigQuery Billing Query - products with highest aggregate cost
+SELECT
+  product,
+  ROUND(SUM(cost),2) AS total_cost
+FROM
+  `myproject.mydataset.imported_billing_data`
+GROUP BY
+  product
+ORDER BY
+  total_cost DESC
