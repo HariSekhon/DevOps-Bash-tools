4524c95e5026b7485e1c3eddc87c9495d3e5ff27
Hari Sekhon <harisekhon@gmail.com>
2020-02-05 15:09:04 +0000

    added impala_tables_locations.sh

---
+++ impala_tables_locations.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-12-10 11:33:52 +0000 (Tue, 10 Dec 2019)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Show table location for all tables via Impala shell
+#
+# Tested on Impala 2.7.0, 2.12.0 on CDH 5.10, 5.16 with Kerberos and SSL
+#
+# For more documentation see the comments at the top of impala_shell.sh
+
+# For a better version written in Python see DevOps Python tools repo:
+#
+# https://github.com/harisekhon/devops-python-tools
+
+# you will almost certainly have to comment out / remove '-o pipefail' to skip authorization errors such as that documented in impala_list_tables.sh
+set -eu  # -o pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "$0")"
+
+for arg; do
+    if [[ "$arg" =~ -h|--help ]]; then
+        echo "usage: ${0##*/} [impala_shell_options]"
+        exit 3
+    fi
+done
+
+query_template="describe formatted {table}"
+
+# exit the loop subshell if you Control-C
+trap 'exit 130' INT
+
+"$srcdir/impala_list_tables.sh" "$@" |
+while read -r db table; do
+    if [ -n "${FILTER:-}" ] &&
+       ! [[ "$db.$table" =~ $FILTER ]]; then
+        continue
+    fi
+    printf '%s.%s\t' "$db" "$table"
+    query="${query_template//\{db\}/\`$db\`}"
+    query="${query//\{table\}/\`$table\`}"
+    { "$srcdir/impala_shell.sh" --quiet -Bq "USE \`$db\`; $query" "$@" || echo ERROR; } |
+    grep ^Location |
+    sed 's/^Location:[[:space:]]*//; s/[[:space:]]*NULL[[:space:]]*$//'
+done
