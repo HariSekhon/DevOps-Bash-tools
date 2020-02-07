26acbde727456772b962ec4b59679c3bc5ef10d4
Hari Sekhon <harisekhon@gmail.com>
2020-02-07 15:48:35 +0000

    updated create_status_page.sh

--- setup/create_status_page.sh
+++ setup/create_status_page.sh
@@ -26,6 +26,8 @@ get_repos(){
     page=1
     while true; do
         echo "fetching repos page $page" >&2
+        # use authenticated requests if you are hitting the API rate limit
+        # eg. CURL_OPTS="-u harisekhon:$GITHUB_TOKEN" ...
         # shellcheck disable=SC2086
         if ! output="$(curl -sS --connect-timeout 3 ${CURL_OPTS:-} "https://api.github.com/users/HariSekhon/repos?page=$page&per_page=100")"; then
             echo "ERROR" >&2
