959fad231227c0ecdb3067351f72e9b9e01b8e0d
Hari Sekhon <harisekhon@gmail.com>
2020-02-12 17:44:13 +0000

    added github_workflows_status.sh

---
+++ github_workflows_status.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-02-12 16:21:52 +0000 (Wed, 12 Feb 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "$0")"
+
+usage(){
+    cat <<EOF
+$*
+
+Script to get GitHub Workflow runs status via the API
+
+If no repo arg is given and is inside a git repo then takes determines the repo from the first git remote listed
+
+\$REPO and \$WORKFLOW_ID environment variables are also supported with positional args taking precedence
+
+usage: ${0##*/} <repo> [<workflow_id>]
+
+EOF
+    exit 3
+}
+
+workflows="$(
+    "$srcdir/github_workflows.sh" "$@" |
+    jq -r '.workflows[].path' |
+    sed 's|.github/workflows/||;s|\.yaml$||'
+)"
+
+
+for workflow_name in $workflows; do
+    {
+    output="$(
+        printf "%s\t" "$workflow_name"
+        "$srcdir/github_workflow_runs.sh" "$workflow_name" |
+        jq -r 'limit(1; .workflow_runs[] | .conclusion?)'
+    )"
+    echo "$output"
+    } &
+done |
+sort |
+column -t
