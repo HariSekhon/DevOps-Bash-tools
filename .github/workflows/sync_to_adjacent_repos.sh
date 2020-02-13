0e7bbf6fe0ca776667c62aaafa7122456bdccff4
Hari Sekhon <harisekhon@gmail.com>
2020-02-13 17:13:10 +0000

    added sync_to_adjacent_repos.sh

---
+++ .github/workflows/sync_to_adjacent_repos.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-02-13 15:36:35 +0000 (Thu, 13 Feb 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "$0")"
+
+cd "$srcdir"
+
+sed 's/#.*//; s/:/ /' ../../setup/repolist.txt |
+grep -v -e bash-tools -e '^[[:space:]]*$' |
+while read -r repo dir; do
+    if [ -z "$dir" ]; then
+        dir="$repo"
+    fi
+    repo="$(tr '[:upper:]' '[:lower:]' <<< "$repo")"
+    if ! [ -d "../../../$dir" ]; then
+        echo "WARNING: repo dir $dir not found, skipping..."
+        continue
+    fi
+    for filename in *.yaml; do
+        if grep -q '^[[:space:]]*container:' "$filename"; then
+            echo "syncing $filename -> ../../../$dir/.github/workflows/$filename"
+            sed "s/bash-tools/$repo/" "$filename" > "../../../$dir/.github/workflows/$filename"
+        fi
+    done
+done
