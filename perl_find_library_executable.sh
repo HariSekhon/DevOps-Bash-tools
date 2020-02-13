425a29de6083534192936561333a4ce2c19c160d
Hari Sekhon <harisekhon@gmail.com>
2020-02-13 11:24:54 +0000

    updated perl_find_library_executable.sh

--- perl_find_library_executable.sh
+++ perl_find_library_executable.sh
@@ -22,7 +22,7 @@ set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(dirname "$0")"
 
-# shellcheck disable=SC1090
+# shellcheck source=lib/utils.sh
 . "$srcdir/lib/utils.sh"
 
 perl="${PERL:-perl}"
