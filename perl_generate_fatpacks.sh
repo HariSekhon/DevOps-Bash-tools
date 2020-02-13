6a2a12e71aa78537cd09c91effe942e6c85dfdaa
Hari Sekhon <harisekhon@gmail.com>
2020-02-13 11:25:50 +0000

    updated perl_generate_fatpacks.sh

--- perl_generate_fatpacks.sh
+++ perl_generate_fatpacks.sh
@@ -21,10 +21,10 @@ set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(dirname "$0")"
 
-# shellcheck disable=SC1090
+# shellcheck source=lib/utils.sh
 . "$srcdir/lib/utils.sh"
 
-# shellcheck disable=SC1090
+# shellcheck source=lib/docker.sh
 . "$srcdir/lib/docker.sh"
 
 output_dir="fatpacks"
