8bbd270dbf8c9167612962e00fe1a6e074be9cf5
Hari Sekhon <harisekhon@gmail.com>
2020-02-13 11:44:15 +0000

    updated python_find_unused_pip_modules.sh

--- python_find_unused_pip_modules.sh
+++ python_find_unused_pip_modules.sh
@@ -19,7 +19,7 @@ set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(dirname "$0")"
 
-# shellcheck disable=SC1090
+# shellcheck source=lib/utils.sh
 . "$srcdir/lib/utils.sh"
 
 # shellcheck disable=SC2034
