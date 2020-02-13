ad1d30fccb4b3eaa68ef98538245f0581f183897
Hari Sekhon <harisekhon@gmail.com>
2020-02-13 11:24:39 +0000

    updated kafka_topics.sh

--- kafka_topics.sh
+++ kafka_topics.sh
@@ -18,7 +18,7 @@ set -u
 srcdir="$(dirname "$0")"
 
 # sources heap, kerberos, brokers, zookeepers etc
-# shellcheck disable=SC1090
+# shellcheck source=.bash.d/kafka.sh
 . "$srcdir/.bash.d/kafka.sh"
 
 # old version of kafka used --zookeeper, deprecated now
