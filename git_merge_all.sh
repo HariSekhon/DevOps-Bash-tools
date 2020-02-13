89db66c210cefae3fd8c065004f4916e73891834
Hari Sekhon <harisekhon@gmail.com>
2020-02-13 11:23:10 +0000

    updated git_merge_all.sh

--- git_merge_all.sh
+++ git_merge_all.sh
@@ -17,7 +17,7 @@ set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# shellcheck disable=SC1090
+# shellcheck source=lib/git.sh
 . "$srcdir/lib/git.sh"
 
 # For Git < 2.0 may need to set:
