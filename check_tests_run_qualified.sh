45c8816d83ce9234eb9fd02cc8f623e7bc120102
Hari Sekhon <harisekhon@gmail.com>
2020-02-13 11:20:10 +0000

    updated check_tests_run_qualified.sh

--- check_tests_run_qualified.sh
+++ check_tests_run_qualified.sh
@@ -17,7 +17,7 @@ set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# shellcheck disable=SC1090
+# shellcheck source=lib/utils.sh
 . "$srcdir/lib/utils.sh"
 
 section "Checking all test_*.sh run calls are fully qualified"
