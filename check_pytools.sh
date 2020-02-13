6c358a4fd0018cb41dceecaf1fa7a2c208a576a5
Hari Sekhon <harisekhon@gmail.com>
2020-02-13 11:19:45 +0000

    updated check_pytools.sh

--- check_pytools.sh
+++ check_pytools.sh
@@ -18,9 +18,10 @@ set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 srcdir2="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# shellcheck disable=SC1090
+# shellcheck source=lib/utils.sh
 . "$srcdir2/lib/utils.sh"
-# shellcheck disable=SC1090
+
+# shellcheck source=lib/docker.sh
 . "$srcdir2/lib/docker.sh"
 
 srcdir="$srcdir2"
