950eb8fd662097bbef169db31ac36b3cd8de836a
Hari Sekhon <harisekhon@gmail.com>
2020-02-26 10:51:08 +0000

    added generate_status_page.sh

---
+++ generate_status_page.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-02-07 15:01:31 +0000 (Fri, 07 Feb 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Script to generate STATUS.md containing all GitHub repos and DockerHub / Docker Cloud repos build statuses on a single page
+#
+# uses adjacent github_generate_status_page.sh and docker_generate_status_page.sh scripts
+#
+#   GITHUB_USER=HariSekhon DOCKER_USER=harisekhon ./generate_status_page.sh
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "$0")"
+
+trap 'echo ERROR >&2' exit
+
+cd "$srcdir"
+
+file="STATUS.md"
+
+{
+"$srcdir/github_generate_status_page.sh"
+"$srcdir/docker_generate_status_page.sh"
+} | tee "$file"
+
+trap '' exit
