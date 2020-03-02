999cc204c036168dc52ac3d5924ec9e355a69b78
Hari Sekhon <harisekhon@gmail.com>
2020-03-02 18:00:30 +0000

    added wercker_api_app.sh

---
+++ wercker_api_app.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-03-02 17:53:39 +0000 (Mon, 02 Mar 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Fetch Wercker App details - needed for getting Wercker CI build IDs (eg. for shields.io)
+
+# https://devcenter.wercker.com/development/api/endpoints/applications/
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "$0")"
+
+cd "$srcdir"
+
+usage(){
+    echo "${0##*/} <user>/<application>"
+    exit 3
+}
+
+if [ $# -ne 1 ]; then
+    usage
+fi
+
+application="${1:-}"
+
+if ! [[ "$application" =~ / ]]; then
+    application="${GITHUB_USER:-${GIT_USER:-${USER:-}}}/$application"
+fi
+
+curl -sS "https://app.wercker.com/api/v3/applications/$application" |
+jq
