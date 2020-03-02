9de03b0b552cce920308205904b6c18001e82bc2
Hari Sekhon <harisekhon@gmail.com>
2020-03-02 11:11:22 +0000

    added install_oh-my-zsh.sh

---
+++ setup/install_oh-my-zsh.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-03-01 21:01:50 +0000 (Sun, 01 Mar 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+echo "downloading Oh-my-Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
