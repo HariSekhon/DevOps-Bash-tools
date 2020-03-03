e7f509fc140ddaa4ff65ebaafb7d6f01ef7c9bd1
Hari Sekhon <harisekhon@gmail.com>
2020-03-03 10:55:44 +0000

    added wercker_api_workflows.sh

---
+++ wercker_api_workflows.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-03-02 17:53:39 +0000 (Mon, 02 Mar 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Fetch Wercker App workflows by name
+
+# https://devcenter.wercker.com/development/api/endpoints/workflows/
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "$0")"
+
+cd "$srcdir"
+
+usage(){
+    echo "${0##*/} <user>/<application_name>"
+    exit 3
+}
+
+if [ $# -ne 1 ]; then
+    usage
+fi
+
+for arg; do
+    case "$arg" in
+        -*) usage
+            ;;
+    esac
+done
+
+application="${1:-}"
+
+application_id="$("$srcdir/wercker_app_id.sh" "$application")"
+
+curl -sS "https://app.wercker.com/api/v3/workflows?applicationId=$application_id"
