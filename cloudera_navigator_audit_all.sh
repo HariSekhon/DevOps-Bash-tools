94f8e47b4e8c202192f56dc2ece2bd170a50eb48
Hari Sekhon <harisekhon@gmail.com>
2020-03-06 12:29:39 +0000

    updated cloudera_navigator_audit_all.sh

--- cloudera_navigator_audit_all.sh
+++ cloudera_navigator_audit_all.sh
@@ -25,24 +25,47 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 trap 'echo ERROR >&2' exit
 
-current_year="$(date +%Y)"
+services="
+hive
+impala
+hdfs
+hbase
+scm
+"
 
 compress_cmd="bzip2 -9 -c"
+
+current_year="$(date +%Y)"
+
+if [[ "${1:-}" =~ ^service== ]]; then
+    single_service="${1##service==}"
+    shift
+fi
+
+download_audit_logs(){
+    local year="$1"; shift
+    local service="$2"; shift
+    log="navigator_audit_${service}_${year}.csv"
+    echo "Querying Cloudera Navigator for $year logs for $service"
+    time "$srcdir/cloudera_navigator_audit.sh" "$year-01-01T00:00:00" "$((year+1))-01-01T00:00:00" "service==$service" "$@" | "$srcdir/progress_dots.sh" > "$log"
+    compressed_log="$log.bz2"
+    echo "Compressing audit log:  $log > $compressed_log"
+    # want splitting
+    # shellcheck disable=SC2086
+    $compress_cmd "$log" > "$compressed_log" &
+}
 
 # works on Mac but seq on Linux doesn't do reverse, outputs nothing
 #for year in $(seq "$current_year" 2009); do
 # On Mac tac requires gnu coreutils to be installed via Homebrew
 for year in $(seq 2009 "$current_year" | tac); do
-    for service in hive impala hdfs hbase scm; do
-        log="navigator_audit_${service}_${year}.csv"
-        echo "Querying Cloudera Navigator for $year logs for $service"
-        time "$srcdir/cloudera_navigator_audit.sh" "$year-01-01T00:00:00" "$((year+1))-01-01T00:00:00" "service==$service" "$@" | "$srcdir/progress_dots.sh" > "$log"
-        compressed_log="$log.bz2"
-        echo "Compressing audit log:  $log > $compressed_log"
-        # want splitting
-        # shellcheck disable=SC2086
-        $compress_cmd "$log" > "$compressed_log" &
-    done
+    if [ -n "${single_service:-}" ]; then
+        download_audit_logs "$year" "$single_service" "$@"
+    else
+        for service in $services; do
+            download_audit_logs "$year" "$service" "$@"
+        done
+    fi
 done
 echo "Finished querying Cloudera Navigator API"
 echo "Waiting for log compression to finish"
