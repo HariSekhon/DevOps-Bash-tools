936d2fb5b33aa20132414d3586c2a134b9bd0072
Hari Sekhon <harisekhon@gmail.com>
2020-03-09 23:16:16 +0000

    added install_appveyor_byoc.sh

---
+++ setup/install_appveyor_byoc.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-03-09 22:38:36 +0000 (Mon, 09 Mar 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Installs PowerShell on Mac and various Linux distros
+#
+# https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+"$srcdir/install_powershell.sh"
+
+pwsh <<EOF
+    Install-Module AppVeyorBYOC -Scope CurrentUser -Force; Import-Module AppVeyorBYOC
+EOF
