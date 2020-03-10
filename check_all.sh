5b6ed16d8a15e46a5b7def0f24d68bbb5a3ed4e7
Hari Sekhon <harisekhon@gmail.com>
2020-03-10 14:33:08 +0000

    updated check_all.sh

--- check_all.sh
+++ check_all.sh
@@ -99,6 +99,7 @@ WARN_ONLY=1 . "$srcdir/check_python_asserts.sh"
 . "$srcdir/check_bash_arrays.sh"
 
 . "$srcdir/check_travis_yml.sh"
+. "$srcdir/check_circle_ci_yml.sh"
 
 . "$srcdir/check_tld_chars.sh"
 
