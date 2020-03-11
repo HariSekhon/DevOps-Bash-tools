2822d04b93088d86c0e95b02d9c55a71630e8a10
Hari Sekhon <harisekhon@gmail.com>
2020-03-11 18:05:55 +0000

    added buildkite_agent.sh

---
+++ buildkite_agent.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-03-11 18:02:32 +0000 (Wed, 11 Mar 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Runs BuildKite Agent
+#
+# see /usr/local/etc/buildkite-agent/buildkite-agent.cfg for config on Mac
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+if [ -z "${BUILDKITE_TOKEN:-}" ]; then
+    echo "BUILDKITE_TOKEN environment variable not defined"
+fi
+
+# Mac / Linux
+#buildkite-agent start
+
+# Docker
+tag="latest"
+if [ -n "${BIG:-}" ]; then
+    tag="ubuntu"
+fi
+
+docker run -e BUILDKITE_AGENT_TOKEN="$BUILDKITE_TOKEN" buildkite/agent:"$tag"
