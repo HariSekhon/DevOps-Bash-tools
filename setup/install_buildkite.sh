fd142aae978bf5c801b716cc12bc3bd1293620cc
Hari Sekhon <harisekhon@gmail.com>
2020-03-12 15:59:57 +0000

    updated install_buildkite.sh

--- setup/install_buildkite.sh
+++ setup/install_buildkite.sh
@@ -28,8 +28,8 @@ if is_linux; then
     export PATH="$PATH:$HOME/.buildkite-agent/bin"
 fi
 
-if [ -z "${BUILDKITE_TOKEN:-}" ]; then
-    echo "BUILDKITE_TOKEN environment variable not defined"
+if [ -z "${BUILDKITE_AGENT_TOKEN:-}" ]; then
+    echo "BUILDKITE_AGENT_TOKEN environment variable not defined"
     exit
 fi
 
@@ -37,9 +37,9 @@ if type -P buildkite-agent &>/dev/null; then
     if is_mac; then
         config=/usr/local/etc/buildkite-agent/buildkite-agent.cfg
         if [ -f "$config" ]; then
-            if ! grep "$BUILDKITE_TOKEN" "$config"; then
+            if ! grep "$BUILDKITE_AGENT_TOKEN" "$config"; then
                 echo "injecting buildkite token into $config"
-                sed -i.bak "s/^token=.*/token=$BUILDKITE_TOKEN/" "$config"
+                sed -i.bak "s/^token=.*/token=$BUILDKITE_AGENT_TOKEN/" "$config"
             fi
         fi
     fi
@@ -50,12 +50,12 @@ fi
 if is_mac; then
     brew tap buildkite/buildkite
     # inserts token in to /usr/local/etc/buildkite-agent/buildkite-agent.cfg
-    brew install --token="$BUILDKITE_TOKEN" buildkite-agent
+    brew install --token="$BUILDKITE_AGENT_TOKEN" buildkite-agent
     # run as a background agent
     # brew services start buildkite/buildkite/buildkite-agent
     # run as foreground agent
     buildkite-agent start
 else
-    TOKEN="$BUILDKITE_TOKEN" bash -c "$(curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh)"
+    TOKEN="$BUILDKITE_AGENT_TOKEN" bash -c "$(curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh)"
     ~/.buildkite-agent/bin/buildkite-agent start
 fi
