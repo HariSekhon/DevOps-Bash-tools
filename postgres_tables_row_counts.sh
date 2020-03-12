e67fd00c0f11f63b8af3241f79e643a6aa5ab6b8
Hari Sekhon <harisekhon@gmail.com>
2020-03-12 17:27:17 +0000

    added postgres_tables_row_counts.sh

---
+++ postgres_tables_row_counts.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-12-10 11:33:52 +0000 (Tue, 10 Dec 2019)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Counts rows for all PostgreSQL tables in all databases using adjacent psql.sh script
+#
+# TSV Output format:
+#
+# <database>    <schema>    <table>     <row_count>
+#
+# FILTER environment variable will restrict to matching fully qualified tables (<db>.<schema>.<table>)
+#
+# Tested on AWS RDS PostgreSQL 9.5.15
+
+set -eu  # -o pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "$0")"
+
+"$srcdir/postgres_foreach_table.sh" "SELECT COUNT(*) FROM {db}.{schema}.{table}" "$@" |
+sed '/^[[:space:]]*$/d'
