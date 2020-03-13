896ad66359cd450f53ebafbb877b710127fb01a0
Hari Sekhon <harisekhon@gmail.com>
2020-03-13 17:20:04 +0000

    updated buildkite_agent.sh

--- buildkite_agent.sh
+++ buildkite_agent.sh
@@ -20,15 +20,14 @@
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 
-if [ -z "${BUILDKITE_AGENT_TOKEN:-}" ]; then
-    echo "BUILDKITE_AGENT_TOKEN environment variable not defined"
-fi
+# ============================================================================ #
+#                        M a c  /  L i n u x   A g e n t
+# ============================================================================ #
 
 buildkite_tags="os=linux"
 
 export PATH="$PATH:$HOME/.buildkite-agent/bin"
 
-# Mac / Linux
 if type -P buildkite-agent &>/dev/null; then
     if [ -z "${BUILDKITE_DOCKER:-}" ]; then
         uname_s="$(uname -s)"
@@ -43,7 +42,18 @@ if type -P buildkite-agent &>/dev/null; then
     fi
 fi
 
-# Docker
+# falls through to Docker agent
+
+# ============================================================================ #
+#                                  D o c k e r
+# ============================================================================ #
+
+# only necessary for Docker, installed agent will have config file containing this token from setup/install_buildkite.sh
+if [ -z "${BUILDKITE_AGENT_TOKEN:-}" ]; then
+    echo "BUILDKITE_AGENT_TOKEN environment variable not defined"
+    exit 1
+fi
+
 docker_tag="latest"
 #docker_tag="alpine"
 #docker_tag="centos"
