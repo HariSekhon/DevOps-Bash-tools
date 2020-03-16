75b8ada074848acc61f86fdbb3a9cbb9868eb8a8
Hari Sekhon <harisekhon@gmail.com>
2020-03-16 13:26:43 +0000

    added postgres.sh

---
+++ .bash.d/postgres.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-03-16 13:20:17 +0000 (Mon, 16 Mar 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# ============================================================================ #
+#                              P o s t g r e S Q L
+# ============================================================================ #
+
+bash_tools="${bash_tools:-$(dirname "${BASH_SOURCE[0]}")/..}"
+
+# shellcheck disable=SC1090
+. "$bash_tools/.bash.d/os_detection.sh"
+
+# shellcheck disable=SC1090
+. "$bash_tools/.bash.d/functions.sh"
+
+# highest priority env var
+alias pgpass='pass PGPASSWORD'
+# mac
+alias postgresd='postgres -D /usr/local/var/postgres'
