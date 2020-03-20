55cbfc47b049824267f1ee95d690a2a3c686138c
Hari Sekhon <harisekhon@gmail.com>
2020-03-20 18:10:54 +0000

    added .bash.d/bash_it.sh

---
+++ .bash.d/bash_it.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-02-26 15:21:46 +0000 (Wed, 26 Feb 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# ============================================================================ #
+#                                 B a s h   I T
+# ============================================================================ #
+
+return
+
+if ! [ -d ~/.bash_it ]; then
+    git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+fi
+
+export BASH_IT=~/.bash_it
+
+export BASH_IT_THEME='bobby'
