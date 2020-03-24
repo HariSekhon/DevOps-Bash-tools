a3f382326193de717b0742da10d3a8c8fd736a1f
Hari Sekhon <harisekhon@gmail.com>
2020-03-24 16:33:24 +0000

    updated shippable_projects.sh

--- shippable_projects.sh
+++ shippable_projects.sh
@@ -22,7 +22,9 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_description="Returns a list of Shippable projects ids and names"
+usage_description="Returns a list of Shippable projects ids and names, or a single project name if the project id is given as an arg"
+# shellcheck disable=SC2034
+usage_args="[<project_id>]"
 
 if [ -z "$SHIPPABLE_ACCOUNT_ID" ]; then
     usage "SHIPPABLE_ACCOUNT_ID environment variable is not set (get this value from your Web UI Dashboard)"
@@ -32,8 +34,25 @@ for arg; do
     case "$arg" in
         -h|--help) usage
                    ;;
-   esac
+    esac
 done
 
-"$srcdir/shippable_api.sh" "/projects?sortBy=createdAt&sortOrder=-1&ownerAccountIds=$SHIPPABLE_ACCOUNT_ID" "$@" |
-jq -r '.[] | [.id, .name] | @tsv'
+project_id=""
+if [ $# -gt 0 ]; then
+    project_id="$1"
+    shift || :
+fi
+
+if [ -n "$project_id" ] &&
+   ! [[ "$project_id" =~ ^[[:alnum:]]+$ ]]; then
+    usage "invalid project id '$project_id' argument specified, must be alphanumeric"
+fi
+
+# API returns list without project id or hashmap with project id
+jq_query='.[] | [.id, .name] | @tsv'
+if [ -n "$project_id" ]; then
+    jq_query='[.id, .name] | @tsv'
+fi
+
+"$srcdir/shippable_api.sh" "/projects/$project_id?sortBy=createdAt&sortOrder=-1&ownerAccountIds=$SHIPPABLE_ACCOUNT_ID" "$@" |
+jq -r "$jq_query"
