3c823839b6b414c7746b83810ebc95e409808b45
Hari Sekhon <harisekhon@gmail.com>
2020-03-26 02:35:51 +0000

    updated check_all.sh

--- check_all.sh
+++ check_all.sh
@@ -98,6 +98,8 @@ WARN_ONLY=1 . "$srcdir/check_python_asserts.sh"
 
 . "$srcdir/check_bash_arrays.sh"
 
+"$srcdir/check_readme_badges.sh"
+
 . "$srcdir/check_travis_yml.sh"
 . "$srcdir/check_circle_ci_yml.sh"
 if ! is_CI &&
