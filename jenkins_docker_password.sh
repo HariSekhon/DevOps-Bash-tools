d2f4a9a9f272e072896b291c299a030c3ef3c04b
Hari Sekhon <harisekhon@gmail.com>
2020-03-28 15:17:14 +0000

    added jenkins_docker_password.sh

---
+++ jenkins_docker_password.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-03-28 15:16:18 +0000 (Sat, 28 Mar 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+config="$srcdir/setup/jenkins-docker-compose.yml"
+
+docker-compose -f "$config" exec jenkins-server cat /var/jenkins_home/secrets/initialAdminPassword
