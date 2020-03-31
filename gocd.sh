991e38369307c19a630513851f9e60c40e8cf42e
Hari Sekhon <harisekhon@gmail.com>
2020-03-31 15:23:20 +0100

    updated gocd.sh

--- gocd.sh
+++ gocd.sh
@@ -72,7 +72,7 @@ when_url_content "$server/go/pipelines#!/" '(?i:gocd)'
 echo
 
 while curl -sS "$server" | grep -q 'GoCD server is starting'; do
-    echo 'waiting for server to finish starting up'
+    tstamp 'waiting for server to finish starting up (remove message "GoCD server is starting)'
     sleep 3
 done
 echo
