3b044f02840047b21931ecb54ab50e72fcf51efd
Hari Sekhon <harisekhon@gmail.com>
2020-03-31 11:17:19 +0100

    updated git_foreach_repo.sh

--- git_foreach_repo.sh
+++ git_foreach_repo.sh
@@ -20,8 +20,8 @@ srcdir="$(cd "$(dirname "$0")" && pwd)"
 
 # access to useful functions and aliases
 # shellcheck disable=SC1090
-#. "$srcdir/.bash.d/aliases.sh"
-#. "$srcdir/.bash.d/functions.sh"
+. "$srcdir/.bash.d/aliases.sh"
+. "$srcdir/.bash.d/functions.sh"
 . "$srcdir/.bash.d/git.sh"
 
 #git_url="${GIT_URL:-https://github.com}"
