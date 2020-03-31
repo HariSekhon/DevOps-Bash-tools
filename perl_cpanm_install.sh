d8b136a2c857113595758ad9241d5cf2fdbff8c1
Hari Sekhon <harisekhon@gmail.com>
2020-03-31 12:50:24 +0100

    updated perl_cpanm_install.sh

--- perl_cpanm_install.sh
+++ perl_cpanm_install.sh
@@ -26,6 +26,11 @@ srcdir="$(dirname "$0")"
 CPANM="${CPANM:-cpanm}"
 
 # to avoid perl: warning: Falling back to the standard locale ("C")
+#
+# might have to run this on Debian/Ubuntu:
+#
+# sudo locale-gen en_US.UTF-8
+#
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
