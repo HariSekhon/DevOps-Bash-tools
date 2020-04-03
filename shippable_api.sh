388877bb75ae275add0e2cdc058d8cb42ba593d1
Hari Sekhon <harisekhon@gmail.com>
2020-04-03 14:01:08 +0100

    updated shippable_api.sh

--- shippable_api.sh
+++ shippable_api.sh
@@ -44,4 +44,9 @@ done
 url_path="${1##/}"
 shift || :
 
-curl -sSH 'Accept: application/json' -H "Authorization: apiToken $SHIPPABLE_TOKEN" "https://api.shippable.com/$url_path" "$@"
+if is_curl_min_version 7.55; then
+    # hide token from process list if curl is new enough to support this trick
+    curl -sSH 'Accept: application/json' -H @<(cat <<< "Authorization: apiToken $SHIPPABLE_TOKEN") "https://api.shippable.com/$url_path" "$@"
+else
+    curl -sSH 'Accept: application/json' -H "Authorization: apiToken $SHIPPABLE_TOKEN" "https://api.shippable.com/$url_path" "$@"
+fi
