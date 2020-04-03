e25529cc71d75e096c8909090d78178f12860b44
Hari Sekhon <harisekhon@gmail.com>
2020-04-03 14:03:58 +0100

    added shippable_account_id.sh

---
+++ shippable_account_id.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-03-24 15:27:07 +0000 (Tue, 24 Mar 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# pending support ticket around permissions issue
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+#curl -sSH 'Accept: application/json' 'https://api.shippable.com/projects?sortBy=createdAt&sortOrder=-1&ownerAccountIds=harisekhon'
+"$srcdir/shippable_api.sh" '/accounts'
