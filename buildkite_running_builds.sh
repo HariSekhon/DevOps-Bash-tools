cb0e8d1ea29687a7d490b7ca6e32deebd41cfcf7
Hari Sekhon <harisekhon@gmail.com>
2020-04-04 14:27:59 +0100

    updated buildkite_running_builds.sh

--- buildkite_running_builds.sh
+++ buildkite_running_builds.sh
@@ -13,21 +13,25 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Lists BuildKite running builds via its API
-#
-# https://buildkite.com/docs/apis/rest-api/builds
-
 set -euo pipefail
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_description="
+Lists BuildKite running builds via its API
+
+https://buildkite.com/docs/apis/rest-api/builds
+"
+
+# shellcheck disable=SC2034
+usage_args="[<curl_options>]"
+
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
 
-# used by usage() in lib/utils.sh
-# shellcheck disable=SC2034
-usage_description="Lists BuildKite running builds via its API"
-# shellcheck disable=SC2034
-usage_args="[<curl_options>]"
+help_usage "$@"
 
 "$srcdir/buildkite_api.sh" 'builds?state=running' "$@"
