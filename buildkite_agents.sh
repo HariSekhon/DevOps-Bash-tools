4ab2a14d4479796b53e69f390308fc3ddcd358b2
Hari Sekhon <harisekhon@gmail.com>
2020-04-04 14:24:26 +0100

    updated buildkite_agents.sh

--- buildkite_agents.sh
+++ buildkite_agents.sh
@@ -13,28 +13,30 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
+set -euo pipefail
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_description="
 # Lists BuildKite Agents via BuildKite API
 #
 # https://buildkite.com/docs/apis/rest-api/agents
+"
 
-set -euo pipefail
+# shellcheck disable=SC2034
+usage_args="[<curl_options>]"
+
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
 
-# used by usage() in lib/utils.sh
-# shellcheck disable=SC2034
-usage_description="Lists BuildKite Agents via BuildKite API"
-# shellcheck disable=SC2034
-usage_args="[<curl_options>]"
-
 BUILDKITE_ORGANIZATION="${BUILDKITE_ORGANIZATION:-${BUILDKITE_USER:-}}"
 
-if [ -z "${BUILDKITE_ORGANIZATION:-}" ]; then
-    usage "\$BUILDKITE_ORGANIZATION not defined"
-fi
+check_env_defined BUILDKITE_ORGANIZATION
+
+help_usage "$@"
 
 "$srcdir/buildkite_api.sh" "organizations/$BUILDKITE_ORGANIZATION/agents" "$@" |
 jq -r '.[] | [.hostname, .ip_address, .created_at, .user_agent] | @tsv'
