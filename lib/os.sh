370f471263ba619c93a1cc1684bbdb05daaadae4
Hari Sekhon <harisekhon@gmail.com>
2020-04-05 13:40:55 +0100

    forked os.sh from utils.sh

---
+++ lib/os.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#  shellcheck disable=SC2128,SC2230,SC1090
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-05-25 01:38:24 +0100 (Mon, 25 May 2015)
+#
+#  https://github.com/harisekhon/devops-bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -eu
+[ -n "${DEBUG:-}" ] && set -x
+
+is_linux(){
+    if [ "$(uname -s)" = "Linux" ]; then
+        return 0
+    fi
+    return 1
+}
+
+is_mac(){
+    if [ "$(uname -s)" = "Darwin" ]; then
+        return 0
+    fi
+    return 1
+}
