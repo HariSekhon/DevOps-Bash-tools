4883abc860d5f6e0c8b476ed47d09f8805a70196
Hari Sekhon <harisekhon@gmail.com>
2020-04-10 22:47:47 +0100

    updated paths.sh

--- .bash.d/paths.sh
+++ .bash.d/paths.sh
@@ -127,43 +127,6 @@ fi
 
 
 # ============================================================================ #
-#                              R u b y   /   G e m
-# ============================================================================ #
-
-# gems will be installed to ~/.gem/ruby/x.y.z/bin
-
-# RVM stuff
-# usually /usr/local/rvm/bin
-if [ -n "${rvm_bin_path:-}" ];then
-    add_PATH "$rvm_bin_path"
-fi
-if [ -d /usr/local/rvm/bin ]; then
-    add_PATH /usr/local/rvm/bin
-fi
-
-# add newest ruby to path first
-ruby_bins="$(find ~/.gem/ruby -maxdepth 2 -name bin -type d 2>/dev/null)"
-if isMac; then
-    ruby_bins_newest="$(tail -r <<< "$ruby_bins")"
-else
-    ruby_bins_newest="$(tac <<< "$ruby_bins")"
-fi
-for ruby_bin in $ruby_bins_newest; do
-    add_PATH "$ruby_bin"
-done
-unset ruby_bins
-unset ruby_bins_newest
-
-alias lsrubybin='ls -d ~/.gem/ruby/*/bin/* 2>/dev/null'
-alias llrubybin='ls -ld ~/.gem/ruby/*/bin/* 2>/dev/null'
-
-# HomeBrew install on Linux (for AWS SAM CLI)
-if [ -d ~/.linuxbrew/Homebrew/Library/Homebrew/vendor/portable-ruby/current/bin ]; then
-    add_PATH ~/.linuxbrew/Homebrew/Library/Homebrew/vendor/portable-ruby/current/bin
-fi
-
-
-# ============================================================================ #
 #                                  G o l a n g
 # ============================================================================ #
 # Golang
