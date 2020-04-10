81b28fbb4d023baefd59c853b6f4fc8da86e75f0
Hari Sekhon <harisekhon@gmail.com>
2020-04-10 22:49:40 +0100

    updated paths.sh

--- .bash.d/paths.sh
+++ .bash.d/paths.sh
@@ -127,47 +127,6 @@ fi
 
 
 # ============================================================================ #
-#                                  G o l a n g
-# ============================================================================ #
-# Golang
-
-export GOPATH="$github/go-tools"
-alias gopath='cd "$GOPATH"'
-alias gogo='gopath'
-alias cdgo='gopath'
-alias gosrc='cd "$GOPATH/src"'
-
-if [ -d ~/go/bin ]; then
-    add_PATH ~/go/bin
-fi
-
-# manual installation of 1.5 mismatches with HomeBrew 1.6 installed to $PATH and
-#export GOROOT="/usr/local/go"
-# causes:
-# imports runtime/internal/sys: cannot find package "runtime/internal/sys" in any of:
-# /usr/local/go/src/runtime/internal/sys (from $GOROOT)
-# /Users/hari/github/go-tools/src/runtime/internal/sys (from $GOPATH)
-if type -P go &>/dev/null; then
-    if isMac; then
-        GOROOT="$(dirname "$(dirname "$(greadlink -f "$(type -P go)")")")"
-    else
-        GOROOT="$(dirname "$(dirname "$(readlink -f "$(type -P go)")")")"
-    fi
-    export GOROOT
-    add_PATH "$GOROOT/bin"
-    add_PATH "$GOROOT/libexec/bin"
-    add_PATH "$GOPATH/bin"
-fi
-
-if type -P colorgo &>/dev/null; then
-    alias go=colorgo
-fi
-
-alias lsgobin='ls -d ~/go/bin/* "$GOROOT"/{bin,libexec/bin}/* "$GOPATH/bin/"* 2>/dev/null'
-alias llgobin='ls -ld ~/go/bin/* "$GOROOT"/{bin,libexec/bin}/* "$GOPATH/bin/"* 2>/dev/null'
-
-
-# ============================================================================ #
 #                         M y   G i t H u b   r e p o s
 # ============================================================================ #
 
