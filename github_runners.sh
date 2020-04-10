5558a35163e0b6c839f06789075f49720ba34055
Hari Sekhon <harisekhon@gmail.com>
2020-04-10 17:29:05 +0100

    added github_runners.sh

---
+++ github_runners.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-02-12 16:21:52 +0000 (Wed, 12 Feb 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "$0")"
+
+# shellcheck source=.bash.d/git.sh
+. "$srcdir/lib/utils.sh"
+
+# shellcheck source=.bash.d/git.sh
+. "$srcdir/.bash.d/git.sh"
+
+usage(){
+    cat <<EOF
+
+Script to get GitHub Actions Runners for the local git repo via the GitHub API
+
+If no repo arg is given and is inside a git repo then takes determines the repo from the first git remote listed
+
+\$REPO environment variable may be used with first args taking precedence
+
+usage: ${0##*/} <repo>
+
+EOF
+    exit 3
+}
+
+repo="${1:-${REPO:-}}"
+
+if [ -z "$repo" ]; then
+    repo="$(git_repo)"
+fi
+
+if [ -z "$repo" ]; then
+    usage "repo not specified and couldn't determine from git remote command"
+fi
+
+any_opt_usage "$@"
+
+USER="${GITHUB_USER:-${USERNAME:-${USER}}}"
+PASSWORD="${GITHUB_PASSWORD:-${GITHUB_TOKEN:-${PASSWORD:-}}}"
+
+if ! [[ $repo =~ / ]]; then
+    repo="$USER/$repo"
+fi
+
+"$srcdir/github_api.sh" "/repos/$repo/actions/runners" |
+jq -r '.runners[] | [.id, .status, .os, .name] | @tsv' |
+column -t
