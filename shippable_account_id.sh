14e4b519c3dd57691f3d68685365a402f714a402
Hari Sekhon <harisekhon@gmail.com>
2020-04-15 17:44:16 +0100

    updated shippable_account_id.sh

--- shippable_account_id.sh
+++ shippable_account_id.sh
@@ -19,5 +19,19 @@ set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_description="Returns Shippable accounts for the given \$SHIPPABLE_TOKEN
+
+Caveat: this API endpoint only works for paid accounts :-(
+
+https://github.com/Shippable/support/issues/5068
+"
+
+help_usage "$@"
+
 #curl -sSH 'Accept: application/json' 'https://api.shippable.com/projects?sortBy=createdAt&sortOrder=-1&ownerAccountIds=harisekhon'
 "$srcdir/shippable_api.sh" '/accounts'
