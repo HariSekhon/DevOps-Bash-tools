4a716f61c7ccd0eec4a9208d64e1e1618d9e6548
Hari Sekhon <harisekhon@gmail.com>
2020-06-04 00:16:57 +0100

    updated colors.sh

--- .bash.d/colors.sh
+++ .bash.d/colors.sh
@@ -24,7 +24,7 @@
 colors(){
     local text="hari"
 
-    echo -e "\n                  40m      41m      42m      43m      44m      45m      46m      47m";
+    echo -e '\n                  40m      41m      42m      43m      44m      45m      46m      47m';
 
     for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
                '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
