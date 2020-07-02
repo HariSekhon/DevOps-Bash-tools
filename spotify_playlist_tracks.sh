f97fdac7379779938f4c2bc03c0c8cba572416e6
Hari Sekhon <harisekhon@gmail.com>
2020-07-03 00:37:14 +0100

    updated spotify_playlist_tracks.sh

--- spotify_playlist_tracks.sh
+++ spotify_playlist_tracks.sh
@@ -46,14 +46,7 @@ if [ -z "$playlist_id" ]; then
     usage "playlist id not defined"
 fi
 
-# if it's not a playlist id, scan all playlists and take the ID of the first matching playlist name
-if ! [[ "$playlist_id" =~ ^[[:alnum:]]{22}$ ]]; then
-    playlist_id="$("$srcdir/spotify_playlists.sh" | awk "/$playlist_id/{print \$1; exit}" || :)"
-    if [ -z "$playlist_id" ]; then
-        echo "failed to find playlist matching name regex given" >&2
-        exit 1
-    fi
-fi
+playlist_id="$("$srcdir/spotify_playlist_name_to_id.sh" "$playlist_id" "$@")"
 
 offset="${OFFSET:-0}"
 
