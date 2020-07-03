6f3340eaf305c5fa2bc1b18eb318f7564e13ddd8
Hari Sekhon <harisekhon@gmail.com>
2020-07-03 17:13:38 +0100

    split single vs bulk api track lookups to only happen when no local tracks in order to preserve correct playlist ordering

--- spotify_track_uri_to_name.sh
+++ spotify_track_uri_to_name.sh
@@ -43,8 +43,15 @@ or if \$SPOTIFY_CSV environment variable is set then:
 
 Useful for saving Spotify playlists in a format that is easier to understand, revision control changes or export to other music systems
 
+Uses the Spotify bulk track API if there are no local track references in a playlist, otherwise falls back to individual track lookups
+in order to preserve the correct playlist ordering between Spotify and local tracks
+
 Requires \$SPOTIFY_CLIENT_ID and \$SPOTIFY_CLIENT_SECRET to be defined in the environment
 "
+
+# https://developer.spotify.com/documentation/web-api/reference/tracks/get-track/
+
+# https://developer.spotify.com/documentation/web-api/reference/tracks/get-several-tracks/
 
 # shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
@@ -53,23 +60,13 @@ help_usage "$@"
 
 url_base="/v1/tracks"
 
-output(){
-    if [ -n "${SPOTIFY_CSV:-}" ]; then
-        jq -r '.tracks[] | [([.artists[].name] | join(",")), .name] | @csv'
-    else
-        jq -r '.tracks[] | [([.artists[].name] | join(",")), "-", .name] | @tsv'
-    fi <<< "$output" |
-    tr '\t' ' ' |
-    sed 's/^[[:space:]]*//; s/[[:space:]]*$//'
-}
-
 if [ -z "${SPOTIFY_ACCESS_TOKEN:-}" ]; then
-    export SPOTIFY_ACCESS_TOKEN="$("$srcdir/spotify_api_token.sh")"
+    SPOTIFY_ACCESS_TOKEN="$("$srcdir/spotify_api_token.sh")"
+    export SPOTIFY_ACCESS_TOKEN
 fi
 
-while true; do
-    ids=""
-    for ((i=0; i<50; i++)); do
+track_by_track(){
+    while true; do
         read -r -s track_uri || break
         if [[ "$track_uri" =~ ^spotify:local: ]]; then
             track_uri="${track_uri#spotify:local:}"
@@ -78,27 +75,96 @@ while true; do
             track_uri="${track_uri%:*}"
             track_uri="${track_uri//+/ }"
             "$srcdir/urldecode.sh" <<< "$track_uri"
-            ((i-=1))
             continue
         fi
         if ! [[ "$track_uri" =~ ^(spotify:track:|http://open.spotify.com/track/)?[[:alnum:]]+$ ]]; then
             echo "Invalid track URI provided: $track_uri" >&2
             exit 1
         fi
-        track_uri="${track_uri##*[:/]}"
-        ids+=",$track_uri"
+        track_id="${track_uri##*[:/]}"
+        url_path="$url_base/$track_id"
+        output="$("$srcdir/spotify_api.sh" "$url_path" "$@")"
+        # shellcheck disable=SC2181
+        if [ $? != 0 ] || [ "$(jq -r '.error' <<< "$output")" != null ]; then
+            echo "$output" >&2
+            exit 1
+        fi
+        output
+        sleep 0.1
     done
-    if [ -z "$ids" ]; then
-        break
-    fi
-    ids="${ids#,}"
-    url_path="$url_base?ids=$ids"
-    output="$("$srcdir/spotify_api.sh" "$url_path" "$@")"
-    # shellcheck disable=SC2181
-    if [ $? != 0 ] || [ "$(jq -r '.error' <<< "$output")" != null ]; then
-        echo "$output" >&2
-        exit 1
-    fi
-    output
-    sleep 0.1
-done
+}
+
+track_by_bulk(){
+    while true; do
+        ids=""
+        for ((i=0; i<50; i++)); do
+            read -r -s track_uri || break
+            if [[ "$track_uri" =~ ^spotify:local: ]]; then
+                track_uri="${track_uri#spotify:local:}"
+                track_uri="${track_uri#:}"
+                track_uri="${track_uri#:}"
+                track_uri="${track_uri%:*}"
+                track_uri="${track_uri//+/ }"
+                "$srcdir/urldecode.sh" <<< "$track_uri"
+                ((i-=1))
+                continue
+            fi
+            if ! [[ "$track_uri" =~ ^(spotify:track:|http://open.spotify.com/track/)?[[:alnum:]]+$ ]]; then
+                echo "Invalid track URI provided: $track_uri" >&2
+                exit 1
+            fi
+            track_uri="${track_uri##*[:/]}"
+            ids+=",$track_uri"
+        done
+        if [ -z "$ids" ]; then
+            break
+        fi
+        ids="${ids#,}"
+        url_path="$url_base?ids=$ids"
+        output="$("$srcdir/spotify_api.sh" "$url_path" "$@")"
+        # shellcheck disable=SC2181
+        if [ $? != 0 ] || [ "$(jq -r '.error' <<< "$output")" != null ]; then
+            echo "$output" >&2
+            exit 1
+        fi
+        output_bulk
+        sleep 0.1
+    done
+}
+
+output(){
+    if [ -n "${SPOTIFY_CSV:-}" ]; then
+        jq -r '[([.artists[].name] | join(",")), .name] | @csv'
+    else
+        jq -r '[([.artists[].name] | join(",")), "-", .name] | @tsv'
+    fi <<< "$output" |
+    clean_output
+}
+
+output_bulk(){
+    if [ -n "${SPOTIFY_CSV:-}" ]; then
+        jq -r '.tracks[] | [([.artists[].name] | join(",")), .name] | @csv'
+    else
+        jq -r '.tracks[] | [([.artists[].name] | join(",")), "-", .name] | @tsv'
+    fi <<< "$output" |
+    clean_output
+}
+
+clean_output(){
+    tr '\t' ' ' |
+    sed '
+        s/^[[:space:]]*-//;
+        s/^[[:space:]]*//;
+        s/[[:space:]]*$//
+    '
+}
+
+# must slurp in to memory and check all track URIs for local references before knowing if it's safe to use bulk track API
+track_uris="$(cat)"
+
+if grep -q 'spotify:local:' <<< "$track_uris"; then
+    # in order to preserve the correct playlist ordering
+    track_by_track "$@" <<< "$track_uris"
+else
+    track_by_bulk "$@" <<< "$track_uris"
+fi
