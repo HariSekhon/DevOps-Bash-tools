951c8609603dddc5ceddd23744c6e056113e9c75
Hari Sekhon <harisekhon@gmail.com>
2020-07-05 02:33:23 +0100

    added spotify_rename_playlist.sh

---
+++ spotify_rename_playlist.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-07-04 13:14:15 +0100 (Sat, 04 Jul 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/.bash.d/git.sh"
+
+rename(){
+    gitrename "$1" "$2"
+
+    gitrename "spotify/$1" "spotify/$2"
+}
+
+rename "$1" "$2"
