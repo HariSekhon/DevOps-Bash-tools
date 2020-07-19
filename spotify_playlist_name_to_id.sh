d3c7ff0d02f1f08ff831e109bc06188a1580ed98
Hari Sekhon <harisekhon@gmail.com>
2020-07-19 18:24:50 +0100

    updated spotify_playlist_name_to_id.sh

--- spotify_playlist_name_to_id.sh
+++ spotify_playlist_name_to_id.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #  vim:ts=4:sts=4:sw=4:et
-#  args: "My Shazam Tracks" | tee /dev/stderr | xargs spotify_playlist_id_to_name.sh
+#  args: "My Shazam Tracks" | tee /dev/stderr | spotify_playlist_id_to_name.sh
 #
 #  Author: Hari Sekhon
 #  Date: 2020-07-03 00:25:24 +0100 (Fri, 03 Jul 2020)
@@ -40,32 +40,41 @@ usage_args="<playlist> [<curl_options>]"
 
 help_usage "$@"
 
-min_args 1 "$@"
-
-playlist_name="$1"
-shift || :
-
 #if [ "$(uname -s)" = Darwin ]; then
 #    awk(){
 #        command gawk "$@"
 #    }
 #fi
 
-# if it's not a playlist id, scan all playlists and take the ID of the first matching playlist name
-if [[ "$playlist_name" =~ ^[[:alnum:]]{22}$ ]]; then
-    echo "$playlist_name"
-else
-    # works but could get needlessly complicated to escape all possible regex special chars, switching to partial string match instead
-    #playlist_regex="${playlist_id//\//\\/}"
-    #playlist_regex="${playlist_regex//\(/\\(}"
-    #playlist_regex="${playlist_regex//\)/\\)}"
-                   #awk "BEGIN{IGNORECASE=1} /${playlist_regex//\//\\/}/ {print \$1; exit}" || :)"
-    playlist_id="$(SPOTIFY_PLAYLISTS_ALL=1 "$srcdir/spotify_playlists.sh" "$@" |
-                   grep -Fi -m1 "$playlist_name" |
-                   awk '{print $1}' || :)"
-    if [ -z "$playlist_id" ]; then
-        echo "Error: failed to find playlist ID matching given playlist name '$playlist_name'" >&2
-        exit 1
+playlist_name_to_id(){
+    local playlist_name="$1"
+    shift || :
+    # if it's not a playlist id, scan all playlists and take the ID of the first matching playlist name
+    if [[ "$playlist_name" =~ ^[[:alnum:]]{22}$ ]]; then
+        echo "$playlist_name"
+    else
+        # works but could get needlessly complicated to escape all possible regex special chars, switching to partial string match instead
+        #playlist_regex="${playlist_id//\//\\/}"
+        #playlist_regex="${playlist_regex//\(/\\(}"
+        #playlist_regex="${playlist_regex//\)/\\)}"
+                       #awk "BEGIN{IGNORECASE=1} /${playlist_regex//\//\\/}/ {print \$1; exit}" || :)"
+        playlist_id="$(SPOTIFY_PLAYLISTS_ALL=1 "$srcdir/spotify_playlists.sh" "$@" |
+                       grep -Fi -m1 "$playlist_name" |
+                       awk '{print $1}' || :)"
+        if [ -z "$playlist_id" ]; then
+            echo "Error: failed to find playlist ID matching given playlist name '$playlist_name'" >&2
+            exit 1
+        fi
+        echo "$playlist_id"
     fi
-    echo "$playlist_id"
+}
+
+if [ $# -gt 0 ]; then
+    playlist_name="$1"
+    shift || :
+    playlist_name_to_id "$playlist_name" "$@"
+else
+    while read -r playlist_name; do
+        playlist_name_to_id "$playlist_name" "$@"
+    done
 fi
