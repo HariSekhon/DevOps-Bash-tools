cff4d9e0ea5fd7f077dc9efc413ddfb9ed7e1a1d
Hari Sekhon <harisekhon@gmail.com>
2020-07-19 17:52:41 +0100

    updated spotify_backup_playlist.sh

--- spotify_backup_playlist.sh
+++ spotify_backup_playlist.sh
@@ -29,14 +29,14 @@ Backs up a given public Spotify playlists for a given user to text files in both
 Spotify track URI format can be copied and pasted back in to Spotify to restore a playlist to a previous state
 (for example if you accidentally deleted a track and didn't do an immediate Ctrl-Z / Cmd-Z)
 
-Requires \$SPOTIFY_USER be set in the environment for public playlists, inferred from authorized token for private playlists where \$SPOTIFY_PRIVATE=1 is set
+For public playlists, \$SPOTIFY_USER be set in the environment
+For private playlists, the user is inferred from the authorized token
 
 Requires \$SPOTIFY_ACCESS_TOKEN, or \$SPOTIFY_ID and \$SPOTIFY_SECRET to be defined in the environment
 
-Caveat: due to limitations of the Spotify API, this by default only works for public playlists.
-For private playlists export SPOTIFY_PRIVATE=1 and get an interactively authorized access token for your shell to prevent repeated web authorization pop-ups:
+Caveat: due to limitations of the Spotify API, by default works on public playlists.
+For private playlists you must export SPOTIFY_PRIVATE=1 and preferably pre-generate the token in your shell to prevent repeated web authorization pop-ups:
 
-export SPOTIFY_PRIVATE=1
 export SPOTIFY_ACCESS_TOKEN=\"\$(\"$srcdir/spotify_api_token.sh\")\"
 "
 
