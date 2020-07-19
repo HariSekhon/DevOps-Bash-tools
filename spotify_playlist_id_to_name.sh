a3b6cb7d9ce6bd7d6ce777d332414a515bf38601
Hari Sekhon <harisekhon@gmail.com>
2020-07-19 18:25:08 +0100

    updated spotify_playlist_id_to_name.sh

--- spotify_playlist_id_to_name.sh
+++ spotify_playlist_id_to_name.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #  vim:ts=4:sts=4:sw=4:et
-#  args: 2ddv4fdbsD7WOnmY30g40i | tee /dev/stderr | spotify_playlist_name_to_id.sh "`cat`"
+#  args: 2ddv4fdbsD7WOnmY30g40i | tee /dev/stderr | spotify_playlist_name_to_id.sh
 #
 #  Author: Hari Sekhon
 #  Date: 2020-07-03 00:25:24 +0100 (Fri, 03 Jul 2020)
@@ -26,6 +26,8 @@ Uses Spotify API to translate a Spotify public playlist ID to a name
 
 If a spotify playlist name is given instead of an ID, returns it as is
 
+A single playlist ID can be given as an argument, or a list can be passed via stdin
+
 Needed by several other adjacent spotify tools
 
 Caveat: due to limitations of the Spotify API, this only works for public playlists
@@ -40,20 +42,34 @@ usage_args="<playlist> [<curl_options>]"
 
 help_usage "$@"
 
-min_args 1 "$@"
-
-playlist_id="$1"
-shift || :
-
-# if it's not a playlist id, scan all playlists and take the ID of the first matching playlist name
-if [[ "$playlist_id" =~ ^[[:alnum:]]{22}$ ]]; then
-    playlist_name="$("$srcdir/spotify_api.sh" "/v1/playlists/$playlist_id" "$@" |
-                jq -r '.name' || :)"
-    if [ -z "$playlist_name" ]; then
-        echo "Error: failed to find playlist name matching ID '$playlist_id'" >&2
-        exit 1
+playlist_id_to_name(){
+    local playlist_id="$1"
+    shift || :
+    # if it's not a playlist id, scan all playlists and take the ID of the first matching playlist name
+    if [[ "$playlist_id" =~ ^[[:alnum:]]{22}$ ]]; then
+        playlist_name="$("$srcdir/spotify_api.sh" "/v1/playlists/$playlist_id" "$@" |
+                    jq -r '.name' || :)"
+        if [ -z "$playlist_name" ]; then
+            echo "Error: failed to find playlist name matching ID '$playlist_id'" >&2
+            exit 1
+        fi
+        echo "$playlist_name"
+    else
+        echo "$playlist_id"
     fi
-    echo "$playlist_name"
+}
+
+if [ -z "${SPOTIFY_ACCESS_TOKEN:-}" ]; then
+    SPOTIFY_ACCESS_TOKEN="$("$srcdir/spotify_api_token.sh")"
+    export SPOTIFY_ACCESS_TOKEN
+fi
+
+if [ $# -gt 0 ]; then
+    playlist_id="$1"
+    shift || :
+    playlist_id_to_name "$playlist_id" "$@"
 else
-    echo "$playlist_id"
+    while read -r playlist_id; do
+        playlist_id_to_name "$playlist_id" "$@"
+    done
 fi
