7d7cbc5317191767f30daf04d873585f2eb890c8
Hari Sekhon <harisekhon@gmail.com>
2020-07-19 19:36:20 +0100

    updated spotify_playlist_tracks.sh

--- spotify_playlist_tracks.sh
+++ spotify_playlist_tracks.sh
@@ -67,9 +67,10 @@ fi
 
 playlist_id="$("$srcdir/spotify_playlist_name_to_id.sh" "$playlist_id" "$@")"
 
-offset="${OFFSET:-0}"
+offset="${SPOTIFY_OFFSET:-0}"
+limit="${SPOTIFY_LIMIT:-50}"
 
-url_path="/v1/playlists/$playlist_id/tracks?limit=50&offset=$offset"
+url_path="/v1/playlists/$playlist_id/tracks?limit=$limit&offset=$offset"
 
 output(){
     if [ -n "${SPOTIFY_CSV:-}" ]; then
