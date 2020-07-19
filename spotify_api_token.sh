8d26d1e42d4d4e627e379b4b7db7d994a652ef85
Hari Sekhon <harisekhon@gmail.com>
2020-07-19 11:33:16 +0100

    merged spotify_api_token_interactive.sh and spotify_api_token.sh

--- spotify_api_token.sh
+++ spotify_api_token.sh
@@ -23,14 +23,16 @@ usage_args="[<curl_options>]"
 
 # shellcheck disable=SC2034
 usage_description="
-Returns a Spotify access token from the Spotify API
+Returns a Spotify access token from the Spotify API, needed to access the Spotify API
 
-This token can be used to access the Spotify API but not user private data - for that you must use the adjacent
-spotify_api_token_interactive.sh in order to obtain an authorized token instead
+Due to quirks of the Spotify API, by default returns a non-interactive access token that cannot access private user data
+
+To get a token to access the private user data API endpoints, set \$SPOTIFY_PRIVATE=1
+This will then require a browser prompt to authorize, at which point this will capture and output the resulting token
 
 Requires \$SPOTIFY_ID and \$SPOTIFY_SECRET to be defined in the environment
 
-Generate an App client ID and secret here:
+Generate an App client ID and secret here and add a callback URL of 'http://localhost:12345/callback':
 
 https://developer.spotify.com/dashboard/applications
 "
@@ -81,22 +83,20 @@ scope="$(tr '\n' '+' <<< "$scope" | sed 's/^+//; s/+*$//')"
 #
 #   https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
 #
-output="$(curl -sSL -u "$SPOTIFY_ID:$SPOTIFY_SECRET" -X 'POST' -d 'grant_type=client_credentials' -d "scope=$scope" https://accounts.spotify.com/api/token "$@")"
+if [ -z "${SPOTIFY_PRIVATE:-}" ]; then
+    output="$(curl -sSL -u "$SPOTIFY_ID:$SPOTIFY_SECRET" -X 'POST' -d 'grant_type=client_credentials' -d "scope=$scope" https://accounts.spotify.com/api/token "$@")"
+fi
 
 # ============================================================================ #
 
-# These next 3 methods won't never work because it relies on live Login challenge - only relevant to webapps
-
-#redirect_uri="https:%2F%2Fgithub.com%2Fharisekhon"
+redirect_uri='http://localhost:12345/callback'
 
 # ============================================================================ #
-# Authorization Code Flow
+# Implicit Grant Method
 #
-# use spotify_api_token_interactive.sh for this method
+#   https://developer.spotify.com/documentation/general/guides/authorization-guide/#implicit-grant-flow
 #
-#   https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow
-#
-#output="$(curl -sSL -X GET "https://accounts.spotify.com/authorize?client_id=$SPOTIFY_ID&redirect_uri=$redirect_uri&scope=$scope&response_type=code")"
+#output="$(curl -sSL -X GET "https://accounts.spotify.com/authorize?client_id=$SPOTIFY_ID&redirect_uri=$redirect_uri&scope=$scope&response_type=token")"
 
 # ============================================================================ #
 # Authorization Code Flow with Proof Key for Code Exchange (PKCE)
@@ -112,11 +112,54 @@ output="$(curl -sSL -u "$SPOTIFY_ID:$SPOTIFY_SECRET" -X 'POST' -d 'grant_type=cl
 #output="$(curl -sSL -X GET "https://accounts.spotify.com/authorize?client_id=$SPOTIFY_ID&redirect_uri=$redirect_uri&scope=$scope&response_type=code&code_challenge_method=S256&code_challenge=$code_challenge")"
 
 # ============================================================================ #
-# Implicit Grant Method
+# Authorization Code Flow
 #
-#   https://developer.spotify.com/documentation/general/guides/authorization-guide/#implicit-grant-flow
+# use spotify_api_token_interactive.sh for this method
 #
-#output="$(curl -sSL -X GET "https://accounts.spotify.com/authorize?client_id=$SPOTIFY_ID&redirect_uri=$redirect_uri&scope=$scope&response_type=token")"
+#   https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow
+#
+#output="$(curl -sSL -X GET "https://accounts.spotify.com/authorize?client_id=$SPOTIFY_ID&redirect_uri=$redirect_uri&scope=$scope&response_type=code")"
+
+if [ -n "${SPOTIFY_PRIVATE:-}" ]; then
+    url="https://accounts.spotify.com/authorize?client_id=$SPOTIFY_ID&redirect_uri=$redirect_uri&scope=$scope&response_type=code"
+    {
+    if [ "$(uname -s)" = Darwin ]; then
+        open "$url"
+    else
+        echo "Go to the following URL in your browser, authorize and then the token will be output on the command line:"
+        echo
+        echo "$url"
+        echo
+    fi
+
+    log "waiting to catch callback"
+    response="$(nc -l localhost 12345 <<EOF
+HTTP/1.1 200 OK
+
+OK, now return to command line
+EOF
+    )"
+    log "callback caught"
+
+    code="$(grep -Eo "GET.*code=([^?]+)" <<< "$response" | sed 's/.*code=//; s/[[:space:]].*$//' || :)"
+    if [ -z "$code" ]; then
+        echo "failed to parse code, authentication failure or authorization denied?"
+        exit 1
+    fi
+    log "Parsed code: $code"
+    log
+    log "Requesting API token using code"
+
+    # or send client_id + client_secret fields in POST body - using curl_auth.sh now to avoid this appearing in process list / logs
+    #basic_auth_token="$(base64 <<< "$SPOTIFY_ID:$SPOTIFY_SECRET")"
+
+    # output everything that isn't the token to stderr as it's almost certainly user information or errors and we don't want that to be captured by client scripts
+    } >&2
+
+    #curl -H "Authorization: Basic $basic_auth_token" -d grant_type=authorization_code -d code="$code" -d redirect_uri="$redirect_uri" https://accounts.spotify.com/api/token
+    # won't appear in process list
+    output="$(USERNAME="$SPOTIFY_ID" PASSWORD="$SPOTIFY_SECRET" "$srcdir/curl_auth.sh" -sSL -d grant_type=authorization_code -d code="$code" -d redirect_uri="$redirect_uri" https://accounts.spotify.com/api/token)"
+fi
 
 # shellcheck disable=SC2181
 if [ $? != 0 ] || [[ "$output" =~ error_description ]]; then
