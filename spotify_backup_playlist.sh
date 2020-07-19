15b8e356cc580afa70372570f24cb90f787ab913
Hari Sekhon <harisekhon@gmail.com>
2020-07-19 15:52:19 +0100

    updated spotify_backup_playlist.sh

--- spotify_backup_playlist.sh
+++ spotify_backup_playlist.sh
@@ -33,9 +33,10 @@ Requires \$SPOTIFY_USER be set in the environment or else given as the second ar
 
 Requires \$SPOTIFY_ID and \$SPOTIFY_SECRET to be defined in the environment
 
-Caveat: due to limitations of the Spotify API, this by default only works for public playlists. For private playlists you must get an interactively authorized access token like so:
+Caveat: due to limitations of the Spotify API, this by default only works for public playlists. For private playlists export SPOTIFY_PRIVATE=1 and get an interactively authorized access token for your shell to prevent repeated web authorization pop-ups:
 
-export SPOTIFY_ACCESS_TOKEN=\"\$(\"$srcdir/spotify_api_token_interactive.sh\")\"
+export SPOTIFY_PRIVATE=1
+export SPOTIFY_ACCESS_TOKEN=\"\$(\"$srcdir/spotify_api_token.sh\")\"
 "
 
 # shellcheck disable=SC1090
@@ -80,7 +81,7 @@ mkdir -vp "$backup_dir_spotify"
 
 if [ -z "${SPOTIFY_ACCESS_TOKEN:-}" ]; then
     if liked; then
-        SPOTIFY_ACCESS_TOKEN="$("$srcdir/spotify_api_token_interactive.sh")"
+        SPOTIFY_ACCESS_TOKEN="$(SPOTIFY_PRIVATE=1 "$srcdir/spotify_api_token.sh")"
     else
         SPOTIFY_ACCESS_TOKEN="$("$srcdir/spotify_api_token.sh")"
     fi
