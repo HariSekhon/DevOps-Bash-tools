201e324b886664051809b555d55e1f27e503f368
Hari Sekhon <harisekhon@gmail.com>
2020-07-20 10:47:20 +0100

    updated spotify_liked_tracks_uri.sh

--- spotify_liked_tracks_uri.sh
+++ spotify_liked_tracks_uri.sh
@@ -32,7 +32,7 @@ $usage_auth_msg
 
 Caveat: due to limitations of the Spotify API, this requires an interactively authorized access token, which you will be prompted for if you haven't already got one in your shell environment. To set up an authorized token for an hour in your current shell, you can run the following command (make sure you don't have an access token in the environment from spotify_api_token.sh otherwise you will get a 401 error):
 
-export SPOTIFY_ACCESS_TOKEN=\"\$(SPOTIFY_PRIVATE=1 '$srcdir/spotify_api_token_interactive.sh')\"
+$usage_token_private
 "
 
 # used by usage() in lib/utils.sh
