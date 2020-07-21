5a2af11163c247d511da5a28864bf44708861f23
Hari Sekhon <harisekhon@gmail.com>
2020-07-21 12:05:27 +0100

    added media.sh

---
+++ .bash.d/media.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-07-21 11:36:49 +0100 (Tue, 21 Jul 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# set the Track number metadata on mp3 files in the order that they are given
+# eg. mp3_renumber *.mp3 - assuming the files are in the correct lexical order they will receive the right metadata track order which some apps need
+mp3_renumber(){
+    local i=0
+    for x in "$@"; do
+        ((i+=1))
+        id3v2 --track "$i" "$x"
+    done
+}
