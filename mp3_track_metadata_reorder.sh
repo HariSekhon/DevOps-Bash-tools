17ca9582358fa1045276a7c69c9a581a44c3e23b
Hari Sekhon <harisekhon@gmail.com>
2020-07-22 16:07:06 +0100

    updated mp3_track_metadata_reorder.sh

--- mp3_track_metadata_reorder.sh
+++ mp3_track_metadata_reorder.sh
@@ -19,9 +19,9 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034
 usage_description="
-Adds / Modifies track metadata across all mp3 in the directory tree to restructure Audiobooks to be contiguous for Mac's Books.app
+Adds / Modifies track metadata across all MP3 in the directory tree to restructure Audiobooks to be contiguous for Mac's Books.app
 
-If no directory argument is given, works on \$PWD
+If no directory argument is given, works on MP3s under \$PWD
 
 Shows the list of MP3 files and the tags they would be changed to a depth of 1 subdirectory and prompts for confirmation before proceeding for safety
 
@@ -30,7 +30,7 @@ MP3 filenames should be in lexical order before running this
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="[<dir1>]"
+usage_args="[<dir1> <dir2> ...]"
 
 # shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
@@ -39,9 +39,7 @@ help_usage "$@"
 
 check_bin id3v2
 
-target_dir="${1:-$PWD}"
-
-mp3_files="$(find "$target_dir" -maxdepth 2 -iname '*.mp3')"
+mp3_files="$(for dir in "${@:-$PWD}"; do find "$dir" -maxdepth 2 -iname '*.mp3' || exit 1; done)"
 
 echo "List of MP3 files and their metadata track ordering:"
 echo
