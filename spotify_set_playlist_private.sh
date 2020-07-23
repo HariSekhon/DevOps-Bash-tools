8153d4d3724b0c6a28ac5cba6fe48e1e2e97154f
Hari Sekhon <harisekhon@gmail.com>
2020-07-24 00:49:03 +0100

    updated spotify_set_playlist_private.sh

--- spotify_set_playlist_private.sh
+++ spotify_set_playlist_private.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #  vim:ts=4:sts=4:sw=4:et
 #
-#  args: "Driving (auto-generated as reverse of Upbeat & Sexual Pop)"
+#  args: "Driving (Auto-generated as reverse of Upbeat & Sexual Pop)"
 #
 #  Author: Hari Sekhon
 #  Date: 2020-07-23 23:26:15 +0100 (Thu, 23 Jul 2020)
