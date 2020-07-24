5d7517884dec48af2a60c77cb72a02b0cf1f5d5f
Hari Sekhon <harisekhon@gmail.com>
2020-07-24 17:53:57 +0100

    added git_repos_pull.sh

---
+++ git_repos_pull.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2016-01-17 12:14:06 +0000 (Sun, 17 Jan 2016)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Pulls all my Git repos listed in setup/repolist.txt to ~/github/
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "$0")" && pwd)"
+
+if [ "${GIT_HTTPS:-}" ]; then
+    git_url="${GIT_URL:-https://github.com/}"
+    if [ -n "${GITHUB_TOKEN:-}" ] && ! [[ "$git_url" =~ @ ]]; then
+        git_url="https://$GITHUB_TOKEN@${git_url#https://}"
+    fi
+else
+    git_url="${GIT_URL:-ssh://git@github.com:}"
+fi
+
+git_base_dir=~/github
+
+mkdir -pv "$git_base_dir"
+
+cd "$git_base_dir"
+
+repolist="${*:-${REPOS:-}}"
+if [ -z "$repolist" ]; then
+    repolist="$("$srcdir/git_repos.sh")"
+fi
+
+run(){
+    local repolist="$*"
+    for repo in $repolist; do
+        repo_dir="${repo##*/}"
+        repo_dir="${repo_dir##*:}"
+        repo="${repo%%:*}"
+        if ! echo "$repo" | grep -q "/"; then
+            repo="HariSekhon/$repo"
+        fi
+        if [ -d "$repo_dir" ]; then
+            pushd "$repo_dir"
+            # make update does git pull but if that mechanism is broken then this first git pull will allow the repo to self-fix itself
+            git pull --no-edit
+            popd
+        else
+            git clone "${git_url}${repo}" "$repo_dir"
+        fi
+    done
+}
+
+run "$repolist"
