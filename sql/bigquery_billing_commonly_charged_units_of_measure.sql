f44b454d385daf67fa4ed30fdb7f0369d39f7c40
Hari Sekhon <harisekhon@gmail.com>
2020-08-05 15:16:51 +0100

    updated bigquery_billing_commonly_charged_units_of_measure.sql

--- sql/bigquery_billing_commonly_charged_units_of_measure.sql
+++ sql/bigquery_billing_commonly_charged_units_of_measure.sql
@@ -2,7 +2,7 @@
 --  Author: Hari Sekhon
 --  Date: 2020-02-02 00:37:06 +0000 (Sun, 02 Feb 2020)
 --
---  vim:ts=2:sts=2:sw=2:et
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
 --
 --  https://github.com/harisekhon/bash-tools
 --
@@ -14,6 +14,7 @@
 --
 
 -- BigQuery Billing Query - commonly charged units of measure
+
 SELECT
   usage_unit,
   COUNT(*) AS billing_records
