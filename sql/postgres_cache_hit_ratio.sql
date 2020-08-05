79d680028e63b4d3c82237b3c59bcff7f5f3b347
Hari Sekhon <harisekhon@gmail.com>
2020-08-05 15:03:20 +0100

    added postgres_cache_hit_ratio.sql

---
+++ sql/postgres_cache_hit_ratio.sql
@@ -0,0 +1,23 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-08-05 14:57:37 +0100 (Wed, 05 Aug 2020)
+--
+--  vim:ts=4:sts=4:sw=4:et
+--
+--  https://github.com/harisekhon/bash-tools
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- PostgreSQL Cache-hit ratio (should be closer to 1, eg. 0.99)
+
+SELECT SUM(heap_blks_read) AS heap_read,
+       SUM(heap_blks_hit)  AS heap_hit,
+              SUM(heap_blks_hit) /
+    (SUM(heap_blks_hit) + SUM(heap_blks_read))
+                     AS ratio
+FROM pg_statio_user_tables;
