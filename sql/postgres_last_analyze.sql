b3d7e9a504025b51fba01bca1b2afda3283f67aa
Hari Sekhon <harisekhon@gmail.com>
2020-08-05 17:45:42 +0100

    added postgres_last_analyze.sql

---
+++ sql/postgres_last_analyze.sql
@@ -0,0 +1,28 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-08-05 17:30:04 +0100 (Wed, 05 Aug 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/bash-tools
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- PostgreSQL analyze info for tables with rows modified since last analyze
+
+SELECT
+  schemaname,
+  relname,
+  n_mod_since_analyze,
+  last_analyze,
+  last_autoanalyze,
+  analyze_count,
+  autoanalyze_count
+FROM pg_stat_user_tables
+ORDER BY
+  n_mod_since_analyze DESC;
