850ba96f2bdb106b9abb425e2124d129e2f87bb1
Hari Sekhon <harisekhon@gmail.com>
2020-08-05 15:21:34 +0100

    updated postgres_running_queries_pre92.sql

--- sql/postgres_running_queries_pre92.sql
+++ sql/postgres_running_queries_pre92.sql
@@ -2,7 +2,7 @@
 --  Author: Hari Sekhon
 --  Date: 2020-08-05 12:52:58 +0100 (Wed, 05 Aug 2020)
 --
---  vim:ts=4:sts=4:sw=4:et
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
 --
 --  https://github.com/harisekhon/bash-tools
 --
@@ -15,11 +15,16 @@
 
 -- Running queries for PostgreSQL < 9.2
 
-SELECT procpid,
-	   age(clock_timestamp(), query_start),
-	   usename,
- 	   current_query
-FROM pg_stat_activity
-WHERE current_query != '<IDLE>'
-  AND current_query NOT ILIKE '%pg_stat_activity%'
-ORDER BY query_start desc;
+SELECT
+  procpid,
+  age(clock_timestamp(), query_start),
+  usename,
+  current_query
+FROM
+  pg_stat_activity
+WHERE
+  current_query != '<IDLE>'
+    AND
+  current_query NOT ILIKE '%pg_stat_activity%'
+ORDER BY
+  query_start DESC;
