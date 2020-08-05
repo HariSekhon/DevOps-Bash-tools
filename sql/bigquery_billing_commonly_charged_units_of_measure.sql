5217199d9b717411ac95a3554b43ebe8921e3b0e
Hari Sekhon <harisekhon@gmail.com>
2020-08-05 15:14:08 +0100

    updated bigquery_billing_commonly_charged_units_of_measure.sql

--- sql/bigquery_billing_commonly_charged_units_of_measure.sql
+++ sql/bigquery_billing_commonly_charged_units_of_measure.sql
@@ -2,7 +2,7 @@
 --  Author: Hari Sekhon
 --  Date: 2020-02-02 00:37:06 +0000 (Sun, 02 Feb 2020)
 --
---  vim:ts=4:sts=4:sw=4:et
+--  vim:ts=2:sts=2:sw=2:et
 --
 --  https://github.com/harisekhon/bash-tools
 --
