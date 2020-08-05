15c6d70946696001722f5fad94eee703bcf54a0f
Hari Sekhon <harisekhon@gmail.com>
2020-08-05 15:17:37 +0100

    updated bigquery_billing_products_with_most_billing_records.sql

--- sql/bigquery_billing_products_with_most_billing_records.sql
+++ sql/bigquery_billing_products_with_most_billing_records.sql
@@ -2,7 +2,7 @@
 --  Author: Hari Sekhon
 --  Date: 2020-02-02 00:37:06 +0000 (Sun, 02 Feb 2020)
 --
---  vim:ts=4:sts=4:sw=4:et
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
 --
 --  https://github.com/harisekhon/bash-tools
 --
@@ -14,6 +14,7 @@
 --
 
 -- BigQuery Billing Query - products with most billing records
+
 SELECT
   product,
   COUNT(*) AS billing_records
