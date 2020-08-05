581cee68b00f717b59a662109798a917c3c71efc
Hari Sekhon <harisekhon@gmail.com>
2020-08-05 17:43:03 +0100

    added postgres_last_vacuum.sql

---
+++ sql/postgres_last_vacuum.sql
@@ -0,0 +1,32 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-08-05 17:30:04 +0100 (Wed, 05 Aug 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/bash-tools
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- PostgreSQL vacuum info for tables with dead tuples
+
+SELECT
+  schemaname,
+  relname,
+  n_live_tup,
+  n_dead_tup,
+  n_dead_tup / GREATEST(n_live_tup + n_dead_tup, 1)::float * 100 AS dead_percentage,
+  last_vacuum,
+  last_autovacuum,
+  vacuum_count,
+  autovacuum_count
+FROM pg_stat_user_tables
+WHERE
+  n_dead_tup > 0
+ORDER BY
+  n_dead_tup DESC;
