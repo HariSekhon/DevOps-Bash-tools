676c50315f399d5d7e351ff6a5e44263432e5c22
Hari Sekhon <harisekhon@gmail.com>
2020-08-08 00:23:31 +0100

    updated mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -22,22 +22,37 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
 
+# shellcheck disable=SC1090
+. "$srcdir/lib/dbshell.sh"
+
+# defined in lib/dbshell.sh
+# shellcheck disable=SC2154
+shell_description="$sql_mount_description
+
+Source a sql script:
+
+source mysql_info.sql
+
+
+Get shell access:
+
+\! bash
+
+
+List available SQL scripts:
+
+\! ls -l
+"
+
 # shellcheck disable=SC2034,SC2154
 usage_description="
 Boots a quick MariaDB docker container and drops you in to the 'mysql' shell
 
-Automatically createst shared bind mount points inside the container for convenience:
-
-sql   => /sql
-repo  => /code
-\$PWD => /pwd
-
-You can quickly test scripts via include, eg.
-
-\i /sql/mysql_running_queries.sql
-
 Multiple invocations of this script will connect to the same MariaDB container if already running
 and the last invocation of this script to exit from the mysql shell will delete that container
+
+Automatically creates shared bind mount points from host to container for convenience:
+$shell_description
 "
 
 # used by usage() in lib/utils.sh
@@ -46,75 +61,33 @@ usage_args=""
 
 help_usage "$@"
 
+docker_image=mariadb
 container_name=mariadb
-version="${MYSQL_VERSION:-latest}"
+version="${MARIADB_VERSION:-latest}"
 
 password="${MYSQL_ROOT_PASSWORD:-${MYSQL_PWD:-${MYSQL_PASSWORD:-test}}}"
 
-sql_scripts="$srcdir/sql"
-if [ -d "$srcdir/../sql" ]; then
-    sql_scripts="$srcdir/../sql"
-fi
-
 if ! docker ps -qf name="$container_name" | grep -q .; then
-    timestamp 'booting MariaDB container:'
-    docker run -d -ti \
-               --name "$container_name" \
-               -p 3306:3306 \
-               -e MYSQL_ROOT_PASSWORD="$password" \
-               -v "$srcdir:/bash" \
-               -v "$sql_scripts:/sql" \
-               -v "$HOME/github:/github" \
-               -v "$PWD:/pwd" \
-               mariadb:"$version"
-               #-v "$srcdir/setup/mysql/conf.d/my.cnf:/etc/mysql/conf.d/" \
+    timestamp "booting MariaDB container from image '$docker_image:$version':"
+    # defined in lib/dbshell.sh
+    # shellcheck disable=SC2154
+    eval docker run -d \
+        --name "$container_name" \
+        -p 3306:3306 \
+        -e MYSQL_ROOT_PASSWORD="$password" \
+        "$docker_sql_mount_switches" \
+        "$docker_image":"$version"
+        #-v "$srcdir/setup/mysql/conf.d/my.cnf:/etc/mysql/conf.d/" \
 
-    tries=0
-    while true; do
-        ((tries+=1))
-        if [ $((tries % 5)) = 0 ]; then
-            timestamp 'waiting for mysqld to be ready to accept connections before connecting mysql shell...'
-        fi
-        if docker logs --tail 10 "$container_name" |
-            grep -e 'mysqld.*ready for connections' \
-                 -e 'mysqld.*ready to accept connections'; then
-            break
-        fi
-        sleep 1
-        if [ $tries -gt 60 ]; then
-            echo "MariaDB container failed to become ready for connections within reasonable time, check logs (format may have changed):"
-            echo
-            docker logs "$container_name"
-            exit 1
-        fi
-    done
+    wait_for_mysql_ready "$container_name"
     echo
 fi
 
 cat <<EOF
-SQL  scripts are mounted at /sql
-Bash scripts are mounted at /bash
-\$PWD          is mounted at /pwd
-\$HOME/github  is mounted at /github
-
-To source a SQL script::
-
-source /sql/<file>.sql
-
-\.     /sql/<file>.sql
-
-
-To get to shell command line:
-
-\! bash
+$shell_description
 
 EOF
 
 docker exec -ti -w /sql "$container_name" mysql -u root -p"$password"
 
-if [ "$(lsof -lnt "$0" | grep -c .)" -lt 2 ]; then
-#if [ "$(pgrep -lf "bash.*${0##*/}" | grep -c .)" -lt 2 ]; then
-#if [ "$(ps -ef | grep -c "[b]ash.*${0##*/}")" -lt 2 ]; then
-    echo "last session closing, deleting container:"
-    docker rm -f "$container_name"
-fi
+docker_rm_when_last_connection "$0" "$container_name"
