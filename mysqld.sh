d513c6b84faf9e0b25f1fd52071a75d079c203da
Hari Sekhon <harisekhon@gmail.com>
2020-08-08 09:24:27 +0100

    updated mysqld.sh

--- mysqld.sh
+++ mysqld.sh
@@ -67,6 +67,9 @@ version="${MYSQL_VERSION:-latest}"
 
 password="${MYSQL_ROOT_PASSWORD:-${MYSQL_PWD:-${MYSQL_PASSWORD:-test}}}"
 
+# ensures version is correct before we kill any existing test env to switch versions
+docker pull "$docker_image:$version"
+
 if [ "$(docker ps --filter "name=$container_name" --format '{{.Image}}')" != "$docker_image:$version" ]; then
     MYSQL_RESTART=1
 fi
