dca5ba7b1d999bbc890c0c67596be92b2a580d33
Hari Sekhon <harisekhon@gmail.com>
2020-08-12 11:43:17 +0100

    updated mariadb_test_scripts.sh

--- mariadb_test_scripts.sh
+++ mariadb_test_scripts.sh
@@ -150,7 +150,7 @@ for version in $mariadb_versions; do
     } |
     # forcing mysql shell --table output as though interactive
     MYSQL_OPTS="--table" \
-    "$srcdir/mariadb.sh" "$version"
+    "$srcdir/mariadb.sh" "$version" --restart
     timestamp "Succeeded testing scripts for MariaDB $version"
     echo >&2
     echo >&2
