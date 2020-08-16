39c5c8782c2d3a1c2986f76f17375cf1f91a20e8
Hari Sekhon <harisekhon@gmail.com>
2020-08-16 18:17:27 +0100

    added gitlab_get_user_public_key.sh

---
+++ gitlab_get_user_public_key.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#
+#  Author: Hari Sekhon
+#  Date: 2020-08-16 18:13:02 +0100 (Sun, 16 Aug 2020)
+#
+#  https://github.com/harisekhon/devops-bash-tools
+#
+#  License: see accompanying LICENSE file
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+usage(){
+    cat <<EOF
+Fetches a GitLab user's public SSH key from the GitLab API
+
+User can be given as first argument, or environment variables \$GITLAB_USER or \$USER
+
+${0##*/} <user>
+EOF
+    exit 3
+}
+
+for arg; do
+    case "$arg" in
+        -*)     usage
+                ;;
+    esac
+done
+
+if [ $# -gt 1 ]; then
+    usage
+elif [ $# -eq 1 ]; then
+    user="$1"
+elif [ -n "${GITLAB_USER:-}" ]; then
+    user="$GITLAB_USER"
+elif [ -n "${USER:-}" ]; then
+    if [[ "$USER" =~ hari|sekhon ]]; then
+        user=harisekhon
+    else
+        user="$USER"
+    fi
+else
+    usage
+fi
+
+echo "# Fetching SSH Public Key(s) from GitLab for account:  $user" >&2
+echo "#" >&2
+curl -sS "https://gitlab.com/api/v4/users/$user/keys" |
+jq -r '.[].key'
