d565729a211a27d26f4235414afba56cc727aed0
Hari Sekhon <harisekhon@gmail.com>
2020-08-17 20:24:44 +0100

    added mac.sh

---
+++ lib/mac.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-08-17 20:21:43 +0100 (Mon, 17 Aug 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# designed to be included from utils.sh
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+if ! is_mac; then
+    return
+fi
+
+if ! type tac &>/dev/null; then
+    tac(){
+        gtac "$@"
+    }
+fi
