cc6f287714c1f4bb351db704e6705e3a6612e78e
Hari Sekhon <harisekhon@gmail.com>
2020-08-17 23:39:11 +0100

    updated mac_configuration.sh

--- setup/mac_configuration.sh
+++ setup/mac_configuration.sh
@@ -51,6 +51,8 @@ defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 # ============================================================================ #
 # Finder:
 
+# TODO: set Finder to columns that are auto-wide
+
 # set Downloads as the default location for new Finder windows
 defaults write com.apple.finder NewWindowTarget -string "PfDe"
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Downloads/"
@@ -148,28 +150,28 @@ defaults write com.apple.dock autohide-time-modifier -float 0
 
 # Change indexing order and disable some search results
 #defaults write com.apple.spotlight orderedItems -array \
-#	'{"enabled" = 1;"name" = "APPLICATIONS";}' \
-#	'{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
-#	'{"enabled" = 0;"name" = "DIRECTORIES";}' \
-#	'{"enabled" = 0;"name" = "PDF";}' \
-#	'{"enabled" = 0;"name" = "FONTS";}' \
-#	'{"enabled" = 0;"name" = "DOCUMENTS";}' \
-#	'{"enabled" = 0;"name" = "MESSAGES";}' \
-#	'{"enabled" = 0;"name" = "CONTACT";}' \
-#	'{"enabled" = 0;"name" = "EVENT_TODO";}' \
-#	'{"enabled" = 0;"name" = "IMAGES";}' \
-#	'{"enabled" = 0;"name" = "BOOKMARKS";}' \
-#	'{"enabled" = 0;"name" = "MUSIC";}' \
-#	'{"enabled" = 0;"name" = "MOVIES";}' \
-#	'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-#	'{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-#	'{"enabled" = 0;"name" = "SOURCE";}' \
-#	'{"enabled" = 1;"name" = "MENU_DEFINITION";}' \
-#	'{"enabled" = 0;"name" = "MENU_OTHER";}' \
-#	'{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
-#	'{"enabled" = 1;"name" = "MENU_EXPRESSION";}' \
-#	'{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
-#	'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+#   '{"enabled" = 1;"name" = "APPLICATIONS";}' \
+#   '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+#   '{"enabled" = 0;"name" = "DIRECTORIES";}' \
+#   '{"enabled" = 0;"name" = "PDF";}' \
+#   '{"enabled" = 0;"name" = "FONTS";}' \
+#   '{"enabled" = 0;"name" = "DOCUMENTS";}' \
+#   '{"enabled" = 0;"name" = "MESSAGES";}' \
+#   '{"enabled" = 0;"name" = "CONTACT";}' \
+#   '{"enabled" = 0;"name" = "EVENT_TODO";}' \
+#   '{"enabled" = 0;"name" = "IMAGES";}' \
+#   '{"enabled" = 0;"name" = "BOOKMARKS";}' \
+#   '{"enabled" = 0;"name" = "MUSIC";}' \
+#   '{"enabled" = 0;"name" = "MOVIES";}' \
+#   '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+#   '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+#   '{"enabled" = 0;"name" = "SOURCE";}' \
+#   '{"enabled" = 1;"name" = "MENU_DEFINITION";}' \
+#   '{"enabled" = 0;"name" = "MENU_OTHER";}' \
+#   '{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
+#   '{"enabled" = 1;"name" = "MENU_EXPRESSION";}' \
+#   '{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
+#   '{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
 
 echo "Some settings won't take effect until you restart Finder. When you're ready, run:
 
