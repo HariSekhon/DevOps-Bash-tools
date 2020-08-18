6ab4c249f888b5e97c1035167d674e882554ffae
Hari Sekhon <harisekhon@gmail.com>
2020-08-18 01:09:03 +0100

    updated yaml2json.sh

--- yaml2json.sh
+++ yaml2json.sh
@@ -35,20 +35,22 @@ usage_args="[<filename>]"
 help_usage "$@"
 
 yaml2json(){
-    if type -P python &>/dev/null && python -c 'import yaml' &>/dev/null; then
+    # yaml is a 3rd party library, and in old 2.x versions so was json - only run the Python conversion if we have both libraries installed
+    if type -P python &>/dev/null && python -c 'import yaml, json' &>/dev/null; then
         python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin, Loader=yaml.FullLoader), sys.stdout, indent=4)'
+    # needs 3rd party modules installed (YAML::XS, JSON::XS), so check we have both modules first
+    elif type -P perl &>/dev/null &&
+        perl -MYAML::XS=Load -MJSON::XS=encode_json -e '' &>/dev/null; then
+        #perl -MYAML::XS=LoadFile -MJSON::XS=encode_json -e 'for (@ARGV) { for (LoadFile($_)) { print encode_json($_),"\n" } }'
+        perl -MYAML::XS=Load -MJSON::XS=encode_json -e '$/ = undef; print encode_json(Load(<STDIN>)) . "\n"'
     elif type -P ruby &>/dev/null; then
         # don't want variable expansion
         # shellcheck disable=SC2016
         ruby -r yaml -r json -e 'puts YAML.load($stdin.read).to_json'
-    # needs 3rd party modules installed (YAML::XS, JSON::XS), so try this one last
-    elif type -P perl &>/dev/null; then
-        #perl -MYAML::XS=LoadFile -MJSON::XS=encode_json -e 'for (@ARGV) { for (LoadFile($_)) { print encode_json($_),"\n" } }'
-        perl -MYAML::XS=Load -MJSON::XS=encode_json -e '$/ = undef; print encode_json(Load(<STDIN>)) . "\n"'
     # don't use yq - there are 2 completely different 'yq' which could appear in \$PATH, so this is unreliable
     #elif type -P yq &>/dev/null; then
     else
-        die "ERROR: unable to convert yaml to json since not one of the following tools were found: python, perl, ruby"
+        die "ERROR: unable to convert yaml to json since not one of the following tools were found: Python PyYaml, Perl YAML::XS + JSON::XS, Ruby"
     fi
 }
 
