fc3d01d37ad48f25eb24354541c2edfb736e1bc5
Hari Sekhon <harisekhon@gmail.com>
2020-08-19 14:55:12 +0100

    updated vagrant.sh

--- .bash.d/vagrant.sh
+++ .bash.d/vagrant.sh
@@ -20,15 +20,19 @@
 #export VAGRANT_HOME=~/vagrant
 #export VAGRANT_BOXES=~/boxes
 
+# $bash_tools defined in adjacent shell modules
+# shellcheck disable=SC2154
+export vagrant="$bash_tools/vagrant"
+
 #alias cd_vagrant='[ "$PWD" = "$VAGRANT_HOME" ] || cd "$VAGRANT_HOME"'
 #alias cd_vagrant='cd "$VAGRANT_HOME"'
-alias cd_vagrant='cd "$bash_tools/vagrant"'
+alias cd_vagrant='cd "$vagrant"'
 alias cdv=cd_vagrant
 alias vhome=cd_vagrant
 #alias v='cd_vagrant; vagrant'
 # aliased to vim now because I run that command far more often
 #alias v='vagrant'
-alias vf='cd_vagrant; vim Vagrantfile; vagrant_gen_etc_hosts; eval "$(vagrant_gen_aliases)"'
+#alias vf='cd_vagrant; vim Vagrantfile; vagrant_gen_etc_hosts; eval "$(vagrant_gen_aliases)"'
 alias vst='vagrant status'
 alias vrun='vst | grep running'
 # vr is aliased to vbox_running in virtualbox.sh
@@ -38,54 +42,62 @@ alias vrr='vrun'
 alias vssh='vagrant ssh'
 alias boxes='cd $VAGRANT_BOXES'
 
-vagrant_parse_hosts(){
-    if ! [ -f "$VAGRANT_HOME/Vagrantfile" ]; then
-        return
-    fi
-    #grep '[^#]*config.vm.define' "$VAGRANT_HOME/Vagrantfile" | awk -F'"' '{print $2}'
-    sed 's/#.*//;/^[[:space:]]*$/d' "$VAGRANT_HOME/Vagrantfile" |
-    grep -e host_name -e network |
-    grep -B1 -e network |
-    grep -v -e "^--" -e "default_hostname" |
-    sed 's/^.*[[:space:]]"//;s/"//' |
-    tr '\n' ' ' |
-    perl -pn -e 's/(\.\d+)\s/$1\n/g'
-}
+while read -r directory; do
+    # shellcheck disable=SC2139,SC2140
+    alias "v${directory##*/}"="cd $directory"
+done < <(find "$vagrant" -maxdepth 1 -type d)
 
-vagrant_gen_aliases(){
-    vagrant_parse_hosts |
-    while read -r host ip rest; do
-        if ! type "$host" &>/dev/null; then
-            #echo "alias $host='ssh root@$host'"
-            #echo "alias $host='vups $host'"
-            echo "alias $host='vssh $host'"
-        fi
-    done
-}
+# see ../vagrant_hosts.sh for similar parsing
+#
+#vagrant_parse_hosts(){
+#    if ! [ -f "$VAGRANT_HOME/Vagrantfile" ]; then
+#        return
+#    fi
+#    #grep '[^#]*config.vm.define' "$VAGRANT_HOME/Vagrantfile" | awk -F'"' '{print $2}'
+#    sed 's/#.*//;/^[[:space:]]*$/d' "$VAGRANT_HOME/Vagrantfile" |
+#    grep -e host_name -e network |
+#    grep -B1 -e network |
+#    grep -v -e "^--" -e "default_hostname" |
+#    sed 's/^.*[[:space:]]"//;s/"//' |
+#    tr '\n' ' ' |
+#    perl -pn -e 's/(\.\d+)\s/$1\n/g'
+#}
+#
+#vagrant_gen_aliases(){
+#    vagrant_parse_hosts |
+#    while read -r host ip rest; do
+#        if ! type "$host" &>/dev/null; then
+#            #echo "alias $host='ssh root@$host'"
+#            #echo "alias $host='vups $host'"
+#            echo "alias $host='vssh $host'"
+#        fi
+#    done
+#}
+#
+# don't really use these aliases to vagrant boxes any more
+#eval "$(vagrant_gen_aliases)"
 
-eval "$(vagrant_gen_aliases)"
-
-vagrant_gen_etc_hosts(){
-    vagrant_parse_hosts |
-    while read -r host ip rest; do
-        if [ -n "${rest:-}" ]; then
-            echo "error third token '$rest' detected from pipe line '$ip $host $rest'"
-            return 1
-        fi
-        [ "${host}" = "localhost" ] && continue
-        host_record="$ip $host.local $host"
-        # sudo auto-defined in .bashrc
-        # shellcheck disable=SC2154
-        $sudo perl -pi -e "s/^$ip\\s+.*/$host_record/" /etc/hosts
-        if [ -n "$ip" ]; then
-            grep -q "^$host_record" /etc/hosts ||
-            $sudo sh -c "echo '$host_record' >> /etc/hosts"
-        else
-            echo "no ip passed in pipe for host '$host'! "
-            return 1
-        fi
-    done
-}
+#vagrant_gen_etc_hosts(){
+#    vagrant_parse_hosts |
+#    while read -r host ip rest; do
+#        if [ -n "${rest:-}" ]; then
+#            echo "error third token '$rest' detected from pipe line '$ip $host $rest'"
+#            return 1
+#        fi
+#        [ "${host}" = "localhost" ] && continue
+#        host_record="$ip $host.local $host"
+#        # sudo auto-defined in .bashrc
+#        # shellcheck disable=SC2154
+#        $sudo perl -pi -e "s/^$ip\\s+.*/$host_record/" /etc/hosts
+#        if [ -n "$ip" ]; then
+#            grep -q "^$host_record" /etc/hosts ||
+#            $sudo sh -c "echo '$host_record' >> /etc/hosts"
+#        else
+#            echo "no ip passed in pipe for host '$host'! "
+#            return 1
+#        fi
+#    done
+#}
 
 vagrant_usage(){
     if [ -z "$1" ]; then
