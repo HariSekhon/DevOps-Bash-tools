75c9c8b5e2ce0cf18ef21a37dca783ff8e8ff085
Hari Sekhon <harisekhon@gmail.com>
2020-08-20 10:17:24 +0100

    updated mysqld.sh

--- mysqld.sh
+++ mysqld.sh
@@ -152,7 +152,7 @@ if ! docker ps -qf name="$container_name" | grep -q .; then
     # defined in lib/dbshell.sh
     # shellcheck disable=SC2154,SC2086
     docker run -d \
-        --name '"$container_name"' \
+        --name "$container_name" \
         $docker_opts \
         -e MYSQL_ROOT_PASSWORD='"$password"' \
         $docker_sql_mount_switches \
