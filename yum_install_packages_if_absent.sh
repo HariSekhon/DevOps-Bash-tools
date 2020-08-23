134b22eb745e8c27f1084c884e15dc20a2610a28
Hari Sekhon <harisekhon@gmail.com>
2020-08-23 19:02:53 +0100

    updated yum_install_packages_if_absent.sh

--- yum_install_packages_if_absent.sh
+++ yum_install_packages_if_absent.sh
@@ -59,6 +59,7 @@ fi
 
 tr ' ' '\n' <<< "${packages[*]}" |
 sort -u |
+grep -v '^[[:space:]]*$' |
 grep -vFx -f <(rpm -qa --queryformat '%{RPMTAG_NAME}\n') |
 while read -r package; do
     # accounts for vim being provided by vim-enhanced, so we don't try to install the metapackage again and again
