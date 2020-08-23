981995d0fe02b15c4fea251a44a4cc102525bdba
Hari Sekhon <harisekhon@gmail.com>
2020-08-24 00:17:37 +0100

    updated apk_install_packages_if_absent.sh

--- apk_install_packages_if_absent.sh
+++ apk_install_packages_if_absent.sh
@@ -13,62 +13,36 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Install Apk packages in a forgiving way - useful for installing Perl CPAN and Python PyPI modules that may or may not be available
-#
-# combine with later use of the following scripts to only build packages that aren't available in the Linux distribution:
-#
-# perl_cpanm_install_if_absent.sh
-# python_pip_install_if_absent.sh
-
 set -eu
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(dirname "$0")"
 
+# shellcheck disable=SC1090
+. "$srcdir/lib/packages.sh"
+
+# shellcheck disable=SC2154
 usage(){
-    echo "Installs Alpine APK  package lists if the packages aren't already installed"
-    echo
-    echo "Takes a list of apk packages as arguments or via stdin, and for any arguments that are plaintext files, reads the packages from those given files (one package per line)"
-    echo
-    echo "usage: ${0##*/} <list_of_packages>"
-    echo
+    cat <<EOF
+Installs Alpine APK package lists if the packages aren't already installed
+
+$package_args_description
+
+Tested on Alpine
+
+
+usage: ${0##*/} <packages>
+EOF
     exit 3
 }
 
-for x in "$@"; do
-    case "$x" in
-        -*) usage
-            ;;
+for arg; do
+    case "$arg" in
+        -*)     usage
+                ;;
     esac
 done
 
-packages=""
 
-process_args(){
-    for arg in "$@"; do
-        if [ -f "$arg" ] && file "$arg" | grep -q ASCII; then
-            echo "adding packages from file:  $arg"
-            packages="$packages $(sed 's/#.*//;/^[[:space:]]*$$/d' "$arg")"
-            echo
-        else
-            packages="$packages $arg"
-        fi
-    done
-}
-
-if [ -n "${*:-}" ]; then
-    process_args "$@"
-else
-    # shellcheck disable=SC2046
-    process_args $(cat)
-fi
-
-installed_packages="$(mktemp)"
-
-apk info > "$installed_packages"
-
-echo "$packages" |
-tr ' ' '\n' |
-sort -u |
-grep -v '^[[:space:]]*$' |
-grep -vFx -f "$installed_packages" |
+process_package_args "$@" |
+"$srcdir/apk_filter_not_installed.sh" |
 xargs -r "$srcdir/apk_install_packages.sh"
