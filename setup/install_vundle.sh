0a62e6116e6d55229f6da4fa6a398da000ce18c7
Hari Sekhon <harisekhon@gmail.com>
2020-08-23 16:17:37 +0100

    updated install_vundle.sh

--- setup/install_vundle.sh
+++ setup/install_vundle.sh
@@ -29,5 +29,7 @@ if ! [ -e "$target" ]; then
     git clone https://github.com/gmarik/Vundle.vim.git "$target"
 fi
 
+date "+%F %T  Installing Vim Vundle plugins..."
 # this tends to mess up the terminal and requires a reset afterwards
-vim +PluginInstall +qall
+vim --not-a-term +PluginInstall +qall >/dev/null
+date "+%F %T  Finished installing Vim Vundle plugins"
