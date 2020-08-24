abae168cdcec625640c57fcfc768893e5d60c2a6
Hari Sekhon <harisekhon@gmail.com>
2020-08-24 13:10:03 +0100

    added $TOKEN support

--- curl_auth.sh
+++ curl_auth.sh
@@ -20,7 +20,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # used by utils.sh usage()
 # shellcheck disable=SC2034
 usage_description="Runs curl with either Kerberos SpNego (if \$KRB5 is set) or
-a ram file descriptor using \$PASSWORD to avoid credentials being exposed via process list or command line logging
+a ram file descriptor using \$PASSWORD or \$TOKEN to avoid credentials being exposed via process list or command line logging
 
 Requires prefixing url with http:// or https:// to work on older versions of curl in order to parse hostname
 for constructing the authentication string to be specific to the host as using netrc default login doesn't always work
@@ -53,64 +53,75 @@ USERNAME="${USERNAME:-$USER}"
 
 # Only do password mechanism and netrc_contents workaround if not using Kerberos
 if [ -z "${KRB5:-${KERBEROS:-}}" ]; then
-    if [ -z "${PASSWORD:-}" ]; then
+    if [ -z "${PASSWORD:-}" ] &&
+       [ -z "${TOKEN:-}" ]; then
         pass
     fi
 
-# ==============================================
-# option 1
+    # ==============================================
+    # option 1
 
-# works on Mac but not on Linux, so going back to parsing the hostname and dynamic loading
-# curl 7.64.1 (x86_64-apple-darwin19.0) libcurl/7.64.1 (SecureTransport) LibreSSL/2.8.3 zlib/1.2.11 nghttp2/1.39.2
-# curl 7.35.0 (x86_64-pc-linux-gnu) libcurl/7.35.0 OpenSSL/1.0.1f zlib/1.2.8 libidn/1.28 librtmp/2.3
-if is_curl_min_version 7.64; then
-    netrc_contents="default login $USERNAME password $PASSWORD"
-fi
-
-# ==============================================
-# option 2
-#
-#hosts="$(awk '{print $1}' < ~/.ssh/known_hosts 2>/dev/null | sed 's/,.*//' | sort -u)"
-
-# use built-in echo if availble, cat is slow with ~1000 .ssh/known_hosts
-#if help echo &>/dev/null; then
-#    netrc_contents="$(for host in $hosts; do echo "machine $host login $USERNAME password $PASSWORD"; done)"
-#else
-#    # slow fallback with lots of forks
-#    netrc_contents="$(for host in $hosts; do cat <<< "machine $host login $USERNAME password $PASSWORD"; done)"
-#fi
-
-# ==============================================
-# option 3
-
-# Instead of generating this for all known hosts above just do it for the host extracted from the args url now
-
-if [ -z "${netrc_contents:-}" ]; then
-    if ! [[ "$*" =~ :// ]]; then
-        usage "http(s):// not specified in URL"
+    # works on Mac but not on Linux, so going back to parsing the hostname and dynamic loading
+    # curl 7.64.1 (x86_64-apple-darwin19.0) libcurl/7.64.1 (SecureTransport) LibreSSL/2.8.3 zlib/1.2.11 nghttp2/1.39.2
+    # curl 7.35.0 (x86_64-pc-linux-gnu) libcurl/7.35.0 OpenSSL/1.0.1f zlib/1.2.8 libidn/1.28 librtmp/2.3
+    if [ -z "${TOKEN:-}" ] &&
+       is_curl_min_version 7.64; then
+        netrc_contents="default login $USERNAME password $PASSWORD"
     fi
 
-    # sed https* - because sed on Mac doesn't respect either ? or \? and otherwise I'd have to use:
+    # ==============================================
+    # option 2
     #
-    # perl -pe 's/https?:\/\///'
-    #
-    # or else wrap
-    #
-    # if is_mac; then
-    #       sed(){ gsed "$@" }
-    # fi
-    #
-    host="$(grep -Eom 1 'https?://[^:\/[:space:]]+' <<< "$*" | sed 's,https*://,,' | grep -v localhost | head -n1)"
+    #hosts="$(awk '{print $1}' < ~/.ssh/known_hosts 2>/dev/null | sed 's/,.*//' | sort -u)"
 
-    netrc_contents="machine $host login $USERNAME password $PASSWORD"
+    # use built-in echo if availble, cat is slow with ~1000 .ssh/known_hosts
+    #if help echo &>/dev/null; then
+    #    netrc_contents="$(for host in $hosts; do echo "machine $host login $USERNAME password $PASSWORD"; done)"
+    #else
+    #    # slow fallback with lots of forks
+    #    netrc_contents="$(for host in $hosts; do cat <<< "machine $host login $USERNAME password $PASSWORD"; done)"
+    #fi
+
+    # ==============================================
+    # option 3
+
+    # Instead of generating this for all known hosts above just do it for the host extracted from the args url now
+
+    if [ -z "${TOKEN:-}" ] &&
+       [ -z "${netrc_contents:-}" ]; then
+        if ! [[ "$*" =~ :// ]]; then
+            usage "http(s):// not specified in URL"
+        fi
+
+        # sed https* - because sed on Mac doesn't respect either ? or \? and otherwise I'd have to use:
+        #
+        # perl -pe 's/https?:\/\///'
+        #
+        # or else wrap
+        #
+        # if is_mac; then
+        #       sed(){ gsed "$@" }
+        # fi
+        #
+        host="$(grep -Eom 1 'https?://[^:\/[:space:]]+' <<< "$*" | sed 's,https*://,,' | grep -v localhost | head -n1)"
+
+        netrc_contents="machine $host login $USERNAME password $PASSWORD"
+    fi
 fi
 
 # ==============================================
-
-fi
 
 if [ -n "${KRB5:-${KERBEROS:-}}" ]; then
     command curl -u : --negotiate "$@"
+elif [ -n "${TOKEN:-}" ]; then
+    auth_header="${CURL_AUTH_HEADER:-Authorization: Bearer} $TOKEN"
+    if is_curl_min_version 7.55; then
+        # this trick doesn't work, file descriptor is lost by next line
+        #filedescriptor=<(cat <<< "Private-Token: $GITLAB_TOKEN")
+        command curl -H @<(cat <<< "$auth_header") "$@"
+    else
+        command curl -H "$auth_header" "$@"
+    fi
 else
     command curl --netrc-file <(cat <<< "$netrc_contents") "$@"
 fi
