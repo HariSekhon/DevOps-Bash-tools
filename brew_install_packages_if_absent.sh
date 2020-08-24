3e45c8043727fe188ec73166f008096c34501669
Hari Sekhon <harisekhon@gmail.com>
2020-08-24 01:12:27 +0100

    updated brew_install_packages_if_absent.sh

--- brew_install_packages_if_absent.sh
+++ brew_install_packages_if_absent.sh
@@ -20,69 +20,27 @@ set -eu #o pipefail  # undefined in /bin/sh
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(dirname "${BASH_SOURCE[0]}")"
 
-usage(){
-    echo "Installs Mac Homebrew package lists if the packages aren't already installed"
-    echo
-    echo "Takes a list of yum packages as arguments or via stdin, and for any arguments that are plaintext files, reads the packages from those given files (one package per line)"
-    echo
-    echo "usage: ${0##*} <list_of_packages>"
-    echo
-    exit 3
-}
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
 
-for x in "$@"; do
-    case "$x" in
-        -*) usage
-            ;;
-    esac
-done
+# shellcheck disable=SC1090
+. "$srcdir/lib/packages.sh"
 
-packages=""
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs Mac Homebrew package lists if the packages aren't already installed
 
-process_args(){
-    for arg in "$@"; do
-        if [ -f "$arg" ] && file "$arg" | grep -q ASCII; then
-            echo "adding packages from file:  $arg"
-            packages="$packages $(sed 's/#.*//;/^[[:space:]]*$$/d' "$arg")"
-            echo
-        else
-            packages="$packages $arg"
-        fi
-        if [ -z "${TAP:-}" ]; then
-            # uniq
-            packages="$(echo "$packages" | tr ' ' ' \n' | sort -u | tr '\n' ' ')"
-        fi
-    done
-}
+$package_args_description
 
-if [ -n "${*:-}" ]; then
-    process_args "$@"
-else
-    # shellcheck disable=SC2046
-    process_args $(cat)
-fi
+Tested on CentOS
+"
 
-if [ -n "${TAP:-}" ]; then
-    # convert to array
-    # need splitting
-    # shellcheck disable=SC2206
-    packages_array=($packages)
-    if [ -n "${NO_FAIL:-}" ]; then
-        set +e
-    fi
-    installed_packages="$(brew list)"
-    for((i=0; i < ${#packages_array[@]}; i+=2)); do
-        tap="${packages_array[$i]}"
-        package="${packages_array[(($i+1))]}"
-        grep -Fxq "$package" <<< "$installed_packages" ||
-        echo "$tap $package"
-    done
-else
-    # do not quote cask, blank quotes break shells and there will never be any token splitting anyway
-    # shellcheck disable=SC2046
-    tr ' ' '\n' <<< "${packages[*]}" |
-    grep -vFx -f <(brew $([ -z "${CASK:-}" ] || echo cask) list)
-fi |
-sort -u |
-grep -v '^[[:space:]]*$' |
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<packages>"
+
+help_usage "$@"
+
+process_package_args "$@" |
+"$srcdir/brew_filter_not_installed.sh" |
 gxargs --no-run-if-empty "$srcdir/brew_install_packages.sh"
