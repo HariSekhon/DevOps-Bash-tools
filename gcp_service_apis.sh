0ae3d03731fba86d0fbf8466f76c7005b490e521
Hari Sekhon <harisekhon@gmail.com>
2020-08-26 18:59:15 +0100

    updated gcp_service_apis.sh

--- gcp_service_apis.sh
+++ gcp_service_apis.sh
@@ -22,7 +22,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-List GCP Servies, APIs and their states:
+List GCP Services, APIs and their states:
 
 Output:
 
