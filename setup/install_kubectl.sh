bca52e2e7cc9b13d12f0bde804011431b2f6080d
Hari Sekhon <harisekhon@gmail.com>
2020-08-27 12:14:41 +0100

    added install_kubectl.sh

---
+++ setup/install_kubectl.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-08-27 12:08:44 +0100 (Thu, 27 Aug 2020)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+cd ~/bin
+
+# https://kubernetes.io/docs/tasks/tools/install-kubectl/
+
+#if [ "$(uname -s)" = Darwin ]; then
+#    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl"
+#else
+#    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+#fi
+
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/$(uname -s | tr [:upper:] [:lower:])/amd64/kubectl"
+
+chmod +x kubectl
