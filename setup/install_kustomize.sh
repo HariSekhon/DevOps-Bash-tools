a1bf3343a667255988794166191fb9afb289f895
Hari Sekhon <harisekhon@gmail.com>
2020-08-27 12:10:56 +0100

    added install_kustomize.sh

---
+++ setup/install_kustomize.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-08-27 12:08:44 +0100 (Thu, 27 Aug 2020)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+cd ~/bin
+
+# https://kubernetes-sigs.github.io/kustomize/installation/binaries/
+
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
