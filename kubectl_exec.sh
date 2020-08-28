627e66c88f9a776c784b8ee7801214e0fb1b03ac
Hari Sekhon <harisekhon@gmail.com>
2020-08-28 12:29:59 +0100

    updated kubectl_exec.sh

--- kubectl_exec.sh
+++ kubectl_exec.sh
@@ -22,7 +22,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Kubectl exec's to the first pod in the current or given namespace
+Kubectl exec's to the first pod matching the given filter criteria
 
 Shows the full auto-generated 'kubectl exec' command for clarity
 
