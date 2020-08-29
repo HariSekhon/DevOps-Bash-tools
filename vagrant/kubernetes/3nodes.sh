9fba1df6b4af791701ad5f2b96ca63404816bfa5
Hari Sekhon <harisekhon@gmail.com>
2020-08-29 09:22:29 +0100

    added 3nodes.sh

---
+++ vagrant/kubernetes/3nodes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-08-29 09:19:22 +0100 (Sat, 29 Aug 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$srcdir"
+
+vagrant up master1 worker1 worker2
