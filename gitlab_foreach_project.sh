c3c182c2534be906bf99b1b3d6b6cbf34d286b15
Hari Sekhon <harisekhon@gmail.com>
2020-08-30 10:22:08 +0100

    added gitlab_foreach_project.sh

---
+++ gitlab_foreach_project.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#  args: echo user={user} name={name} repo={project}
+#
+#  Author: Hari Sekhon
+#  Date: 2020-08-30 10:08:07 +0100 (Sun, 30 Aug 2020)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "$0")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Run a command for each original non-fork GitLab project (repo)
+
+All arguments become the command template
+
+The command template replaces the following for convenience in each iteration:
+
+{username}, {user}    =>    your authenticated user
+{name}                =>    the repo / project name without the user prefix
+{project}, {repo}     =>    the repo / project path with the user prefix
+
+eg.
+    ${0##*/} echo user={user} name={name} repo={project}
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<cmd>"
+
+help_usage "$@"
+
+min_args 1 "$@"
+
+cmd_template="$*"
+
+if [ -n "${GITLAB_USER:-}" ]; then
+    user="$GITLAB_USER"
+else
+    # get currently authenticated user
+    user="$(gitlab_api.sh /user | jq -r .username)"
+fi
+
+while read -r name repo; do
+    echo "# ============================================================================ #" >&2
+    echo "# $repo" >&2
+    echo "# ============================================================================ #" >&2
+    cmd="$cmd_template"
+    cmd="${cmd//\{username\}/$user}"
+    cmd="${cmd//\{user\}/$user}"
+    cmd="${cmd//\{project\}/$repo}"
+    cmd="${cmd//\{repo\}/$repo}"
+    cmd="${cmd//\{name\}/$name}"
+    eval "$cmd"
+done < <("$srcdir/gitlab_api.sh" "/users/$user/projects" | jq -r '.[] | [ .path, .path_with_namespace ] | @tsv')
