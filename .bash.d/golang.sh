859c3095aacefd7a04d4ebfa79e7a9edc4821592
Hari Sekhon <harisekhon@gmail.com>
2020-09-02 17:37:50 +0100

    updated golang.sh

--- .bash.d/golang.sh
+++ .bash.d/golang.sh
@@ -21,6 +21,9 @@
 
 bash_tools="${bash_tools:-$(dirname "${BASH_SOURCE[0]}")/..}"
 
+# unreliable that this is set, ensure shell evaluates to the right thing before we use it
+HOME=~
+
 github="${github:-$HOME/github}"
 
 # shellcheck disable=SC1090
