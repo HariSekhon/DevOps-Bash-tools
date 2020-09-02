55a6cf58a2cddc0bd82e427c7b517ec68a0962f0
Hari Sekhon <harisekhon@gmail.com>
2020-09-02 20:05:03 +0100

    updated teamcity_api.sh

--- teamcity_api.sh
+++ teamcity_api.sh
@@ -198,7 +198,7 @@ url_path="${url_path##/}"
 
 # need CURL_OPTS splitting, safer than eval
 # shellcheck disable=SC2086
-#                                  XML by default :-/
+#                                                        XML by default :-/
 "$srcdir/curl_auth.sh" "$url_base/$url_path" -H "Accept: application/json" "$@" $CURL_OPTS
 chmod 0600 "$cookie_jar"
 
