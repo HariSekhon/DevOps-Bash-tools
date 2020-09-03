98d9abf023cb36e58827eefd7393d8f1ac6108c5
Hari Sekhon <harisekhon@gmail.com>
2020-09-03 11:01:10 +0100

    updated gcp.sh

--- lib/gcp.sh
+++ lib/gcp.sh
@@ -17,6 +17,8 @@
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 
+gcp_info_formatting_help="Prints fancy boxes around GCP info in interactive mode. Skips formatting if the output is sent through a pipe to other commands, redirected to file, or \$NO_FORMATTING=1 is set in environment, to allow for easier parsing / grepping"
+
 if is_piped || [ -n "${NO_FORMATTING:-}" ]; then
     formatting=''
 else
@@ -28,15 +30,19 @@ fi
 gcp_info(){
     local title="$1"
     shift || :
-    #perl -e "print '=' x (${#title} + 1);"
-    for ((i=0; i <= ${#title}; i++)); do
-        #printf '='
-        # built-in - not as portable eg. sh, but given we explicitly execute in bash should be ok
-        echo -n '='
-    done
-    echo
-    echo "$title:"
+    if [ -z "$formatting" ]; then
+        #perl -e "print '=' x (${#title} + 1);"
+        for ((i=0; i <= ${#title}; i++)); do
+            #printf '='
+            # built-in - not as portable eg. sh, but given we explicitly execute in bash should be ok
+            echo -n '='
+        done
+        echo
+        echo "$title:"
+    fi
     # better than eval'ing the entire command line to evaluate $title in the formatting string interpolation
     "$@" --format="$(eval echo "$formatting")"
-    echo
+    if [ -z "$formatting" ]; then
+        echo
+    fi
 }
