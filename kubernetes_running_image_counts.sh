8cdad25a95b35a33fa7c6c7ce8de4701b4df2299
Hari Sekhon <harisekhon@gmail.com>
2020-09-09 11:43:16 +0100

    updated kubernetes_running_image_counts.sh

--- kubernetes_running_image_counts.sh
+++ kubernetes_running_image_counts.sh
@@ -22,7 +22,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Lists Kubernetes running docker image counts sorted descending
+Lists Kubernetes docker images running counts sorted descending
 
 Requires kubectl to be in \$PATH and configured
 "
