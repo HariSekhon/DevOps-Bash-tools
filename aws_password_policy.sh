4260ef57714fb9cb162fd0ffb1a6116421582fd5
Hari Sekhon <harisekhon@gmail.com>
2020-09-09 10:02:18 +0100

    updated aws_password_policy.sh

--- aws_password_policy.sh
+++ aws_password_policy.sh
@@ -13,10 +13,13 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Dumps password policy to key = value pairs for easy viewing / grepping
+# Prints password policy in 'key = value' pairs for easy viewing / grepping
 #
-# adjacent script aws_harden_password_policy.sh calls this before and after changing
-# the password policy to be hardened according to the CIS Foundations Benchmark recommendations
+# Adjacent script
+#
+#   aws_harden_password_policy.sh
+#
+# calls this before and after changing the password policy to be hardened according to the CIS Foundations Benchmark recommendations
 
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
