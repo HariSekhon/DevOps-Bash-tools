54c90ca9456bbc78dd78a2a9517f569453793c41
Hari Sekhon <harisekhon@gmail.com>
2020-09-16 09:38:59 +0100

    updated hive_list_databases.sh

--- hive_list_databases.sh
+++ hive_list_databases.sh
@@ -13,24 +13,37 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# List all Hive databases via beeline
-#
-# FILTER environment variable will restrict to matching databases (if giving <db>.<table>, matches up to the first dot)
-#
-# Tested on Hive 1.1.0 on CDH 5.10, 5.16
-
-# For a better version written in Python see DevOps Python tools repo:
-#
-# https://github.com/harisekhon/devops-python-tools
-
-# you will need to comment out / remove '-o pipefail' below to skip errors if you aren't authorized to use
-# any of the databases to avoid the script exiting early upon encountering any authorization error such:
-#
-# ERROR: AuthorizationException: User '<user>@<domain>' does not have privileges to access: default   Default Hive database.*.*
-#
-set -eu -o pipefail
+set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(dirname "$0")"
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+List all Hive databases via beeline
+
+FILTER environment variable will restrict to matching databases (if giving <db>.<table>, matches up to the first dot)
+
+Tested on Hive 1.1.0 on CDH 5.10, 5.16
+
+For a better version written in Python see DevOps Python tools repo:
+
+    https://github.com/harisekhon/devops-python-tools
+
+you will need to comment out / remove '-o pipefail' below to skip errors if you aren't authorized to use
+any of the databases to avoid the script exiting early upon encountering any authorization error such:
+
+ERROR: AuthorizationException: User '<user>@<domain>' does not have privileges to access: default   Default Hive database.*.*
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<beeline_options>]"
+
+help_usage "$@"
+
 
 "$srcdir/beeline.sh" --silent=true --outputformat=tsv2 -e 'SHOW DATABASES' "$@" |
 tail -n +2 |
