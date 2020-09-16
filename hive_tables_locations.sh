f88ac4c07dc4686c0d8cf5d5cd425a42e9de0773
Hari Sekhon <harisekhon@gmail.com>
2020-09-16 09:41:35 +0100

    updated hive_tables_locations.sh

--- hive_tables_locations.sh
+++ hive_tables_locations.sh
@@ -13,21 +13,34 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Show table location for all tables via Impala shell
-#
-# FILTER environment variable will restrict to matching fully qualified tables (<db>.<table>)
-#
-# Tested on Hive 1.1.0 on CDH 5.10, 5.16
-#
-# For more documentation see the comments at the top of beeline.sh
-
-# For a better version written in Python see DevOps Python tools repo:
-#
-# https://github.com/harisekhon/devops-python-tools
-
 # you will almost certainly have to comment out / remove '-o pipefail' to skip authorization errors such as that documented in impala_list_tables.sh
 set -eu  # -o pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(dirname "$0")"
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Show table location for all tables via Impala shell
+
+FILTER environment variable will restrict to matching fully qualified tables (<db>.<table>)
+
+Tested on Hive 1.1.0 on CDH 5.10, 5.16
+
+For more documentation see the comments at the top of beeline.sh
+
+For a better version written in Python see DevOps Python tools repo:
+
+    https://github.com/harisekhon/devops-python-tools
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<beeline_options>]"
+
+help_usage "$@"
+
 
 exec "$srcdir/hive_tables_metadata.sh" Location "$@"
