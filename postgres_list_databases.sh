2e11e41883f64846c4f592a57d3a62d05aeaf4ef
Hari Sekhon <harisekhon@gmail.com>
2020-09-16 09:34:18 +0100

    updated postgres_list_databases.sh

--- postgres_list_databases.sh
+++ postgres_list_databases.sh
@@ -13,15 +13,28 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Lists all PostgreSQL databases using adjacent psql.sh script
-#
-# FILTER environment variable will restrict to matching databases (if giving <db>.<table>, matches up to the first dot)
-#
-# Tested on AWS RDS PostgreSQL 9.5.15
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(dirname "$0")"
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Lists all PostgreSQL databases using adjacent psql.sh script
+
+FILTER environment variable will restrict to matching databases (if giving <db>.<table>, matches up to the first dot)
+
+Tested on AWS RDS PostgreSQL 9.5.15
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<psql_options>]"
+
+help_usage "$@"
+
 
 "$srcdir/psql.sh" -q -t -c 'SELECT DISTINCT(table_catalog) FROM information_schema.tables ORDER BY table_catalog;' "$@" |
 sed 's/^[[:space:]]*//; s/[[:space:]]*$//; /^[[:space:]]*$/d' |
