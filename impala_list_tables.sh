76c6ba46398db995e6ac50d66c1c4cd08ec135f7
Hari Sekhon <harisekhon@gmail.com>
2020-09-16 09:48:31 +0100

    updated impala_list_tables.sh

--- impala_list_tables.sh
+++ impala_list_tables.sh
@@ -13,26 +13,39 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Lists all Impala tables in all databases using adjacent impala_shell.sh script
-#
-# FILTER environment variable will restrict to matching fully qualified tables (<db>.<table>)
-#
-# Tested on Impala 2.7.0, 2.12.0 on CDH 5.10, 5.16 with Kerberos and SSL
-#
-# For more documentation see the comments at the top of impala_shell.sh
-
-# For a better version written in Python see DevOps Python tools repo:
-#
-# https://github.com/harisekhon/devops-python-tools
-
-# you will need to comment out / remove '-o pipefail' below to skip errors if you aren't authorized to use
-# any of the databases to avoid the script exiting early upon encountering any authorization error such:
-#
-# ERROR: AuthorizationException: User '<user>@<domain>' does not have privileges to access: default   Default Hive database.*.*
-#
-set -eu -o pipefail
+set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(dirname "$0")"
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Lists all Impala tables in all databases using adjacent impala_shell.sh script
+
+FILTER environment variable will restrict to matching fully qualified tables (<db>.<table>)
+
+Tested on Impala 2.7.0, 2.12.0 on CDH 5.10, 5.16 with Kerberos and SSL
+
+For more documentation see the comments at the top of impala_shell.sh
+
+For a better version written in Python see DevOps Python tools repo:
+
+    https://github.com/harisekhon/devops-python-tools
+
+you will need to comment out / remove 'set -o pipefail' below to skip errors if you aren't authorized to use
+any of the databases to avoid the script exiting early upon encountering any authorization error such:
+
+ERROR: AuthorizationException: User '<user>@<domain>' does not have privileges to access: default   Default Hive database.*.*
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<impala_shell_options>]"
+
+help_usage "$@"
+
 
 "$srcdir/impala_list_databases.sh" |
 while read -r db; do
