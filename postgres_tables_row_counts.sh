4ffe2adfb94a6de1f25af1d2b9f6ec6b53670bcb
Hari Sekhon <harisekhon@gmail.com>
2020-09-16 09:35:51 +0100

    updated postgres_tables_row_counts.sh

--- postgres_tables_row_counts.sh
+++ postgres_tables_row_counts.sh
@@ -13,19 +13,32 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Counts rows for all PostgreSQL tables in all databases using adjacent psql.sh script
-#
-# TSV Output format:
-#
-# <database>.<schema>.<table>     <row_count>
-#
-# FILTER environment variable will restrict to matching fully qualified tables (<db>.<schema>.<table>)
-#
-# Tested on AWS RDS PostgreSQL 9.5.15
-
-set -eu  # -o pipefail
+set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(dirname "$0")"
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Counts rows for all PostgreSQL tables in all databases using adjacent psql.sh script
+
+TSV Output format:
+
+<database>.<schema>.<table>     <row_count>
+
+FILTER environment variable will restrict to matching fully qualified tables (<db>.<schema>.<table>)
+
+Tested on AWS RDS PostgreSQL 9.5.15
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<psql_options>]"
+
+help_usage "$@"
+
 
 "$srcdir/postgres_foreach_table.sh" "SELECT COUNT(*) FROM {db}.{schema}.{table}" "$@" |
 sed '/^[[:space:]]*$/d'
