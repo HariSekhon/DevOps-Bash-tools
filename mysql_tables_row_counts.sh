0955272f003723ee96a7c218df3b429c1b787f41
Hari Sekhon <harisekhon@gmail.com>
2020-09-16 09:31:05 +0100

    updated mysql_tables_row_counts.sh

--- mysql_tables_row_counts.sh
+++ mysql_tables_row_counts.sh
@@ -13,18 +13,31 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Counts rows for all MySQL tables in all databases using adjacent mysql.sh script
-#
-# TSV Output format:
-#
-# <database>.<table>     <row_count>
-#
-# FILTER environment variable will restrict to matching fully qualified tables (<db>.<table>)
-#
-# Tested on MySQL 8.0.15
-
 set -eu  # -o pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(dirname "$0")"
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Counts rows for all MySQL tables in all databases using adjacent mysql.sh script
+
+TSV Output format:
+
+<database>.<table>     <row_count>
+
+FILTER environment variable will restrict to matching fully qualified tables (<db>.<table>)
+
+Tested on MySQL 8.0.15
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<mysql_options>]"
+
+help_usage "$@"
+
 
 exec "$srcdir/mysql_foreach_table.sh" "SELECT COUNT(*) FROM {db}.{table}" "$@"
