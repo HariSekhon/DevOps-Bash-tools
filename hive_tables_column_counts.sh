5f7ce9acfb194b225f64e4b9f31676390ec76ba9
Hari Sekhon <harisekhon@gmail.com>
2020-09-16 09:41:07 +0100

    updated hive_tables_column_counts.sh

--- hive_tables_column_counts.sh
+++ hive_tables_column_counts.sh
@@ -13,33 +13,35 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Print each table's DDL metadata field eg. Location
-#
-# FILTER environment variable will restrict to matching fully qualified tables (<db>.<table>)
-#
-# Tested on Hive 1.1.0 on CDH 5.10, 5.16
-#
-# For more documentation see the comments at the top of beeline.sh
-
-# For a better version written in Python see DevOps Python tools repo:
-#
-# https://github.com/harisekhon/devops-python-tools
-
 # you will almost certainly have to comment out / remove '-o pipefail' to skip authorization errors such as that documented in impala_list_tables.sh
 set -eu  # -o pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(dirname "$0")"
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
 
-usage(){
-    echo "usage: ${0##*/} [beeline_options]"
-    exit 3
-}
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
 
-for arg; do
-    if [[ "$arg" =~ -h|--help ]]; then
-        usage
-    fi
-done
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Print each table's DDL metadata field eg. Location
+
+FILTER environment variable will restrict to matching fully qualified tables (<db>.<table>)
+
+Tested on Hive 1.1.0 on CDH 5.10, 5.16
+
+For more documentation see the comments at the top of beeline.sh
+
+For a better version written in Python see DevOps Python tools repo:
+
+    https://github.com/harisekhon/devops-python-tools
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<beeline_options>]"
+
+help_usage "$@"
+
 
 query_template="describe {table}"
 
