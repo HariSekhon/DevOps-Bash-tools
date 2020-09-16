f4b886cb17bb73020fc685c3db7e76e333e34853
Hari Sekhon <harisekhon@gmail.com>
2020-09-16 09:34:44 +0100

    updated postgres_list_schemas.sh

--- postgres_list_schemas.sh
+++ postgres_list_schemas.sh
@@ -13,17 +13,30 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Lists all PostgreSQL schemas using adjacent psql.sh script
-#
-# FILTER environment variable will restrict to matching schemas (matches against fully qualified schema name <db>.<schema>)
-#
-# AUTOFILTER if set to any value skips information_schema and pg_catalog schemas
-#
-# Tested on AWS RDS PostgreSQL 9.5.15
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(dirname "$0")"
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Lists all PostgreSQL schemas using adjacent psql.sh script
+
+FILTER environment variable will restrict to matching schemas (matches against fully qualified schema name <db>.<schema>)
+
+AUTOFILTER if set to any value skips information_schema and pg_catalog schemas
+
+Tested on AWS RDS PostgreSQL 9.5.15
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<psql_options>]"
+
+help_usage "$@"
+
 
 "$srcdir/psql.sh" -q -t -c "SELECT DISTINCT table_catalog, table_schema FROM information_schema.tables ORDER BY table_catalog, table_schema;" "$@" |
 sed 's/|//g; s/^[[:space:]]*//; s/[[:space:]]*$//; /^[[:space:]]*$/d' |
