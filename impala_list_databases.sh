f9f45b0808a1b1028856f9e5f19b55050c87e28c
Hari Sekhon <harisekhon@gmail.com>
2020-09-16 09:47:38 +0100

    updated impala_list_databases.sh

--- impala_list_databases.sh
+++ impala_list_databases.sh
@@ -13,21 +13,34 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Lists all Impala databases using adjacent impala_shell.sh script
-#
-# FILTER environment variable will restrict to matching databases (if giving <db>.<table>, matches up to the first dot)
-#
-# Tested on Impala 2.7.0, 2.12.0 on CDH 5.10, 5.16 with Kerberos and SSL
-#
-# For more documentation see the comments at the top of impala_shell.sh
-
-# For a better version written in Python see DevOps Python tools repo:
-#
-# https://github.com/harisekhon/devops-python-tools
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(dirname "$0")"
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Lists all Impala databases using adjacent impala_shell.sh script
+
+FILTER environment variable will restrict to matching databases (if giving <db>.<table>, matches up to the first dot)
+
+Tested on Impala 2.7.0, 2.12.0 on CDH 5.10, 5.16 with Kerberos and SSL
+
+For more documentation see the comments at the top of impala_shell.sh
+
+For a better version written in Python see DevOps Python tools repo:
+
+https://github.com/harisekhon/devops-python-tools
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<impala_shell_options>]"
+
+help_usage "$@"
+
 
 # strip comments after database name, eg.
 # default Default Hive database
