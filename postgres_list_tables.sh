6d0cc5c14d9abb6ffe12256a70fc94603674787b
Hari Sekhon <harisekhon@gmail.com>
2020-09-16 09:35:17 +0100

    updated postgres_list_tables.sh

--- postgres_list_tables.sh
+++ postgres_list_tables.sh
@@ -13,17 +13,30 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Lists all PostgreSQL tables using adjacent psql.sh script
-#
-# FILTER environment variable will restrict to matching tables (matches against fully qualified table name <db>.<schema>.<table>)
-#
-# AUTOFILTER if set to any value skips information_schema and pg_catalog schemas
-#
-# Tested on AWS RDS PostgreSQL 9.5.15
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(dirname "$0")"
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Lists all PostgreSQL tables using adjacent psql.sh script
+
+FILTER environment variable will restrict to matching tables (matches against fully qualified table name <db>.<schema>.<table>)
+
+AUTOFILTER if set to any value skips information_schema and pg_catalog schemas
+
+Tested on AWS RDS PostgreSQL 9.5.15
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<psql_options>]"
+
+help_usage "$@"
+
 
 #"$srcdir/psql.sh" -q -t -c "SELECT table_catalog || '.' || table_schema || '.' || table_name FROM information_schema.tables ORDER BY table_catalog, table_schema, table_name;" "$@" |
 "$srcdir/psql.sh" -q -t -c "SELECT table_catalog, table_schema, table_name FROM information_schema.tables ORDER BY table_catalog, table_schema, table_name;" "$@" |
