6d9182343a0675dc8d01fe5cd511f416c982b104
Hari Sekhon <harisekhon@gmail.com>
2020-09-17 08:36:15 +0100

    added gcr_find_alternate_tags.sh

---
+++ gcr_find_alternate_tags.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-09-15 14:52:47 +0100 (Tue, 15 Sep 2020)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Finds all tags for the given Google Cloud Registry docker image:tag
+
+For example if a container is tagged with a build hashref as well as latest, you can find out what the hashref is by querying for the floating tag 'latest' which gets reassigned to the newest build
+
+Each tag for the given image:tag is output on a separate line for easy further piping and filtering, including the originally supplied tag
+
+If the image isn't found in GCR, will return nothing and no error code since this is the default GCloud SDK behaviour
+
+Requires GCloud SDK to be installed and configured
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<gcr.io>/<project_id>/<image>:<tag>"
+
+help_usage "$@"
+
+exact_args 1 "$@"
+
+image_tag="$1"
+
+if ! [[ "$image_tag" =~ ^([^\.]+\.)?gcr\.io/[^/]+/[^:]+:.+$ ]]; then
+    usage 'unrecognized GCR image name - should be in a format matching this regex: ^([^\.]+\.)?gcr\.io/[^/]+/[^:]+:.+$'
+fi
+
+image="${image_tag%%:*}"
+tag="${image_tag##*:}"
+
+gcloud container images list-tags "$image" --format='csv[no-heading,delimiter="\n"](tags[])' --filter="tags=$tag" # |
+#while read -r tag; do
+#    printf "%s:%s\n" "$image" "$tag"
+#done
