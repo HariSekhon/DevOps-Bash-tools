768cb92c8d54b53bec40d6978c6e5517e11696b8
Hari Sekhon <harisekhon@gmail.com>
2020-09-21 10:46:59 +0100

    updated spotify_liked_tracks.sh

--- spotify_liked_tracks.sh
+++ spotify_liked_tracks.sh
@@ -52,10 +52,14 @@ url_path="/v1/me/tracks?limit=$limit&offset=$offset"
 
 output(){
     # some tracks come out with blank artists and track name, skip these using select(name != "") filter to avoid blank lines
+    # unfortunately some tracks actually do come out with blank artist and track name, this must be a bug inside Spotify, but
+    # filtering it like this throws off the line counts verification and also the track might be blank but the artist might not be
     if not_blank "${SPOTIFY_CSV:-}"; then
-        jq -r '.items[].track | select(.name != "") | [([.artists[].name] | join(", ")), .name] | @csv'
+        #jq -r '.items[].track | select(.name != "") | [([.artists[].name] | join(", ")), .name] | @csv'
+        jq -r '.items[].track | [([.artists[].name] | join(", ")), .name] | @csv'
     else
-        jq -r '.items[].track | select(.name != "") | [([.artists[].name] | join(", ")), "-", .name] | @tsv'
+        #jq -r '.items[].track | select(.name != "") | [([.artists[].name] | join(", ")), "-", .name] | @tsv'
+        jq -r '.items[].track | [([.artists[].name] | join(", ")), "-", .name] | @tsv'
     fi <<< "$output" |
     tr '\t' ' ' |
     sed '
