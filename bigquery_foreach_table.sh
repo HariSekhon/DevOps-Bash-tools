086fa3d23546d86abb4b6bf0f4f9ab92b0156341
Hari Sekhon <harisekhon@gmail.com>
2020-09-25 15:30:54 +0100

    updated bigquery_foreach_table.sh

--- bigquery_foreach_table.sh
+++ bigquery_foreach_table.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #  vim:ts=4:sts=4:sw=4:et
-#  args: bigquery-public-data.github_repos echo "dataset = {dataset}, schema = {schema}, table = {table}"
+#  args: bigquery-public-data.github_repos echo "project = {project}, dataset = {dataset} / schema = {schema}, table = {table}"
 #
 #  Author: Hari Sekhon
 #  Date: 2020-09-16 08:54:54 +0100 (Wed, 16 Sep 2020)
@@ -25,7 +25,7 @@ srcdir="$(dirname "${BASH_SOURCE[0]}")"
 usage_description="
 Execute a command against all Google BigQuery tables in a given dataset in the current project
 
-Command can contain {dataset}, {schema} and {table} placeholders which will be replaced for each table
+Command can contain {project}, {dataset} / {schema} and {table} placeholders which will be replaced for each table
 
 Requires GCloud SDK which must be configured and authorized for the project
 
@@ -50,15 +50,15 @@ command_template="$*"
 trap 'exit 130' INT
 
 "$srcdir/bigquery_list_tables.sh" "$dataset" |
-while read -r dataset schema table; do
+while read -r project dataset table; do
     if [ -z "${NO_HEADING:-}" ]; then
         hr
-        echo "Dataset = $dataset, Schema = $schema, Table = $table"
+        echo "Project = $project, Dataset/Schema = $dataset, Table = $table"
         hr
     fi >&2
-    command_template="${command_template//\{dataset_id\}/$dataset}"
+    command_template="${command_template//\{project\}/$project}"
     command_template="${command_template//\{dataset\}/$dataset}"
-    command_template="${command_template//\{schema\}/$schema}"
+    command_template="${command_template//\{schema\}/$dataset}"
     command_template="${command_template//\{table\}/$table}"
     eval "$command_template"
     if [ -z "${NO_HEADING:-}" ]; then
