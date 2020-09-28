3735d89a209efb83447407b4ed46cd8594c0b342
Hari Sekhon <harisekhon@gmail.com>
2020-09-28 19:00:57 +0100

    updated mac_configuration.sh

--- setup/mac_configuration.sh
+++ setup/mac_configuration.sh
@@ -15,6 +15,97 @@
 
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# ============================================================================ #
+
+# TIP: how to figure out settings keys
+#
+# 1. defaults read > settings.json
+# 2. Make your changes in the UI
+# 3. defaults read > settings2.json
+# 4. diff settings.json settings2.json
+
+# ============================================================================ #
+#                                T r a c k p a d
+# ============================================================================ #
+
+# tap to click
+defaults write com.apple.trackpad forceClick -bool false
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool false
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool false
+
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerTapGesture -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerTapGesture -int 0
+
+# tap to click on login screen
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# OS version on login screen
+defaults write com.apple.loginwindow AdminHostInfo HostName
+
+# Trackpad Speed - choose an int/float in the range 0-3:
+# 0 = slowest
+# 3 = fastest
+defaults write com.apple.trackpad.scaling -float 3
+
+# right-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+# click bottom right corner for right-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+
+# Haptic feedback
+# 0: Light
+# 1: Medium
+# 2: Firm
+defaults write com.apple.AppleMultitouchTrackpad.FirstClickThreshold -int 0
+defaults write com.apple.AppleMultitouchTrackpad.SecondClickThreshold -int 0
+defaults write com.apple.AppleMultitouchTrackpad.ForceSuppressed -int 0
+defaults write com.apple.AppleMultitouchTrackpad ActuationStrength -int 0
+
+# Enable “natural” (Lion-style) scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
+
+# ============================================================================ #
+#                                K e y b o a r d
+# ============================================================================ #
+
+# "Apple Global Domain" === NSGlobalDomain
+defaults write NSGlobalDomain InitialKeyRepeat -int 15;
+defaults write NSGlobalDomain KeyRepeat -int 2;
+defaults write NSGlobalDomain AppleActionOnDoubleClick -string Maximize
+
+# make Fn key show hotkeys in touch bar on newer Macs
+defaults write com.apple.touchbar.agent.PresentationModeFnModes.appWithControlStrip -string fullControlStrip
+
+# ============================================================================ #
+#                                T e r m i n a l
+# ============================================================================ #
+
+# use the custom 'Hari' profile
+defaults write com.apple.Terminal "Default Window Settings" -string "Hari"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Hari"
+
+# ===============
+# open maximized:
+# - MacBook Pro 15" 2013 has 204 x 59
+# - MacBook Pro 15" 2018 has 239 x 72
+# - if you're running a 13" Macbook Pro this will be different again, let's infer the size using the current terminal
+# - $LINES and $COLUMNS aren't automatically available in a non-interactive script shell, so if not set fall back to 'tput'
+#   - tput comes out to 80 cols x 70 lines too conservative
+#     - now instead relying on $COLUMNS and $LINES being exported by shell profile .bash.d/env.sh
+COLUMNS="${COLUMNS:-$(tput cols)}"
+LINES="${LINES:-$(tput lines)}"
+
+# this is a blob and cannot be descended in to using 'defaults' command :'-(
+# so we load from a saved Terminal -> Preferences -> Profiles -> Hari -> Settings -> Export file and override the $LINES and $COLUMNS
+mac_terminal_settings="$(cat "$srcdir/Hari.terminal")"
+mac_terminal_settings="${mac_terminal_settings/<integer>239</<integer>$COLUMNS<}"
+mac_terminal_settings="${mac_terminal_settings/<integer>72</<integer>$LINES<}"
+defaults write com.apple.Terminal "Window Settings" -dict-add Hari "$mac_terminal_settings"
+
+
+# ============================================================================ #
 
 # require password immediately after sleep / screen saver
 defaults write com.apple.screensaver askForPassword -int 1
@@ -27,15 +118,7 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 #defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
 # Disable local Time Machine snapshots
-#sudo tmutil disablelocal
-
-# Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
-# Enable “natural” (Lion-style) scrolling
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
+sudo tmutil disable local
 
 # Increase sound quality for Bluetooth headphones/headsets
 defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
@@ -49,7 +132,8 @@ defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
 # ============================================================================ #
-# Finder:
+#                                  F i n d e r
+# ============================================================================ #
 
 # TODO: set Finder to columns that are auto-wide
 
@@ -61,13 +145,13 @@ defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Down
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
 # show the ~/Library folder
-#chflags nohidden ~/Library
+chflags nohidden ~/Library
 
 # Empty Trash securely by default
 defaults write com.apple.finder EmptyTrashSecurely -bool true
 
 # disable the warning before emptying the Trash
-#defaults write com.apple.finder WarnOnEmptyTrash -bool false
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
 # show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
@@ -89,21 +173,24 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
 # Show icons for hard drives, servers, and removable media on the desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop         -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop     -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop     -bool true
 
 #killall Finder
 
 # ============================================================================ #
-# Preview:
+#                                 P r e v i e w
+# ============================================================================ #
+
 # set to false to not re-open previous documents from last Preview session
 defaults write com.apple.Preview NSQuitAlwaysKeepsWindows -bool true
 
 #killall Preview
 
 # ============================================================================ #
-# Screenshots:
+#                             S c r e e n s h o t s
+# ============================================================================ #
 
 # save screenshots to Desktop
 defaults write com.apple.screencapture location -string "${HOME}/Desktop"
@@ -121,22 +208,27 @@ defaults write com.apple.screencapture disable-shadow -bool true
 # killall SystemUIServer
 
 # ============================================================================ #
-# Dock:
+#                                    D o c k
+# ============================================================================ #
 
-# hide non-active apps
-#defaults write com.apple.dock static-only -bool TRUE
-
-# stop apps saving to iCloud by default
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+# auto-hide dock
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock static-only -bool true
 
 # pop-up instantly
 defaults write com.apple.dock autohide-delay         -float 0  # secs
 # disappear instantly
 defaults write com.apple.dock autohide-time-modifier -float 0
 
-# revert to default delay
+# revert to default dock delays
 #defaults delete com.apple.dock autohide-delay
-#defaults write com.apple.dock autohide-time-modifier
+#defaults delete com.apple.dock autohide-time-modifier
+
+# hide non-active apps
+#defaults write com.apple.dock static-only -bool true
+
+# stop apps saving to iCloud by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 # enable Dashboard with widgets
 #defaults write com.apple.dashboard mcx-disabled -boolean false
@@ -173,9 +265,12 @@ defaults write com.apple.dock autohide-time-modifier -float 0
 #   '{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
 #   '{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
 
+# ============================================================================ #
+
 echo "Some settings won't take effect until you restart Finder. When you're ready, run:
 
 killall Finder
 killall Dock
 killall SystemUIServer
+killall Terminal
 "
