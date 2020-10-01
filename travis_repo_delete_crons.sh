4fc93a10af96f56909717dd42f1b82ecfad6728c
Hari Sekhon <harisekhon@gmail.com>
2020-10-01 10:37:26 +0100

    updated travis_repo_delete_crons.sh

--- travis_repo_delete_crons.sh
+++ travis_repo_delete_crons.sh
@@ -21,24 +21,31 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
 
+# shellcheck disable=SC1090
+. "$srcdir/lib/travis.sh"
+
 # shellcheck disable=SC2034,SC2154
 usage_description="
 Deletes all cron jobs for the given Travis CI repo
+
+If the repo doesn't have a user / organization prefix, then queries
+the Travis CI API for the currently authenticated username first
 
 Uses the adjacent travis_*.sh scripts
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="<repo>"
+usage_args="[<user/]<repo> [<curl_options>]"
 
 help_usage "$@"
 
 min_args 1 "$@"
 
 repo="$1"
+shift || :
 
 timestamp "Deleting all cron jobs for repo '$repo'"
 while read -r cron_id rest; do
-    "$srcdir/travis_delete_cron.sh" "$cron_id"
-done < <("$srcdir/travis_repo_crons.sh" "$repo")
+    "$srcdir/travis_delete_cron.sh" "$cron_id" "$@"
+done < <("$srcdir/travis_repo_crons.sh" "$repo" "$@")
