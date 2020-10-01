95e7e6fb43d699e493c20b5591e12ab158dce515
Hari Sekhon <harisekhon@gmail.com>
2020-10-01 10:38:26 +0100

    updated travis_repos.sh

--- travis_repos.sh
+++ travis_repos.sh
@@ -20,6 +20,9 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
 
+# shellcheck disable=SC1090
+. "$srcdir/lib/travis.sh"
+
 # shellcheck disable=SC2034,SC2154
 usage_description="
 Lists all Travis CI repos using the Travis CI API
@@ -32,7 +35,7 @@ Uses the adjacent travis_api.sh script
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args=""
+usage_args="[<curl_options>]"
 
 help_usage "$@"
 
@@ -40,8 +43,9 @@ next="/repos"
 
 get_repos(){
     local url_path="$1"
+    shift || :
     local output
-    output="$("$srcdir/travis_api.sh" "$url_path")"
+    output="$("$srcdir/travis_api.sh" "$url_path" "$@")"
     jq -r '.repositories[] | select(.active == true) | .slug' <<< "$output"
     next="$(jq -r '.["@pagination"].next["@href"]' <<< "$output")"
 }
@@ -49,5 +53,5 @@ get_repos(){
 # iterate over all next hrefs to get through all pages of repos
 while [ -n "$next" ] &&
       [ "$next" != null ]; do
-    get_repos "$next"
+    get_repos "$next" "$@"
 done
