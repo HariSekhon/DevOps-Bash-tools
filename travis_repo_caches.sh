9a92620937132721a2c8b185db11eb77578e0e07
Hari Sekhon <harisekhon@gmail.com>
2020-10-01 10:32:55 +0100

    updated travis_repo_caches.sh

--- travis_repo_caches.sh
+++ travis_repo_caches.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #  vim:ts=4:sts=4:sw=4:et
+#  args: DevOps-Bash-tools
 #  args: HariSekhon/DevOps-Bash-tools
 #
 #  Author: Hari Sekhon
@@ -21,22 +22,30 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
 
+# shellcheck disable=SC1090
+. "$srcdir/lib/travis.sh"
+
 # shellcheck disable=SC2034,SC2154
 usage_description="
 Shows the caches for a given Travis CI repo in JSON format
+
+If the repo doesn't have a user / organization prefix, then queries
+the Travis CI API for the currently authenticated username first
 
 Uses the adjacent travis_*.sh scripts
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="<repo>"
+usage_args="[<user>/]<repo> [<curl_options>]"
 
 help_usage "$@"
 
 min_args 1 "$@"
 
 repo="$1"
-repo="${repo//\//%2F}"
+shift || :
 
-"$srcdir/travis_api.sh" "/repo/$repo/caches"
+repo="$(travis_prefix_encode_repo "$repo")"
+
+"$srcdir/travis_api.sh" "/repo/$repo/caches" "$@"
