72ef9bab5b43227788c421c8db2e90effa48b4bb
Hari Sekhon <harisekhon@gmail.com>
2020-10-02 10:43:52 +0100

    updated gitlab_add_ssh_public_keys.sh

--- gitlab_add_ssh_public_keys.sh
+++ gitlab_add_ssh_public_keys.sh
@@ -24,7 +24,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Adds the given SSH public key(s) to your GitLab account via the GitLab API
+Adds the given SSH public key(s) to the currently authenticated GitLab account via the GitLab API
 
 If no SSH public key is given, defaults to using ~/.ssh/id_rsa.pub
 
