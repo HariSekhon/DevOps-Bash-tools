42ea16c4fd18747fa54819a0fadf5121981dcfc0
Hari Sekhon <harisekhon@gmail.com>
2020-10-02 11:23:56 +0100

    updated gitlab_get_ssh_public_keys.sh

--- gitlab_get_ssh_public_keys.sh
+++ gitlab_get_ssh_public_keys.sh
@@ -24,7 +24,7 @@ SSH Keys can be found in the Web UI here:
 
     https://gitlab.com/profile/keys
 
-If you get a 404 error it is likely that your \$GITLAB_TOKEN doesn't have the read_api or api (full) permissions
+If you get a 404 error it is likely that your \$GITLAB_TOKEN doesn't have the 'read_api' or 'api' (full) permissions
 You can regenerate a token with the right permissions here:
 
     https://gitlab.com/profile/personal_access_tokens
