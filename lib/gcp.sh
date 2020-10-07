e9762b7a4c903abdc6c9ab274b159c13464f6db0
Hari Sekhon <harisekhon@gmail.com>
2020-10-07 17:41:23 +0100

    updated gcp.sh

--- lib/gcp.sh
+++ lib/gcp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #  vim:ts=4:sts=4:sw=4:et
-#  shellcheck disable=SC1090
+#  shellcheck disable=SC1090,SC2034
 #
 #  Author: Hari Sekhon
 #  Date: 2020-08-13 19:38:39 +0100 (Thu, 13 Aug 2020)
@@ -20,11 +20,11 @@ set -euo pipefail
 gcp_info_formatting_help="Prints fancy boxes around GCP info in interactive mode. Skips formatting if the output is sent through a pipe to other commands, redirected to file, or \$NO_FORMATTING=1 is set in environment, to allow for easier parsing / grepping"
 
 if is_piped || [ -n "${NO_FORMATTING:-}" ]; then
-    formatting=''
+    gcloud_formatting=''
 else
     # want deferred expansion
     # shellcheck disable=SC2016
-    formatting='"[box,title=\"$title\"]"'
+    gcloud_formatting='"[box,title=\"$title\"]"'
 fi
 
 gcr_image_regex='^([^\.]+\.)?gcr\.io/[^/]+/[^:]+$'
@@ -34,7 +34,7 @@ gcr_image_optional_tag_regex='^([^\.]+\.)?gcr\.io/[^/]+/[^:]+(:.+)?$'
 gcp_info(){
     local title="$1"
     shift || :
-    if [ -z "$formatting" ]; then
+    if [ -z "$gcloud_formatting" ]; then
         #perl -e "print '=' x (${#title} + 1);"
         for ((i=0; i <= ${#title}; i++)); do
             #printf '='
@@ -44,9 +44,15 @@ gcp_info(){
         echo
         echo "$title:"
     fi
-    # better than eval'ing the entire command line to evaluate $title in the formatting string interpolation
-    "$@" --format="$(eval echo "$formatting")" || return
-    if [ -z "$formatting" ]; then
-        echo
+    if [[ "$*" =~ --format ]]; then
+        # eval formatting for table only to get late evaluated $title
+        "${@//--format=table(/--format=table$(eval echo "$gcloud_formatting")(}"
+    else
+        # formatting has to be eval'd in order to pick up latest $title as a late binding
+        # better than eval'ing the entire command line to evaluate $title in the formatting string interpolation
+        "$@" --format="$(eval echo "$gcloud_formatting")" || return
+        if [ -z "$gcloud_formatting" ]; then
+            echo
+        fi
     fi
 }
