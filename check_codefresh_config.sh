f13912f4b1165e322ca767b8b0ab0e7abbd99a8b
Hari Sekhon <harisekhon@gmail.com>
2020-10-09 17:10:54 +0100

    updated check_codefresh_config.sh

--- check_codefresh_config.sh
+++ check_codefresh_config.sh
@@ -34,15 +34,19 @@ section "C o d e f r e s h"
 start_time="$(start_timer)"
 
 if type -P codefresh &>/dev/null; then
-    while read -r config; do
-        echo "Validating $config"
-        codefresh validate "$config" || exit $?
-        echo
-    done <<< "$files"
+    if [ -n "${CODEFRESH_KEY:-}" ]; then
+        while read -r config; do
+            echo "Validating $config"
+            codefresh validate "$config" || exit $?
+            echo
+        done <<< "$files"
+        time_taken "$start_time"
+        section2 "Codefresh config checks passed"
+    else
+        echo "\$CODEFRESH_KEY not found in environment, can't validate without API authentication, skipping codefresh config checks"
+    fi
 else
     echo "Codefresh command not found in \$PATH, skipping codefresh config checks"
 fi
 
-time_taken "$start_time"
-section2 "Codefresh config checks passed"
 echo
