2a25b48d1b752d6b1160bbd514917fdfd66002e9
Hari Sekhon <harisekhon@gmail.com>
2020-11-10 19:02:24 +0000

    added k8s race condition protection, explicit namespace, and timestamp logging

--- gcp_secrets_to_kubernetes_multipart.sh
+++ gcp_secrets_to_kubernetes_multipart.sh
@@ -22,29 +22,34 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Loads given list of GCP Secret Manager secrets to a single Kubernetes secret
+Loads a given list of GCP Secret Manager secrets to a single Kubernetes secret
 
 Use the following command to see secrets you may want to combine and load to Kubernetes (eg. jwt-private-pem + jwt-public-pem):
 
     gcloud secrets list
 
-Loads to the current Kubernetes namespace since there is no namespace information in Google Secret Manager, so you may
-want to switch to the right namespace first (see kcd in .bash.d/kubernetes for a convenient way to persist this in your session)
+Loads to the specified explicit Kubernetes namespace since multiple GCP secrets labels might conflict
 
 See Also:
 
     gcp_secrets_to_kubernetes.sh - for linear 1-to-1 secret auto-loading
 
-    kubernetes_get_secret_values.sh - for checking what was auto-loaded into to a given kubernetes secret
+    kubernetes_get_secret_values.sh - for checking what was auto-loaded into a given kubernetes secret
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="<kubernetes_secret_name> <gcp_secret_1> [<gcp_secret_2> ...]"
+usage_args="<kubernetes_secret_name> <kubernetes_namespace> <gcp_secret_1> [<gcp_secret_2> ...]"
 
 help_usage "$@"
 
-min_args 2 "$@"
+min_args 3 "$@"
+
+# fix kube cluster to protect consistency against k8s race conditions
+kubeconfig="/tmp/.kube/config.${EUID:-$UID}.$$"
+mkdir -p "$(dirname "$kubeconfig")"
+cp -f "${KUBECONFIG:-$HOME/.kube/config}" "$kubeconfig"
+export KUBECONFIG="$kubeconfig"
 
 get_latest_version(){
     local secret="$1"
@@ -54,10 +59,12 @@ get_latest_version(){
 }
 
 kubernetes_secret="$1"
+namespace="$2"
+shift || :
 shift || :
 
-if kubectl get secret "$kubernetes_secret" &>/dev/null; then
-    echo "WARNING: kubernetes secret '$kubernetes_secret' already exists, skipping creation..." >&2
+if kubectl get secret "$kubernetes_secret" -n "$namespace" &>/dev/null; then
+    timestamp "kubernetes secret '$kubernetes_secret' already exists in namespace '$namespace', skipping creation..." >&2
     exit 1
 fi
 
@@ -65,18 +72,24 @@ fi
 kubectl_cmd="kubectl create secret generic '$kubernetes_secret' "
 
 for gcp_secret; do
-    latest_version="$(get_latest_version "$gcp_secret")"
-    value="$(gcloud secrets versions access "$latest_version" --secret="$gcp_secret")"
+    latest_secret_version="$(get_latest_version "$gcp_secret")"
+
+    value="$(gcloud secrets versions access "$latest_secret_version" --secret="$gcp_secret")"
+
     # this is all really annoying because there is no right answer to this convention
     # because GCP Secret Manager won't let you use dots so mangle jwt-private-pem => jwt-private.pem
     #gcp_secret="$(perl -pe 's/[_-]pem$/.pem/' <<< "$gcp_secret")"
     # actually apps expect just private.pem and public.pem in the secret mounted directory
     if [[ "$gcp_secret" =~ -private-pem$ ]]; then
-        gcp_secret="private.pem"
+        key="private.pem"
     elif [[ "$gcp_secret" =~ -public-pem$ ]]; then
-        gcp_secret="public.pem"
+        key="public.pem"
+    else
+        key="$gcp_secret"
     fi
-    kubectl_cmd+=" --from-literal='$gcp_secret=$value'"
+
+    kubectl_cmd+=" --from-literal='$key=$value'"
 done
 
+timestamp "creating kubernetes secret '$kubernetes_secret' in namespace '$namespace' from GCP secrets: $*"
 eval "$kubectl_cmd"
