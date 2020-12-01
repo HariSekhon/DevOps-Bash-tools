4eb532e747a488e181def4fec0f3ab913dd8b8cf
Hari Sekhon <harisekhon@gmail.com>
2020-12-01 19:20:14 +0000

    updated teamcity_api.sh

--- teamcity_api.sh
+++ teamcity_api.sh
@@ -151,24 +151,7 @@ shift || :
 
 url_path="${url_path##/}"
 
-# arrays can't be exported so have to pass as a string and then split to array
-if [ -n "${CURL_OPTS:-}" ]; then
-    read -r -a CURL_OPTS <<< "$CURL_OPTS"
-else
-    read -r -a CURL_OPTS <<< "-sS --fail --connect-timeout 3"
-fi
-
-# case insensitive regex matching
-shopt -s nocasematch
-# XML by default :-/
-if ! [[ "$*" =~ Accept: ]]; then
-    CURL_OPTS+=(-H "Accept: application/json")
-fi
-if ! [[ "$*" =~ Content-Type: ]]; then
-    CURL_OPTS+=(-H "Content-Type: application/json")
-fi
-# unset to return to default setting for safety to avoid hard to debug changes of behaviour elsewhere
-shopt -u nocasematch
+curl_api_opts
 
 # don't enforce as hard requirements here, instead try alternation further down and construct from what's available
 #check_env_defined "TEAMCITY_URL"
