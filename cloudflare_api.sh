5c46cc9bfb9ea7f3d45bed3198503937ce8f4c00
Hari Sekhon <harisekhon@gmail.com>
2020-12-01 19:47:20 +0000

    updated cloudflare_api.sh

--- cloudflare_api.sh
+++ cloudflare_api.sh
@@ -29,121 +29,121 @@ Queries the Cloudflare API (v4)
 
 Requires \$CLOUDFLARE_TOKEN be available in the environment, generation a token here:
 
-https://dash.cloudflare.com/profile/api-tokens
+    https://dash.cloudflare.com/profile/api-tokens
 
 
 API Reference:
 
-https://api.cloudflare.com/
+    https://api.cloudflare.com/
 
 
 Examples:
 
 
-Test your token is working:
+# Test your token is working:
 
-${0##*/} /user/tokens/verify | jq .
-
-
-List the currently authenticated user:
-
-${0##*/} /user | jq .
+    ${0##*/} /user/tokens/verify | jq .
 
 
-List accounts:
+# List the currently authenticated user:
 
-${0##*/} /accounts | jq .
-
-
-List currently authenticated user's account memberships and permissions:
-
-${0##*/} /memberships | jq .
+    ${0##*/} /user | jq .
 
 
-List all zones:
+# List accounts:
 
-${0##*/} /zones | jq .
-
-
-List all DNS records in a given zone (see cloudflare_dns_records.sh / cloudflare_dns_records_all_zones.sh):
-
-${0##*/} /zones/<zone_id>/dns_records
+    ${0##*/} /accounts | jq .
 
 
-Export your DNS records in Bind config format:
+# List currently authenticated user's account memberships and permissions:
 
-${0##*/} /zones/<zone_id>/dns_records/export
-
-
-DNS analytics reports:
-
-${0##*/} /zones/<zone_id>/dns_analytics/report
-${0##*/} /zones/<zone_id>/dns_analytics/report/bytime
+    ${0##*/} /memberships | jq .
 
 
-Details about DNSSEC status and configuration (see cloudflare_dnssec.sh for status across all zones)
+# List all zones:
 
-${0##*/} zones/<zone_id>/dnssec
-
-
-List DNS Firewall clusters for an account:
-
-${0##*/} /accounts/<account_id>/virtual_dns
+    ${0##*/} /zones | jq .
 
 
-List the IPv4 and IPv6 cidr ranges for Cloudflare (see cloudflare_cidr_ranges.sh for a ready parsed example of this):
+# List all DNS records in a given zone (see cloudflare_dns_records.sh / cloudflare_dns_records_all_zones.sh):
 
-${0##*/} /ips | jq .
-
-
-List custom certificates (.result.status and .result.expires_on fields may be of interest):
-
-${0##*/} /zones/<zone_id>/custom_certificates
+    ${0##*/} /zones/<zone_id>/dns_records
 
 
-Gets Cloudflare zone SSL verification status for a given zone (see cloudflare_ssl_verified.sh):
+# Export your DNS records in Bind config format:
 
-${0##*/} /zones/<zone_id>/ssl/verification
-
-
-Get Firewall Rules for a zone:
-
-${0##*/} /zones/<zone_id>/firewall/rules
+    ${0##*/} /zones/<zone_id>/dns_records/export
 
 
-Get Firewall Security Events for a zone:
+# DNS analytics reports:
 
-${0##*/} /zones/<zone_id>/security/events
-
-
-List account rules lists:
-
-${0##*/}  /accounts/<account_id>/rules/lists
+    ${0##*/} /zones/<zone_id>/dns_analytics/report
+    ${0##*/} /zones/<zone_id>/dns_analytics/report/bytime
 
 
-List load balancer pools:
+# Details about DNSSEC status and configuration (see cloudflare_dnssec.sh for status across all zones)
 
-${0##*/} /user/load_balancers/pools
-
-
-List load balancer monitors:
-
-${0##*/} /user/load_balancers/monitors
+    ${0##*/} zones/<zone_id>/dnssec
 
 
-List load balancers for a zone:
+# List DNS Firewall clusters for an account:
 
-${0##*/}  /zones/<zone_id>/load_balancers
-
-
-Get all cidr ranges owned by the account:
-
-${0##*/} /accounts/<account_id>/addressing/prefixes
+    ${0##*/} /accounts/<account_id>/virtual_dns
 
 
-List healthchecks for a zone:
+# List the IPv4 and IPv6 cidr ranges for Cloudflare (see cloudflare_cidr_ranges.sh for a ready parsed example of this):
 
-${0##*/} /zones/<zone_id>/healthchecks
+    ${0##*/} /ips | jq .
+
+
+# List custom certificates (.result.status and .result.expires_on fields may be of interest):
+
+    ${0##*/} /zones/<zone_id>/custom_certificates
+
+
+# Gets Cloudflare zone SSL verification status for a given zone (see cloudflare_ssl_verified.sh):
+
+    ${0##*/} /zones/<zone_id>/ssl/verification
+
+
+# Get Firewall Rules for a zone:
+
+    ${0##*/} /zones/<zone_id>/firewall/rules
+
+
+# Get Firewall Security Events for a zone:
+
+    ${0##*/} /zones/<zone_id>/security/events
+
+
+# List account rules lists:
+
+    ${0##*/}  /accounts/<account_id>/rules/lists
+
+
+# List load balancer pools:
+
+    ${0##*/} /user/load_balancers/pools
+
+
+# List load balancer monitors:
+
+    ${0##*/} /user/load_balancers/monitors
+
+
+# List load balancers for a zone:
+
+    ${0##*/}  /zones/<zone_id>/load_balancers
+
+
+# Get all cidr ranges owned by the account:
+
+    ${0##*/} /accounts/<account_id>/addressing/prefixes
+
+
+# List healthchecks for a zone:
+
+    ${0##*/} /zones/<zone_id>/healthchecks
 "
 
 # used by usage() in lib/utils.sh
@@ -167,6 +167,4 @@ url_path="${url_path##/}"
 
 export TOKEN="$CLOUDFLARE_TOKEN"
 
-# need CURL_OPTS splitting, safer than eval
-# shellcheck disable=SC2086
 "$srcdir/curl_auth.sh" "$url_base/$url_path" "${CURL_OPTS[@]}" "$@"
