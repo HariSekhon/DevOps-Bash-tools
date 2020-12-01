e3b93a982f092257119727f282d64c059fda35a2
Hari Sekhon <harisekhon@gmail.com>
2020-12-01 17:20:22 +0000

    updated teamcity_projects_download.sh

--- teamcity_projects_download.sh
+++ teamcity_projects_download.sh
@@ -31,30 +31,36 @@ If arguments are specified then only downloads those named Projects, otherwise f
 Uses the adjacent teamcity_api.sh and jq (installed by 'make')
 
 See teamcity_api.sh for required connection settings and authentication
+
+See teamcity_projects.sh for the list of projects and their IDs vs Names
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="[<project1> <project2> ...]"
+usage_args="[<project_id1> <project_id2> ...]"
 
 help_usage "$@"
 
 #min_args 1 "$@"
 
 if [ $# -gt 0 ]; then
-    for project_name in "$@"; do
-        echo "$project_name"
+    for project_id in "$@"; do
+        echo "$project_id"
     done
 else
     "$srcdir/teamcity_api.sh" /projects |
-    jq -r '.project[].name'
+    jq -r '.project[] | [.id, .name] | @tsv'
 fi |
 grep -v '^[[:space:]]*$' |
-while read -r project_name; do
-    filename="$project_name.json"
+while read -r project_id project_name; do
+    # basing the filename off the ID instead of the Name is because it's more suitable for filenames
+    # instead of '<Root>.json', '_Root.json' is safer and easier to use in daily practice
+    filename="$project_id.json"
+    project_name="${project_name:-$project_id}"
     timestamp "downloading project '$project_name' to '$filename'"
-    project_name="$("$srcdir/urlencode.sh" <<< "$project_name")"
-    "$srcdir/teamcity_api.sh" "/projects/$project_name" |
+    #project_name="$("$srcdir/urlencode.sh" <<< "$project_name")"
+    #"$srcdir/teamcity_api.sh" "/projects/$project_name" |
+    "$srcdir/teamcity_api.sh" "/projects/$project_id" |
     # using jq just for formatting
     jq . > "$filename" || :  # some projects get 400 errors, ignore these
 done
