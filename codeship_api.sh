d048a769b0ed8bf80a7e64a97c14dfa0dc0f51da
Hari Sekhon <harisekhon@gmail.com>
2020-12-01 19:22:01 +0000

    updated codeship_api.sh

--- codeship_api.sh
+++ codeship_api.sh
@@ -131,12 +131,7 @@ if [ -n "${repo:-}" ]; then
     url_path="${url_path/<repo>/$repo}"
 fi
 
-# arrays can't be exported so have to pass as a string and then split to array
-if [ -n "${CURL_OPTS:-}" ]; then
-    read -r -a CURL_OPTS <<< "$CURL_OPTS"
-else
-    read -r -a CURL_OPTS <<< "-sS --fail --connect-timeout 3"
-fi
+curl_api_opts
 
 # case insensitive regex matching
 shopt -s nocasematch
