f8b322ee0a36713e1251e607b5faa3eacde76303
Hari Sekhon <harisekhon@gmail.com>
2020-12-11 14:16:30 +0000

    updated aws_users.sh

--- aws_users.sh
+++ aws_users.sh
@@ -17,6 +17,25 @@
 
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Lists AWS users
+
+
+$usage_aws_cli_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
 
 #aws iam list-users | jq -r '.Users[].UserName'
 aws iam list-users --query 'Users[*].UserName' --output text | tr '[:space:]' '\n'
