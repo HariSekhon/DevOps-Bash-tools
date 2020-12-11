f8357c9bbd3d11332c377d6580796d2b456cfb70
Hari Sekhon <harisekhon@gmail.com>
2020-12-11 14:18:16 +0000

    updated aws_users_access_key_age_report.sh

--- aws_users_access_key_age_report.sh
+++ aws_users_access_key_age_report.sh
@@ -13,37 +13,53 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Prints users access key status and age using a credential report (faster for many users)
-#
-# CSV Output format:
-#
-# user,access_key_1_active,access_key_1_last_rotated,access_key_2_active,access_key_2_last_rotated
-#
-#
-# See also:
-#
-# aws_users_access_key_age.sh
-#
-#
-# aws_users_access_key_age.py - in DevOps Python Tools which is able to filter by age and status
-#
-# https://github.com/harisekhon/devops-python-tools
-#
-#
-# awless list accesskeys --format tsv | grep 'years[[:space:]]*$'
-#
-#
-# AWS Config rule compliance:
-#
-# https://<region>.console.aws.amazon.com/config/home?region=<region>&v2=true#/rules/details?configRuleName=access-keys-rotated
-#
-# eg.
-#
-# https://eu-west-1.console.aws.amazon.com/config/home?region=eu-west-1&v2=true#/rules/details?configRuleName=access-keys-rotated
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(dirname "${BASH_SOURCE[0]}")"
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Prints users access key status and age using a credential report (faster for many users)
+
+CSV Output format:
+
+user,access_key_1_active,access_key_1_last_rotated,access_key_2_active,access_key_2_last_rotated
+
+
+See Also:
+
+    aws_users_access_key_age.sh
+
+
+    aws_users_access_key_age.py - in DevOps Python Tools which is able to filter by age and status
+
+    https://github.com/harisekhon/devops-python-tools
+
+
+    awless list accesskeys --format tsv | grep 'years[[:space:]]*$'
+
+
+AWS Config rule compliance:
+
+    https://<region>.console.aws.amazon.com/config/home?region=<region>&v2=true#/rules/details?configRuleName=access-keys-rotated
+
+eg.
+
+    https://eu-west-1.console.aws.amazon.com/config/home?region=eu-west-1&v2=true#/rules/details?configRuleName=access-keys-rotated
+
+
+$usage_aws_cli_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
 
 "$srcdir/aws_iam_generate_credentials_report_wait.sh" >&2
 
