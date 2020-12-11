448fb26cbf7078a75ec639f58a5d761578570cb0
Hari Sekhon <harisekhon@gmail.com>
2020-12-11 14:00:20 +0000

    updated aws_cloudtrails_s3_accesslogging.sh

--- aws_cloudtrails_s3_accesslogging.sh
+++ aws_cloudtrails_s3_accesslogging.sh
@@ -13,18 +13,37 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Lists Cloud Trails buckets and their Access Logging prefix and target bucket
-#
-# Output Format:
-#
-# CloudTrail_S3_Bucket      TargetPrefix    TargetBucket
-#
-# If access logging isn't configured on the bucket, outputs:
-#
-# CloudTrail_S3_Bucket      S3_ACCESS_LOGGING_NOT_CONFIGURED
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Lists Cloud Trails buckets and their Access Logging prefix and target bucket
+
+Output Format:
+
+CloudTrail_S3_Bucket      TargetPrefix    TargetBucket
+
+If access logging isn't configured on the bucket, outputs:
+
+CloudTrail_S3_Bucket      S3_ACCESS_LOGGING_NOT_CONFIGURED
+
+
+$usage_aws_cli_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+#min_args 1 "$@"
+
 
 aws cloudtrail describe-trails --query 'trailList[*].S3BucketName' |
 jq -r '.[]' |
