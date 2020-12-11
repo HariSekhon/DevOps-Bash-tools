5e3b42c1df8e8ed543c30b0d593428116b812d76
Hari Sekhon <harisekhon@gmail.com>
2020-12-11 14:03:08 +0000

    updated aws_config_all_types.sh

--- aws_config_all_types.sh
+++ aws_config_all_types.sh
@@ -13,14 +13,31 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Lists AWS Config recorders, checking all resource types are supported (should be true) and includes global resources (should be true)
-#
-# eg.
-#
-# awsconfig  true  true
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Lists AWS Config recorders, checking all resource types are supported (should be true) and includes global resources (should be true)
+
+eg.
+
+awsconfig  true  true
+
+
+$usage_aws_cli_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
 
 aws configservice describe-configuration-recorders |
 jq -r '.ConfigurationRecorders[] | [.name, .recordingGroup.allSupported, .recordingGroup.includeGlobalResourceTypes] | @tsv' |
