8b7c847e4bf58d498d471abd1bacd147a9fdfa3d
Hari Sekhon <harisekhon@gmail.com>
2020-12-11 14:22:12 +0000

    updated aws_users_mfa_serials.sh

--- aws_users_mfa_serials.sh
+++ aws_users_mfa_serials.sh
@@ -13,14 +13,31 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Prints users MFAs serial numbers to differentiate Virtual vs Hardware MFAs
-#
-# Virtual MFAs have a SerialNumber in the format:
-#
-# arn:aws:iam::<account_id>:mfa/<mfa>
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Prints users MFAs serial numbers to differentiate Virtual vs Hardware MFAs
+
+Virtual MFAs have a SerialNumber in the format:
+
+arn:aws:iam::<account_id>:mfa/<mfa>
+
+
+$usage_aws_cli_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
 
 aws iam list-virtual-mfa-devices |
 jq -r '.VirtualMFADevices[] | [.User.UserName, .SerialNumber] | @tsv' |
