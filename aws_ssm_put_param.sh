7ea92fe1bc271eaa876acbbc0e29053ece8467b6
Hari Sekhon <harisekhon@gmail.com>
2020-12-11 14:15:59 +0000

    updated aws_ssm_put_param.sh

--- aws_ssm_put_param.sh
+++ aws_ssm_put_param.sh
@@ -13,16 +13,32 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Reads a value from the command line and saves it to AWS Systems Manager Parameter Store
-#
-# usage: aws_ssm_put_param.sh [<key>] [<value>]
-#
-# first argument is used as key - if not given prompts for it
-# second argument is used as value - if not given prompts for it (recommended for secrets)
-#
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Reads a value from the command line and saves it to AWS Systems Manager Parameter Store
+
+usage: aws_ssm_put_param.sh [<key>] [<value>]
+
+first argument is used as key - if not given prompts for it
+second argument is used as value - if not given prompts for it (recommended for secrets)
+
+
+$usage_aws_cli_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
 
 key="${1:-}"
 value="${2:-}"
