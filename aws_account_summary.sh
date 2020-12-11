172fa2ce2b36d9225ebfef849fa479f12039d44b
Hari Sekhon <harisekhon@gmail.com>
2020-12-11 13:57:40 +0000

    updated aws_account_summary.sh

--- aws_account_summary.sh
+++ aws_account_summary.sh
@@ -13,27 +13,46 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Prints AWS account summary in 'key = value' pairs for easy viewing / grepping
-#
-# Useful information in here is whether the root account has MFA enabled and no access keys:
-#
-# AccountAccessKeysPresent = 0
-# AccountMFAEnabled = 1
-#
-# or comparing number of users to number of MFA devices eg.
-#
-# MFADevices = 6
-# MFADevicesInUse = 6
-# ...
-# Users = 14
-#
-# See Also:
-#
-#   aws_users_mfa_active_report.sh (adjacent)
-#   check_aws_root_account.py   -   in The Advanced Nagios Plugins collection (https://github.com/harisekhon/nagios-plugins)
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Prints AWS account summary in 'key = value' pairs for easy viewing / grepping
+
+Useful information in here is whether the root account has MFA enabled and no access keys:
+
+AccountAccessKeysPresent = 0
+AccountMFAEnabled = 1
+
+or comparing number of users to number of MFA devices eg.
+
+MFADevices = 6
+MFADevicesInUse = 6
+...
+Users = 14
+
+See Also:
+
+    aws_users_mfa_active_report.sh (adjacent)
+    check_aws_root_account.py   -   in The Advanced Nagios Plugins collection (https://github.com/harisekhon/nagios-plugins)
+
+
+$usage_aws_cli_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+#min_args 1 "$@"
+
 
 aws iam get-account-summary |
 jq -r '.SummaryMap | to_entries | map(.key + " = " + (.value | tostring)) | .[]' |
