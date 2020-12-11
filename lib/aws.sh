98ae409cedcdc0d7b936d3c1dbac2544c8e39900
Hari Sekhon <harisekhon@gmail.com>
2020-12-11 13:56:05 +0000

    added aws.sh

---
+++ lib/aws.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-12-11 13:53:11 +0000 (Fri, 11 Dec 2020)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/utils.sh"
+
+# used in client scripts
+# shellcheck disable=SC2034
+usage_aws_cli_required="AWS CLI is required to be installed and configured, as well as jq  (run 'make aws && aws configure')"
