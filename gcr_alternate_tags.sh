8e618cc52654e2d92dfead3b1907297a8382df05
Hari Sekhon <harisekhon@gmail.com>
2020-12-14 12:27:31 +0000

    updated gcr_alternate_tags.sh

--- gcr_alternate_tags.sh
+++ gcr_alternate_tags.sh
@@ -56,8 +56,8 @@ fi
 
 # $gcr_image_tag_regex is defined in lib/gcp.sh
 # shellcheck disable=SC2154
-if ! [[ "$image_tag" =~ $gcr_image_regex|$gcr_image_tag_regex ]]; then
-    usage "unrecognized GCR image name - should be in a format matching this regex: $gcr_image_regex or $gcr_image_tag_regex"
+if ! [[ "$image_tag" =~ ^($gcr_image_regex|$gcr_image_tag_regex)$ ]]; then
+    usage "unrecognized GCR image name - should be in a format matching this regex: ^$gcr_image_regex$ or ^$gcr_image_tag_regex$"
 fi
 
 image="${image_tag%%:*}"
