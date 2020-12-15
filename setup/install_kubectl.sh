748153644031fe700236a380e21266fec6ff2c44
Hari Sekhon <harisekhon@gmail.com>
2020-12-15 14:32:02 +0000

    added cert-manager plugin

--- setup/install_kubectl.sh
+++ setup/install_kubectl.sh
@@ -16,8 +16,13 @@
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 
+cert_manager_version="1.1.0"
+
+platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+
 cd /tmp
 
+# =======================================================
 # https://kubernetes.io/docs/tasks/tools/install-kubectl/
 
 #if [ "$(uname -s)" = Darwin ]; then
@@ -27,14 +32,31 @@ cd /tmp
 #fi
 
 date "+%F %T  downloading kubectl"
-curl -sSLO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/$(uname -s | tr '[:upper:]' '[:lower:]')/amd64/kubectl"
+curl -sSLO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/$platform/amd64/kubectl"
 
 date "+%F %T  downloaded kubectl"
 date "+%F %T  chmod'ing and moving to ~/bin"
 chmod +x kubectl
-
 mkdir -pv ~/bin
 unalias mv &>/dev/null || :
 mv -vf kubectl ~/bin/
-
+echo
 ~/bin/kubectl version --client
+
+
+# =======================================================
+# https://cert-manager.io/docs/usage/kubectl-plugin/
+
+echo
+echo
+kubectl_cert_manager="kubectl-cert_manager"
+date "+%F %T  downloading kubectl cert-manager plugin"
+curl -sSLo kubectl-cert-manager.tar.gz "https://github.com/jetstack/cert-manager/releases/download/v$cert_manager_version/kubectl-cert_manager-$platform-amd64.tar.gz"
+date "+%F %T  downloaded kubectl cert-manager plugin"
+tar xzf kubectl-cert-manager.tar.gz
+date "+%F %T  chmod'ing and moving to ~/bin"
+chmod +x "$kubectl_cert_manager"
+mv -vf "$kubectl_cert_manager" ~/bin/
+
+echo
+~/bin/"$kubectl_cert_manager" version
