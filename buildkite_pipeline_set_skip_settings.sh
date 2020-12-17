5db9e818c72a5ad459c8489a3608b6e07405317d
Hari Sekhon <harisekhon@gmail.com>
2020-12-17 15:24:57 +0000

    updated buildkite_pipeline_set_skip_settings.sh

--- buildkite_pipeline_set_skip_settings.sh
+++ buildkite_pipeline_set_skip_settings.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 #  vim:ts=4:sts=4:sw=4:et
-#  args: devops-bash-tools
 #
 #  Author: Hari Sekhon
 #  Date: 2020-12-17 15:07:11 +0000 (Thu, 17 Dec 2020)
@@ -23,20 +22,32 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Sets a BuildKite pipeline to skip intermediate queued builds and also cancel running builds in favour of the latest build
+Sets given BuildKite pipelines to skip intermediate queued builds and also cancel running builds in favour of the latest build
+
+If not pipelines are given, iterates over all pipelines
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="<pipeline>"
+usage_args="[<pipeline>]"
 
 help_usage "$@"
 
-min_args 1 "$@"
+set_pipeline_settings(){
+    local pipeline="$1"
+    echo "Setting pipeline '$pipeline' to skip intermediate queued builds and cancel intermediate running builds"
+    "$srcdir/buildkite_api.sh" "/organizations/{organization}/pipelines/$pipeline" -X PATCH -d '{ "skip_queued_branch_builds": true, "cancel_running_branch_builds": true }' >/dev/null
+    echo
+    "$srcdir/buildkite_pipeline_skip_settings.sh" "$pipeline"
+    echo
+}
 
-pipeline="${1:-}"
-
-echo "Setting pipeline '$pipeline' to skip intermediate queued builds and cancel intermediate running builds"
-"$srcdir/buildkite_api.sh" "/organizations/{organization}/pipelines/$pipeline" -X PATCH -d '{ "skip_queued_branch_builds": true, "cancel_running_branch_builds": true }' >/dev/null
-echo
-"$srcdir/buildkite_pipeline_skip_settings.sh" "$pipeline"
+if [ $# -gt 1 ]; then
+    for pipeline in "$@"; do
+        set_pipeline_settings "$pipeline"
+    done
+else
+    for pipeline in $("$srcdir/buildkite_pipelines.sh"); do
+        set_pipeline_settings "$pipeline"
+    done
+fi
