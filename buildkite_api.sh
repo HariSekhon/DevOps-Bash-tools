06ec48ada5b2a7d192347bee4e19380646fa514d
Hari Sekhon <harisekhon@gmail.com>
2020-12-17 14:19:14 +0000

    added organization and pipeline replacement support, folding lib/buildkite.sh in to buildkite_api.sh

--- buildkite_api.sh
+++ buildkite_api.sh
@@ -29,23 +29,29 @@ https://buildkite.com/docs/apis/rest-api
 
 Examples:
 
-    ${0##*/} user | jq
+    ${0##*/} /user | jq .
 
-    ${0##*/} organizations | jq
+    ${0##*/} /organizations | jq .
 
-    ${0##*/} organizations/hari-sekhon/pipelines | jq
+    ${0##*/} /organizations/{organization}/pipelines | jq .
 
-    ${0##*/} organizations/hari-sekhon/pipelines/devops-bash-tools | jq
+    ${0##*/} /organizations/{organization}/pipelines/{pipeline} | jq .
 
-    ${0##*/} builds | jq
+    ${0##*/} /builds | jq .
 
-    ${0##*/} organizations/hari-sekhon/builds | jq
+    ${0##*/} /organizations/{organization}/builds | jq .
 
-    ${0##*/} organizations/hari-sekhon/pipelines/devops-bash-tools/builds/<num> | jq
+    ${0##*/} /organizations/{organization}/pipelines/{pipeline}/builds/<num> | jq .
 
-    ${0##*/} organizations/hari-sekhon/agents | jq
+    ${0##*/} /organizations/{organization}/agents | jq .
 
-    ${0##*/} organizations/hari-sekhon/emojis | jq
+    ${0##*/} /organizations/{organization}/emojis | jq .
+
+
+Replacements:
+
+{organization}    \$BUILDKITE_ORGANIZATION, \$BUILDKITE_USER, or queries /organizations (if more than one org is found you must set \$BUILDKITE_ORGANIZATION to avoid ambiguity)
+{pipeline}        \$BUILDKITE_PIPELINE
 "
 
 # used by usage() in lib/utils.sh
@@ -73,5 +79,28 @@ url_path="${url_path#$url_base}"
 url_path="${url_path##/}"
 
 export TOKEN="$BUILDKITE_TOKEN"
+
+# remember to set this eg. BUILDKITE_ORGANIZATION="{organization}"
+BUILDKITE_ORGANIZATION="${BUILDKITE_ORGANIZATION:-${BUILDKITE_USER:-}}"
+
+if [[ "$url_path" =~ {organization} ]]; then
+    organizations="$("$srcdir/curl_auth.sh" -sS --fail "$url_base/organizations" | jq -r '.[].slug')"
+    num_organizations="$(wc -w <<< "$organizations" | sed 's/[[:space:]]//g')"
+    if [ "$num_organizations" -eq 0 ]; then
+        usage "\$BUILDKITE_ORGANIZATION / \$BUILDKITE_USER not set and could not find any organizations for the \$BUILDKITE_TOKEN"
+    elif [ "$num_organizations" -eq 1 ]; then
+        BUILDKITE_ORGANIZATION="$organizations"
+        url_path="${url_path//\{organization\}/$BUILDKITE_ORGANIZATION}"
+    elif [ "$num_organizations" -gt 1 ]; then
+        usage "\$BUILDKITE_ORGANIZATION / \$BUILDKITE_USER not set and found more than 1 organization registered to the \$BUILDKITE_TOKEN - must specify \$BUILDKITE_ORGANIZATION to avoid ambiguity"
+    fi
+fi
+
+if [[ "$url_path" =~ {pipeline} ]]; then
+    if [ -z "\$BUILDKITE_PIPELINE" ]; then
+        usage "\$BUILDKITE_PIPELINE is not set, cannot do replacement of {pipeline} given in the url path"
+    fi
+    url_path="${url_path//\{pipeline\}/$BUILDKITE_PIPELINE}"
+fi
 
 "$srcdir/curl_auth.sh" -sS --fail "$url_base/$url_path" "$@"
