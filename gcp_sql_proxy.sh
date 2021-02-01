ef10fd0e3f4bf8d0f574bb68ab65275195696466
Hari Sekhon <harisekhon@gmail.com>
2021-02-01 17:05:47 +0000

    updated gcp_sql_proxy.sh

--- gcp_sql_proxy.sh
+++ gcp_sql_proxy.sh
@@ -20,6 +20,8 @@ srcdir="$(dirname "${BASH_SOURCE[0]}")"
 # shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
 
+SOCKDIR=~/cloud_sql.socks
+
 # shellcheck disable=SC2034,SC2154
 usage_description="
 Opens a Cloud SQL Proxy for all instances in given or all GCP Projects
@@ -27,19 +29,29 @@ Opens a Cloud SQL Proxy for all instances in given or all GCP Projects
 Populates \$HOME/cloud_sql.socks/ directory with all your Cloud SQL instances sockets to make it quicker and easier to connect than slow 'gcloud sql connect'
 
 
+Usage:
+
+    ${0##*/}
+
+    psql -h ~/cloud_sql.socks/<instance> ...
+
+    mysql -S ~/cloud_sql.socks/<instance> ...
+
+
+
 Auto-installs Google Cloud SQL Proxy if not found in \$PATH
 
 
-Restart to pick up any new Cloud SQL instances. Useful for quick interactive work.
+Restart to pick up any new Cloud SQL instances
 
-For production, use dedicated Cloud SQL Proxy instances with service account credentials (avoid application default login expiry or restarts for picking up new instances)
+Useful to quick interactive DBA work
+
+For production long-lived proxying, use dedicated Cloud SQL Proxy instances with service account credentials (avoid application default login expiry or restarts for picking up new instances)
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
 usage_args="<projects>"
-
-SOCKDIR=~/cloud_sql.socks
 
 help_usage "$@"
 
