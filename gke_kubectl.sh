60cc5fe4bd4332f3761d0b11c16c5b3a5b1cba39
Hari Sekhon <harisekhon@gmail.com>
2021-03-03 11:33:34 +0000

    updated gke_kubectl.sh

--- gke_kubectl.sh
+++ gke_kubectl.sh
@@ -37,9 +37,16 @@ Eg. running:
 
 either by your hand or in other concurrently executing scripts changes your global kubectl context to run on the given cluster, which could divert your command or concurrently long running scripts in other windows to run kubectl commands on the wrong cluster, leading to cross environment misconfigurations and real world outages (I've seen this personally)
 
-If GKE_CONTEXT is set in the environment and matches a pre-existing context, skips pulling GKE creds to speed and noise reduction.
+If GKE_CONTEXT is set in the environment and matches a pre-existing context, skips pulling GKE creds for speed and noise reduction.
 
-For frequent more convenient usage you will want to shorten the CLI by copying this script to a local copy in each cluster's yaml config directory and hardcoding the CONTEXT (use gke_kube_creds.sh to pre-populate the context and credentials) or PROJECT, CLUSTER and ZONE variables if pulling GKE creds.
+if GKE_CONTEXT is not set then requires the following to be set in the environment in order to obtain the credentials to the GKE cluster:
+
+GKE_CLUSTER             - the name of your GKE cluster
+CLOUDSDK_CORE_PROJECT   - the project in which your GKE cluster resides (will try to auto-infer from gcloud config if not set)
+CLOUDSDK_COMPUTE_REGION - the region in which to find your GKE cluster (will try to auto-infer from  gcloud config if not set)
+                          If the CLOUDSDK variables are not set and cannot be inferred from gcloud config, then errors out. If they are set though, they may be pointing to the wrong project or region so it is recommended to set them
+
+For frequent more convenient usage you will want to shorten the CLI by copying this script to a local copy in each cluster's yaml config directory and hardcoding the GKE_CONTEXT (use gke_kube_creds.sh to pre-populate the context and credentials) or CLOUDSDK_CORE_PROJECT, CLOUDSDK_COMPUTE_REGION and GKE_CLUSTER variables if pulling GKE creds.
 
 Could also use main kube config with kubectl switches --cluster / --context (after configuring, see gke_kube_creds.sh), but this is more convenient, especially when hardcoded for the local copy in each cluster's k8s yaml dir
 
@@ -51,42 +58,49 @@ See Also:
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="<project> <cluster> <zone> <kubectl_options>"
+usage_args="<kubectl_options>"
 
 help_usage "$@"
+
+min_args 1 "$@"
 
 # ============================================================
 # HARDCODE THIS SECTION FOR SHORTER CLI convenience
 # REMOVE if hardcoding
-min_args 4 "$@"
 
-# fixed to this environment - thou shalt deploy to no other cluster from this script
+#GKE_CONTEXT=gke_<myproject>_<myregion>_<name>
 
-# HARDCODE THESE for frequent shorter CLI usage
-PROJECT="$1"  # used explicitly for easier tracking/debugging rather relying on implicit GOOGLE_PROJECT_ID which might not be what we expect
-CLUSTER="$2"  # eg. <myproject>-europe-west1
-ZONE="$3"     # eg. europe-west1
+if [ -z "${GKE_CONTEXT:-}" ]; then
 
-# if set and available in original kube config, will just copy config and switch to this context (faster and less noisy than re-pulling creds from GKE)
-CONTEXT="${GKE_CONTEXT:-gke_$CLUSTER-$CLUSTER}"
+    # fixed to this environment - thou shalt deploy to no other cluster from this script
 
-# REMOVE if hardcoding
-shift || :
-shift || :
-shift || :
+    # HARDCODE THESE for frequent shorter CLI usage
+    #CLOUDSDK_CORE_PROJECT=myproject
+    #CLOUDSDK_COMPUTE_REGION=europe-west1
+    #GKE_CLUSTER="$2"  # eg. <myproject>-europe-west1
+
+    check_env_defined GKE_CLUSTER
+    CLOUDSDK_CORE_PROJECT="${CLOUDSDK_CORE_PROJECT:-$(gcloud config list --format="get(core.project)")}"
+    CLOUDSDK_COMPUTE_REGION="${CLOUDSDK_COMPUTE_REGION:-$(gcloud config list --format="get(compute.region)")}"
+    check_env_defined CLOUDSDK_CORE_PROJECT
+    check_env_defined CLOUDSDK_COMPUTE_REGION
+
+    # if set and available in original kube config, will just copy config and switch to this context (faster and less noisy than re-pulling creds from GKE)
+    GKE_CONTEXT="gke_${CLOUDSDK_CORE_PROJECT}_${CLOUDSDK_COMPUTE_REGION}_${GKE_CLUSTER}"
+fi
 # ============================================================
 
 kube_config_isolate
 
 # if original kube config contains the context, copy and reuse it (faster and less noisy than re-pulling the creds from GKE), especially when called in script iterations
-if [ -n "${CONTEXT:-}" ] &&
-   kubectl config get-contexts -o name | grep -Fxq "$CONTEXT"; then
+if [ -n "${GKE_CONTEXT:-}" ] &&
+   kubectl config get-contexts -o name | grep -Fxq "$GKE_CONTEXT"; then
     # switch context if not already the current context (avoids repeating "switching context" output noise when this script it called iteratively in loop by other scripts)
-    if [ "$(kubectl config current-context)" != "$CONTEXT" ]; then
-        kubectl config use-context "$CONTEXT"
+    if [ "$(kubectl config current-context)" != "$GKE_CONTEXT" ]; then
+        kubectl config use-context "$GKE_CONTEXT"
     fi
 else
-    gcloud container clusters get-credentials "$CLUSTER" --zone "$ZONE" --project "$PROJECT"
+    gcloud container clusters get-credentials "$GKE_CLUSTER"  # --region "$CLOUDSDK_COMPUTE_REGION" --project "$CLOUDSDK_CORE_PROJECT"
     echo >&2
 fi
 
