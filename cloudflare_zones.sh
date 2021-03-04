74e189664e7f034bebab350bf301bb11e4bddf36
Hari Sekhon <harisekhon@gmail.com>
2021-03-04 16:02:42 +0000

    added cloudflare_zones.sh

---
+++ cloudflare_zones.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2021-03-04 16:00:54 +0000 (Thu, 04 Mar 2021)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Lists Cloudflare Zone names and IDs (needed for Terraform)
+
+Output:
+
+<id>    <name>
+
+Uses cloudflare_api.sh - see there for authentication API key details
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+"$srcdir/cloudflare_api.sh" /zones |
+jq -r '.result[] | [.id, .name] | @tsv'
