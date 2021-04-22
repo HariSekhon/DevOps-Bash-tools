b2a91f1b010935f5ace058167dd750c38ec102a4
Hari Sekhon <harisekhon@gmail.com>
2021-04-22 18:58:51 +0100

    updated sync_ci_to_adjacent_repos.sh

--- sync_ci_to_adjacent_repos.sh
+++ sync_ci_to_adjacent_repos.sh
@@ -45,6 +45,7 @@ while read -r repo dir; do
         echo "WARNING: repo dir $dir not found, skipping..."
         continue
     fi
+    sed 's/#.*//; /^[[:space:]]*$/d' "$srcdir/setup/ci.txt" |
     while read -r filename; do
         target="../$dir/$filename"
         mkdir -pv "${target%/*}"
@@ -53,6 +54,6 @@ while read -r repo dir; do
         if [ "$repo" = "nagios-plugins" ]; then
             perl -pi -e 's/(^[[:space:]]+make ci$)/\1 ci zookeeper-retry/' "$target"
         fi
-    done < <(sed 's/#.*//; /^[[:space:]]*$/d' "$srcdir/setup/ci.txt")
+    done
 done
 "$srcdir/.github/workflows/sync_to_adjacent_repos.sh" "$@"
