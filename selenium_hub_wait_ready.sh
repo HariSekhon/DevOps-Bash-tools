e1426aa965cd167a96400e9126e865c2a34688bd
Hari Sekhon <harisekhon@gmail.com>
2021-05-16 10:36:07 +0100

    updated selenium_hub_wait_ready.sh

--- selenium_hub_wait_ready.sh
+++ selenium_hub_wait_ready.sh
@@ -22,7 +22,7 @@ srcdir="$(dirname "${BASH_SOURCE[0]}")"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Waits for Selenium Grid to be ready
+Waits for Selenium Grid Hub status to be ready
 "
 
 # used by usage() in lib/utils.sh
