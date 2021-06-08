cdb53d258bdb7ffa0a9bdc3d4110198f65aceec9
Hari Sekhon <harisekhon@gmail.com>
2021-06-08 12:18:30 +0100

    updated buildkite_rebuild_cancelled_builds.sh

--- buildkite_rebuild_cancelled_builds.sh
+++ buildkite_rebuild_cancelled_builds.sh
@@ -20,10 +20,16 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
 
+# shellcheck disable=SC1090
+. "$srcdir/lib/git.sh"
+
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
 usage_description="
-Rebuilds cancelled builds in BuildKite via its API (inverse of buildkite_cancel_scheduled_builds.sh)
+Rebuilds the last N cancelled builds in BuildKite via its API
+
+Useful to retrying N builds across projects where they may have failed due to agent problems
+
 
 https://buildkite.com/docs/apis/rest-api/builds
 "
@@ -33,7 +39,22 @@ usage_args="[<curl_options>]"
 
 help_usage "$@"
 
-"$srcdir/buildkite_api.sh" "builds?state=canceled" "$@" |
+#min_args 1 "$@"
+
+pipeline="${1:-$(git_repo_name_lowercase)}"
+
+num="${2:-10}"
+
+if ! is_int "$num"; then
+    usage "num builds must be an integer"
+fi
+
+if [ "$num" -lt 1 ] || [ "$num" -gt 100 ]; then
+    usage "num builds must be an integer between 1 and 100"
+fi
+
+# shellcheck disable=SC2154
+"$srcdir/buildkite_api.sh" "/organizations/{organization}/pipelines/$pipeline/builds?state=failed&per_page=$num" |
 jq -r '.[] | [.pipeline.slug, .number, .url] | @tsv' |
 while read -r name number url; do
     url="${url#https://api.buildkite.com/v2/}"
