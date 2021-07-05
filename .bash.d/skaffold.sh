edd89b5067aa280ef2967c96d2ba0a182fa9ccc6
Hari Sekhon <harisekhon@gmail.com>
2021-07-05 16:39:09 +0100

    added skaffold.sh

---
+++ .bash.d/skaffold.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2021-07-05 16:31:14 +0100 (Mon, 05 Jul 2021)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+# ============================================================================ #
+#                                S k a f f o l d
+# ============================================================================ #
+
+#bash_tools="${bash_tools:-$(dirname "${BASH_SOURCE[0]}")/..}"
+
+# shellcheck disable=SC1090,SC1091
+#type add_PATH &>/dev/null || . "$bash_tools/.bash.d/paths.sh"
+
+autocomplete skaffold
