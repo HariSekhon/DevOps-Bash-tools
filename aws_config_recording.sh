887d558cb35c74fec66ffd2b63acd9fb98c5ba1f
Hari Sekhon <harisekhon@gmail.com>
2021-07-19 15:46:55 +0100

    updated aws_config_recording.sh

--- aws_config_recording.sh
+++ aws_config_recording.sh
@@ -39,6 +39,6 @@ usage_args=""
 help_usage "$@"
 
 
-aws configservice describe-configuration-recorder-status --output json |
+aws configservice describe-configuration-recorder-status --output json --no-paginate |
 jq -r '.ConfigurationRecordersStatus[] | [.name, .recording, .lastStatus] | @tsv' |
 column -t
