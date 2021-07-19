c60292a3fa322c13b0a349c4e33dfec7400dfbae
Hari Sekhon <harisekhon@gmail.com>
2021-07-19 15:33:51 +0100

    updated aws_config_all_types.sh

--- aws_config_all_types.sh
+++ aws_config_all_types.sh
@@ -39,6 +39,6 @@ usage_args=""
 help_usage "$@"
 
 
-aws configservice describe-configuration-recorders |
+aws configservice describe-configuration-recorders --output json |
 jq -r '.ConfigurationRecorders[] | [.name, .recordingGroup.allSupported, .recordingGroup.includeGlobalResourceTypes] | @tsv' |
 column -t
