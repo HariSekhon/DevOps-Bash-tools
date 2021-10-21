2b5d58bc8510e4687a85cd239aa342a3f321ea7b
Hari Sekhon <harisekhon@gmail.com>
2021-10-21 15:58:39 +0100

    updated kubectl_container_counts.sh

--- kubectl_container_counts.sh
+++ kubectl_container_counts.sh
@@ -22,9 +22,9 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Counts the number of running Docker containers of each name on the current Kubernetes cluster in the current namespace
+Lists Kubernetes containers running counts by name sorted descending on the current Kubernetes cluster in the current namespace
 
-Specify the --namespace or use --all-namespaces to get the total across all namespaces, including the kube-system containers
+Specify the --namespace or use --all-namespaces to get the total across all namespaces, including the kube-system namespace
 
 Requires kubectl to be in \$PATH and configured
 "
