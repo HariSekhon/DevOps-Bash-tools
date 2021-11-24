be37590d1da5efa73d03ff8903d9880a1a543fe2
Hari Sekhon <harisekhon@gmail.com>
2021-11-24 16:23:04 +0000

    added aws_secret_get.sh

---
+++ aws_secret_get.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2021-11-24 12:40:18 +0000 (Wed, 24 Nov 2021)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Retrieves a secret value from a given AWS Secrets Manager secret name
+
+First argument is used as secret name - if not given prompts for it
+Second or more args are passed to 'aws secretsmanager'
+
+
+$usage_aws_cli_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<name>"
+
+help_usage "$@"
+
+min_args 1 "$@"
+
+name="$1"
+shift || :
+
+aws secretsmanager get-secret-value --secret-id "$name" "$@" |
+jq -r .SecretString
