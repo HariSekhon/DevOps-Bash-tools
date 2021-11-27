5e20ae712364639ed6660707750ea879288ca5f0
Hari Sekhon <harisekhon@gmail.com>
2021-11-27 12:13:39 +0000

    added github_actions_foreach_workflow.sh

---
+++ github_actions_foreach_workflow.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#  args: devops-bash-tools echo user={user} repo={repo} name={name} workflow_name={workflow} id={id} state={state}
+#
+#  Author: Hari Sekhon
+#  Date: 2021-11-27 11:21:14 +0000 (Sat, 27 Nov 2021)
+#
+#  https://github.com/harisekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "$0")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/github.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Run a command for each GitHub Actions workflow in the given repo
+
+All arguments after the repo become the command template
+
+\$GITHUB_ORGANIZATION / \$GITHUB_USER - the user or organization to iterate the repos on - if not specified then determines the currently authenticated github user from your \$GITHUB_TOKEN
+
+The command template replaces the following for convenience in each iteration:
+
+{organization}, {org} => the organization account being iterated
+{username}, {user}    => the user account being iterated
+{repo}                => the repo name with the user prefix
+{workflow}, {name}    => the workflow name
+{id}                  => the workflow id
+{state}               => the workflow state (enabled/disable)
+
+eg.
+    ${0##*/} devops-bash-tools echo user={user} repo={repo} name={name} workflow_name={workflow} id={id} state={state}
+    ${0##*/} devops-bash-tools echo org={org}   repo={repo} name={name} workflow_name={workflow} id={id} state={state}
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<repo> <command> <args>"
+
+help_usage "$@"
+
+min_args 2 "$@"
+
+repo="$1"
+shift || :
+
+cmd_template="$*"
+
+user="${GITHUB_USER:-$(get_github_user)}"
+user_or_org="${GITHUB_ORGANIZATION:-$user}"
+
+"$srcdir/github_api.sh" "/repos/$user_or_org/$repo/actions/workflows" |
+jq -r '.workflows[] | [.id, .state, .name] | @tsv' |
+while read -r id state workflow; do
+    echo "# ============================================================================ #" >&2
+    echo "# $workflow" >&2
+    echo "# ============================================================================ #" >&2
+    cmd="$cmd_template"
+    cmd="${cmd//\{username\}/${user:-}}"
+    cmd="${cmd//\{user\}/${user:-}}"
+    cmd="${cmd//\{organization\}/${GITHUB_ORGANIZATION:-}}"
+    cmd="${cmd//\{org\}/${GITHUB_ORGANIZATION:-}}"
+    cmd="${cmd//\{repo\}/$repo}"
+    cmd="${cmd//\{name\}/$workflow}"
+    cmd="${cmd//\{workflow\}/$workflow}"
+    cmd="${cmd//\{id\}/$id}"
+    cmd="${cmd//\{state\}/$state}"
+    eval "$cmd"
+    echo >&2
+done
