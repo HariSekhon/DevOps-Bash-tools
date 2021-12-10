c6fe59899fe3465164d0af9df7ecc9bc7cd14bdb
Hari Sekhon <harisekhon@gmail.com>
2021-12-10 13:07:23 +0000

    added aws_ecr_delete_tag.sh

---
+++ aws_ecr_delete_tag.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#  args: haritest:1.0 stable
+#
+#  Author: Hari Sekhon
+#  Date: 2021-12-10 11:53:32 +0000 (Fri, 10 Dec 2021)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Deletes a tag for the given AWS ECR docker image
+
+
+$usage_aws_cli_required
+
+
+Similar scripts:
+
+    aws_ecr_*.sh - scripts for AWS Elastic Container Registry
+
+    gcr_*.sh - scripts for Google Container Registry
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<image> <tag> [<aws_cli_options>]"
+
+help_usage "$@"
+
+num_args 2 "$@"
+
+image="$1"
+tag="$2"
+shift || :
+shift || :
+
+aws ecr batch-delete-image --repository-name "$image" --image-ids "imageTag=$tag" "$@"
