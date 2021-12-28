3935766c237716c04a8767c8509465f57f7764c3
Hari Sekhon <harisekhon@gmail.com>
2021-12-28 17:15:22 +0000

    added terraform_cloud_organizations.sh

---
+++ terraform_cloud_organizations.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#  args:
+#
+#  Author: Hari Sekhon
+#  Date: 2021-12-21 13:30:39 +0000 (Tue, 21 Dec 2021)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Lists Terraform Cloud organization IDs (needed by many adjacent scripts)
+
+Output:
+
+<id>
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+#min_args 1 "$@"
+
+# TODO: add pagination support
+"$srcdir/terraform_cloud_api.sh" "/organizations" |
+jq -r '.data[].id'
