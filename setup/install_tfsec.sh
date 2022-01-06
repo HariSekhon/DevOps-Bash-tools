f81ab61afed418a0848f32831e929c3aab0a36c4
Hari Sekhon <harisekhon@gmail.com>
2022-01-06 15:19:05 +0000

    added install_tfsec.sh

---
+++ setup/install_tfsec.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-01-06 15:09:39 +0000 (Thu, 06 Jan 2022)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/../lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs TFSec CLI by AquaSec
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+#min_args 1 "$@"
+
+version="${1:-0.63.1}"
+
+if ! [[ "$version" =~ ^v ]]; then
+    version="v$version"
+fi
+
+"$srcdir/../install_binary.sh" "https://github.com/aquasecurity/tfsec/releases/download/$version/tfsec-{os}-{arch}"
