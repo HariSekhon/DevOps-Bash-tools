570cdfa9d47fced2d30a1139e4ba6521ad038c18
Hari Sekhon <harisekhon@gmail.com>
2022-01-07 11:02:41 +0000

    updated check_python_exception_pass.sh

--- check_python_exception_pass.sh
+++ check_python_exception_pass.sh
@@ -35,21 +35,21 @@ section "Python - finding any usage of exception pass"
 
 start_time="$(start_timer)"
 
-for x in $files; do
-    type isExcluded &>/dev/null && isExcluded "$x" && echo -n '-' && continue
-    [[ "$x" =~ /test/ ]] && echo -n '-' && continue
+while read -r filename; do
+    type isExcluded &>/dev/null && isExcluded "$filename" && echo -n '-' && continue
+    [[ "$filename" =~ /test/ ]] && echo -n '-' && continue
     echo -n '.'
-    if grep -E -B3 -A1 '^[[:space:]]+\bpass\b' "$x" | grep -Eq '^[^#]*\bexcept\b'; then
+    if grep -E -B3 -A1 '^[[:space:]]+\bpass\b' "$filename" | grep -Eq '^[^#]*\bexcept\b'; then
         echo
-        grep -EHnB 5 -A1 '^[[:space:]]+\bpass\b' "$x" | grep -E -5 '^[^#]*\bexcept\b'
+        grep -EHnB 5 -A1 '^[[:space:]]+\bpass\b' "$filename" | grep -E -5 '^[^#]*\bexcept\b'
         echo
         echo
-        echo "WARNING: $x contains 'pass'!! Check this code isn't being sloppy"
+        echo "WARNING: $filename contains 'pass'!! Check this code isn't being sloppy"
         #if ! is_CI; then
         #    exit 1
         #fi
     fi
-done
+done <<< "$files"
 
 time_taken "$start_time"
 section2 "Python OK - no except pass usage found"
