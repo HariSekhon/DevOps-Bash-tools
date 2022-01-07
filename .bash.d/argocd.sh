250a95851edf9173de78de27e50c90f837fa045c
Hari Sekhon <harisekhon@gmail.com>
2022-01-07 17:46:14 +0000

    updated argocd.sh

--- .bash.d/argocd.sh
+++ .bash.d/argocd.sh
@@ -25,7 +25,9 @@ alias argopass="argocd_password.sh | copy_to_clipboard"
 # ARGOCD_SERVER=localhost:8080  # without the http:// or https:// prefix
 # ARGOCD_AUTH_TOKEN='<token>'
 
-export ARGOCD_OPTS="--grpc-web"
+if ! [[ "${ARGOCD_OPTS:-}" =~ --grpc-web ]]; then
+    export ARGOCD_OPTS="$ARGOCD_OPTS --grpc-web"
+fi
 #export ARGOCD_OPTS="--grpc-web --insecure"  # only in local dev
 
 argosync(){
@@ -38,8 +40,18 @@ argosync(){
         fi
     fi
     if [ -n "${ARGOCD_APP:-}" ]; then
+        timestamp "ArgoCD Syncing App '$ARGOCD_APP'"
+        echo
         argocd app sync "$ARGOCD_APP" --force "$@"
         argocd app wait "$ARGOCD_APP" --timeout "$seconds" "$@"
+        echo
+        if [ "$ARGOCD_APP" = argocd ]; then
+            for x in projects apps; do
+                if argocd app list -o name | grep -Fxq "$x"; then
+                    ARGOCD_APP="$x" argosync "$@"
+                fi
+            done
+        fi
     else
         echo "\$ARGOCD_APP is not set" >&2
         return 1
