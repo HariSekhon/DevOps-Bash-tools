b8ba2abf149c4e84ea2a854910b2062ad3f381c7
Hari Sekhon <harisekhon@gmail.com>
2022-01-10 18:31:57 +0000

    updated install_syft.sh

--- setup/install_syft.sh
+++ setup/install_syft.sh
@@ -18,9 +18,6 @@ set -euo pipefail
 srcdir="$(dirname "${BASH_SOURCE[0]}")"
 
 # shellcheck disable=SC1090
-. "$srcdir/../lib/utils.sh"
-
-# shellcheck disable=SC1090
 . "$srcdir/../lib/github.sh"
 
 # shellcheck disable=SC2034,SC2154
