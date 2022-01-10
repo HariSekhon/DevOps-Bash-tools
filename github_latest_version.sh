c185a3ec533b114d43bc46aa80c4eecd807c96e7
Hari Sekhon <harisekhon@gmail.com>
2022-01-10 18:38:45 +0000

    added github_latest_version.sh

---
+++ github_latest_version.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-01-10 18:33:13 +0000 (Mon, 10 Jan 2022)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Returns the latest version of a given 'owner/repo' via the GitHub API
+
+Requires curl and jq to be installed
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<owner>/<repo>"
+
+help_usage "$@"
+
+check_bin curl
+check_bin jq
+
+min_args 1 "$@"
+
+owner_repo="$1"
+
+# TODO: find authoritative source on what is allowed in these names before enabling this
+#if ! [[ "$owner_repo" =~ ^[[:alnum:]-]+[[:alnum:]-]+$ ]]; then
+#    die "Invalid owner/repo argument given: $owner_repo"
+#fi
+
+curl -sSL --fail "https://api.github.com/repos/$owner_repo/releases/latest" |
+jq -r .name
