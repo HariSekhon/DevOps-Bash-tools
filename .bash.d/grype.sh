a2b0711702facbf7af97fab9aaeb1382fe2f7a61
Hari Sekhon <harisekhon@gmail.com>
2022-01-10 18:00:15 +0000

    added grype.sh

---
+++ .bash.d/grype.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-01-10 17:58:03 +0000 (Mon, 10 Jan 2022)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+# ============================================================================ #
+#                                   G r y p e
+# ============================================================================ #
+
+#set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+#eval "$(grype completion bash)"
+
+# generates auto-completion file to avoid repeating running auto-completion command, and sources from there
+autocomplete grype
