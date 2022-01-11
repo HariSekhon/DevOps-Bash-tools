ba98f1edb38be7ef252f7706998a1aedcbb2ce43
Hari Sekhon <harisekhon@gmail.com>
2022-01-11 10:20:45 +0000

    added install_github_cli.sh

---
+++ setup/install_github_cli.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-01-11 10:07:36 +0000 (Tue, 11 Jan 2022)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/../lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs GitHub CLI
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<version>]"
+
+help_usage "$@"
+
+#min_args 1 "$@"
+
+#version="${1:-2.4.0}"
+version="${1:-latest}"
+
+owner_repo="cli/cli"
+
+if [ "$version" = latest ]; then
+    timestamp "determining latest version of '$owner_repo' via GitHub API"
+    version="$("$srcdir/../github_repo_latest_release.sh" "$owner_repo")"
+    version="${version#v}"
+    timestamp "latest version is '$version'"
+else
+    is_semver "$version" || die "non-semver version argument given: '$version' - should be in format: N.N.N"
+fi
+
+os="$(get_os)"
+if [ "$os" = darwin ]; then
+    os=macOS
+fi
+
+"$srcdir/../install_binary.sh" "https://github.com/cli/cli/releases/download/v$version/gh_${version}_${os}_{arch}.tar.gz" "gh_${version}_${os}_{arch}/bin/gh"
