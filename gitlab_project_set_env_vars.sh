7620dd677a6fba176132c881f74e7a23cff57952
Hari Sekhon <harisekhon@gmail.com>
2022-01-13 18:08:03 +0000

    updated gitlab_project_set_env_vars.sh

--- gitlab_project_set_env_vars.sh
+++ gitlab_project_set_env_vars.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #  vim:ts=4:sts=4:sw=4:et
+#  args: HariSekhon/DevOps-Bash-tools haritest=stuff
 #
 #  Author: Hari Sekhon
 #  Date: 2021-12-03 17:41:23 +0000 (Fri, 03 Dec 2021)
@@ -27,6 +28,9 @@ usage_description="
 Adds / updates GitLab CI project-level masked environment variable(s) from args or stdin
 
 If no second argument is given, reads environment variables from standard input, one per line in 'key=value' format or 'export key=value' shell format
+
+If GITLAB_VARIABLES_PROTECTED=1 is set in the environment, then will create a protected environment variable which is only available to protected branches or tags
+If GITLAB_VARIABLES_UNMASKED=1 is set in the environment, then environment variables will not be masked
 
 Examples:
 
@@ -62,25 +66,33 @@ project_slug="${project_slug//\//%2F}"
 
 existing_env_vars="$("$srcdir/gitlab_api.sh" "projects/$project_slug/variables" | jq -r '.[].key')"
 
+protected=false
+if [ -n "${GITLAB_VARIABLES_PROTECTED:-}" ]; then
+    protected=true
+fi
+
 add_env_var(){
     local env_var="$1"
     parse_export_key_value "$env_var"
     local masked=true
     # shellcheck disable=SC2154
-    if [ "${#value}" -lt 8 ]; then  # avoids 400 errors from the API if sending < 8 chars with masked=true
+    if [ "${GITLAB_VARIABLES_UNMASKED=1}" ]; then
+        echo "WARNING: value for key '$key' will not be masked because GITLAB_VARIABLES_UNMASKED is set in the environment" >&2
+        masked=false
+    elif [ "${#value}" -lt 8 ]; then  # avoids 400 errors from the API if sending < 8 chars with masked=true
         echo "WARNING: value for key '$key' is less than 8 characters so can't be masked in GitLab" >&2
         masked=false
     fi
     # shellcheck disable=SC2154
     if grep -Fxq "$key" <<< "$existing_env_vars"; then
         timestamp "updating GitLab environment variable '$key' in project '$project_slug'"
-        "$srcdir/gitlab_api.sh" "projects/$project_slug/variables/$key?masked=$masked" -X PUT \
+        "$srcdir/gitlab_api.sh" "projects/$project_slug/variables/$key?masked=$masked&protected=$protected" -X PUT \
             -F "value=$value" \
             -H 'Content-Type: multipart/form-data'
     else
         timestamp "adding GitLab environment variable '$key' to project '$project_slug'"
-        # could also use a project_id                                           # change if needing protected variables
-        "$srcdir/gitlab_api.sh" "projects/$project_slug/variables?masked=$masked&protected=false" -X POST \
+                                          # could also use a project_id
+        "$srcdir/gitlab_api.sh" "projects/$project_slug/variables?masked=$masked&protected=$protected" -X POST \
             -F "key=$key" \
             -F "value=$value" \
             -H 'Content-Type: multipart/form-data'
