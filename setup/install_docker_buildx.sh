08b52bd763f3f1cef4e79c5bc382ae60452b789a
Hari Sekhon <harisekhon@gmail.com>
2022-01-14 11:37:22 +0000

    added install_docker_buildx.sh

---
+++ setup/install_docker_buildx.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-01-14 11:29:58 +0000 (Fri, 14 Jan 2022)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/../lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs Docker Buildx plugin to ~/.docker/cli-plugins
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<version>]"
+
+help_usage "$@"
+
+#min_args 1 "$@"
+
+version="${1:-latest}"
+
+owner_repo="docker/buildx"
+
+if [ "$version" = latest ]; then
+    timestamp "determining latest version of '$owner_repo' via GitHub API"
+    version="$("$srcdir/../github_repo_latest_release.sh" "$owner_repo")"
+    timestamp "latest version is '$version'"
+else
+    is_semver "$version" || die "non-semver version argument given: '$version' - should be in format: N.N.N"
+    [[ "$version" =~ ^v ]] || version="v$version"
+fi
+
+install_dir=~/.docker/cli-plugins
+
+mkdir -p -v "$install_dir"
+
+"$srcdir/../install_binary.sh" "https://github.com/docker/buildx/releases/download/$version/buildx-$version.{os}-{arch}" docker-buildx "$install_dir/docker-buildx"
