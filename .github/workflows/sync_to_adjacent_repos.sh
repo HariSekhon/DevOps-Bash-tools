19b5c828a37591e17ed8a23834f5afd494a6629c
Hari Sekhon <harisekhon@gmail.com>
2022-01-19 18:36:44 +0000

    updated sync_to_adjacent_repos.sh

--- .github/workflows/sync_to_adjacent_repos.sh
+++ .github/workflows/sync_to_adjacent_repos.sh
@@ -49,7 +49,9 @@ sync_file(){
 }
 
 sed 's/#.*//; s/:/ /' ../../setup/repos.txt |
-grep -v -e bash-tools -e '^[[:space:]]*$' |
+grep -v -e bash-tools \
+        -e github-actions \
+        -e '^[[:space:]]*$' |
 while read -r repo dir; do
     if [ $# -gt 0 ]; then
         for filename in "$@"; do
