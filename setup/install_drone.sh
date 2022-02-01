5aaba4de2ca5644b81784f14de1eee2ff334017f
Hari Sekhon <harisekhon@gmail.com>
2022-02-01 12:21:24 +0000

    added install_drone.sh

---
+++ setup/install_drone.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-02-01 12:18:29 +0000 (Tue, 01 Feb 2022)
+#
+#  https://github.com/HariSekhon/bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1090
+. "$srcdir/../lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs Drone CI CLI
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<version>]"
+
+help_usage "$@"
+
+#min_args 1 "$@"
+
+version="${1:-latest}"
+
+owner_repo="harness/drone-cli"
+
+if [ "$version" = latest ]; then
+    timestamp "determining latest version of '$owner_repo' via GitHub API"
+    version="$("$srcdir/../github_repo_latest_release.sh" "$owner_repo")"
+    timestamp "latest version is '$version'"
+else
+    is_semver "$version" || die "non-semver version argument given: '$version' - should be in format: N.N.N"
+    [[ "$version" =~ ^v ]] || version="v$version"
+fi
+
+"$srcdir/../install_binary.sh" "https://github.com/harness/drone-cli/releases/latest/download/drone_{os}_{arch}.tar.gz" drone
