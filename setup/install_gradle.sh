1b8d4961b613f019d737244402b947f92419f228
Hari Sekhon <harisekhon@gmail.com>
2022-02-01 11:47:10 +0000

    updated install_gradle.sh

--- setup/install_gradle.sh
+++ setup/install_gradle.sh
@@ -20,35 +20,47 @@ echo "==========================================================================
 echo "                           G r a d l e   I n s t a l l"
 echo "================================================================================"
 
-GRADLE_VERSION=${1:-${GRADLE_VERSION:-2.14.1}}
+GRADLE_VERSION=${1:-${GRADLE_VERSION:-7.3.3}}
 
-BASE=/opt
+am_root(){
+    [ "${EUID:-${UID:-$(id -n)}}" = 0 ]
+}
+if am_root; then
+    BASE=/opt
+else
+    BASE=~/bin
+fi
 
 echo
 date '+%F %T  Starting...'
 start_time="$(date +%s)"
 echo
 
-if ! [ -e "$BASE/gradle" ]; then
-    mkdir -p "$BASE"
+if [ -e "$BASE/gradle" ]; then
+    echo "$BASE/gradle already exists, skipping download"
+else
+    mkdir -pv "$BASE"
     cd "$BASE"
-    wget -t 100 --retry-connrefused "https://services.gradle.org/distributions/gradle-$GRADLE_VERSION-bin.zip"
+    wget -c -t 10 --retry-connrefused "https://services.gradle.org/distributions/gradle-$GRADLE_VERSION-bin.zip"
     unzip "gradle-$GRADLE_VERSION-bin.zip"
-    ln -sv "gradle-$GRADLE_VERSION" gradle && \
     rm -f "gradle-$GRADLE_VERSION-bin.zip"
     echo
     echo "Gradle Install done"
-else
-    echo "$BASE/gradle already exists - doing nothing"
 fi
-if ! [ -e /etc/profile.d/gradle.sh ]; then
-    echo "Adding /etc/profile.d/gradle.sh"
-    # shell execution tracing comes out in the file otherwise
-    set +x
-    cat >> /etc/profile.d/gradle.sh <<EOF
+if [ "${EUID:-${UID:-$(id -n)}}" = 0 ]; then
+    ln -sv "gradle-$GRADLE_VERSION" gradle
+    if ! [ -e /etc/profile.d/gradle.sh ]; then
+        echo "Adding /etc/profile.d/gradle.sh"
+        # shell execution tracing comes out in the file otherwise
+        set +x
+        cat >> /etc/profile.d/gradle.sh <<EOF
 export GRADLE_HOME=/opt/gradle
 export PATH=\$PATH:\$GRADLE_HOME/bin
 EOF
+    fi
+else
+    ln -fsv ~/bin/"gradle-$GRADLE_VERSION"/bin/gradle ~/bin
+    echo "Ensure you have ~/bin set in your \$PATH"
 fi
 
 echo
