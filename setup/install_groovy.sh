ebda0d2c660dff9f4808dd2eef2327ec146742d2
Hari Sekhon <harisekhon@gmail.com>
2022-02-03 11:56:41 +0000

    updated install_groovy.sh

--- setup/install_groovy.sh
+++ setup/install_groovy.sh
@@ -20,9 +20,16 @@ echo "==========================================================================
 echo "                           G r o o v y   I n s t a l l"
 echo "================================================================================"
 
-GROOVY_VERSION=${1:-${GROOVY_VERSION:-2.4.7}}
+GROOVY_VERSION=${1:-${GROOVY_VERSION:-4.0.0}}
 
-BASE=/opt
+am_root(){
+    [ "${EUID:-${UID:-$(id -n)}}" = 0 ]
+}
+if am_root; then
+    BASE=/opt
+else
+    BASE=~/bin
+fi
 
 date '+%F %T  Starting...'
 start_time="$(date +%s)"
@@ -31,7 +38,7 @@ echo
 if ! [ -e "$BASE/groovy" ]; then
     mkdir -p "$BASE"
     cd "$BASE"
-    wget -t 100 --retry-connrefused "https://dl.bintray.com/groovy/maven/apache-groovy-binary-$GROOVY_VERSION.zip"
+    wget -t 100 --retry-connrefused "https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/apache-groovy-binary-$GROOVY_VERSION.zip"
     unzip "apache-groovy-binary-$GROOVY_VERSION.zip"
     ln -sv "groovy-$GROOVY_VERSION" groovy
     rm -f "apache-groovy-binary-$GROOVY_VERSION.zip"
@@ -40,16 +47,20 @@ if ! [ -e "$BASE/groovy" ]; then
 else
     echo "$BASE/groovy already exists - doing nothing"
 fi
-if [ -d /etc/profile.d/groovy.sh ]; then
-    if ! [ -e /etc/profile.d/groovy.sh ]; then
-        echo "Adding /etc/profile.d/groovy.sh"
-        # shell execution tracing comes out in the file otherwise
-        set +x
-        cat >> /etc/profile.d/groovy.sh <<EOF
-export GROOVY_HOME=/opt/groovy
-export PATH=\$PATH:\$GROOVY_HOME/bin
+if am_root; then
+    if [ -d /etc/profile.d/groovy.sh ]; then
+        if ! [ -e /etc/profile.d/groovy.sh ]; then
+            echo "Adding /etc/profile.d/groovy.sh"
+            # shell execution tracing comes out in the file otherwise
+            set +x
+            cat >> /etc/profile.d/groovy.sh <<EOF
+    export GROOVY_HOME=/opt/groovy
+    export PATH=\$PATH:\$GROOVY_HOME/bin
 EOF
+        fi
     fi
+else
+    echo "Ensure you have ~/bin/groovy/bin set in your \$PATH"
 fi
 
 echo
