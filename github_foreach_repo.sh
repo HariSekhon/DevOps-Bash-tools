10ef812289a1a7166b2bdbbcdc06b28de63ee0a4
Hari Sekhon <harisekhon@gmail.com>
2022-02-14 16:56:46 +0000

    updated github_foreach_repo.sh

--- github_foreach_repo.sh
+++ github_foreach_repo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #  vim:ts=4:sts=4:sw=4:et
-#  args: echo user={user} name={name} repo={repo}
+#  args: echo owner={owner} repo={repo}
 #
 #  Author: Hari Sekhon
 #  Date: 2020-08-30 10:08:07 +0100 (Sun, 30 Aug 2020)
@@ -32,15 +32,10 @@ All arguments become the command template
 The command template replaces the following for convenience in each iteration:
 
 {owner}               => the user or organization that owns the repo eg. HariSekhon or MyOrg, whichever owns the repo
-{organization}, {org} => the organization account being iterated eg. MyOrg
-{username}, {user}    => the user account you are authenticated as eg. HariSekhon
-{name}                => the repo name without the owner prefix eg. DevOps-Bash-tools
-{repo}                => the repo name with the owner prefix eg. HariSekhon/DevOps-Bash-tools or MyOrg/DevOps-Bash-tools
+{repo}                => the repo name without the owner prefix eg. DevOps-Bash-tools
 
 eg.
     ${0##*/} echo owner={owner}  name={name} repo={repo}
-    ${0##*/} echo user={user}    name={name} repo={repo}  # the user you are authenticated as may not be the owner for organization repos, safer to use {owner}
-    ${0##*/} echo org={org}      name={name} repo={repo}
 "
 
 # used by usage() in lib/utils.sh
@@ -57,19 +52,18 @@ user="${GITHUB_USER:-$(get_github_user)}"
 user_or_org="${GITHUB_ORGANIZATION:-$user}"
 
 get_github_repos "$user_or_org" "${GITHUB_ORGANIZATION:-}" |
-while read -r name; do
-    repo="$user_or_org/$name"
+while read -r repo; do
+    owner_repo="$user_or_org/$repo"
     echo "# ============================================================================ #" >&2
-    echo "# $repo" >&2
+    echo "# $owner_repo" >&2
     echo "# ============================================================================ #" >&2
     cmd="$cmd_template"
     cmd="${cmd//\{owner\}/$user_or_org}"
-    cmd="${cmd//\{username\}/${user:-}}"
-    cmd="${cmd//\{user\}/${user:-}}"
-    cmd="${cmd//\{organization\}/${GITHUB_ORGANIZATION:-}}"
-    cmd="${cmd//\{org\}/${GITHUB_ORGANIZATION:-}}"
     cmd="${cmd//\{repo\}/$repo}"
-    cmd="${cmd//\{name\}/$name}"
+    #cmd="${cmd//\{username\}/${user:-}}"
+    #cmd="${cmd//\{user\}/${user:-}}"
+    #cmd="${cmd//\{organization\}/${GITHUB_ORGANIZATION:-}}"
+    #cmd="${cmd//\{org\}/${GITHUB_ORGANIZATION:-}}"
     eval "$cmd"
     echo >&2
 done
