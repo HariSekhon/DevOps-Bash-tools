738ddecd5e7f0f72cc606f687ec582591a18e9af
Hari Sekhon <harisekhon@gmail.com>
2022-02-16 18:06:25 +0000

    updated github_repo_fork_update.sh

--- github_repo_fork_update.sh
+++ github_repo_fork_update.sh
@@ -45,7 +45,12 @@ Creates Pull Requests for branches given as arguments or set in \$BRANCHES_TO_PR
 $BRANCHES_TO_PR_DEFAULT
 
 Auto-merges the PRs for branches set in \$BRANCHES_TO_AUTOMERGE or the following default branches:
-$BRANCHES_TO_AUTOMERGE_DEFAULT"
+$BRANCHES_TO_AUTOMERGE_DEFAULT
+
+See also:
+
+    github_repo_fork_recreate.sh - recreates a forked repo to clean out PRs and reset branches to be clean fast-forward merges
+"
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
