a2b12bddd37f4c55cb33e4c38447fdb4b7de954a
Hari Sekhon <harisekhon@gmail.com>
2022-03-09 17:22:45 +0000

    updated teamcity.sh

--- .bash.d/teamcity.sh
+++ .bash.d/teamcity.sh
@@ -21,7 +21,7 @@
 teamcity_local(){
     TEAMCITY_SUPERUSER_TOKEN="$(
         # project name must match COMPOSE_PROJECT_NAME from teamcit.sh otherwise will fail to find token
-        docker-compose -p bash-tools -f "$(dirname "${BASH_SOURCE[0]}")/../setup/teamcity-docker-compose.yml" \
+        docker-compose -p bash-tools -f "$(dirname "${BASH_SOURCE[0]}")/../docker-compose/teamcity.yml" \
             logs teamcity-server | \
         grep -E -o 'Super user authentication token: [[:alnum:]]+' | \
         tail -n1 | \
