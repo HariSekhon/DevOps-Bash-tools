4d6a29bbdb00a2619dcf0e6306f69e2437df6f79
Hari Sekhon <harisekhon@gmail.com>
2022-03-15 19:12:52 +0000

    updated decomment.sh

--- decomment.sh
+++ decomment.sh
@@ -30,18 +30,25 @@ for arg; do
     esac
 done
 
-if [ "$(head -n1 "$@" | cut -f 1 | head -n1)" = "<" ]; then
+stdin=""
+if [ $# -eq 0 ]; then
+    # slurping is not memory efficient, not suitable for large files, but is the only way to be able to
+    # backtracking testing heads for type of decommenting and still retain full data for processing
+    stdin="$(cat)"
+fi
+
+if [ "$(head -n1 "$@" <<< "$stdin" | cut -c 1)" = "<" ]; then
     if type -P decomment-xml.pl &>/dev/null; then
-        decomment-xml.pl "$@"
+        decomment-xml.pl "$@" <<< "$stdin"
     else
-        echo "decomment-xml.pl from DevOps Perl Tools repo not found in \$PATH - ensure you have downloaded and built it before running this against XML files"
+        echo "ERROR: decomment-xml.pl from DevOps Perl Tools repo not found in \$PATH - ensure you have downloaded and built it before running this against XML files" >&2
         exit 1
     fi
-elif [ "$(head -n1 "$@" | cut -f 1-2 | head -n1)" = "--" ]; then
-    sed 's/--.*$//; /^[[:space:]]*$/d' "$@"
+elif [ "$(head -n1 "$@" <<< "$stdin" | cut -f 1-2)" = "--" ]; then
+    sed 's/--.*$//; /^[[:space:]]*$/d' "$@" <<< "$stdin"
 else
     sed 's/#.*$//;
          s/^[[:space:]]*\/\/.*$//;
          s/[[:space:]]\/\/.*$//;
-        /^[[:space:]]*$/d' "$@"
+        /^[[:space:]]*$/d' "$@" <<< "$stdin"
 fi
