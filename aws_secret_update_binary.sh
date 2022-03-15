bb61e80148fff7163d277c7dec78368e9fa5b490
Hari Sekhon <harisekhon@gmail.com>
2022-03-15 12:13:34 +0000

    updated aws_secret_update_binary.sh

--- aws_secret_update_binary.sh
+++ aws_secret_update_binary.sh
@@ -42,7 +42,7 @@ $usage_aws_cli_required
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="<name> <file> [<aws_options>]"
+usage_args="<name> <file> [--description 'My changed description' <aws_options>]"
 
 help_usage "$@"
 
