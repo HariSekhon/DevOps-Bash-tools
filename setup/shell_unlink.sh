bbae5ec889667d01bc95618192f7b9258282b8d0
Hari Sekhon <harisekhon@gmail.com>
2022-03-23 15:45:51 +0000

    updated shell_unlink.sh

--- setup/shell_unlink.sh
+++ setup/shell_unlink.sh
@@ -17,7 +17,7 @@
 
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 conf_files="$(sed 's/#.*//; /^[[:space:]]*$/d' "$srcdir/setup/files.txt")"
 
