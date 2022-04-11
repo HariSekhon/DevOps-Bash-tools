3543906620b07ae6b42718bb549669122a2f656a
Hari Sekhon <harisekhon@gmail.com>
2022-04-11 19:15:41 +0100

    updated install_sbt.sh

--- setup/install_sbt.sh
+++ setup/install_sbt.sh
@@ -53,7 +53,7 @@ elif command -v apt-get 2>/dev/null; then
         $sudo tee /etc/apt/sources.list.d/sbt_old.list
     $sudo apt-get install -y apt-transport-https curl gnupg
     curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" |
-        $sudo -H gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
+        ${sudo:+$sudo -H} gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
     $sudo chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
     $sudo apt-get update
     $sudo apt-get install -y sbt
