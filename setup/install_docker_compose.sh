b8583df607683bf2b5ef2765f4bc38a2bdf89970
Hari Sekhon <harisekhon@gmail.com>
2022-04-11 10:57:52 +0100

    updated install_docker_compose.sh

--- setup/install_docker_compose.sh
+++ setup/install_docker_compose.sh
@@ -3,27 +3,55 @@
 #  Author: Hari Sekhon
 #  Date: 2020-03-19 19:31:41 +0000 (Thu, 19 Mar 2020)
 #
-#  https://github.com/harisekhon/devops-bash-tools
+#  https://github.com/HariSekhon/DevOps-Bash-tools
 #
 #  License: see accompanying LICENSE file
 #
-#  https://www.linkedin.com/in/harisekhon
+#  https://www.linkedin.com/in/HariSekhon
 #
-
-# Install Docker Compose
 
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if type -P docker-compose &>/dev/null; then
-    echo "Docker Compose already installed, skipping install..."
+# shellcheck disable=SC1090
+. "$srcdir/../lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs Docker Compose
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<version>]"
+
+export PATH="$PATH:$HOME/bin"
+
+help_usage "$@"
+
+#min_args 1 "$@"
+
+#version="${1:-2.4.0}"
+version="${1:-latest}"
+
+owner_repo="docker/compose"
+
+if [ "$version" = latest ]; then
+    timestamp "determining latest version of '$owner_repo' via GitHub API"
+    version="$("$srcdir/../github_repo_latest_release.sh" "$owner_repo")"
+    timestamp "latest version is '$version'"
 else
-    echo "========================="
-    echo "Installing Docker Compose"
-    echo "========================="
-    echo
-    dir=~/bin
-    mkdir -pv "$dir"
-    wget -c "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -O "$dir/docker-compose"
-    chmod +x "$dir/docker-compose"
+    is_semver "$version" || die "non-semver version argument given: '$version' - should be in format: N.N.N"
+    version="v$version"
 fi
+
+arch="$(get_arch)"
+if [ "$arch" = amd64 ]; then
+    arch=x86_64
+fi
+
+"$srcdir/../install_binary.sh" "https://github.com/docker/compose/releases/download/$version/docker-compose-{os}-$arch" "docker-compose"
+
+echo
+docker-compose version
