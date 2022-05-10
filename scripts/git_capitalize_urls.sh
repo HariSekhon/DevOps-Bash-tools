307636d451f222f000dca8aad58faded58de45b8
Hari Sekhon <harisekhon@gmail.com>
2022-05-10 12:19:40 +0100

    updated git_capitalize_urls.sh

--- scripts/git_capitalize_urls.sh
+++ scripts/git_capitalize_urls.sh
@@ -4,7 +4,7 @@
 #  Author: Hari Sekhon
 #  Date: 2022-04-28 19:22:01 +0100 (Thu, 28 Apr 2022)
 #
-#  https://github.com/HariSekhon/bash-tools
+#  https://github.com/HariSekhon/DevOps-Bash-tools
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
@@ -41,8 +41,8 @@ $(
             echo "s|https://github.com/harisekhon/$repo|https://github.com/HariSekhon/$repo|gi;"
         fi
         if [ -n "$shortname" ]; then
-            #echo "s|https://github.com/harisekhon/$shortname|https://github.com/HariSekhon/$repo|gi;"
-            :
+            echo "s|https://github.com/harisekhon/$shortname$|https://github.com/HariSekhon/$repo|gi;"
+            #echo "s|https://github.com/harisekhon/$shortname/|https://github.com/HariSekhon/$repo/|gi;"
         fi
         echo
     done
