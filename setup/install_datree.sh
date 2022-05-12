94bbcd5ac11cad9a09686430b603b2d91ce865cc
Hari Sekhon <harisekhon@gmail.com>
2022-05-12 17:27:53 +0100

    updated install_datree.sh

--- setup/install_datree.sh
+++ setup/install_datree.sh
@@ -4,7 +4,7 @@
 #  Author: Hari Sekhon
 #  Date: 2022-04-14 10:51:19 +0100 (Thu, 14 Apr 2022)
 #
-#  https://github.com/HariSekhon/bash-tools
+#  https://github.com/HariSekhon/DevOps-Bash-tools
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
