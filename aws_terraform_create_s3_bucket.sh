8307fdab8df4ee11b0834e7a47c9ea0a04bd00ee
Hari Sekhon <harisekhon@gmail.com>
2022-05-27 18:36:31 +0100

    added aws_terraform_create_s3_bucket.sh

---
+++ aws_terraform_create_s3_bucket.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#  args: haritest-terraform-state
+#
+#  Author: Hari Sekhon
+#  Date: 2022-05-27 18:03:32 +0100 (Fri, 27 May 2022)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Creates an S3 bucket for storing Terraform state with the following optimizations:
+
+- Enables Versioning
+- Enables Encryption
+- Locks out Power Users
+- Optionally locks out any additional given user/group/role arns
+
+Idempotent: skips bucket creation is already exists, applies versioning and encryption, applies bucket policy is none exists
+
+Region: will create the bucket in your configured region, to override locally set \$AWS_DEFAULT_REGION
+
+
+$usage_aws_cli_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<bucket_name> [<disallowed_users>]"
+
+help_usage "$@"
+
+min_args 1 "$@"
+
+bucket="$1"
+shift || :
+
+export AWS_DEFAULT_OUTPUT=json
+
+if ! aws s3 ls "s3://$bucket" &>/dev/null; then
+	timestamp "Creating S3 bucket"
+	aws s3 mb "s3://$bucket" || :
+	echo >&2
+fi
+
+timestamp "Enabling S3 versioning"
+aws s3api put-bucket-versioning --bucket "$bucket" --versioning-configuration 'Status=Enabled'
+echo >&2
+
+timestamp "Enabling S3 server-side encryption"
+aws s3api put-bucket-encryption --bucket "$bucket" --server-side-encryption-configuration '{"Rules": [{"ApplyServerSideEncryptionByDefault": {"SSEAlgorithm": "AES256"}}]}'
+echo >&2
+
+timestamp "Enabling S3 Block Public Access"
+aws s3api put-public-access-block --bucket "$bucket" --public-access-block-configuration 'BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true'
+echo >&2
+
+timestamp "Checking for Power User role"
+power_user_arn="$(aws iam list-roles | jq -r '.Roles[].Arn' | grep -i AWSPowerUserAccess || :)"
+
+if [ -n "$power_user_arn" ]; then
+    timestamp "Power User role ARN found:  $power_user_arn"
+    timestamp "Checking for existing S3 bucket policy"
+    if [ -n "$(aws s3api get-bucket-policy --bucket "$bucket" --query Policy --output text 2>/dev/null)" ]; then
+        timestamp "WARNING: bucket policy already exists, not overwriting for safety, must edit manually"
+    else
+        timestamp "Creating bucket policy to lock out Power Users"
+        aws s3api put-bucket-policy --bucket "$bucket" --policy "$(cat <<EOF
+{
+  "Id": "Policy1653672260380",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "DisallowPowerUsers",
+      "Action": "s3:*",
+      "Effect": "Deny",
+      "Resource": [
+        "arn:aws:s3:::$bucket",
+        "arn:aws:s3:::$bucket/*"
+      ],
+      "Principal": {
+        "AWS": [
+          "$power_user_arn"
+        ]
+      }
+    }
+  ]
+}
+EOF
+)"
+		timestamp "Bucket policy created"
+    fi
+fi
