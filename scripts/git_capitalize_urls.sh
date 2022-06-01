087a08b9dbd4f30822c8b020b8fe5da69fde114d
Hari Sekhon <harisekhon@gmail.com>
2022-06-01 18:00:15 +0100

    updated git_capitalize_urls.sh

--- scripts/git_capitalize_urls.sh
+++ scripts/git_capitalize_urls.sh
@@ -32,18 +32,18 @@ usage_args=""
 help_usage "$@"
 
 sed_script="
-s|https://www.linkedin.com/in/HariSekhon|https://www.linkedin.com/in/HariSekhon|gi;
-s|https://ghcr.io/harisekhon/|https://ghcr.io/HariSekhon/|gi;
+s|https\\?://www.linkedin.com/in/HariSekhon|https://www.linkedin.com/in/HariSekhon|gi;
+s|https\\?://ghcr.io/harisekhon/|https://ghcr.io/HariSekhon/|gi;
 
 $(
     sed 's/#.*//; /^[[:space:]]*$/d; s/:/ /' "$srcdir/../setup/repos.txt" |
     while read -r repo shortname; do
         if [ -n "$repo" ]; then
-            echo "s|https://github.com/harisekhon/$repo|https://github.com/HariSekhon/$repo|gi;"
+            echo "s|https\\?://github.com/harisekhon/$repo|https://github.com/HariSekhon/$repo|gi;"
         fi
         if [ -n "$shortname" ]; then
-            echo "s|https://github.com/harisekhon/$shortname$|https://github.com/HariSekhon/$repo|gi;"
-            echo "s|https://github.com/harisekhon/$shortname/|https://github.com/HariSekhon/$repo/|gi;"
+            echo "s|https\\?://github.com/harisekhon/$shortname$|https://github.com/HariSekhon/$repo|gi;"
+            echo "s|https\\?://github.com/harisekhon/$shortname/|https://github.com/HariSekhon/$repo/|gi;"
         fi
         echo
     done
