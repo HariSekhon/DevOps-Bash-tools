2c34376538ee09a5348734903c2cb27d9ab4abba
Hari Sekhon <harisekhon@gmail.com>
2022-06-22 16:55:55 +0100

    updated install_kustomize.sh

--- setup/install_kustomize.sh
+++ setup/install_kustomize.sh
@@ -57,12 +57,6 @@ fi
 #
 #     Error: accumulating resources: accumulateFile "accumulating resources from 'github.com/argoproj/argo-cd//manifests/cluster-install?ref=v2.0.3': evalsymlink failure on '/tmp/git@repo/argocd/overlay/github.com/argoproj/argo-cd/manifests/cluster-install?ref=v2.0.3' : lstat /tmp/git@repo/argocd/overlay/github.com: no such file or directory", accumulateDirector: "recursed accumulation of path '/tmp/kustomize-881686007/repo': accumulating resources: accumulateFile \"accumulating resources from '../namespace-install': evalsymlink failure on '/tmp/kustomize-881686007/namespace-install' : lstat /tmp/kustomize-881686007/namespace-install: no such file or directory\", loader.New \"Error loading ../namespace-install with git: url lacks host: ../namespace-install, dir: evalsymlink failure on '/tmp/kustomize-881686007/namespace-install' : lstat /tmp/kustomize-881686007/namespace-install: no such file or directory, get: invalid source string: ../namespace-install\""
 
-"$srcdir/../install_binary.sh" "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F$version/kustomize_${version}_{os}_amd64.tar.gz" kustomize
+export RUN_VERSION_ARG=1
 
-# called as part of download script - call manually now
-echo
-if am_root; then
-    /usr/local/bin/kustomize version
-else
-    ~/bin/kustomize version
-fi
+"$srcdir/../install_binary.sh" "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F$version/kustomize_${version}_{os}_amd64.tar.gz" kustomize
