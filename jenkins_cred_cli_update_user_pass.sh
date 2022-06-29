3baa43faf24c59f06d2b90599ca0bf95f45bc1d7
Hari Sekhon <harisekhon@gmail.com>
2022-06-29 10:50:36 +0100

    updated jenkins_cred_cli_update_user_pass.sh

--- jenkins_cred_cli_update_user_pass.sh
+++ jenkins_cred_cli_update_user_pass.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #  vim:ts=4:sts=4:sw=4:et
-#  args: haritest-cli-credential hari-updated-user my-updated-password
+#  args: hari-test-cli-user-pass hari-updated-user my-updated-password '' '' 'My Updated Username + Password'
 #
 #  Author: Hari Sekhon
 #  Date: 2022-06-28 18:34:34 +0100 (Tue, 28 Jun 2022)
@@ -27,7 +27,32 @@ Updates the given Jenkins credential in the given credential store and domain
 
 Defaults to the 'system::system::jenkins' provider store and global domain '_'
 
+If credential id, user and password aren't given as arguments, then reads from stdin, reading in KEY=VALUE
+or standard shell export format - useful for piping from tools like aws_csv_creds.sh
+
+If standard input does not have an id field, the id will be generated from the username lowercased with -user-pass appended.
+
+In cases where you are reading secrets from stdin, you can set the store and domain via the environment variables
+\$JENKINS_SECRET_STORE and \$JENKINS_SECRET_DOMAIN
+
 Uses the adjacent jenkins_cli.sh - see there for authentication details
+
+
+Examples:
+
+    # update a credential with id 'hari-user-pass' to have username 'hari' and password 'myNewPassword':
+
+        ${0##*/} hari-user-pass hari myNewPassword
+
+    # with a description, leaving the store and domain as the default global one:
+
+        ${0##*/} hari-user-pass hari myNewPassword '' '' 'My Updated Username + Password'
+
+    # or piped from standard input:
+
+        #export JENKINS_SECRET_STORE and JENKINS_SECRET_DOMAIN if using stdin but not using system global store
+
+        echo 'hari=myNewPassword' | ${0##*/}
 "
 
 # used by usage() in lib/utils.sh
@@ -39,31 +64,48 @@ help_usage "$@"
 id="${1:-}"
 user="${2:-}"
 password="${3:-}"
-store="${4:-system::system::jenkins}"
-domain="${5:-_}"
+store="${4:-${JENKINS_SECRET_STORE:-system::system::jenkins}}"
+domain="${5:-${JENKINS_SECRET_DOMAIN:-_}}"
 description="${6:-}"
 
 update_credential(){
     local id="$1"
-    local user="$2"
-    local password="$3"
+    local key_value="$2"
+    parse_export_key_value "$key_value"
+    # key/value are exported by above function
+    # shellcheck disable=SC2154
+    local user="$key"
+    # shellcheck disable=SC2154
+    local password="$value"
+    local domain_name="$domain"
+    if [ "$domain_name" = '_' ]; then
+        domain_name='GLOBAL'
+    fi
     xml="<com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl>
-  <scope>GLOBAL</scope>
+  <scope>$domain_name</scope>
   <id>$id</id>
   <description>$description</description>
   <username>$user</username>
   <password>$password</password>
   <usernameSecret>false</usernameSecret>
 </com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl>"
+    timestamp "Updating Jenkins username/password credential '$id' in store '$store' domain '$domain_name'"
     "$srcdir/jenkins_cli.sh" update-credentials-by-xml "$store" "$domain" "$id" <<< "$xml"
+    timestamp "Secret '$id' updated"
 }
 
 if [ -n "$password" ]; then
-    update_credential "$id" "$user" "$password"
+    update_credential "$id" "$user"="$password" "$description"
 else
-    # XXX: switch this to accepting standard 'export KEY=VALUE' format for pipe chaining with other tools
-    while read -r id user password; do
-        [ -n "$password" ] || continue
-        update_credential "$id" "$user" "$password"
-    done
+    while read -r id user_password; do
+        if [ -z "${user_password:-}" ] && [[ "$id" =~ = ]]; then
+            user_password="$id"
+            id="${id%%=*}-user-pass"
+            id="$(tr '[:upper:]' '[:lower:]' <<< "$id")"
+        else
+            timestamp "WARNING: invalid line detected, skipping creating credential"
+            continue
+        fi
+        update_credential "$id" "$user_password" "$description"
+    done < <(sed 's/^[[:space:]]*export[[:space:]]*//; /^[[:space:]]*$/d')
 fi
