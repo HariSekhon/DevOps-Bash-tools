724e3c03638c2acf770e725bc63e202d98188a28
Hari Sekhon <harisekhon@gmail.com>
2022-06-29 09:47:29 +0100

    updated jenkins_cred_add2_user_pass.sh

--- jenkins_cred_add2_user_pass.sh
+++ jenkins_cred_add2_user_pass.sh
@@ -27,6 +27,12 @@ Creates a Jenkins credential in the given credential store and domain
 
 Defaults to the 'system::system::jenkins' provider store and global domain '_'
 
+If credential, user and password aren't given as arguments, then reads from stdin, reading in KEY=VALUE
+or standard shell export format - useful for piping from tools like aws_csv_creds.sh
+
+In cases where you are reading secrets from stdin, you can set the store and domain via the environment variables
+\$JENKINS_SECRET_STORE and \$JENKINS_SECRET_DOMAIN
+
 Uses the adjacent jenkins_cli.sh - see there for authentication details
 "
 
@@ -39,31 +45,48 @@ help_usage "$@"
 id="${1:-}"
 user="${2:-}"
 password="${3:-}"
-store="${4:-system::system::jenkins}"
-domain="${5:-_}"
+store="${4:-${JENKINS_SECRET_STORE:-system::system::jenkins}}"
+domain="${5:-${JENKINS_SECRET_DOMAIN:-_}}"
 description="${6:-}"
 
 create_credential(){
     local id="$1"
-    local user="$2"
-    local password="$3"
+    local key_value="$2"
+    parse_export_key_value "$key_value"
+    # key/value are exported by above function
+    # shellcheck disable=SC2154
+    local user="$key"
+    # shellcheck disable=SC2154
+    local password="$value"
+    local domain_name="$domain"
+    if [ "$domain_name" = '_' ]; then
+        domain_name='GLOBAL'
+    fi
     xml="<com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl>
-  <scope>GLOBAL</scope>
+  <scope>$domain_name</scope>
   <id>$id</id>
   <description>$description</description>
   <username>$user</username>
   <password>$password</password>
   <usernameSecret>false</usernameSecret>
 </com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl>"
+    timestamp "Creating Jenkins username/password secret '$id' in store '$store' domain '$domain_name'"
     "$srcdir/jenkins_cli.sh" create-credentials-by-xml "$store" "$domain" <<< "$xml"
+    timestamp "Secret '$id' created"
 }
 
 if [ -n "$password" ]; then
-    create_credential "$id" "$user" "$password"
+    create_credential "$id" "$user"="$password"
 else
-    # XXX: switch this to accepting standard 'export KEY=VALUE' format for pipe chaining with other tools
-    while read -r id user password; do
-        [ -n "$password" ] || continue
-        create_credential "$id" "$user" "$password"
-    done
+    while read -r id user_password; do
+        if [ -z "${user_password:-}" ] && [[ "$id" =~ = ]]; then
+            user_password="$id"
+            id="${id%%=*}"
+            id="$(tr '[:upper:]' '[:lower:]' <<< "$id")"
+        else
+            timestamp "WARNING: invalid line detected, skipping creating credential"
+            continue
+        fi
+        create_credential "$id" "$user_password"
+    done < <(sed 's/^[[:space:]]*export[[:space:]]*//; /^[[:space:]]*$/d')
 fi
