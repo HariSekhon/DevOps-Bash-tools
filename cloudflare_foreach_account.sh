cfe272ab984ab3f12924a543147f49e11a28409c
Hari Sekhon <harisekhon@gmail.com>
2022-06-30 17:19:46 +0100

    updated cloudflare_foreach_account.sh

--- cloudflare_foreach_account.sh
+++ cloudflare_foreach_account.sh
@@ -27,6 +27,8 @@ Run a command against each Cloudflare account
 
 All arguments become the command template
 
+WARNING: do not run any command reading from standard input, otherwise it will consume the account id/names and exit after the first iteration
+
 The command template replaces the following for convenience in each iteration:
 
 {id}   - with the account id   <-- this is the one you want for chaining API queries with cloudflare_api.sh
