f08f6d350f89c34bb2f03ae76f5c7f3e2f1797b9
Hari Sekhon <harisekhon@gmail.com>
2022-06-30 17:20:58 +0100

    updated git_foreach_branch.sh

--- git_foreach_branch.sh
+++ git_foreach_branch.sh
@@ -26,6 +26,24 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #. "$srcdir/.bash.d/functions.sh"
 . "$srcdir/.bash.d/git.sh"
 
-foreachbranch "$@"
+usage_description="
+Run a command against each Git branch in the current repo checkout
 
-git checkout master
+Checks out each branch and runs the command, before returning to the original branch
+
+If the command fails on a branch, this script will exit and leave you checked out on that branch
+
+One use case for this is merging your trunk branch into all your feature branches in an automated manner
+
+This is powerful so use carefully!
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<command> <args>"
+
+help_usage "$@"
+
+min_args 1 "$@"
+
+foreachbranch "$@"
