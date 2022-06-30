a3db82e2fb3ecabced0bc986abe52ccc0ef56eec
Hari Sekhon <harisekhon@gmail.com>
2022-06-30 17:20:02 +0100

    updated cloudflare_foreach_zone.sh

--- cloudflare_foreach_zone.sh
+++ cloudflare_foreach_zone.sh
@@ -27,6 +27,8 @@ Run a command against each Cloudflare zone
 
 All arguments become the command template
 
+WARNING: do not run any command reading from standard input, otherwise it will consume the zone id/names and exit after the first iteration
+
 The command template replaces the following for convenience in each iteration:
 
 {id}   - with the zone id   <-- this is the one you want for chaining API queries with cloudflare_api.sh
