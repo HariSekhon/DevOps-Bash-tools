be9cf9a5f56ac965a84c8274170cf6c7e0b5bba2
Hari Sekhon <harisekhon@gmail.com>
2022-07-08 15:17:17 +0100

    added install_k6.sh

---
+++ setup/install_k6.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-07-08 15:17:15 +0100 (Fri, 08 Jul 2022)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/../lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs the K6 load testing CLI by Grafana Labs
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<version>]"
+
+export PATH="$PATH:$HOME/bin"
+
+help_usage "$@"
+
+#min_args 1 "$@"
+
+#version="${1:-0.39.0}"
+version="${1:-latest}"
+
+owner_repo='grafana/k6'
+
+if [ "$version" = latest ]; then
+    timestamp "determining latest version of '$owner_repo' via GitHub API"
+    version="$("$srcdir/../github_repo_latest_release.sh" "$owner_repo")"
+    version="${version#v}"
+    timestamp "latest version is '$version'"
+else
+    is_semver "$version" || die "non-semver version argument given: '$version' - should be in format: N.N.N"
+fi
+
+export RUN_VERSION_ARG=1
+
+os="$(get_os)"
+if [ "$os" = darwin ]; then
+    os=macos
+fi
+
+"$srcdir/../install_binary.sh" "https://github.com/grafana/k6/releases/download/v$version/k6-v$version-$os-{arch}.zip" "k6-v$version-$os-{arch}/k6" k6
