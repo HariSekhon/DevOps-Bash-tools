81c22595aeabcccd6c3d7d3c9a247f79edef8fb6
Hari Sekhon <harisekhon@gmail.com>
2022-07-11 12:27:08 +0100

    updated python_mac_upgrade_ssl_fix.sh

--- setup/python_mac_upgrade_ssl_fix.sh
+++ setup/python_mac_upgrade_ssl_fix.sh
@@ -22,7 +22,7 @@ cd /usr/local/Cellar/openssl/*/lib
 sudo cp libssl.1.0.0.dylib libcrypto.1.0.0.dylib /usr/local/lib/
 cd /usr/local/lib
 [ -f libssl.dylib ] ||
-    $sudo ln -s libssl.1.0.0.dylib libssl.dylib
+    $sudo ln -s -- libssl.1.0.0.dylib libssl.dylib
 [ -f libcrypto.dylib ] ||
-    $sudo ln -s libcrypto.1.0.0.dylib libcrypto.dylib
+    $sudo ln -s -- libcrypto.1.0.0.dylib libcrypto.dylib
 #pip3 install --upgrade packagename
