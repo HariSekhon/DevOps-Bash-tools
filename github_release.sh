fa8d9443e01a59597fa28327f5badcf3ba4983af
Hari Sekhon <harisekhon@gmail.com>
2022-07-12 14:56:26 +0100

    updated github_release.sh

--- github_release.sh
+++ github_release.sh
@@ -24,6 +24,9 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 usage_description="
 Creates a GitHub Release and Git Tag, auto-incrementing the default vYYYY.NN release if one isn't given
 
+Determines the GitHub repository to create a release in from the local checkout from which it is executed,
+unless \$GITHUB_OWNER_REPO is set in the environment or '-R <owner>/<repo>' are given as the final args
+
 The first argument is the version, which is recommended to set to vN.N.N eg. v1.0.0 as per semantic versioning standards
 
 If the first argument is 'date', will determine the next available release in the format vYYYYMMDD.NN where NN is incremented from 1
@@ -32,15 +35,15 @@ If the first argument is 'year', will determine the next available release in th
 
 These formats don't have dashes in them like ISO dates so that if you move from YYYY to YYYYMM format or YYYYMMDD format, GitHub will recognize the newer format as the Latest release
 
-If you later return to short format releases of just year or month, GitHub won't detect them as the Latest release during my testing.
+If you later return to short format releases of just year or month, GitHub won't detect them as the Latest release (determined via testing).
 
 
-$usage_github_cli_required
+Requires GitHub CLI to be installed and configured
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="[<version> <title> <description>]"
+usage_args="[<version> <title> <description> <gh_cli_options>]"
 
 help_usage "$@"
 
@@ -49,6 +52,14 @@ help_usage "$@"
 version="${1:-year}"
 title="${2:-}"
 description="${3:-}"
+shift || :
+shift || :
+shift || :
+
+owner_repo=()
+if [ -n "${GITHUB_OWNER_REPO:-}" ]; then
+    owner_repo=(-R "$GITHUB_OWNER_REPO")
+fi
 
 generate_version=0
 prefix='v'
@@ -68,7 +79,7 @@ elif [ "$version" = day ]; then
 fi
 
 if [ "$generate_version" = 1 ]; then
-    latest_releases="$(gh release list -L 200 --exclude-drafts | awk '{print $1}')"
+    latest_releases="$(gh release list ${owner_repo:+"${owner_repo[@]}"} -L 200 --exclude-drafts "$@" | awk '{print $1}')"
 
     number="$(grep -Eo "^$version"'\.\d+' <<< "$latest_releases" | head -n 1 | sed "s/^$version\\.//" || echo 1)"
 
@@ -87,4 +98,4 @@ if is_blank "$title"; then
     title="$version"
 fi
 
-gh release create "$version" --title "$version" --notes "$description"
+gh release create ${owner_repo:+"${owner_repo[@]}"} "$version" --title "$version" --notes "$description" "$@"
