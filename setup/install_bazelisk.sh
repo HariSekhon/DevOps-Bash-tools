aed57b1cbc852224b0d9d4dd8bcc7bda2e1bd4e1
Hari Sekhon <harisekhon@gmail.com>
2022-07-12 16:26:55 +0100

    updated install_bazelisk.sh

--- setup/install_bazelisk.sh
+++ setup/install_bazelisk.sh
@@ -33,27 +33,9 @@ export PATH="$PATH:$HOME/bin"
 
 help_usage "$@"
 
-#min_args 1 "$@"
-
 #version="${1:-1.10.1}"
 version="${1:-latest}"
 
-owner_repo="bazelbuild/bazelisk"
-
-if [ "$version" = latest ]; then
-    timestamp "determining latest version of '$owner_repo' via GitHub API"
-    version="$("$srcdir/../github_repo_latest_release.sh" "$owner_repo")"
-    version="${version#v}"
-    timestamp "latest version is '$version'"
-else
-    is_semver "$version" || die "non-semver version argument given: '$version' - should be in format: N.N.N"
-fi
-
-os="$(get_os)"
-if [ "$os" = darwin ]; then
-    os=macOS
-fi
-
 export RUN_VERSION_ARG=1
 
-"$srcdir/../install_binary.sh" "https://github.com/$owner_repo/releases/download/v$version/bazelisk-{os}-{arch}" bazelisk
+"$srcdir/../github_install_binary.sh" bazelbuild/bazelisk "bazelisk-{os}-{arch}" bazelisk "$version"
