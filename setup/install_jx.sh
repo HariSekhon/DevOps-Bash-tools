9f14dca00bb27221e0504a2304dfc43a2d672eb5
Hari Sekhon <harisekhon@gmail.com>
2022-07-12 16:55:28 +0100

    updated install_jx.sh

--- setup/install_jx.sh
+++ setup/install_jx.sh
@@ -35,30 +35,12 @@ export PATH="$PATH:$HOME/bin"
 
 help_usage "$@"
 
-#min_args 1 "$@"
-
 #version="${1:-2.4.0}"
 version="${1:-latest}"
 
-owner_repo="jenkins-x/jx"
-
-if [ "$version" = latest ]; then
-    timestamp "determining latest version of '$owner_repo' via GitHub API"
-    version="$("$srcdir/../github_repo_latest_release.sh" "$owner_repo")"
-    version="${version#v}"
-    timestamp "latest version is '$version'"
-else
-    is_semver "$version" || die "non-semver version argument given: '$version' - should be in format: N.N.N"
-fi
-
-os="$(get_os)"
-if [ "$os" = darwin ]; then
-    os=macOS
-fi
-
 export RUN_VERSION_ARG=1
 
-"$srcdir/../install_binary.sh" "https://github.com/$owner_repo/releases/download/v$version/jx-{os}-{arch}.tar.gz" jx
+"$srcdir/../github_install_binary.sh" jenkins-x/jx 'jx-{os}-{arch}.tar.gz' "$version" jx
 
 echo >&2
 jx version --short
