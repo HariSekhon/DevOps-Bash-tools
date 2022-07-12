44efcbc9b5fe82f2494224888ef8d02bfba499b6
Hari Sekhon <harisekhon@gmail.com>
2022-07-12 16:07:06 +0100

    updated install_polaris.sh

--- setup/install_polaris.sh
+++ setup/install_polaris.sh
@@ -48,4 +48,4 @@ fi
 
 export RUN_VERSION_ARG=1
 
-"$srcdir/../install_binary.sh" "https://github.com/FairwindsOps/polaris/releases/download/$version/polaris_{os}_{arch}.tar.gz" polaris
+"$srcdir/../install_binary.sh" "https://github.com/$owner_repo/releases/download/$version/polaris_{os}_{arch}.tar.gz" polaris
