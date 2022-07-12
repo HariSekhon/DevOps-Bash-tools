31c5fccafe3bd307ff3aa522447bff884a3a3d9b
Hari Sekhon <harisekhon@gmail.com>
2022-07-12 17:05:08 +0100

    updated install_pluto.sh

--- setup/install_pluto.sh
+++ setup/install_pluto.sh
@@ -31,22 +31,9 @@ usage_args="[<version>]"
 
 help_usage "$@"
 
-#min_args 1 "$@"
-
 #version="${1:-5.6.0}"
 version="${1:-latest}"
 
-owner_repo="FairwindsOps/pluto"
-
-if [ "$version" = latest ]; then
-    timestamp "determining latest version of '$owner_repo' via GitHub API"
-    version="$("$srcdir/../github_repo_latest_release.sh" "$owner_repo")"
-    timestamp "latest version is '$version'"
-else
-    is_semver "$version" || die "non-semver version argument given: '$version' - should be in format: N.N.N"
-    [[ "$version" =~ ^v ]] || version="v$version"
-fi
-
 export RUN_VERSION_ARG=1
 
-"$srcdir/../install_binary.sh" "https://github.com/$owner_repo/releases/download/$version/pluto_${version#v}_{os}_{arch}.tar.gz" pluto
+"$srcdir/../github_install_binary.sh" FairwindsOps/pluto 'pluto_{version}_{os}_{arch}.tar.gz' "$version" pluto
