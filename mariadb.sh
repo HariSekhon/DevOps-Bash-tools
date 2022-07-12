31fbd23a3911c116c01c427276a34a6cd902812d
Hari Sekhon <harisekhon@gmail.com>
2022-07-12 16:02:01 +0100

    updated mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -147,7 +147,7 @@ if [ -n "${MARIADB_RESTART:-}" ]; then
     docker_pull "$docker_image:$version"
 
     timestamp "killing existing MariaDB container:"
-    docker rm -f "$container_name" 2>/dev/null || :
+    docker rm -f -- "$container_name" 2>/dev/null || :
 fi
 
 if ! docker_container_exists "$container_name"; then
