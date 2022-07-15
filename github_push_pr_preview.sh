73aa15114107cdbe9cae46ac70e0b915a86f23d3
Hari Sekhon <harisekhon@gmail.com>
2022-07-15 15:50:30 +0100

    updated github_push_pr_preview.sh

--- github_push_pr_preview.sh
+++ github_push_pr_preview.sh
@@ -22,14 +22,14 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Pushes the current branch to GitHub origin, setting upstream branch, then opens a Pull Request preview from this to the default branch
+Pushes the current branch to GitHub origin, setting upstream branch, then opens a Pull Request preview from this to the given or default branch
 
 Assumes that GitHub is the remote origin, and checks for this for safety
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args=""
+usage_args="[<target_base_branch> <head_branch>]"
 
 help_usage "$@"
 
@@ -41,4 +41,4 @@ current_branch="$(current_branch)"
 
 git push --set-upstream origin "$current_branch"
 
-"$srcdir/github_pull_request_preview.sh"
+"$srcdir/github_pull_request_preview.sh" "$@"
