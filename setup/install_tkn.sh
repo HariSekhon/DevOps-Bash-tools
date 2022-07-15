ea91fe434874005f670d3e456b35e49059ea419d
Hari Sekhon <harisekhon@gmail.com>
2022-07-15 19:37:05 +0100

    added install_tkn.sh

---
+++ setup/install_tkn.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-07-15 19:34:06 +0100 (Fri, 15 Jul 2022)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/../lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs Tekton CLI
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<version>]"
+
+export PATH="$PATH:$HOME/bin"
+
+help_usage "$@"
+
+#version="${1:-2.4.0}"
+version="${1:-latest}"
+
+os="$(get_os)"
+if [ "$os" = darwin ]; then
+    os=macOS
+fi
+
+export RUN_VERSION_ARG=1
+
+os="$(uname -s)"
+arch="$(uname -m)"
+if [ "$os" = Darwin ]; then
+    arch=all
+fi
+
+package="tkn_{version}_${os}_${arch}.tar.gz"
+
+"$srcdir/../github_install_binary.sh" tektoncd/cli "$package" "$version" tkn
