32f88a6dcd491b46e17be784d35b6aec7401a3d5
Hari Sekhon <harisekhon@gmail.com>
2022-07-15 16:12:29 +0100

    updated github_merge_branch.sh

--- github_merge_branch.sh
+++ github_merge_branch.sh
@@ -58,7 +58,10 @@ output="$("$srcdir/github_pull_request_create.sh" "$@")"
 if [ -n "$output" ]; then
     # defined in lib/github.sh
     # shellcheck disable=SC2154
-    url="$(grep -Eom1 "$github_pull_request_url_regex" <<< "$output" || die "Failed to generate Pull Request or couldn't find pull request URL in output")"
+    url="$(
+        grep -Eom1 "$github_pull_request_url_regex" <<< "$output" ||
+        die "Failed to generate Pull Request or couldn't find pull request URL in output"
+    )"
     timestamp "Merging Pull Request $url"
     gh pr merge --merge "$url"
     echo >&2
