9c2b27bf346a996eb7a16de9db113cdb5dbe463e
Hari Sekhon <harisekhon@gmail.com>
2022-07-21 09:46:56 +0100

    added install_pulumi_cli.sh

---
+++ setup/install_pulumi_cli.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-07-21 09:46:37 +0100 (Thu, 21 Jul 2022)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# installs to ~/.pulumi/bin/
+curl -fsSL https://get.pulumi.com | sh
