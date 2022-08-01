f22e8abd7aae2b703578a64f1dfe9629ff7b7ec7
Hari Sekhon <harisekhon@gmail.com>
2022-08-01 15:15:07 +0100

    updated kubectl_get_annotation.sh

--- kubectl_get_annotation.sh
+++ kubectl_get_annotation.sh
@@ -22,11 +22,11 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Returns all objects of a given type that have the given annotation set, optionally to a given value
+Returns objects of a given type that have the given annotation set, optionally to a given value
 
 Filtering by label is easy, but when you want to filter by annotation this is trickier
 
-This script makes it easy to do
+This script makes it easy to do in the current namespace
 
 Output:
 
@@ -38,7 +38,7 @@ Kubectl needs to be in the \$PATH and configured
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="<object_kind> <annotation> [<value>]"
+usage_args="<object_kind> <annotation> [<value> <kubectl_options>]"
 
 help_usage "$@"
 
@@ -47,13 +47,16 @@ min_args 2 "$@"
 kind="$1"
 annotation="$2"
 value="${3:-}"
+shift || :
+shift || :
+shift || :
 
 annotation_escaped="${annotation//./\\.}"
 value_escaped="${value//\"/\\\"}"
 
 # can't decompose this line across lines unfortunately for clarity, fails to parse
 if [ -n "$value" ]; then
-    kubectl get "$kind" -o jsonpath="{range .items[?(@.metadata.annotations.$annotation_escaped==\"$value_escaped\")]}{.metadata.name}{\"\\t$annotation=\"}{.metadata.annotations.$annotation_escaped}{\"\\n\"}" | column -t
+    kubectl get "$kind" "$@" -o jsonpath="{range .items[?(@.metadata.annotations.$annotation_escaped==\"$value_escaped\")]}{.metadata.name}{\"\\t$annotation=\"}{.metadata.annotations.$annotation_escaped}{\"\\n\"}" | column -t
 else
-    kubectl get "$kind" -o jsonpath="{range .items[?(@.metadata.annotations.$annotation_escaped)]}{.metadata.name}{\"\\t$annotation=\"}{.metadata.annotations.$annotation_escaped}{\"\\n\"}" | column -t
+    kubectl get "$kind" "$@" -o jsonpath="{range .items[?(@.metadata.annotations.$annotation_escaped)]}{.metadata.name}{\"\\t$annotation=\"}{.metadata.annotations.$annotation_escaped}{\"\\n\"}" | column -t
 fi
