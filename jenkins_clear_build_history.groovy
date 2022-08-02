9fb17ecd5385ed1a72d14b47b02c7be113507409
Hari Sekhon <harisekhon@gmail.com>
2022-08-02 17:29:57 +0100

    updated jenkins_clear_build_history.groovy

--- jenkins_clear_build_history.groovy
+++ jenkins_clear_build_history.groovy
@@ -17,6 +17,13 @@
 //
 // Jenkins -> Manage Jenkins -> Script Console
 
+// WARNING: you can run into all sorts of state problems with the Milestone plugin preventing your builds from running due to a remnant reference to a higher build number - this also happens on job restores which start from build #1 again
+//
+//			[2022-08-02T16:14:46.501Z] Trying to pass milestone 0
+//			[2022-08-02T16:14:46.501Z] Canceled since build #3814 already got here
+//
+//			https://issues.jenkins.io/browse/JENKINS-38641
+
 // XXX: Edit this to the name of your job pipeline
 def jobName = "My Dev Pipeline"
 def job = Jenkins.instance.getItem(jobName)
