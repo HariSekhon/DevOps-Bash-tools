b7758e10fd23d45fcd1e92c5f2e623465d96ca43
Hari Sekhon <harisekhon@gmail.com>
2022-08-03 22:52:43 +0100

    updated ci_git_set_dir_safe.sh

--- setup/ci_git_set_dir_safe.sh
+++ setup/ci_git_set_dir_safe.sh
@@ -13,11 +13,19 @@
 #  https://www.linkedin.com/in/HariSekhon
 #
 
+# Necessary for some CI/CD systems like Azure DevOps Pipelines which have incorrect ownership on the git checkout dir triggering this error:
+#
+#    fatal: detected dubious ownership in repository at '/code/sql'
+
+# standalone script without lib dependency so it can be called directly from bootstrapped CI before submodules, since that is the exact problem that needs to be solved to allow CI/CD systems with incorrect ownership of the checkout directory to be able to checkout the necessary git submodules
+
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd "$srcdir/.."
+dir="${1:-$srcdir/..}"
+
+cd "$dir"
 
 echo "Setting directory as safe: $PWD"
 git config --global --add safe.directory "$PWD"
