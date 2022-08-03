c98599b3e2b69dff52ef66bf49a800c63ef82e63
Hari Sekhon <harisekhon@gmail.com>
2022-08-03 23:01:05 +0100

    updated sync_bootstraps_to_adjacent_repos.sh

--- sync_bootstraps_to_adjacent_repos.sh
+++ sync_bootstraps_to_adjacent_repos.sh
@@ -33,14 +33,13 @@ fi |
 grep -v -e bash-tools -e '^[[:space:]]*$' |
 while read -r repo dir; do
     if [ -z "$dir" ]; then
-        dir="$repo"
+        dir="$(tr '[:upper:]' '[:lower:]' <<< "$repo")"
     fi
     # filtered above
     #if ls -lLdi "$dir" "$srcdir" | awk '{print $1}' | uniq -d | grep -q .; then
     #    echo "skipping $dir as it's our directory"
     #    continue
     #fi
-    repo="$(tr '[:upper:]' '[:lower:]' <<< "$repo")"
     if ! [ -d "../$dir" ]; then
         echo "WARNING: repo dir $dir not found, skipping..."
         continue
@@ -50,9 +49,9 @@ while read -r repo dir; do
         mkdir -pv "${target%/*}"
         echo "syncing $filename -> $target"
         perl -pe "
+            s/directory=\"(devops-)*bash-tools/directory=\"$dir/;
             s/(devops-)*bash-tools/$repo/i;
-            s/make install/make/;
-            s/directory=\"devops-/directory=\"/;" \
+            s/make install/make/;" \
             "$filename" > "$target"
         if [ "$repo" = "nagios-plugins" ]; then
             perl -pi -e 's/^(\s*make)$/$1 build zookeeper/' "$target"
