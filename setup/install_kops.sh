33a60ac2f49548a536673fcabae0516c29340f84
Hari Sekhon <harisekhon@gmail.com>
2022-08-10 10:53:22 +0100

    added install_kops.sh

---
+++ setup/install_kops.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-08-10 10:49:47 +0100 (Wed, 10 Aug 2022)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/../lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs Kubernetes Kops CLI
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<version>]"
+
+export PATH="$PATH:$HOME/bin"
+
+help_usage "$@"
+
+#version="${1:-1.24.1}"
+version="${1:-latest}"
+
+export RUN_VERSION_ARG=1
+
+"$srcdir/../github_install_binary.sh" kubernetes/kops 'kops-{os}-{arch}' "$version"
