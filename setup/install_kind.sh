f87028bf397fcc8eec2b11e811396e9bcb615283
Hari Sekhon <harisekhon@gmail.com>
2022-08-13 10:05:36 +0100

    added install_kind.sh

---
+++ setup/install_kind.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-08-13 10:03:47 +0100 (Sat, 13 Aug 2022)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+. "$srcdir/../lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs Kind (Kubernetes in Docker)
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<version>]"
+
+export PATH="$PATH:$HOME/bin"
+
+help_usage "$@"
+
+#version="${1:-2.4.0}"
+version="${1:-latest}"
+
+os="$(get_os)"
+if [ "$os" = darwin ]; then
+    os=macOS
+fi
+
+export RUN_VERSION_ARG=1
+
+"$srcdir/../github_install_binary.sh" kubernetes-sigs/kind 'kind-{os}-{arch}' "$version"
