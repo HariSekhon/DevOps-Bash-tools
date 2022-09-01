89a30ae89fecddacdad7f4d28dafc0d78820cd4d
Hari Sekhon <harisekhon@gmail.com>
2022-09-01 12:33:25 +0100

    updated kustomize_parse_helm_charts.sh

--- kustomize_parse_helm_charts.sh
+++ kustomize_parse_helm_charts.sh
@@ -52,4 +52,5 @@ min_args 1 "$@"
 
 type -P yq &>/dev/null || "$srcdir/setup/install_yq.sh"
 
-yq '.helmCharts[] | [.repo, .name, .version, .valuesFile] | @tsv' "$@"
+yq '.helmCharts[] | [.repo, .name, .version, .valuesFile] | @tsv' "$@" --no-doc --no-colors |
+sed '/^[[:space:]]*$/d'
