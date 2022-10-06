de2e2439cbf1a96c863cdb0946ca25d0e953d3b8
Hari Sekhon <harisekhon@gmail.com>
2022-10-06 15:46:24 +0100

    updated kubernetes_secrets_to_sealed_secrets.sh

--- kubernetes_secrets_to_sealed_secrets.sh
+++ kubernetes_secrets_to_sealed_secrets.sh
@@ -17,10 +17,10 @@ set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# shellcheck disable=SC1090
+# shellcheck disable=SC1090,SC1091
 . "$srcdir/lib/utils.sh"
 
-# shellcheck disable=SC1090
+# shellcheck disable=SC1090,SC1091
 . "$srcdir/lib/kubernetes.sh"
 
 # shellcheck disable=SC2034,SC2154
@@ -68,23 +68,7 @@ grep -v kubernetes.io/service-account-token |
 grep -v '^NAME[[:space:]]' |
 awk '{print $1}' |
 while read -r secret; do
-    yaml="sealed-secret-$secret.yaml"
-
-    timestamp "Generating sealed secret for secret '$secret'"
-
-    kubectl get secret "$secret" -o yaml |
-    kubeseal -o yaml > "$yaml"
-
-    timestamp "Generated:  $yaml"
-
-    timestamp "Annotating secret '$secret' to be managed by sealed-secrets controller"
-
-    kubectl annotate secrets "$secret" 'sealedsecrets.bitnami.com/managed=true' --overwrite
-
-    timestamp "Creating sealed secret '$secret'"
-
-    kubectl apply -f "$yaml"
-
+    "$srcdir/kubernetes_secret_to_sealed_secret.sh" "$secret" ${namespace:+"$namespace"} ${context:+"$context"}
     echo
 done
 
