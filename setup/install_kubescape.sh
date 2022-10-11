d7cd8fd0a3a43516e5e35588a6a46c1953f88c18
Hari Sekhon <harisekhon@gmail.com>
2022-10-11 10:25:08 +0100

    added install_kubescape.sh

---
+++ setup/install_kubescape.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-10-11 10:21:17 +0100 (Tue, 11 Oct 2022)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+curl -s https://raw.githubusercontent.com/kubescape/kubescape/master/install.sh | /bin/bash
