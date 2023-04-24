c019bd3ff65abbd28b9c11a3ea78a1f89ea9c041
Hari Sekhon <harisekhon@gmail.com>
2023-04-24 23:13:07 +0100

    updated word_frequency.sh

--- word_frequency.sh
+++ word_frequency.sh
@@ -23,21 +23,29 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC2034,SC2154
 usage_description="
 Creates a word freqency list ranked by most used words at the top
+
+Works like a standard unix filter program - pass in stdin or give it a filename, and outputs to stdout, so you can continue to pipe or redirect to a file as usual
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="<filename> [<options>]"
+usage_args="[<filename>]"
 
 help_usage "$@"
 
-min_args 1 "$@"
+#min_args 1 "$@"
 
-filename="$1"
+#filename="$1"
+
+if [ $# -eq 0 ]; then
+    echo "Reading from stdin" >&2
+fi
 
 #output_file="$filename.word_frequency.txt"
 
-tr ' ' '\n' < "$filename" |
+# one of the few legit uses of cat - tr can't process a filename arg or stdin
+cat "$@" |
+tr ' ' '\n' |
 sed '
     /^[[:space:]]*$/d;
     # because sometimes you want to see the occurence of emojis in WhatsApp chats
