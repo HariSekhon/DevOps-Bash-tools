32576cee9db44ef51027cc2eab4ffe86960a4867
Hari Sekhon <harisekhon@gmail.com>
2023-05-04 22:27:48 +0100

    updated kubernetes.sh

--- .bash.d/kubernetes.sh
+++ .bash.d/kubernetes.sh
@@ -95,6 +95,8 @@ alias use="k config use-context"
 alias contexts="k config get-contexts"
 #alias context="k config current-context"
 context(){ k config current-context; }
+alias con=context
+alias cons=contexts
 # contexts has this info and is more useful
 #alias clusters="k config get-clusters"
 
