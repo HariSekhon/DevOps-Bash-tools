04aba0732e6d9ad8061ba013a34be8e413c439ff
Hari Sekhon <harisekhon@gmail.com>
2023-05-08 18:06:01 +0100

    added github_purge_camo_cache.sh

---
+++ github_purge_camo_cache.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2023-05-08 18:00:53 +0100 (Mon, 08 May 2023)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/github.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Purges all GitHub camo caches for things like CI/CD badges
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<owner>/<repo>"
+
+help_usage "$@"
+
+min_args 1 "$@"
+
+owner_repo="$1"
+
+if ! is_github_owner_repo "$owner_repo"; then
+	usage "Invalid owner/repo given: $owner_repo"
+fi
+
+url="https://github.com/$owner_repo"
+
+timestamp "Fetching: $url"
+curl -sL "$url" |
+grep -Eo '<img src="https?://camo.githubusercontent.com/[^"]+' |
+sed -e 's/<img src="//' |
+while read -r camo_url; do
+
+    timestamp "Purging: $camo_url"
+    echo "curl -sX PURGE '$camo_url' &>/dev/null"
+
+done |
+parallel -j 10
