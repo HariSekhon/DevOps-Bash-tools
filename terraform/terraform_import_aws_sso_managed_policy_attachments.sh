cf2c0adfd564928a86039ad37f829bd16c7f1f16
Hari Sekhon <harisekhon@gmail.com>
2023-05-09 01:40:41 +0100

    updated terraform_import_aws_sso_managed_policy_attachments.sh

--- terraform/terraform_import_aws_sso_managed_policy_attachments.sh
+++ terraform/terraform_import_aws_sso_managed_policy_attachments.sh
@@ -54,7 +54,7 @@ while read -r name instance_arn managed_policy_arn permission_set_arn; do
     [ -n "$permission_set_arn" ] || continue
     timestamp "Importing $name"
     cmd=(terraform import "$name" "$managed_policy_arn,$permission_set_arn,$instance_arn")
-    echo "${cmd[@]}"
+    timestamp "${cmd[*]}"
     if [ -z "${TERRAFORM_PRINT_ONLY:-}" ]; then
         "${cmd[@]}"
     fi
