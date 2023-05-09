728f070ee2c5e4a09617a4e6284b021e25f36f16
Hari Sekhon <harisekhon@gmail.com>
2023-05-09 02:52:53 +0100

    updated mac_delete_routes_on_interface.sh

--- setup/mac_delete_routes_on_interface.sh
+++ setup/mac_delete_routes_on_interface.sh
@@ -18,7 +18,7 @@ set -euo pipefail
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC1090,SC1091
-. "$srcdir/lib/utils.sh"
+. "$srcdir/../lib/utils.sh"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
