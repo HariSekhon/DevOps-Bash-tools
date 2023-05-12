c0409dd69f19bf7353ead43979a7728a4649904d
Hari Sekhon <harisekhon@gmail.com>
2023-05-12 23:46:26 +0100

    updated kafka.sh

--- .bash.d/kafka.sh
+++ .bash.d/kafka.sh
@@ -20,7 +20,7 @@
 srcdir="${srcdir:-$(dirname "${BASH_SOURCE[0]}")/..}"
 
 # shellcheck disable=SC1090,SC1091
-type add_PATH &>/dev/null || . "$srcdir/.bash.d/paths.sh"
+#type add_PATH &>/dev/null || . "$srcdir/.bash.d/paths.sh"
 
 for directory in \
     /usr/hdp/current/kafka-broker \
