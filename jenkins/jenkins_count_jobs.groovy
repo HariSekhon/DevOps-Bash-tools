f10364c5e5a2e332f050d6c2f288838f3982d0d7
Hari Sekhon <harisekhon@gmail.com>
2023-05-18 23:15:34 +0100

    added jenkins_count_jobs.groovy

---
+++ jenkins/jenkins_count_jobs.groovy
@@ -0,0 +1,20 @@
+//
+//  Author: Hari Sekhon
+//  Date: 2021-04-08 19:16:17 +0100 (Thu, 08 Apr 2021)
+//
+//  vim:ts=4:sts=4:sw=4:noet
+//
+//  https://github.com/HariSekhon/DevOps-Bash-tools
+//
+//  License: see accompanying Hari Sekhon LICENSE file
+//
+//  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+//
+//  https://www.linkedin.com/in/HariSekhon
+//
+
+// Paste this into $JENKINS_URL/script
+//
+// Jenkins -> Manage Jenkins -> Script Console
+
+jenkins.model.Jenkins.instance.items.findAll().size
