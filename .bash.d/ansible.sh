f39e1332ac3eb1e79982f5a3930c0f6e2f9509f0
Hari Sekhon <harisekhon@gmail.com>
2023-05-31 06:12:59 +0100

    updated ansible.sh

--- .bash.d/ansible.sh
+++ .bash.d/ansible.sh
@@ -24,8 +24,6 @@ bash_tools="${bash_tools:-$(dirname "${BASH_SOURCE[0]}")/..}"
 #type add_PATH &>/dev/null || . "$bash_tools/.bash.d/paths.sh"
 
 # env var takes preference, then cwd, then $HOME, then /etc/ansible/ansible.cfg
-# $srcdir set in .bashrc
-# shellcheck disable=SC2154
 export ANSIBLE_CONFIG=~/.ansible.cfg  # symlinked to $bash_tools/configs/.ansible.cfg
 
 if [ -n "${ANSIBLE_HOME:-}" ]; then
