ce2ff840e331b23da573c15fd13fbdd791c2140d
Hari Sekhon <harisekhon@gmail.com>
2023-12-04 13:17:33 +0000

    added wordpress.sh

---
+++ bin/wordpress.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2023-12-04 12:21:15 +0000 (Mon, 04 Dec 2023)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+# https://hub.docker.com/_/wordpress
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# dipping into interactive library for opening browser to Wordpress
+# XXX: order is important here because there is an interactive library of retry() and a scripting library version of retry() and we want the latter, which must be imported second
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/.bash.d/network.sh"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+export WORDPRESS_URL="http://${DOCKER_HOST:-localhost}:8080"
+export COMPOSE_PROJECT_NAME="bash-tools"
+export COMPOSE_FILE="$srcdir/../docker-compose/wordpress.yml"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Boots a quick Wordpress blog container
+
+Increases Wordpress
+
+On Mac also opens the Wordpress URL in the default browser:
+
+  $WORDPRESS_URL
+
+To boot a specific version of Wordpress:
+
+export VERSION=6.4
+
+Tested on Wordpress 6.4
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[ up | down | ui ]"
+
+help_usage "$@"
+
+if ! type docker-compose &>/dev/null; then
+    "$srcdir/../setup/install_docker_compose.sh"
+fi
+
+action="${1:-up}"
+shift || :
+
+if [ "$action" = up ]; then
+    timestamp "Booting Wordpress:"
+    docker-compose up -d "$@"
+    echo
+    when_url_content 60 "$WORDPRESS_URL" '.*'
+    echo
+    timestamp "Setting php_value upload_max_filesize 256M"
+    docker container exec -it wordpress bash -c "echo 'php_value upload_max_filesize 256M' > '/var/www/html/.htaccess'"
+    echo
+    exec "${BASH_SOURCE[0]}" ui
+elif [ "$action" = restart ]; then
+    docker-compose down
+    echo
+    exec "${BASH_SOURCE[0]}" up
+elif [ "$action" = ui ]; then
+    echo "Wordpress URL:  $WORDPRESS_URL"
+    if is_mac; then
+        open "$WORDPRESS_URL"
+    fi
+else
+    docker-compose "$action" "$@"
+    echo
+fi
