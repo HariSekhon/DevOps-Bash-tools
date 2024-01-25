4cef29ab7fe60092ae706452278abdd6a293bc11
Hari Sekhon <harisekhon@gmail.com>
2024-01-25 13:36:24 +0000

    updated env.sh

--- .bash.d/env.sh
+++ .bash.d/env.sh
@@ -83,6 +83,12 @@ fi
 
 #export DISTCC_DIR="/var/tmp/portage/.distcc/"
 
+if is_mac; then
+    if [ -x /opt/homebrew/bin/brew ]; then
+        eval $(/opt/homebrew/bin/brew shellenv)
+    fi
+fi
+
 # ============================================================================ #
 
 if is_mac; then
