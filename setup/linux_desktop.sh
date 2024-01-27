0bd4b8f04cc9d9a3abdd7b8d07f2433882e6eb31
Hari Sekhon <harisekhon@gmail.com>
2024-01-27 01:12:22 +0000

    updated linux_desktop.sh

--- setup/linux_desktop.sh
+++ setup/linux_desktop.sh
@@ -29,13 +29,15 @@ install_scripts="
 install_ansible.sh
 install_diff-so-fancy.sh
 install_gcloud_sdk.sh
-install_parquet-tools.sh
 install_sdkman.sh
 install_sdkman_all_sdks.sh
 install_terraform.sh
-install_travis.sh
 install_vundle.sh
 "
+# don't use this much any more
+#install_parquet-tools.sh
+# Legacy CI
+#install_travis.sh
 
 for x in $install_scripts; do
     run "$x"
