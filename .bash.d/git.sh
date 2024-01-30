77f6766ddc9303322003588d4b90db2608dc23b0
Hari Sekhon <harisekhon@gmail.com>
2024-01-30 18:06:27 +0000

    updated git.sh

--- .bash.d/git.sh
+++ .bash.d/git.sh
@@ -186,7 +186,7 @@ git_url_base(){
     sed 's|^ssh://||;
          s|^https://.*@||;
          s|^https://||;
-         s|:[[:digit:]]*||;
+         s|:[[:digit:]][[:digit:]]*||;
          s/^git@ssh.dev.azure.com:v3/dev.azure.com/;
          s|^git@||;
          s|^|https://|;
