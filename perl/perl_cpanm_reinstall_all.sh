db043314a2cecb97fe0e1b05005f10ac68e6529b
Hari Sekhon <harisekhon@gmail.com>
2024-01-30 21:13:54 +0000

    added perl_cpanm_reinstall_all.sh

---
+++ perl/perl_cpanm_reinstall_all.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2024-01-30 21:00:19 +0000 (Tue, 30 Jan 2024)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+CPANM="${CPANM:-cpanm}"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Re-Installs all currently installed Perl CPAN modules using Cpanm, taking in to account library paths, perlbrew envs etc
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+num_args 0 "$@"
+
+echo "Re-Installing ALL CPAN Modules"
+echo
+
+export CPANM_OPTS="--reinstall"
+
+cpan -l |
+sed $'s/\t/@/; s/@undef$//' |
+xargs "$srcdir/perl_cpanm_install.sh"
