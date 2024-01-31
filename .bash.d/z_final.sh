2d132e816f442fb331db9108a75b1eb98753274d
Hari Sekhon <harisekhon@gmail.com>
2024-01-31 01:53:44 +0000

    added z_final.sh

---
+++ .bash.d/z_final.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2230
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: circa 2006 (forked from .bashrc)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+# ============================================================================ #
+#                               Finalization
+# ============================================================================ #
+
+# place to add last minute clean ups like path deduping
+
+# slows down new shells
+dedupe_paths
