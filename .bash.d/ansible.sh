7894992757e3774b2a83bf6b69447ba2388ff231
Hari Sekhon <harisekhon@gmail.com>
2024-02-23 17:44:52 +0000

    updated ansible.sh

--- .bash.d/ansible.sh
+++ .bash.d/ansible.sh
@@ -24,7 +24,17 @@ bash_tools="${bash_tools:-$(dirname "${BASH_SOURCE[0]}")/..}"
 #type add_PATH &>/dev/null || . "$bash_tools/.bash.d/paths.sh"
 
 # env var takes preference, then cwd, then $HOME, then /etc/ansible/ansible.cfg
-export ANSIBLE_CONFIG=~/.ansible.cfg  # symlinked to $bash_tools/configs/.ansible.cfg
+#
+# the default order is:
+#
+#   $PWD/ansible.cfg
+#   $HOME/.ansible.cfg
+#   /etc/ansible/ansible.cfg
+#
+#
+# so don't set this because it'll cause issues in work repos which would otherwise default to $PWD/ansible.cfg
+#
+#export ANSIBLE_CONFIG=~/.ansible.cfg  # symlinked to $bash_tools/configs/.ansible.cfg
 
 if [ -n "${ANSIBLE_HOME:-}" ]; then
     add_PATH PYTHONPATH "$ANSIBLE_HOME/lib"
