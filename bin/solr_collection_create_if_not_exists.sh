701ad7843f244c38b09f7ff2310996af0b68c06a
Hari Sekhon <harisekhon@gmail.com>
2024-02-26 00:01:09 +0000

    added solr_collection_create_if_not_exists.sh

---
+++ bin/solr_collection_create_if_not_exists.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2024-02-25 23:53:53 +0000 (Sun, 25 Feb 2024)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Creates a Solr Collection if it does not exist
+
+Uses the adjacent script solr_api.sh - see it for required environment variables and authentication
+
+Set environment variables
+
+NUM_SHARDS              defaults to 3
+REPLICATION_FACTOR      defaults to 3
+NUM_SHARDS_PER_NODE     defaults to 9
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<collection_name> [<curl_options>]"
+
+help_usage "$@"
+
+min_args 1 "$@"
+
+collection="$1"
+shift || :
+
+NUM_SHARDS="${NUM_SHARDS:-3}"
+MAX_SHARDS_PER_NODE="${MAX_SHARDS_PER_NODE:-9}"
+REPLICATION_FACTOR="${REPLICATION_FACTOR:-3}"
+
+timestamp "Checking if collection '$collection' already exists"
+if "$srcdir/solr_api.sh" "/solr/admin/collections?action=LIST" | jq -r '.collections[]' | grep -Fxq "$collection"; then
+    timestamp "Collection '$collection already exists, skipping create"
+else
+    "$srcdir/solr_api.sh" "/solr/admin/collections?action=CREATE&name=$collection&numShards=$NUM_SHARDS&maxShardsPerNode=$MAX_SHARDS_PER_NODE&replicationFactor=$REPLICATION_FACTOR&wt=xml&collection.configName=$collection&autoAddReplicas=true"
+fi
