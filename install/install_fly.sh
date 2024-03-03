6384bb3288b5509dbad129483e9a01d021ebe530
Hari Sekhon <harisekhon@gmail.com>
2024-03-03 02:21:54 +0000

    added install_fly.sh

---
+++ install/install_fly.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2024-03-03 02:14:22 +0000 (Sun, 03 Mar 2024)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/../lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs Concourse Fly CLI
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<version>]"
+
+export PATH="$PATH:$HOME/bin"
+
+help_usage "$@"
+
+#version="${1:-2.4.0}"
+version="${1:-latest}"
+
+export RUN_VERSION_ARG=1
+
+"$srcdir/../github/github_install_binary.sh" concourse/concourse "fly-{version}-{os}-{arch}.tgz" "$version" "fly-{version}-{os}-{arch}/bin/fly"
