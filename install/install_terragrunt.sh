4b68222676b3faa812f9477d7adae2d4a536f416
Hari Sekhon <harisekhon@gmail.com>
2024-06-19 18:35:20 +0200

    updated install_terragrunt.sh

--- install/install_terragrunt.sh
+++ install/install_terragrunt.sh
@@ -43,3 +43,6 @@ version="${1:-${TERRAGRUNT_VERSION:-latest}}"
 export RUN_VERSION_OPT=1
 
 "$srcdir/../github/github_install_binary.sh" gruntwork-io/terragrunt 'terragrunt_{os}_{arch}' "$version" terragrunt
+
+#echo
+#terragrunt --install-autocomplete
