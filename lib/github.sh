b31489667603674dbf249eb16b018814d572b7ce
Hari Sekhon <harisekhon@gmail.com>
2024-06-21 14:05:13 +0200

    updated github.sh

--- lib/github.sh
+++ lib/github.sh
@@ -68,14 +68,20 @@ github_origin_owner_repo(){
         ' ||
         :
     )"
-    is_github_owner_repo "$owner_repo" || die "<owner>/<repo> '$owner_repo' does not match expected format"
+    if ! is_github_owner_repo "$owner_repo"; then
+        echo "<owner>/<repo> '$owner_repo' does not match expected format" >&2
+        return 1
+    fi
     echo "$owner_repo"
 }
 
 github_upstream_owner_repo(){
     local owner_repo
     owner_repo="$(gh repo view --json parent | jq -r '.parent | .owner.login + "/" + .name')"
-    is_github_owner_repo "$owner_repo" || die "<owner>/<repo> '$owner_repo' does not match expected format"
+    if is_github_owner_repo "$owner_repo"; then
+        echo "<owner>/<repo> '$owner_repo' does not match expected format" >&2
+        return 1
+    fi
     echo "$owner_repo"
 }
 
