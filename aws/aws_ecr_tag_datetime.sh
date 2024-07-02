786711afe2b95b3ce339f9eb1aa2a2e69c0cdad5
Hari Sekhon <harisekhon@gmail.com>
2024-07-02 19:49:55 +0200

    updated aws_ecr_tag_datetime.sh

--- aws/aws_ecr_tag_datetime.sh
+++ aws/aws_ecr_tag_datetime.sh
@@ -23,7 +23,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Tags a given GCR docker image with it's creation Date and Timestamp
+Tags a given ECR docker image with it's creation Date and Timestamp
 without pulling and pushing the docker image
 
 The timestamp is the created time (either uploaded or created by Google Cloud Build)
