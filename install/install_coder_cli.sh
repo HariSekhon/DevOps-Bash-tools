59c2a1eebd9c4651cfe0c60efcb682fb7732bced
Hari Sekhon <harisekhon@gmail.com>
2024-07-03 23:34:58 +0200

    updated install_coder_cli.sh

--- install/install_coder_cli.sh
+++ install/install_coder_cli.sh
@@ -22,15 +22,36 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Installs the Coder CLI
+Installs the Coder CLI via GitHub binary releases
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args=""
+usage_args="[<version>]"
 
 help_usage "$@"
 
-num_args 0 "$@"
+max_args 1 "$@"
 
-curl -L https://coder.com/install.sh | sh
+#version="${1:-2.13.0}"
+version="${1:-latest}"
+
+# on Mac just runs:
+#
+#   brew install coder/coder/coder
+#
+# which we can do via ../setup/brew-packages-desktop-taps.txt
+#
+#curl -L https://coder.com/install.sh | sh
+
+export ARCH_X86_64=amd64
+export ARCH_ARM64=amd64
+
+export RUN_VERSION_ARG=1
+
+ext="tar.gz"
+if is_mac; then
+    ext="zip"
+fi
+
+"$srcdir/../github/github_install_binary.sh" coder/coder "coder_{version}_{os}_{arch}.$ext" "$version" "coder_{version}_{os}_{arch}/bin/coder"
