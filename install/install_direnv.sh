6aa98a4fb94f8e48b379bd8c69fd275dffd87be9
Hari Sekhon <harisekhon@gmail.com>
2024-08-10 13:19:17 +0300

    updated install_direnv.sh

--- install/install_direnv.sh
+++ install/install_direnv.sh
@@ -43,4 +43,9 @@ if type -P direnv &>/dev/null; then
     exit 0
 fi
 
+# clean PATH because the direnv installer will write the 'direnv' binary to the first available user writeable path
+# and we don't want it putting it somewhere like ~/github/bash-tools - mixed in with git repo and scripts
+PATH="$(tr ':' '\n' <<< "$PATH" | grep -e '^/bin' -e '^/usr' | tr '\n' ':' | sed 's/:$//')"
+export PATH
+
 curl -sfL https://direnv.net/install.sh | bash
