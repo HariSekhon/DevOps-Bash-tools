fcfee848b4f5b4322201de50a12a0c7e58123969
Hari Sekhon <harisekhon@gmail.com>
2024-08-17 01:36:05 +0200

    updated git_pull_merge_trunk.sh

--- github/git_pull_merge_trunk.sh
+++ github/git_pull_merge_trunk.sh
@@ -22,12 +22,14 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-In a forked GitHub repo's checkout, determine the origin of the fork using GitHub CLI,
-configure a git remote to the upstream, pull the default branch and if on a branch other than the default
-then merge the default branch to the local current branch
+Pull the origin, specified or upstream repo's trunk branch and merges it into the local branch
 
-Simplifies and automates keeping your personal forked repo up to date with the upstream to resolve merge conflicts
-locally and submit updated Pull Requests
+In a forked GitHub repo's checkout, determines the origin of the fork using GitHub CLI,
+configures a git remote to the upstream, pulls the default branch and if on a branch other than the default
+then merges the default branch to the local current branch
+
+Simplifies and automates keeping your cloned or forked repo up to date with the original source repo to resolve merge
+conflicts locally and submit updated Pull Requests
 
 Set environment variable GIT_REBASE=true if you want the pulls and merge to current branch to rebase instead of merge commit...
 if you really love violating VCS history integrity! Personally, not a fan. You can also end up in rebase hell for a series of
@@ -54,7 +56,9 @@ if [ $# -gt 0 ]; then
     upstream_owner_repo="$1"
     shift
 else
-    upstream_owner_repo="$(github_upstream_owner_repo || die "Not a forked repo?")"
+    if is_github_fork; then
+        upstream_owner_repo="$(github_upstream_owner_repo || die "Not a forked repo?")"
+    fi
 fi
 
 rebase="${GIT_REBASE:-false}"
@@ -63,8 +67,10 @@ if ! is_bool "$rebase"; then
     usage "GIT_REBASE environment variable must be set to 'true' or 'false'"
 fi
 
-"$srcdir/github_remote_set_upstream.sh" "$upstream_owner_repo"
-echo
+if is_github_fork; then
+    "$srcdir/github_remote_set_upstream.sh" "$upstream_owner_repo"
+    echo
+fi
 
 default_branch="$(default_branch)"
 
@@ -75,21 +81,23 @@ echo
 git pull origin "$default_branch" --rebase="$rebase" --no-edit
 echo
 
-current_branch="$(git rev-parse --abbrev-ref HEAD)"
+if is_github_fork; then
+    current_branch="$(git rev-parse --abbrev-ref HEAD)"
 
-if [ "$current_branch" = "$default_branch" ]; then
-    timestamp "Pulling default branch '$default_branch' from upstream $upstream_owner_repo"
-    git pull upstream "$default_branch" --no-edit
-else
-    timestamp "Fetching default branch '$default_branch' from upstream $upstream_owner_repo"
-    echo
-    git fetch upstream "$default_branch:$default_branch"
-    echo
-    if [ "$rebase" = true ]; then
-        timestamp "Rebasing current branch '$current_branch' from default branch '$default_branch'"
-        git rebase "$default_branch"
+    if [ "$current_branch" = "$default_branch" ]; then
+        timestamp "Pulling default branch '$default_branch' from upstream $upstream_owner_repo"
+        git pull upstream "$default_branch" --no-edit
     else
-        timestamp "Merging default branch '$default_branch' into current branch '$current_branch'"
-        git merge "$default_branch" --no-edit
+        timestamp "Fetching default branch '$default_branch' from upstream $upstream_owner_repo"
+        echo
+        git fetch upstream "$default_branch:$default_branch"
+        echo
+        if [ "$rebase" = true ]; then
+            timestamp "Rebasing current branch '$current_branch' from default branch '$default_branch'"
+            git rebase "$default_branch"
+        else
+            timestamp "Merging default branch '$default_branch' into current branch '$current_branch'"
+            git merge "$default_branch" --no-edit
+        fi
     fi
 fi
