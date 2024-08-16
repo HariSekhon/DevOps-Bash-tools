f63929570054047a95fe9444406e235024870326
Hari Sekhon <harisekhon@gmail.com>
2024-08-16 20:04:59 +0200

    added shields_embed_logo.sh

---
+++ shields_embed_logo.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2024-08-16 19:53:07 +0200 (Fri, 16 Aug 2024)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Base64 encodes a given icon file and prints the logo=... url parameter you need to add the shields.io URL
+
+If you pass a URL will download the URL and base64 the content
+
+Otherwise download the icon to your local disk eg. using SimpleIcons.org and pass that as a parameter
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="filename.svg"
+
+help_usage "$@"
+
+min_args 1 "$@"
+
+icon="$1"
+
+if [[ "$icon" =~ ^https?:// ]]; then
+    content="$(curl -sS "$icon")"
+elif [ -f "$icon" ]; then
+    content="$(cat "$icon")"
+else
+    usage "first argument needs to be a URL or a local file"
+fi
+
+data="$(base64 <<< "$content")"
+
+echo "logo=data:image/svg%2bxml;base64,$data"
