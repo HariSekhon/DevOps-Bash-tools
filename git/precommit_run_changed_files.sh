ac0286725efb79e04508dd49f88d47bc4d47fb03
Hari Sekhon <harisekhon@gmail.com>
2024-08-16 22:26:41 +0200

    updated precommit_run_changed_files.sh

--- git/precommit_run_changed_files.sh
+++ git/precommit_run_changed_files.sh
@@ -18,7 +18,7 @@ set -euo pipefail
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC1090,SC1091
-. "$srcdir/lib/utils.sh"
+. "$srcdir/lib/git.sh"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
@@ -36,6 +36,8 @@ usage_args=""
 help_usage "$@"
 
 num_args 0 "$@"
+
+cd "$(git_root)"
 
 default_branch="$(git symbolic-ref refs/remotes/origin/HEAD | sed 's|.*/||')"
 
