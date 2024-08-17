bc3aac08dd8faa50e3e35b83d55174fd4fc4b4c0
Hari Sekhon <harisekhon@gmail.com>
2024-08-18 00:55:48 +0200

    updated markdown_replace_index.sh

--- git/markdown_replace_index.sh
+++ git/markdown_replace_index.sh
@@ -68,7 +68,6 @@ done
 timestamp "Generating new index for file: $markdown_file"
 
 "$srcdir/markdown_generate_index.sh" "$markdown_file" > "$index_tmp"
-#cat "$index_tmp"
 
 timestamp "Replacing index in file: $markdown_file"
 
