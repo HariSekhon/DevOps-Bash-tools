e3f7853a1105bd4ce70629f4582294193df2c5a6
Hari Sekhon <harisekhon@gmail.com>
2024-08-21 03:40:47 +0200

    added github_repos_stars.sh

---
+++ github/github_repos_stars.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#  args: harisekhon
+#
+#  Author: Hari Sekhon
+#  Date: 2024-08-21 03:35:51 +0200 (Wed, 21 Aug 2024)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck source=lib/utils.sh
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Lists the total number of stars for all public GitHub repos for a given username
+
+If no repo is given, infers from local repo's git remotes
+
+Output format:
+
+<total_repo_stars>
+
+
+Requires GitHub CLI to be installed and configured
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<username>"
+
+help_usage "$@"
+
+min_args 1 "$@"
+
+username="${1:-}"
+
+gh repo list "$username" --limit 99999 \
+						 --json stargazerCount \
+						 --jq '[.[] | .stargazerCount] | add'
