7b4ad8215ec70fc40c59f8ee6f63421556561321
Hari Sekhon <harisekhon@gmail.com>
2024-08-21 04:01:33 +0200

    added github_user_followers.sh

---
+++ github/github_user_followers.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#  args: harisekhon
+#
+#  Author: Hari Sekhon
+#  Date: 2024-08-21 03:35:51 +0200 (Wed, 21 Aug 2024)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck source=lib/utils.sh
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Returns the number of followers for a given username
+
+Output format:
+
+<number_of_followers>
+
+
+Requires GitHub CLI to be installed and configured
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<username>"
+
+help_usage "$@"
+
+num_args 1 "$@"
+
+username="${1:-}"
+
+gh api "users/$username" --jq '.followers'
