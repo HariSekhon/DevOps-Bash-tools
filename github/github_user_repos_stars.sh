ffcd3b7d3031715f958a744d7f6f5f7df58ce459
Hari Sekhon <harisekhon@gmail.com>
2024-08-21 04:07:50 +0200

    updated github_user_repos_stars.sh

--- github/github_user_repos_stars.sh
+++ github/github_user_repos_stars.sh
@@ -23,7 +23,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Returns the total number of stars for all non-forked public GitHub repos for a given username
+Returns the total number of stars for all original source public GitHub repos for a given username
 
 Output format:
 
