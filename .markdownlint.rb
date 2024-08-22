076430ddcc5c80c94339cd6038e1077fc3eb8816
Hari Sekhon <harisekhon@gmail.com>
2024-08-22 02:21:21 +0200

    added .markdownlint.rb

---
+++ .markdownlint.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+#  vim:ts=4:sts=4:sw=4:et:filetype=ruby
+#
+#  Author: Hari Sekhon
+#  Date: 2024-08-22 01:58:12 +0200 (Thu, 22 Aug 2024)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+all
+#exclude_rule 'MD001'
+#exclude_rule 'MD003'
+#exclude_rule 'MD005'
+exclude_rule 'MD007'  # leave 2 space indentation for lists, 3 space is ugly af
+#exclude_rule 'MD012'
+exclude_rule 'MD013'  # long lines cannot be split if they are URLs
+#exclude_rule 'MD022'
+#exclude_rule 'MD025'
+#exclude_rule 'MD031'
+#exclude_rule 'MD032'
+exclude_rule 'MD033'  # inline HTML is important for formatting
+#exclude_rule 'MD039'
+#exclude_rule 'MD056'
