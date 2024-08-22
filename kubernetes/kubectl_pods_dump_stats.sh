01e30a96a3629d4811a54139b89e7bc2639f412a
Hari Sekhon <harisekhon@gmail.com>
2024-08-22 12:10:56 +0200

    updated kubectl_pods_dump_stats.sh

--- kubernetes/kubectl_pods_dump_stats.sh
+++ kubernetes/kubectl_pods_dump_stats.sh
@@ -18,7 +18,7 @@ set -euo pipefail
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC1090,SC1091
-. "$srcdir/lib/utils.sh"
+. "$srcdir/lib/kubernetes.sh"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
