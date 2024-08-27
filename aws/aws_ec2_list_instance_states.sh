565f6a4a401142be92288657f38e70b7208c4ffa
Hari Sekhon <harisekhon@gmail.com>
2024-08-27 11:28:28 +0200

    added aws_ec2_list_instance_states.sh

---
+++ aws/aws_ec2_list_instance_states.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2024-08-27 11:28:20 +0200 (Tue, 27 Aug 2024)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+List AWS EC2 instances and their states in an easy to read table output
+
+Useful for quickly investigating running instances
+
+
+$usage_aws_cli_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+num_args 0 "$@"
+
+# false positive - want single quotes for * to be evaluated within AWS query not shell
+# shellcheck disable=SC2016
+aws ec2 describe-instances \
+    --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value | [0], InstanceId, State.Name]' \
+    --output table
