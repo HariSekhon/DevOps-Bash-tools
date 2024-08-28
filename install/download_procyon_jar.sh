11fd4e82572fc62e2671175355fff0e689506374
Hari Sekhon <harisekhon@gmail.com>
2024-08-28 23:03:37 +0200

    added download_procyon_jar.sh

---
+++ install/download_procyon_jar.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2024-08-28 23:01:46 +0200 (Wed, 28 Aug 2024)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/../lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Quickly determines and downloads latest Procyon java command line decompiler jar or an explicitly given version
+
+Version defaults to 'latest' in which case it determines the latest version from GitHub releases
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<version>]"
+
+version="${1:-latest}"
+
+github_owner_repo="mstrobel/procyon"
+
+if [ "$version" = latest ]; then
+    timestamp "Determining latest version of Procyon available"
+    version="$(github_repo_latest_release.sh "$github_owner_repo")"
+    version="${version#v}"
+    timestamp "Determined latest version of Procyon to be version '$version'"
+fi
+
+download_url="https://github.com/$github_owner_repo/releases/download/v$version/procyon-decompiler-$version.jar"
+
+timestamp "Downloading Java Decompiler Procyon version '$version' from: $download_url"
+echo >&2
+
+jar="procyon-decompiler-$version.jar"
+
+if type -P wget &>/dev/null; then
+    wget -cO "$jar" "$download_url"
+else
+    tmpfile="$(mktemp)"
+    curl --fail "$download_url" > "$tmpfile"
+    unalias mv &>/dev/null || :
+    mv -fv "$tmpfile" "$jar"
+fi
+
+timestamp "Download complete: $jar"
