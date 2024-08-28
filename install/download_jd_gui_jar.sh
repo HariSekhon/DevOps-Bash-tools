77b3adaaf67c8e2a473ff29080dadbb986c5f6fc
Hari Sekhon <harisekhon@gmail.com>
2024-08-29 00:19:03 +0200

    updated download_jd_gui_jar.sh

--- install/download_jd_gui_jar.sh
+++ install/download_jd_gui_jar.sh
@@ -35,27 +35,4 @@ version="${1:-latest}"
 
 github_owner_repo="java-decompiler/jd-gui"
 
-if [ "$version" = latest ]; then
-    timestamp "Determining latest version of Java Decompiler JD GUI available"
-    version="$(github_repo_latest_release.sh "$github_owner_repo")"
-    version="${version#v}"
-    timestamp "Determined latest version of Java Decompiler JD GUI to be version '$version'"
-fi
-
-download_url="https://github.com/$github_owner_repo/releases/download/v$version/jd-gui-$version-min.jar"
-
-timestamp "Downloading Java Decompiler JD GUI version '$version' from: $download_url"
-echo >&2
-
-jar="jd-gui-$version-min.jar"
-
-if type -P wget &>/dev/null; then
-    wget -cO "$jar" "$download_url"
-else
-    tmpfile="$(mktemp)"
-    curl --fail "$download_url" > "$tmpfile"
-    unalias mv &>/dev/null || :
-    mv -fv "$tmpfile" "$jar"
-fi
-
-timestamp "Download complete: $jar"
+"$srcdir/download_github_jar.sh" "https://github.com/$github_owner_repo/releases/download/v{version}/jd-gui-{version}-min.jar" "$version"
