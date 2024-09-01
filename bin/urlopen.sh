8875dee3ffc66fe6e96df43e356ba733161c878c
Hari Sekhon <harisekhon@gmail.com>
2024-09-01 16:28:10 +0200

    updated urlopen.sh

--- bin/urlopen.sh
+++ bin/urlopen.sh
@@ -20,21 +20,11 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1090,SC1091
 . "$srcdir/lib/utils.sh"
 
-# vendor's bash completion and other external sources aren't written defensively enough,
-# ignoring error codes from commands and testing undefined variables :'-(
-set +euo pipefail
-# auto-wraps xargs to gxargs for --no-run-if-empty
-# shellcheck disable=SC1090,SC1091
-#. "$srcdir/.bash.d/mac.sh"
-
-# providers browser abstraction for Linux + Mac
-# shellcheck disable=SC1090,SC1091
-. "$srcdir/.bash.d/network.sh"
-set -euo pipefail
-
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Opens the first URL found in the given file arguments or standard input
+Opens the URL given as an argument or standard input.
+
+If the given arg is a file, then opens the first URL found in the file
 
 Used by .vimrc to instantly open a URL on the given line in the editor
 
@@ -43,8 +33,36 @@ Very useful for quickly referencing inline documentation links in config files a
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="[<files>]"
+usage_args="[<url_or_file_with_url>]"
 
+help_usage "$@"
+
+max_args 1 "$@"
+
+arg="${1:-}"
+
+browse(){
+    local url="$1"
+    if is_mac; then
+        open "$url"
+    else  # assume Linux
+        if type -P xdg-open &>/dev/null; then
+            xdg-open "$url" &
+        elif type -P gnome-open &>/dev/null; then
+            gnome-open "$url" &
+        else
+            die "ERROR: xdg-open and gnome-open not found"
+        fi
+    fi
+}
+
+if [ $# -eq 0 ]; then
+    cat
+elif [ -f "$arg" ]; then
+    cat "$arg"
+else
+    echo "$arg"
+fi |
 {
 # [] break the regex match, even when escaped \[\]
 grep -Eom 1 'https?://[[:alnum:]./?&!$#%@*;+~_=-]+' "$@" ||
@@ -53,5 +71,5 @@ grep -Eom 1 'https?://[[:alnum:]./?&!$#%@*;+~_=-]+' "$@" ||
 # head -n1 because grep -m 1 can't be trusted and sometimes outputs more matches on subsequent lines
 head -n1 |
 while read -r url; do
-    browser "$url"
+    browse "$url"
 done
