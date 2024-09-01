b294c8764fbafa5d3d82dcdb31b1f66b9b7db065
Hari Sekhon <harisekhon@gmail.com>
2024-09-01 15:33:08 +0200

    updated kubectl_port_forward_spark.sh

--- kubernetes/kubectl_port_forward_spark.sh
+++ kubernetes/kubectl_port_forward_spark.sh
@@ -80,7 +80,7 @@ else
 fi
 
 timestamp "Launching port forwarding to pod '$spark_driver_pod' port '$spark_port'"
-echo kubectl port-forward --address 127.0.0.1 ${namespace:+-n "$namespace"} "$spark_driver_pod" "$spark_port":"$spark_port"
+kubectl port-forward --address 127.0.0.1 ${namespace:+-n "$namespace"} "$spark_driver_pod" "$spark_port":"$spark_port" &
 
 if is_mac; then
     echo
