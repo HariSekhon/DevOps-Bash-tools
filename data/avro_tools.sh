613b5c3713749629e42a0da29843711521e62906
Hari Sekhon <harisekhon@gmail.com>
2024-09-16 14:51:44 +0200

    updated avro_tools.sh

--- data/avro_tools.sh
+++ data/avro_tools.sh
@@ -46,9 +46,9 @@ avro_tools_jar="$(find "$srcdir" -maxdepth 1 -name 'avro-tools-*.jar' | sort -Vr
 if [ -z "$avro_tools_jar" ] ||
    # incomplete download, call download again to resume it
    ! jar tf "$avro_tools_jar" &>/dev/null; then
-    pushd "$srcdir" 2>/dev/null || :
+    pushd "$srcdir" 2>/dev/null || die "Failed to pushd to '$srcdir'"
     "$srcdir/../install/download_avro_tools.sh"
-    popd "$srcdir" 2>/dev/null || :
+    popd 2>/dev/null || die "Failed to return to original dir"
 fi
 
 java -jar "$avro_tools_jar" "$@"
