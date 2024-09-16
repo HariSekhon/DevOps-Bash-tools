63b37511a557fc47ac30d726093e6479f67cdb80
Hari Sekhon <harisekhon@gmail.com>
2024-09-16 19:16:25 +0200

    updated .mdl.rb

--- .mdl.rb
+++ .mdl.rb
@@ -25,5 +25,6 @@ exclude_rule 'MD013'  # long lines cannot be split if they are URLs
 #exclude_rule 'MD031'
 #exclude_rule 'MD032'
 exclude_rule 'MD033'  # inline HTML is important for formatting
+exclude_rule 'MD036'  # emphasis used instead of header for footer Ported from lines
 #exclude_rule 'MD039'
 #exclude_rule 'MD056'
