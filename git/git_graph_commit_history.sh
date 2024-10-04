879046f18d32e9d124a5ca142e65aeaf0b1dbb11
Hari Sekhon <harisekhon@gmail.com>
2024-10-04 04:45:17 +0300

    updated git_graph_commit_history.sh

--- git/git_graph_commit_history.sh
+++ git/git_graph_commit_history.sh
@@ -22,7 +22,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Generates graphs of Git commits per year and per month for the entire history of the local Git repo checkout
+Generates GNUplot graphs of Git commits per year and per month for the entire history of the local Git repo checkout
 
 A MermaidJS version of this script is adjacent at:
 
