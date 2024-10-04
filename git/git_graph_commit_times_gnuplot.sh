1bb014dcccbe30d6ef29d9edfa58ee80de966f0e
Hari Sekhon <harisekhon@gmail.com>
2024-10-04 04:49:23 +0300

    updated git_graph_commit_times_gnuplot.sh

--- git/git_graph_commit_times_gnuplot.sh
+++ git/git_graph_commit_times_gnuplot.sh
@@ -34,7 +34,7 @@ A MermaidJS version of this script is adjacent at:
 
 These adjacent scripts perform a similar function but using GitHub API commit data:
 
-    ../github/github_graph_commit_times.sh
+    ../github/github_graph_commit_times_gnuplot.sh
 
     ../github/github_graph_commit_times_mermaidjs.sh
 
