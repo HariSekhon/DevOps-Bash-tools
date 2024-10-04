5562f004abe65bc3c3c3bcaafaddfc03ee2c5c76
Hari Sekhon <harisekhon@gmail.com>
2024-10-04 06:35:38 +0300

    updated github_graph_commit_times_mermaidjs.sh

--- github/github_graph_commit_times_mermaidjs.sh
+++ github/github_graph_commit_times_mermaidjs.sh
@@ -87,19 +87,21 @@ if ! [ -f "$data" ]; then
     echo
 fi
 
-timestamp "Generating MermaidJS code for bar chart of commit times"
+export -f parse_file_col_to_csv
+
+timestamp "Generating MermaidJS code for Commits per Hour"
 cat > "$code" <<EOF
 xychart-beta
     title "Git Commits by Hour"
-    x-axis [ $(awk '{print $1}' "$data" | tr '\n' ',' | sed 's/,/, /g; s/, $//') ]
+    x-axis [ $(parse_file_col_to_csv "$data" 1) ]
     y-axis "Commits"
-    bar    [ $(awk '{print $2}' "$data" | tr '\n' ',' | sed 's/,/, /g; s/, $//') ]
-    %%line [ $(awk '{print $2}' "$data" | tr '\n' ',' | sed 's/,/, /g; s/, $//') ]
+    bar    [ $(parse_file_col_to_csv "$data" 2) ]
+    %%line [ $(parse_file_col_to_csv "$data" 2) ]
 EOF
-timestamp "Generated MermaidJS code"
+timestamp "Generated MermaidJS code: $code"
 echo
 
-timestamp "Generating MermaidJS bar chart image: $image"
+timestamp "Generating MermaidJS bar chart for Commits per Hour"
 mmdc -i "$code" -o "$image" -t dark --quiet # -b transparent
 timestamp "Generated MermaidJS image: $image"
 
@@ -107,5 +109,5 @@ if is_CI; then
     exit 0
 fi
 
-timestamp "Opening generated bar chart"
+timestamp "Opening: $image"
 "$srcdir/../bin/imageopen.sh" "$image"
