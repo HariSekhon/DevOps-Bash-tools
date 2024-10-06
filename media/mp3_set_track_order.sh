b4d1028c849e644e05a35749e303c0f87e2ad23f
Hari Sekhon <harisekhon@gmail.com>
2024-10-06 12:52:37 +0300

    updated mp3_set_track_order.sh

--- media/mp3_set_track_order.sh
+++ media/mp3_set_track_order.sh
@@ -22,7 +22,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Adds / Modifies track number metadata across all MP3 files in the given directories to restructure audiobooks to be contiguous for Mac's Books.app
+Add / Modify track number metadata across all MP3 files in the given directories to restructure audiobooks to be contiguous for Mac's Books.app
 
 $mp3_usage_behaviour_msg
 
