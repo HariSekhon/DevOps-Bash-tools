cfb92d2b7e53bf3411bfd667666c606efd1780b0
Hari Sekhon <harisekhon@gmail.com>
2024-10-08 02:56:08 +0300

    updated install_prometheus_graphite_exporter.sh

--- install/install_prometheus_graphite_exporter.sh
+++ install/install_prometheus_graphite_exporter.sh
@@ -33,7 +33,7 @@ export PATH="$PATH:$HOME/bin"
 
 help_usage "$@"
 
-#version="${1:-1.8.2}"
+#version="${1:-0.15.1}"
 version="${1:-latest}"
 
 "$srcdir/install_prometheus.sh" "$version" graphite_exporter
