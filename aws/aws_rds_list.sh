9e2aeef1057da8e43e22e9ae58c0ca79195bb972
Hari Sekhon <harisekhon@gmail.com>
2024-10-11 04:53:05 +0300

    added aws_rds_list.sh

---
+++ aws/aws_rds_list.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2024-10-11 04:49:35 +0300 (Fri, 11 Oct 2024)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Quickly list RDS instances with a few important fields - Name, Status, Engine, AZ, Instance Type, Storage
+
+$usage_aws_cli_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<rds_name_or_ip>"
+
+help_usage "$@"
+
+min_args 1 "$@"
+
+aws rds describe-db-instances \
+    --query "DBInstances[*].[DBInstanceIdentifier, DBInstanceStatus, Engine, AvailabilityZone, DBInstanceClass, AllocatedStorage]" \
+    --output table
