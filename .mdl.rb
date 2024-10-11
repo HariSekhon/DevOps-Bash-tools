b04f82157e643b9a813af022a306a1fd0c769035
Hari Sekhon <harisekhon@gmail.com>
2024-10-11 19:11:00 +0300

    updated .mdl.rb

--- .mdl.rb
+++ .mdl.rb
@@ -22,6 +22,7 @@ exclude_rule 'MD007'  # leave 2 space indentation for lists, 3 space is ugly af
 exclude_rule 'MD013'  # long lines cannot be split if they are URLs
 #exclude_rule 'MD022'
 #exclude_rule 'MD025'
+exclude_rule 'MD026'  # Trailing punctuation in header - sometimes I want to do etc. or ... at the end of a heading
 #exclude_rule 'MD031'
 #exclude_rule 'MD032'
 exclude_rule 'MD033'  # inline HTML is important for formatting
