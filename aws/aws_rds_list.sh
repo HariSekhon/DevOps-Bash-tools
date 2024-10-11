40db959be6f325792535b4475aea5ebc3100a095
Hari Sekhon <harisekhon@gmail.com>
2024-10-11 05:01:30 +0300

    updated aws_rds_list.sh

--- aws/aws_rds_list.sh
+++ aws/aws_rds_list.sh
@@ -41,4 +41,5 @@ fi
 
 aws rds describe-db-instances \
     --query "DBInstances[*].[DBInstanceIdentifier, DBInstanceStatus, Engine, AvailabilityZone, DBInstanceClass, AllocatedStorage, Endpoint.Address]" \
-    --output table
+    --output table \
+    --headers 'Instance' 'Status' 'Engine' 'Availability Zone' 'Instance Type' 'Allocated Storage' 'Endpoint DNS FQDN Address'
