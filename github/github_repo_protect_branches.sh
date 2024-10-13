520cb58a0f39e1c4f00263c6a95cb7c098570ec9
Hari Sekhon <harisekhon@gmail.com>
2024-10-13 18:30:35 +0300

    updated github_repo_protect_branches.sh

--- github/github_repo_protect_branches.sh
+++ github/github_repo_protect_branches.sh
@@ -66,17 +66,38 @@ usage_args="<owner> <repo> [<branch> <branch2> <branch3> ...]"
 
 help_usage "$@"
 
-min_args 2 "$@"
+#min_args 2 "$@"
 
-owner="$1"
-repo="$2"
+owner="${1:-}"
+repo="${2:-}"
 shift || :
 shift || :
+
+#check_env_defined GH_TOKEN
+
+if ! is_blank "$owner" &&
+   !  is_blank "$repo"; then
+    owner_repo="$owner/$repo"
+else
+    timestamp "No GitHub owner/repo specified - determining from current Git checkout"
+    if ! is_in_git_repo; then
+        usage "Did not specify owner and repo and not in a Git repo to try to infer it"
+    fi
+    owner_repo="$(get_github_repo || :)"
+    if is_blank "$owner_repo"; then
+        owner_repo="$(github_owner_repo)"
+    fi
+    timestamp "Inferred GitHub repo to be: $owner_repo"
+fi
+
+if ! is_github_owner_repo "$owner_repo"; then
+    die "ERROR: invalid GitHub owner/repo provided, failed regex validation: $owner_repo"
+fi
 
 protect_repo_branch(){
     local branch="$1"
-    timestamp "protecting GitHub repo '$owner/$repo' branch '$branch'"
-    "$srcdir/github_api.sh" "/repos/$owner/$repo/branches/$branch/protection" -X PUT -d "$settings" >/dev/null
+    timestamp "protecting GitHub repo '$owner_repo' branch '$branch'"
+    "$srcdir/github_api.sh" "/repos/$owner_repo/branches/$branch/protection" -X PUT -d "$settings" >/dev/null
     timestamp "protection applied to branch '$branch'"
 }
 
@@ -86,7 +107,7 @@ if [ $# -gt 0 ]; then
     done
 else
     timestamp "no branches specified, getting branch list"
-    branches="$(get_github_repo_branches "$owner/$repo")"
+    branches="$(get_github_repo_branches "$owner_repo")"
     for branch in $default_branches_to_protect; do
         timestamp "checking for branch '$branch'"
         if grep -Fxq "$branch" <<< "$branches"; then
