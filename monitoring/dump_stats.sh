44c434d4d11d8f72d5aeaa4d65a9c45d640371ef
Hari Sekhon <harisekhon@gmail.com>
2024-10-15 05:41:02 +0400

    updated dump_stats.sh

--- monitoring/dump_stats.sh
+++ monitoring/dump_stats.sh
@@ -37,6 +37,9 @@ Export LSOF=true to dump the lsof output which is otherwise skipped by default b
 
 
 If NO_REMOVE_STATS_DIR environment variable is set to any value then does not remove the intermediate stats-bundle.YYYY-MM-DD-HHSS directory
+
+
+usage: ${0##*/}
 "
 
 if [ $# -gt 0 ]; then
