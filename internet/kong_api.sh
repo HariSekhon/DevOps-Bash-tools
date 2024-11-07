9d0177fa40d2923cb49553e80b870e9e07ec7f4a
Hari Sekhon <harisekhon@gmail.com>
2024-11-07 08:23:04 +0400

    updated kong_api.sh

--- internet/kong_api.sh
+++ internet/kong_api.sh
@@ -160,5 +160,5 @@ fi
 export CURL_AUTH_HEADER="Kong-Admin-Token:"
 export TOKEN="${KONG_TOKEN:-${TOKEN:-no_token_given}}"
 
-"$srcdir/curl_auth.sh" "$url" ${CURL_OPTS:+"${CURL_OPTS[@]}"} "$@" |
+"$srcdir/../bin/curl_auth.sh" "$url" ${CURL_OPTS:+"${CURL_OPTS[@]}"} "$@" |
 jq_debug_pipe_dump
