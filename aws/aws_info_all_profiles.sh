01cf83721073ccff7803301016ef24ef18c281ab
Hari Sekhon <harisekhon@gmail.com>
2024-11-22 14:24:38 +0400

    added aws_info_all_profiles.sh

---
+++ aws/aws_info_all_profiles.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2024-11-22 14:22:54 +0400 (Fri, 22 Nov 2024)
+#  long overdue port of the adjacent GCP info scripts from years prior
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Gathers AWS Info across all projects
+
+Combines aws_foreach_profile.sh and aws_info.sh
+
+$gcp_info_formatting_help
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+aws_foreach_profile.sh "'$srcdir/aws_info.sh' '{id}'"
