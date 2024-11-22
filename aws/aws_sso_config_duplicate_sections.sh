1b546c7cb589cf02a354527ef4506f08962a8643
Hari Sekhon <harisekhon@gmail.com>
2024-11-22 23:18:25 +0400

    updated aws_sso_config_duplicate_sections.sh

--- aws/aws_sso_config_duplicate_sections.sh
+++ aws/aws_sso_config_duplicate_sections.sh
@@ -22,9 +22,11 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Prints duplicate AWS SSO config sections using the same sso_account_id from the given \$AWS_CONFIG_FILE or given file argument
+Prints duplicate AWS SSO config sections that are using the same sso_account_id
+from the given \$AWS_CONFIG_FILE or given file argument
 
-Useful to find and remove / comment out an ~/.aws/config with a mix of hand crafted and automatically generated AWS SSO configs
+Useful to find and remove / comment out an ~/.aws/config with a mix of hand crafted
+and automatically generated AWS SSO configs
 
 See also:
 
