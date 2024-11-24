02fbc75b1c3275fd71430dec2c27668896e6931b
Hari Sekhon <harisekhon@gmail.com>
2024-11-24 04:19:51 +0400

    updated ini_grep_section.sh

--- data/ini_grep_section.sh
+++ data/ini_grep_section.sh
@@ -22,7 +22,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Outputs the named section in a given .ini file
+Prints the named section from a given .ini file
 "
 
 # used by usage() in lib/utils.sh
