c545a214774d2e31790d34d6030d729f17d1e7d5
Hari Sekhon <harisekhon@gmail.com>
2024-12-05 01:49:18 +0700

    added terraform_registry_url_to_https.sh

---
+++ terraform/terraform_registry_url_to_https.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: Thu Dec 5 00:39:05 2024 +0700
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Converts one or more Terraform Registry URLs given as an argument or standard input from tfr:// to https://registry.terraform.io/ format given as an argument or standard input
+
+If the given arg is a file, then reads all tfr:// urls from the file
+
+Used by .vimrc to convert tfr:// URLs found in a file to HTTPS format to present in a menu
+
+Very useful for quickly referencing Terraform documentation for modules defined in Terraform code
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<tfr_url_or_file_with_tfr_urls>]"
+
+help_usage "$@"
+
+max_args 1 "$@"
+
+arg="${1:-}"
+
+if [ $# -eq 0 ]; then
+    cat
+elif [ -f "$arg" ]; then
+    cat "$arg"
+else
+    echo "$arg"
+fi |
+sed 's|tfr://registry.terraform.io/|https://registry.terraform.io/modules/|; s|$|/latest|g'
