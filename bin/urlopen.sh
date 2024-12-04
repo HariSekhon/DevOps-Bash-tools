d89cf779829a07e59c8b555f06ce1d4e5a7fcda4
Hari Sekhon <harisekhon@gmail.com>
2024-12-05 00:38:02 +0700

    updated urlopen.sh

--- bin/urlopen.sh
+++ bin/urlopen.sh
@@ -22,7 +22,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Opens the URL given as an argument or standard input.
+Opens the URL given as an argument or standard input
 
 If the given arg is a file, then opens the first URL found in the file
 
