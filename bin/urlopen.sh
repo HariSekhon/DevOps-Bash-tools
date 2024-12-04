708685db87f0ca01fc7191b51f9614afdc0037d0
Hari Sekhon <harisekhon@gmail.com>
2024-12-05 02:04:01 +0700

    updated urlopen.sh

--- bin/urlopen.sh
+++ bin/urlopen.sh
@@ -22,9 +22,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Opens the URL given as an arg, file or standard input
-
-If the given arg is a file, then opens the first URL found in the file
+Opens the URL given as an arg, or first URL from standard input or a given file
 
 Used by .vimrc to instantly open a URL on the given line in the editor
 
