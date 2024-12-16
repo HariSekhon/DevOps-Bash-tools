ce192b4afd1ed2f0cb15b5f41aae58638104d04b
Hari Sekhon <harisekhon@gmail.com>
2024-12-17 02:48:49 +0700

    updated aws.sh

--- lib/aws.sh
+++ lib/aws.sh
@@ -23,8 +23,10 @@ libdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # used in client scripts
 # shellcheck disable=SC2034
 usage_aws_cli_required="Requires AWS CLI to be installed and configured (run 'make aws && aws configure')"
+# shellcheck disable=SC2034
 usage_aws_cli_jq_required="Requires AWS CLI to be installed and configured, as well as jq  (run 'make aws && aws configure')"
 
+aws_ecr_regex='[:digit:]]{12}.dkr.ecr.[[:alnum:]-]{4.}.amazonaws.com'
 aws_region_regex='[a-z]{2}-[a-z]+-[[:digit:]]'
 instance_id_regex='i-[0-9a-fA-F]{17}'
 ami_id_regex='ami-[0-9a-fA-F]{8}([0-9a-fA-F]{9})?'
@@ -65,6 +67,15 @@ aws_region(){
         die "Invalid AWS Region returned in lib/aws.sh, failed regex validation: $region"
     fi
     echo "$region"
+}
+
+aws_ecr_registry(){
+    echo "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
+}
+
+is_aws_ecr_registry(){
+    local arg="$1"
+    [[ "$arg" =~ ^$aws_ecr_regex$ ]]
 }
 
 aws_user_exists(){
