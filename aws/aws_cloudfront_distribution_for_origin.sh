4cf5955716ce3af17e92e746116ab0f5a595b775
Hari Sekhon <harisekhon@gmail.com>
2024-12-19 13:57:40 +0700

    added aws_cloudfront_distribution_for_origin.sh

---
+++ aws/aws_cloudfront_distribution_for_origin.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2024-12-19 13:54:16 +0700 (Thu, 19 Dec 2024)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/aws.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Returns the AWS CloudFront ID of the distribution which serves origins containing a given substring
+
+Useful for quickly finding the CloudFront ID needed to give permissions to a private S3 bucket exposed via CloudFront
+
+
+$usage_aws_cli_jq_required
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<domain_substring>"
+
+help_usage "$@"
+
+min_args 1 "$@"
+
+domain_substring="$1"
+
+json="$(
+    aws cloudfront list-distributions \
+        --query "DistributionList.Items[*].{Id:Id, DomainNames:Origins.Items[*].DomainName}" \
+        --output json
+)"
+
+jq -r ".[] | select(.DomainNames | map(ascii_downcase | contains(\"$domain_substring\")) | any) | .Id" <<< "$json"
