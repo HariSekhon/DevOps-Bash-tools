452a32f80382378ce39bab10e641f179b9e4d8e0
Hari Sekhon <harisekhon@gmail.com>
2025-01-08 05:46:28 +0700

    added install_kubectl_plugin_krew.sh

---
+++ install/install_kubectl_plugin_krew.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2025-01-01 04:14:29 +0700 (Wed, 01 Jan 2025)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Installs the latest version of the kubectl krew plugin
+
+Also pre-installs kubectl if not already present
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+num_args 0 "$@"
+
+export HOME="${HOME:-$(cd && pwd)}"
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+if ! type -P kubectl &>/dev/null; then
+    timestamp "Kubectl not installed, pre-installing..."
+    echo
+    "$srcdir/install_kubectl.sh"
+    echo
+fi
+
+tmp="$(mktemp -d)"
+
+cd "$tmp"
+
+os="$(get_os)"
+
+arch="$(get_arch)"
+
+krew="krew-${os}_${arch}"
+
+timestamp "Downloading latest $krew.tar.gz"
+curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/$krew.tar.gz"
+echo
+
+timestamp "Extracting $krew.tar.gz"
+echo
+
+tar zxvf "$krew.tar.gz"
+
+echo
+
+timestamp "Installing krew"
+"./$krew" install krew
+echo
+
+timestamp "Install Complete"
