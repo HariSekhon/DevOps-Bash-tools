1c097ae3bbcd2fc48ff95a7c8bb195834636010e
Hari Sekhon <harisekhon@gmail.com>
2025-01-08 12:25:25 +0700

    updated aws_eks_cluster_versions.sh

--- aws/aws_eks_cluster_versions.sh
+++ aws/aws_eks_cluster_versions.sh
@@ -22,7 +22,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Lists the cluster name and version for each AWS EKS cluster in the current account
+Iterates EKS clusters to list each AWS EKS cluster name and version in the current account
 
 Output format:
 
