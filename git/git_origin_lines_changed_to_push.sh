8fc9367b22a7843ec871301acf0c4bf980f2e77a
Hari Sekhon <harisekhon@gmail.com>
2025-01-23 07:02:33 +0700

    added git_origin_lines_changed_to_push.sh

---
+++ git/git_origin_lines_changed_to_push.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2025-01-23 03:23:55 +0700 (Thu, 23 Jan 2025)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/git.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Shows the Git number of lines changed in local branch that would be pushed to remote origin
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+num_args 0 "$@"
+
+"$srcdir/git_origin_diff_to_push.sh" --unified=0 |
+sed -n '/^[+-]/ {/^\(---\|+++\)/!p}' |
+sed '$ { /^[[:space:]]*$/d }' |
+wc -l |
+sed 's/[[:space:]]*//g'
