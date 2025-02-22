483af98b406b7b88b054901bbd053757d9ebfb67
Hari Sekhon <harisekhon@gmail.com>
2025-02-23 03:23:19 +0700

    updated check_json.sh

--- checks/check_json.sh
+++ checks/check_json.sh
@@ -23,6 +23,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 filelist="$(find "${1:-.}" -type f -name '*.json' | sort)"
 
 if [ -z "$filelist" ]; then
+    # shellcheck disable=SC2317
     return 0 &>/dev/null ||
     exit 0
 fi
@@ -40,7 +41,9 @@ elif [ -n "${QUICK:-}" ]; then
 else
     if ! command -v jsonlint &>/dev/null; then
         echo "jsonlint not found in \$PATH, not running JSON syntax checks"
-        return 0 &>/dev/null || exit 0
+        # shellcheck disable=SC2317
+        return 0 &>/dev/null ||
+        exit 0
     fi
     type -P jsonlint
     printf "version: "
@@ -71,7 +74,9 @@ else
                 echo
             fi
             if [ -z "${NOEXIT:-}" ]; then
-                return 1 &>/dev/null || exit 1
+                # shellcheck disable=SC2317
+                return 1 &>/dev/null ||
+                exit 1
             fi
         fi
         set -eo pipefail
