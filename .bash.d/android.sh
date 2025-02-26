1fedc3abadcd81f782566b7f3f5c32040181103d
Hari Sekhon <harisekhon@gmail.com>
2025-02-27 02:09:42 +0700

    added android.sh

---
+++ .bash.d/android.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2025-02-27 02:08:34 +0700 (Thu, 27 Feb 2025)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+export ANDROID_HOME="$HOME/Android/Sdk"
+
+add_PATH "$ANDROID_HOME/platform-tools"
