97458e2d5cae18940ff5d077008edef41e5188ec
Hari Sekhon <harisekhon@gmail.com>
2025-03-10 21:01:59 +0800

    added git_origin_files_to_push.sh

---
+++ git/git_origin_files_to_push.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2025-03-10 20:59:47 +0800 (Mon, 10 Mar 2025)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/git.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Shows the Git files in local branch that would be pushed to remote origin
+
+You can give git diff options
+
+Example:
+
+    ${0##*/} --color=always | less -FR
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<git_diff_options>]"
+
+help_usage "$@"
+
+#min_args 1 "$@"
+
+log "Determining current branch"
+current_branch="$(current_branch)"
+log "Current branch: $current_branch"
+
+log "Checking we have an origin"
+git remote get-url origin >/dev/null
+
+#git diff --name-status "origin/$current_branch.."
+
+# these next two work even if you haven't pushed this branch yet, but the first one can result in the following error
+#
+#git diff --name-status "$@" "origin.."
+#
+#    fatal: ambiguous argument 'origin..': unknown revision or path not in the working tree.
+#    Use '--' to separate paths from revisions, like this:
+#    'git <command> [<revision>...] -- [<file>...]'
+
+git diff --name-status "$@" "FETCH_HEAD..HEAD"
