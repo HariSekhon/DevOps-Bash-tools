e0773a72bb578643aeaf1c66732ed3aea7280160
Hari Sekhon <harisekhon@gmail.com>
2025-03-10 21:03:29 +0800

    updated git_push_stats.sh

--- git/git_push_stats.sh
+++ git/git_push_stats.sh
@@ -45,7 +45,7 @@ num_lines_diff="$("$srcdir/git_origin_diff_to_push.sh" | wc -l | sed 's/[[:space
 
 # delete the last line of diff only if it's blank,
 # so that when there is nothing to push we get 0 instead of 1 line as the result
-num_lines_changed="$("$srcdir/git_origin_lines_changed_to_push.sh")"
+num_lines_changed="$("$srcdir/git_origin_linecount_to_push.sh")"
 
 cat <<EOF
 Stats for Push to Origin
