014009ce9b4d2285988e6b9dac48670fa8460dd1
Hari Sekhon <harisekhon@gmail.com>
2025-03-15 02:39:43 +0800

    updated ci_bootstrap.sh

--- setup/ci_bootstrap.sh
+++ setup/ci_bootstrap.sh
@@ -72,8 +72,9 @@ elif [ "$(uname -s)" = Linux ]; then
         retry $sudo apk update
         retry $sudo apk add --no-progress bash git make
     elif type apt-get >/dev/null 2>&1; then
-        retry $sudo apt-get -o DPkg::Lock::Timeout=600 update -q
-        retry $sudo apt-get -o DPkg::Lock::Timeout=600 install -qy git make
+        opts="-o DPkg::Lock::Timeout=600"
+        retry $sudo apt-get $opts update -q
+        retry $sudo apt-get $opts install -qy git make
     elif type yum >/dev/null 2>&1; then
         #retry $sudo yum makecache
         retry $sudo yum install -qy git make
