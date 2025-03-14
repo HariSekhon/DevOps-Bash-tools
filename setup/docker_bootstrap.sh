3ac3b375c41dab54e7beed1e1d36fa2bd5ea8518
Hari Sekhon <harisekhon@gmail.com>
2025-03-15 03:09:47 +0800

    updated docker_bootstrap.sh

--- setup/docker_bootstrap.sh
+++ setup/docker_bootstrap.sh
@@ -45,7 +45,7 @@ cd "$basedir"
 if ! command -v curl >/dev/null 2>&1; then
     if command -v apt-get >/dev/null 2>&1; then
         export DEBIAN_FRONTEND=noninteractive
-        opts="-o DPkg::Lock::Timeout=600"
+        opts="-o DPkg::Lock::Timeout=1200"
         apt-get update  $opts
         apt-get install $opts -y curl  # --no-install-recommends  # will omit ca-certificates which will break the ability to curl the bootstrap script further down
     elif command -v yum >/dev/null 2>&1; then
