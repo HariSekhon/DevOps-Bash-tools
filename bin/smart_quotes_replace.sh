f2d71b91bf67f69fe29e9c13f7eea32b0483280c
Hari Sekhon <harisekhon@gmail.com>
2025-03-27 22:32:43 +0800

    updated smart_quotes_replace.sh

--- bin/smart_quotes_replace.sh
+++ bin/smart_quotes_replace.sh
@@ -22,7 +22,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Replace smart quotes with their regular counterparts
+Replace smart quotes with their regular counterparts using sed
 "
 
 # used by usage() in lib/utils.sh
