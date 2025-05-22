58d49c481e1389a034436eef3691b6267c7fc099
Hari Sekhon <harisekhon@gmail.com>
2025-05-22 03:31:46 +0200

    updated shorten_text_selection.sh

--- bin/shorten_text_selection.sh
+++ bin/shorten_text_selection.sh
@@ -24,10 +24,12 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 usage_description="
 Shortens the selected text in the prior window
 
+- Copies the selected text to the clipboard
 - Replaces \"and\" with \"&\"
 - Removes multiple blank lines between paragraphs (which result from the pbpaste/pbcopy pipeline otherwise)
+- Pastes the clipboard text back over the selected text
 
-I use this a lot for LinkedIn comments due to the short 1250 character limit
+I use this a lot for LinkedIn comments in browser due to the short 1250 character limit
 
 Tested on macOS 14 and Debian 11 with Xfce
 "
