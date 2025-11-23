0123e270de7bcbc17c7d7064ebc408a27c846bcc
Hari Sekhon <harisekhon@gmail.com>
2025-11-23 00:10:42 -0600

    updated .mdl.rb

--- .mdl.rb
+++ .mdl.rb
@@ -21,6 +21,7 @@ exclude_rule 'MD007'  # leave 2 space indentation for lists, 3 space is ugly af
 #exclude_rule 'MD012'
 exclude_rule 'MD013'  # long lines cannot be split if they are URLs
 #exclude_rule 'MD022'
+#exclude_rule 'MD024'  # Multiple headers with the same content
 #exclude_rule 'MD025'
 exclude_rule 'MD026'  # Trailing punctuation in header - sometimes I want to do etc. or ... at the end of a heading
 #exclude_rule 'MD031'
