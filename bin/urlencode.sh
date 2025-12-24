9435ff1569f3ad445025a5a6ced4cd360f087976
Hari Sekhon <harisekhon@gmail.com>
2025-12-23 22:06:21 -0600

    updated urlencode.sh

--- bin/urlencode.sh
+++ bin/urlencode.sh
@@ -31,6 +31,6 @@ elif type -p python3 &>/dev/null &&
      python3 -c 'from urllib.parse import quote_plus'; then
      python3 -c 'from urllib.parse import quote_plus; import sys; print(quote_plus(sys.stdin.read().rstrip("\n").rstrip("\r")))'
 else
-    echo "Neither Perl URI::Escape nor Python3 with UrlLib.Parse are available" >&2
+    echo "ERROR: neither Perl URI::Escape nor Python3 with UrlLib.Parse are available" >&2
     exit 1
 fi
