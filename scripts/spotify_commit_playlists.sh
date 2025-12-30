13b7ce156cfcb8607aa1b3df9900b10ee63a2a91
Hari Sekhon <harisekhon@gmail.com>
2025-12-30 15:24:50 -0500

    updated spotify_commit_playlists.sh

--- scripts/spotify_commit_playlists.sh
+++ scripts/spotify_commit_playlists.sh
@@ -66,10 +66,13 @@ commit_playlist(){
     line_count_human_playlist="$(wc -l < "$playlist" | sed 's/[[:space:]]//g')"
     line_count_spotify_playlist="$(wc -l < "spotify/$playlist" | sed 's/[[:space:]]//g')"
     if [ "$line_count_human_playlist" != "$line_count_spotify_playlist" ]; then
-        die "ERROR: line counts between '$playlist' and 'spotify/$playlist' do not match! Partially interrupted download?"
+        warn "ERROR: line counts between '$playlist' and 'spotify/$playlist' do not match! Partially interrupted download?"
     fi
     if git status -s -- "$playlist" "spotify/$playlist" | grep -q '^[?A]'; then
         git add "$playlist" "spotify/$playlist"
+        if [ -f "$playlist.description" ]; then
+            git add "$playlist.description"
+        fi
         git ci -m "added $playlist spotify/$playlist" "$playlist" "spotify/$playlist"
         return
     fi
