35b5c728afe44ffa0537273a6c253d0f9d4edf50
Hari Sekhon <harisekhon@gmail.com>
2026-01-03 03:28:46 -0500

    added text_filter_ending_substrings.sh

---
+++ bin/text_filter_ending_substrings.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2026-01-03 03:15:56 -0500 (Sat, 03 Jan 2026)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+For a given patterns file of substring endings, print all lines that match in the following file
+
+Highly optimized using awk and bucketing to avoid all unrecognized characters but also buckets for
+performance improvements
+
+You can pass subshell content outputs for one of both 'files' using bash file descriptor replacement syntax:
+
+    ${0##*/} <(somecommand) <(somecommand2)
+
+Used by script:
+
+    $srcdir/../spotify/spotify_delete_from_playlist_if_track_in_other_playlists.sh
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<patterns.txt> <data.txt> [<data2.txt> ...]"
+
+help_usage "$@"
+
+min_args 2 "$@"
+
+patterns_file="$1"
+shift || :
+
+awk '
+    # block only executes for 1st file to build up the patterns hash
+    NR==FNR {
+        len = length($0)
+        # length bucketing optimization to not attempt to match lines that are shorter than length
+        patterns[len][$0] = 1
+        #if (len > max) max = len
+        next
+    }
+    {
+        line = $0
+        for (pattern in patterns) {
+            if (length(line) >= pattern) {
+                suffix = substr(line, length(line) - pattern + 1)
+                if (suffix in patterns[pattern]) {
+                    print
+                    break
+                }
+            }
+        }
+    }
+' "$patterns_file" "$@"
