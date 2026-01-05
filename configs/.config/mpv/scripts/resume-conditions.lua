c25ed523b285d96f3ffda859ff3890ebae019b4a
Hari Sekhon <harisekhon@gmail.com>
2026-01-05 13:49:35 -0500

    updated resume-conditions.lua

--- configs/.config/mpv/scripts/resume-conditions.lua
+++ configs/.config/mpv/scripts/resume-conditions.lua
@@ -4,7 +4,7 @@
 --
 --  vim:ts=4:sts=4:sw=4:et
 --
---  https///github.com/HariSekhon/Templates
+--  https///github.com/HariSekhon/DevOps-Bash-tools
 --
 --  License: see accompanying Hari Sekhon LICENSE file
 --
