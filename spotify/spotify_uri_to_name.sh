6a3f2899e79551989918c5dbe923f38d9d014cf8
Hari Sekhon <harisekhon@gmail.com>
2026-01-08 05:40:19 -0500

    updated spotify_uri_to_name.sh

--- spotify/spotify_uri_to_name.sh
+++ spotify/spotify_uri_to_name.sh
@@ -77,71 +77,74 @@ curl_options=()
 
 spotify_token
 
-uri_type="${SPOTIFY_URI_TYPE:-track}"
-
-if ! [[ "$uri_type" =~ ^(track|album|artist|episode)$ ]]; then
-    usage "invalid \$SPOTIFY_URI_TYPE '$uri_type' - must be one of: track, album, artist, episode"
-fi
-
-url_base="/v1/${uri_type}s"
-
-uri_inferred=0
 infer_uri_type(){
     local uri="$1"
-    if [ $uri_inferred = 0 ] && is_blank "${SPOTIFY_URI_TYPE:-}"; then
-        if [[ "$uri" =~ ^spotify:(track|album|artist|episode):|^https?://open.spotify.com/(track|album|artist|episode)/ ]]; then
-            for x in "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}"; do
-                if not_blank "$x"; then
-                    uri_type="$x"
-                    url_base="/v1/${uri_type}s"
-                    break
-                fi
-            done
-        fi
+    if [[ "$uri" =~ ^spotify:(track|album|artist|episode): ]]; then
+        echo "${BASH_REMATCH[1]}"
+    elif [[ "$uri" =~ ^https?://open.spotify.com/(track|album|artist|episode)/ ]]; then
+        echo "${BASH_REMATCH[1]}"
+    else
+        # default fallback
+        echo "${SPOTIFY_URI_TYPE:-track}"
     fi
 }
 
 convert(){
-    while true; do
-        declare -a ids
-        ids=()
-        while [ "${#ids[@]}" -lt 50 ]; do
-            read -r -s uri || break
-            if is_blank "$uri"; then
-                break
-            fi
-            if is_local_uri "$uri"; then
-                if not_blank "${ids[*]:-}"; then
-                    query_bulk "${ids[@]}"
-                    ids=()
-                fi
-                output_local_uri "$uri"
-                continue
-            fi
-            infer_uri_type "$uri"
-            id="$(validate_spotify_uri "$uri")"
-            ids+=("$id")
-        done
-        if is_blank "${ids[*]:-}"; then
-            return
+    # associative array: type -> comma-separated IDs
+    declare -A batch_ids
+    declare -A batch_counts
+
+    while read -r -s uri || [ -n "$uri" ]; do
+        [ -z "$uri" ] && continue
+
+        # skip local URIs first
+        if is_local_uri "$uri"; then
+            # flush all current batches before outputting local
+            for t in "${!batch_ids[@]}"; do
+                query_bulk_type "$t" "${batch_ids[$t]}"
+                batch_ids["$t"]=""
+            done
+            output_local_uri "$uri"
+            continue
         fi
-        query_bulk "${ids[@]}"
+
+        # determine type of URI
+        type="$(infer_uri_type "$uri")"
+        id="$(validate_spotify_uri "$uri")"
+
+        # append to batch
+        batch_ids["$type"]+="$id,"
+        batch_counts["$type"]=$(( ${batch_counts["$type"]:-0} + 1 ))
+
+        # flush batch if it reaches 50
+        if [ "${batch_counts[$type]}" -ge 50 ]; then
+            query_bulk_type "$type" "${batch_ids[$type]}"
+            batch_ids["$type"]=""
+            batch_counts["$type"]=0
+        fi
+    done
+
+    # flush remaining batches
+    for t in "${!batch_ids[@]}"; do
+        [ -n "${batch_ids[$t]}" ] && query_bulk_type "$t" "${batch_ids[$t]}"
     done
 }
 
-query_bulk(){
-    local ids
-    # join array arg on commas
-    { local IFS=','; ids="$*"; }
-    if is_blank "$ids"; then
-        return
-    fi
-    url_path="$url_base?ids=$ids"
-    # cannot quote curl_options as when empty as this results in a blank literal which breaks curl
-    # shellcheck disable=SC2068
-    output="$("$srcdir/spotify_api.sh" "$url_path" ${curl_options[@]:-})"
-    #die_if_error_field "$output"
-    output
+# bulk query the correct endpoint per type to reduce the number of queries and to
+# both improve performance and try to avoid the dredded HTTP 429 Too Many Requests 14 hour ban
+query_bulk_type(){
+    local type="$1"
+    local ids_csv="${2%,}"  # remove trailing comma
+    [ -z "$ids_csv" ] && return
+
+    local url_base="/v1/${type}s"
+
+    if [ "${#curl_options[@]}" -gt 0 ]; then
+        "$srcdir/spotify_api.sh" "$url_base?ids=$ids_csv" "${curl_options[@]}"
+    else
+        "$srcdir/spotify_api.sh" "$url_base?ids=$ids_csv"
+    fi |
+    output  # pipe into jq output()
     sleep "$sleep_secs"
 }
 
@@ -260,9 +263,10 @@ output(){
         empty
       end
     | '"$conversion"'
-    ' <<< "$output" |
+    ' |
     clean_output
 }
+export -f output
 
 clean_output(){
     tr '\t' ' ' |
