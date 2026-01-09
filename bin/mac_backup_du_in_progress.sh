428f20c34be1fb4987eafd5e608a83bac5ee1e67
Hari Sekhon <harisekhon@gmail.com>
2026-01-09 02:27:16 -0500

    added mac_backup_du_in_progress.sh

---
+++ bin/mac_backup_du_in_progress.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2026-01-09 02:25:20 -0500 (Fri, 09 Jan 2026)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Find large files in the currently in-progress Time Machine backup to find out what is taking so long
+and files that you might want to exclude using the adjacent script:
+
+    mac_backup_exclude_paths.sh
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args=""
+
+help_usage "$@"
+
+no_more_args "$@"
+
+sudo du -max /Volumes/*/"$(date '+%F')"-*.inprogress/ |
+sort -k1n |
+tail -n 1000
