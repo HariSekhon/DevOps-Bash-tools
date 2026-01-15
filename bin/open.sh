d3eb155f71c940b715c2b1df14cc9951792927a8
Hari Sekhon <harisekhon@gmail.com>
2026-01-15 16:43:37 -0500

    updated open.sh

--- bin/open.sh
+++ bin/open.sh
@@ -26,7 +26,7 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC2034,SC2154
 usage_description="
-Opens the file or url given as an arg, or first line from standard input or a given file
+Opens the file or url given as an arg, or first line from standard input if no arg is given
 
 Uses the desktop environment's generic open functionally for Mac or Linux
 "
