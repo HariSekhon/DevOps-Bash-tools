c4d812c00604475b96f2480b0bad5f2b6ebf2f5f
Hari Sekhon <harisekhon@gmail.com>
2026-02-01 22:36:39 -0400

    updated os.sh

--- lib/os.sh
+++ lib/os.sh
@@ -55,3 +55,55 @@ windows_only(){
         die "Only Windows is supported"
     fi
 }
+
+get_os(){
+    # shellcheck disable=SC3043
+    local os >/dev/null 2>&1 || :
+    os="$(uname -s | tr '[:upper:]' '[:lower:]')"
+    if [ -n "${OS_DARWIN:-}" ]; then
+        if is_mac; then
+            os="$OS_DARWIN"
+        fi
+    elif [ -n "${OS_LINUX:-}" ]; then
+        if is_linux; then
+            os="$OS_LINUX"
+        fi
+    fi
+    echo "$os"
+}
+
+get_arch(){
+    # shellcheck disable=SC3043
+    local arch >/dev/null 2>&1 || :
+    arch="$(uname -m)"
+    if [ "$arch" = x86_64 ]; then
+        arch=amd64  # files are conventionally usually named amd64 not x86_64
+    fi
+    #if [ "$arch" = aarch64 ]; then
+    #    arch=arm64
+    #fi
+    if [ -n "${ARCH_X86_64:-}" ]; then
+        if [ "$arch" = amd64 ] || [ "$arch" = x86_64 ]; then
+            arch="$ARCH_X86_64"
+        fi
+    fi
+    if [ -n "${ARCH_X86:-}" ]; then
+        if [ "$arch" = i386 ]; then
+            arch="$ARCH_X86"
+        fi
+    fi
+    if [ -n "${ARCH_ARM64:-}" ]; then
+        if [ "$arch" = arm64 ]; then
+            arch="$ARCH_ARM64"
+        fi
+    fi
+    if [ -n "${ARCH_ARM:-}" ]; then
+        if [ "$arch" = arm ]; then
+            arch="$ARCH_ARM"
+        fi
+    fi
+    if [ -n "${ARCH_OVERRIDE:-}" ]; then
+        arch="$ARCH_OVERRIDE"
+    fi
+    echo "$arch"
+}
