f301973b4dd192d9f55affaf4f976525b80802cf
Hari Sekhon <harisekhon@gmail.com>
2026-02-03 00:18:48 -0300

    added yum_upgrade_packages_if_outdated.sh

---
+++ packages/yum_upgrade_packages_if_outdated.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2230
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2026-02-03 00:01:41 -0300 (Tue, 03 Feb 2026)
+#
+#  https://github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -eu
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/packages.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Upgrades Yum RPM package lists if the packages are outdated
+
+$package_args_description
+
+Tested on CentOS
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<packages>"
+
+help_usage "$@"
+
+# get xargs if it's not installed since we call it below in the shell pipeline
+rpm -q findutils &>/dev/null ||
+yum install -y findutils
+
+upgradeable_packages="$(yum check-update)"
+
+process_package_args "$@" |
+while read -r package; do
+    if echo "$upgradeable_packages" | grep -Eq "^$package(.|[[:space:]])"; then
+        echo "$package"
+    fi
+done |
+xargs --no-run-if-empty yum upgrade -y
