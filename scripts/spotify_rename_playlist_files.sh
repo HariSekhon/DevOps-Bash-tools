f039376005278e7d707b0c691ba4cf27f774ce49
Hari Sekhon <harisekhon@gmail.com>
2026-02-09 00:23:20 -0300

    added subdir support for playlist folders

--- scripts/spotify_rename_playlist_files.sh
+++ scripts/spotify_rename_playlist_files.sh
@@ -27,36 +27,49 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 usage_description="
 Renames a Spotify playlist in both the \$PWD and \$PWD/spotify/ directories
 to keep the Spotify backups in sync
+
+With optional third arg <subdir> (path-mapped backups): renames under
+\$PWD/<subdir>/ and \$PWD/spotify/<subdir>/ instead. Invoke from backup base
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="<old_playlist_name> <new_playlist_name>"
+usage_args="<old_playlist_name> <new_playlist_name> [<subdir>]"
 
 help_usage "$@"
 
-num_args 2 "$@"
+min_args 2 "$@"
 
 old="$1"
 new="$2"
+subdir="${3:-}"
 
 old="$("$srcdir/../spotify/spotify_playlist_to_filename.sh" "$old")"
 new="$("$srcdir/../spotify/spotify_playlist_to_filename.sh" "$new")"
 
+# Optional subdir for path-mapped backups: playlists in base/Subdir/, URIs in base/spotify/Subdir/
+if [ -n "$subdir" ]; then
+    prefix="$subdir/"
+    spotify_prefix="spotify/$subdir/"
+else
+    prefix=""
+    spotify_prefix="spotify/"
+fi
+
 # the gitrename function in lib/git.sh has been updated to preserve the new file
 # and restore it after the move to then git diff and commit any updates
-gitrename "$old" "$new"
+gitrename "${prefix}$old" "${prefix}$new"
 
-gitrename "spotify/$old" "spotify/$new"
+gitrename "${spotify_prefix}$old" "${spotify_prefix}$new"
 
-if [ -f "agregations/$old" ]; then
-    gitrename "aggregations/$old" "aggregations/$new"
+if [ -f "aggregations/${prefix}$old" ]; then
+    gitrename "aggregations/${prefix}$old" "aggregations/${prefix}$new"
 fi
 
-if [ -f "$old.description" ]; then
-    gitrename "$old.description" "$new.description"
+if [ -f "${prefix}$old.description" ]; then
+    gitrename "${prefix}$old.description" "${prefix}$new.description"
 fi
 
-if [ -f "id/$old.id.txt" ]; then
-    mv -v "id/$old.id.txt" "id/$new.id.txt"
+if [ -f "id/${prefix}$old.id.txt" ]; then
+    mv -v "id/${prefix}$old.id.txt" "id/${prefix}$new.id.txt"
 fi
