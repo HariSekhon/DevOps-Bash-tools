83b62aa8423789520ccbe719106c5cc786eecbd9
Hari Sekhon <harisekhon@gmail.com>
2026-02-11 01:14:45 -0300

    updated screen_terminal_to_clipboard.sh

--- bin/screen_terminal_to_clipboard.sh
+++ bin/screen_terminal_to_clipboard.sh
@@ -37,8 +37,6 @@ usage_args="[<screen_options>]"
 
 help_usage "$@"
 
-#no_more_args "$@"
-
 # indicate to screen_terminal_to_stdout.sh not to remove the term
 export SCREEN_TERMINAL_NO_DELETE_TEMPFILE=1
 
