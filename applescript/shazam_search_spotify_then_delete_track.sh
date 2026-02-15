64505947b0ec8fca9b8df009be2d0df289eece6a
Hari Sekhon <harisekhon@gmail.com>
2026-02-15 20:14:46 -0300

    added shazam_search_spotify_then_delete_track.sh

---
+++ applescript/shazam_search_spotify_then_delete_track.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2025-11-02 00:30:16 +0300 (Sun, 02 Nov 2025)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Dumps the local Mac Shazam app's tracks one at a time, searches the Spotify app for each one,
+and then deletes it from the Shazam local sqlite DB upon an Enter key press to proceed to the next one
+
+Shazam to Spotify apps workaround to Apple removing Spotify integration from Shazam
+
+Can optionally specify a number of tracks to stop after as an arg,
+or a timeframe today/yesterday/week/YYYY-MM-DD,
+
+You can set this args as an environment variable \$SHAZAM_APP_DUMP_NUM_TRACKS - the arg takes precedence though
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<num_tracks> <db_path>]"
+
+help_usage "$@"
+
+max_args 1 "$@"
+
+mac_only
+
+export SHAZAM_APP_DELETE_TRACK_AFTER_SEARCH=1
+
+"$srcdir/shazam_search_spotify.sh" "$@"
