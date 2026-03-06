20ef0d7f1f9bbd938fa3c091ed1135dee42212e6
Hari Sekhon <harisekhon@gmail.com>
2026-03-06 02:13:19 -0400

    updated urldecode.sh

--- bin/urldecode.sh
+++ bin/urldecode.sh
@@ -46,7 +46,7 @@ if type -P perl &>/dev/null &&
     perl -MURI::Escape -ne 'chomp; print uri_unescape($_) . "\n"'
 elif type -p python3 &>/dev/null &&
      python3 -c 'from urllib.parse import quote_plus'; then
-    python3 -c "import sys, urllib.parse; [print(urllib.parse.unquote(line.strip())) for line in sys.stdin]"
+    python3 -c 'from urllib.parse import unquote_plus; import sys; print(unquote_plus(sys.stdin.read().rstrip("\n").rstrip("\r")))'
 else
     echo "ERROR: neither Perl URI::Escape nor Python3 with UrlLib.Parse are available" >&2
     exit 1
