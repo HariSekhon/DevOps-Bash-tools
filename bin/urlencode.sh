a08bc3044d1b6032021afae2524016e184a6ad55
Hari Sekhon <harisekhon@gmail.com>
2026-03-06 02:07:34 -0400

    updated urlencode.sh

--- bin/urlencode.sh
+++ bin/urlencode.sh
@@ -14,12 +14,27 @@
 #  https://www.linkedin.com/in/HariSekhon
 #
 
-# Quick command line URL encoding
-#
-# Only escapes characters disallowed in URIs
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+URL encodes the given string argument or standard input
+
+Only escapes characters disallowed in URIs
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<string_to_urlencode>]"
+
+help_usage "$@"
+
+#min_args 1 "$@"
 
 if [ $# -gt 0 ]; then
     echo "$@"
