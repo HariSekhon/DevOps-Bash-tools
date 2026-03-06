11e6103ab2e0424e92c6c6e8a0d5db7053558c0c
Hari Sekhon <harisekhon@gmail.com>
2026-03-06 02:08:26 -0400

    updated urldecode.sh

--- bin/urldecode.sh
+++ bin/urldecode.sh
@@ -17,6 +17,23 @@
 
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+URL decodes the given string argument or standard input
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<string_to_urldecode>]"
+
+help_usage "$@"
+
+#min_args 1 "$@"
 
 if [ $# -gt 0 ]; then
     echo "$@"
