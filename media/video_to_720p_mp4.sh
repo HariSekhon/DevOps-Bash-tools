688e53d503b7eaf1ee368694afa4c80e0d37783e
Hari Sekhon <harisekhon@gmail.com>
2026-04-16 03:02:11 +0200

    updated video_to_720p_mp4.sh

--- media/video_to_720p_mp4.sh
+++ media/video_to_720p_mp4.sh
@@ -58,14 +58,14 @@ SECONDS=0
 
 time \
 for filepath in "$@"; do
-    mp4_filepath="${filepath%.*}.720p.mp4"
-    if [ -s "$mp4_filepath" ]; then
-        timestamp "File already exists, skipping: $mp4_filepath"
+    new_mp4_filepath="${filepath%.*}.720p.mp4"
+    if [ -s "$new_mp4_filepath" ]; then
+        timestamp "File already exists, skipping: $new_mp4_filepath"
     else
         # shellcheck disable=SC2016
-        trap_cmd 'echo; echo "removing partially done file:"; rm -fv -- "$mp4_filepath"; untrap'
-        timestamp "converting $filepath => $mp4_filepath"
-        time nice ffmpeg -i "$filepath" -vf "scale=-1:720" -c:v libx264 -crf 23 -preset medium -c:a copy -movflags +faststart -- "$mp4_filepath"
+        trap_cmd 'echo; echo "removing partially done file:"; rm -fv -- "$new_mp4_filepath"; untrap'
+        timestamp "converting $filepath => $new_mp4_filepath"
+        time nice ffmpeg -i "$filepath" -vf "scale=-1:720" -c:v libx264 -crf 23 -preset medium -c:a copy -movflags +faststart -- "$new_mp4_filepath"
         echo >&2
     fi
     "$srcdir/vidopen.sh" "$mp4_filepath"
