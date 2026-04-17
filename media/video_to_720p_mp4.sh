aeb5b08d6386cce7be56435497536a99747ecd05
Hari Sekhon <harisekhon@gmail.com>
2026-04-17 06:49:18 +0200

    updated video_to_720p_mp4.sh

--- media/video_to_720p_mp4.sh
+++ media/video_to_720p_mp4.sh
@@ -32,7 +32,7 @@ instead of 720p and adjust the naming accordingly
 
 Names the generated files the same except with the file extension replaced with '.720p.mp4'
 
-Opens the video upon completion to check it unless environment variable NO_VIDEO_OPEN is set
+Opens the video upon completion to check it unless environment variable NO_VIDEO_OPEN is set to any value
 
 Skips files which already have a corresponding adjacent '.720.mp4' file for safety
 
