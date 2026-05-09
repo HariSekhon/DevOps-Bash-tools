1e373e6bd3470ddc8cc6dbb8357ba5ecf83e42b9
Hari Sekhon <harisekhon@gmail.com>
2026-05-10 01:09:00 +0200

    updated brave.sh

--- internet/brave.sh
+++ internet/brave.sh
@@ -22,11 +22,20 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1090,SC1091
 . "$srcdir/lib/utils.sh"
 
+
+# time between opening multiple tabs from stdin
+sleep_interval="0.3"
+
+default_url="https://google.com"
+
 # shellcheck disable=SC2034,SC2154
 usage_description="
 Opens a URL in the Brave browser in a portable way between Linux and Mac for use from other scripts
 
-If no URL is given then it defaults to Google.com
+If no URL is given defaults to $default_url
+
+To read multiple URLs from stdin, use a dash as an arg.
+Adds a delay of $sleep_interval secs between opening each one in order to not DoS or get rate limited by a website
 
 Opens in most recent Brave window
 
@@ -42,29 +51,65 @@ This site is useful to see more Brave switches which are based on Chromium:
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="[<url> <options>]"
+usage_args="[<options> <urls>]"
 
 help_usage "$@"
 
-#min_args 1 "$@"
-
-#url="${1:-https://google.com}"
-#shift || :
-
-#if is_mac; then
-    # don't use open because it requires figuring out what is the URL (or prefixing it with http(s):// )
-    # and what are the --args to pass to Brave
-    #open -a 'Brave Browser' -u "$url" --args "$@"
-    # just call Brave directly by path for simpler native chrome arg handling to be uniform across platforms
-if is_mac &&
-    [ -x "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser" ]; then
-    "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser" "$@" &
-else
-    if type -P brave-browser &>/dev/null; then
-        brave-browser "$@" &
-    elif type -P brave &>/dev/null; then
-        brave "$@" &
+brave(){
+    #if is_mac; then
+        # don't use open because it requires figuring out what is the URL (or prefixing it with http(s):// )
+        # and what are the --args to pass to Brave
+        #open -a 'Brave Browser' -u "$url" --args "$@"
+        # just call Brave directly by path for simpler native brave arg handling to be uniform across platforms
+    if is_mac &&
+        [ -x "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser" ]; then
+        "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser" "$@"
     else
-        die "ERROR: 'brave-browser' or 'brave' not found in \$PATH"
+        if type -P brave-browser &>/dev/null; then
+            brave-browser "$@"
+        elif type -P brave &>/dev/null; then
+            command brave "$@"
+        else
+            die "ERROR: brave-browser not found in \$PATH"
+        fi
+    fi
+}
+
+brave_stdin(){
+    warn "Reading URLs from stdin"
+    lines=0
+    while read -r line; do
+        is_blank "$line" && continue
+        timestamp "Opening Brave with args + stdin URL: $* $line"
+        brave "$@" "$line"
+        lines="$((lines + 1))"
+        timestamp "Sleeping for $sleep_interval secs"
+        sleep "$sleep_interval"
+    done
+    if [ "$lines" -eq 0 ]; then
+        timestamp "No URLs passed to std, opening Brave to default URL: $default_url"
+        brave "$@" "$default_url"
+    fi
+}
+
+if [ $# -eq 0 ]; then
+    timestamp "Opening Brave to default URL: $default_url"
+    brave "$default_url"
+else
+    stdin=0
+    args=()
+    for arg; do
+        if [ "$arg" = "-" ]; then
+            stdin=1
+        else
+            args+=("$arg")
+        fi
+    done
+
+    if [ "$stdin" = 1 ]; then
+        brave_stdin "${args[@]}"
+    else
+        timestamp "Opening Brave with args: $*"
+        brave "$@"
     fi
 fi
