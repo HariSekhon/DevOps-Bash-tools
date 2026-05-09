c64ec377b8c48b0b6d18a23f723be304c2afcadc
Hari Sekhon <harisekhon@gmail.com>
2026-05-10 01:13:51 +0200

    updated firefox.sh

--- internet/firefox.sh
+++ internet/firefox.sh
@@ -22,11 +22,20 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1090,SC1091
 . "$srcdir/lib/utils.sh"
 
+
+# time between opening multiple tabs from stdin
+sleep_interval="0.3"
+
+default_url="https://google.com"
+
 # shellcheck disable=SC2034,SC2154
 usage_description="
 Opens a URL in the Firefox browser in a portable way between Linux and Mac for use from other scripts
 
-If no URL is given then it defaults to Google.com
+If no URL is given defaults to $default_url
+
+To read multiple URLs from stdin, use a dash as an arg.
+Adds a delay of $sleep_interval secs between opening each one in order to not DoS or get rate limited by a website
 
 Opens in most recent Firefox window
 
@@ -38,32 +47,66 @@ Useful Switches:
 
 Another thing you can do on Mac is to add /Applications/Firefox.app/Contents/MacOS/ to your \$PATH and
 just call the firefox command as its the same on both platforms unlike with Chrome
-
-This script maintains parity with the adjacent chrome.sh script
 "
 
 # used by usage() in lib/utils.sh
 # shellcheck disable=SC2034
-usage_args="[<url> <options>]"
+usage_args="[<options> <urls>]"
 
 help_usage "$@"
 
-#min_args 1 "$@"
-
-#url="${1:-https://google.com}"
-#shift || :
-
-#if is_mac; then
-    # don't use open because it requires figuring out what is the URL (or prefixing it with http(s):// )
-    # and what are the --args to pass to Firefox
-    #open -a 'Firefox' -u "$url" --args "$@"
-    # just call Firefox directly by path for simpler native firefox arg handling to be uniform across platforms
-if is_mac &&
-    [ -x "/Applications/Firefox.app/Contents/MacOS/firefox" ]; then
-    "/Applications/Firefox.app/Contents/MacOS/firefox" "$@" &
-else
-    if ! type -P firefox &>/dev/null; then
-        die "ERROR: firefox not found in \$PATH"
+firefox(){
+    #if is_mac; then
+        # don't use open because it requires figuring out what is the URL (or prefixing it with http(s):// )
+        # and what are the --args to pass to Firefox
+        #open -a 'Firefox' -u "$url" --args "$@"
+        # just call Firefox directly by path for simpler native firefox arg handling to be uniform across platforms
+    if is_mac &&
+        [ -x "/Applications/Firefox.app/Contents/MacOS/firefox" ]; then
+        "/Applications/Firefox.app/Contents/MacOS/firefox" "$@"
+    else
+        if ! type -P firefox &>/dev/null; then
+            die "ERROR: firefox not found in \$PATH"
+        fi
+        command firefox "$@"
     fi
-    firefox "$@" &
+}
+
+firefox_stdin(){
+    warn "Reading URLs from stdin"
+    lines=0
+    while read -r line; do
+        is_blank "$line" && continue
+        timestamp "Opening Firefox with args + stdin URL: $* $line"
+        firefox "$@" "$line"
+        lines="$((lines + 1))"
+        timestamp "Sleeping for $sleep_interval secs"
+        sleep "$sleep_interval"
+    done
+    if [ "$lines" -eq 0 ]; then
+        timestamp "No URLs passed to std, opening Firefox to default URL: $default_url"
+        firefox "$@" "$default_url"
+    fi
+}
+
+if [ $# -eq 0 ]; then
+    timestamp "Opening Firefox to default URL: $default_url"
+    firefox "$default_url"
+else
+    stdin=0
+    args=()
+    for arg; do
+        if [ "$arg" = "-" ]; then
+            stdin=1
+        else
+            args+=("$arg")
+        fi
+    done
+
+    if [ "$stdin" = 1 ]; then
+        firefox_stdin "${args[@]}"
+    else
+        timestamp "Opening Firefox with args: $*"
+        firefox "$@"
+    fi
 fi
