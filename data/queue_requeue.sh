74673cc02bc40524f6aab42736756a7049b86504
Hari Sekhon <harisekhon@gmail.com>
2026-07-03 01:16:13 +0200

    added queue_requeue.sh

---
+++ data/queue_requeue.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2026-07-03 00:15:06 +0200 (Fri, 03 Jul 2026)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/utils.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Moves an item from processing back to pending for a given queue item
+
+Expects a full file path to a queue_basedir/processing/<item> as emitted by the adjacent scripts:
+
+    queue_add.sh
+        or
+    queue_recover.sh
+
+Infers the queue_basedir from the full queue item path
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="<queue_item>"
+
+help_usage "$@"
+
+numx_args 1 "$@"
+
+queue_item="$1"
+
+if ! [[ "$queue_item" =~ /processing/ ]]; then
+    die "Invalid queue item path given, must be a path to a queue_basedir/processing/item"
+fi
+
+pending_dir="${queue_item%%/processing/*}"
+
+mkdir -pv "$pending_dir"
+
+mv -v "$queue_item" "$pending_dir/"
