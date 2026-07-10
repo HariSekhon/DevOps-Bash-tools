4fcaf642878242d5cd3bf41e502ce446798b8279
Hari Sekhon <harisekhon@gmail.com>
2026-07-10 23:23:04 +0200

    added github_forks_containing_commit.sh

---
+++ github/github_forks_containing_commit.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2026-07-10 23:07:28 +0200 (Fri, 10 Jul 2026)
+#
+#  https///github.com/HariSekhon/DevOps-Bash-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090,SC1091
+. "$srcdir/lib/github.sh"
+
+# shellcheck disable=SC2034,SC2154
+usage_description="
+Finds all forks for a given repo that contain a given commit hash to find private information for removal requests
+
+If a repo is not specified, operates on the current repo
+
+Requires GitHub CLI to be installed and configured
+"
+
+# used by usage() in lib/utils.sh
+# shellcheck disable=SC2034
+usage_args="[<owner/repo>] <commit_hashref>"
+
+help_usage "$@"
+
+min_args 1 "$@"
+max_args 2 "$@"
+
+if [ $# -eq 2 ]; then
+    owner_repo="$1"
+    commit_hashref="$2"
+elif [ $# -eq 1 ]; then
+    owner_repo="$(github_owner_repo)"
+    commit_hashref="$1"
+else
+    usage
+fi
+
+if ! is_github_owner_repo "$owner_repo"; then
+    usage "Invalid GitHub owner/repo given: $owner_repo"
+fi
+
+if ! is_git_hashref "$commit_hashref"; then
+    usage "Invalid Git commit hashref given: $commit_hashref"
+fi
+
+gh api "repos/$owner_repo/forks?per_page=100" --paginate --jq '.[].full_name' |
+while read -r fork; do
+    if gh api "repos/$fork/commits/$commit_hashref" >/dev/null 2>&1; then
+        echo "https://github.com/$fork"
+    fi
+done
